e9f3368ade2c2fdd08cde8b5c42b46db1f253e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:55 +0000

    Add package octue/0.15.0/octue-0.15.0.tar.gz

---
+++ packages/octue/octue-0.15.0.tar.gz/octue-0.15.0/octue/resources/dataset.py
@@ -0,0 +1,320 @@
+import concurrent.futures
+import json
+import logging
+import os
+import tempfile
+
+from octue import definitions
+from octue.cloud import storage
+from octue.cloud.storage import GoogleCloudStorageClient
+from octue.exceptions import CloudLocationNotSpecified, InvalidInputException
+from octue.migrations.cloud_storage import translate_bucket_name_and_path_in_bucket_to_cloud_path
+from octue.mixins import Hashable, Identifiable, Labelable, Pathable, Serialisable, Taggable
+from octue.resources.datafile import Datafile
+from octue.resources.filter_containers import FilterSet
+from octue.resources.label import LabelSet
+from octue.resources.tag import TagDict
+
+
+logger = logging.getLogger(__name__)
+
+
+class Dataset(Labelable, Taggable, Serialisable, Pathable, Identifiable, Hashable):
+    """A representation of a dataset, containing files, labels, etc
+
+    This is used to read a list of files (and their associated properties) into octue analysis, or to compile a
+    list of output files (results) and their properties that will be sent back to the octue system.
+
+    :param iter(dict|octue.resources.datafile.Datafile) files: the files belonging to the dataset
+    :return None:
+    """
+
+    _ATTRIBUTES_TO_HASH = ("files",)
+    _SERIALISE_FIELDS = "files", "name", "labels", "tags", "id", "path"
+
+    def __init__(self, files=None, name=None, id=None, path=None, tags=None, labels=None, **kwargs):
+        super().__init__(name=name, id=id, tags=tags, labels=labels, path=path)
+
+        # TODO The decoders aren't being used; utils.decoders.OctueJSONDecoder should be used in twined
+        #  so that resources get automatically instantiated.
+        #  Add a proper `decoder` argument  to the load_json utility in twined so that datasets, datafiles and manifests
+        #  get initialised properly, then remove this hackjob.
+        self._cloud_path = None
+
+        if path and storage.path.is_qualified_cloud_path(path):
+            self._cloud_path = path
+
+        self.files = FilterSet()
+
+        for file in files or []:
+            if isinstance(file, Datafile):
+                self.files.add(file)
+            else:
+                self.files.add(Datafile.deserialise(file, path_from=self))
+
+        self.__dict__.update(**kwargs)
+
+    def __iter__(self):
+        yield from self.files
+
+    def __len__(self):
+        return len(self.files)
+
+    @classmethod
+    def from_local_directory(cls, path_to_directory, recursive=False, **kwargs):
+        """Instantiate a Dataset from the files in the given local directory.
+
+        :param str path_to_directory: path to a local directory
+        :param bool recursive: if `True`, include all files in the directory's subdirectories recursively
+        :param kwargs: other keyword arguments for the `Dataset` instantiation
+        :return Dataset:
+        """
+        datafiles = FilterSet()
+
+        for level, (directory_path, _, filenames) in enumerate(os.walk(path_to_directory)):
+            for filename in filenames:
+
+                if not recursive and level > 0:
+                    break
+
+                datafiles.add(Datafile(path=os.path.join(directory_path, filename)))
+
+        return Dataset(path=path_to_directory, files=datafiles, **kwargs)
+
+    @classmethod
+    def from_cloud(cls, cloud_path=None, bucket_name=None, path_to_dataset_directory=None, recursive=False):
+        """Instantiate a Dataset from Google Cloud storage.
+
+        :param str|None cloud_path: full path to dataset directory in cloud storage (e.g. `gs://bucket_name/path/to/dataset`)
+        :param bool recursive: if `True`, include in the dataset all files in the subdirectories recursively contained in the dataset directory
+        :return Dataset:
+        """
+        if not cloud_path:
+            cloud_path = translate_bucket_name_and_path_in_bucket_to_cloud_path(bucket_name, path_to_dataset_directory)
+
+        bucket_name = storage.path.split_bucket_name_from_gs_path(cloud_path)[0]
+
+        dataset_metadata = cls._get_dataset_metadata(cloud_path=cloud_path)
+
+        if dataset_metadata:
+            return Dataset(
+                id=dataset_metadata.get("id"),
+                name=dataset_metadata.get("name"),
+                path=cloud_path,
+                tags=TagDict(dataset_metadata.get("tags", {})),
+                labels=LabelSet(dataset_metadata.get("labels", [])),
+                files=[Datafile(path=path) for path in dataset_metadata["files"]],
+            )
+
+        datafiles = FilterSet(
+            Datafile(path=storage.path.generate_gs_path(bucket_name, blob.name))
+            for blob in GoogleCloudStorageClient().scandir(cloud_path, recursive=recursive)
+        )
+
+        dataset = Dataset(path=cloud_path, files=datafiles)
+        dataset._upload_dataset_metadata(cloud_path)
+        return dataset
+
+    def to_cloud(self, cloud_path=None, bucket_name=None, output_directory=None):
+        """Upload a dataset to the given cloud path.
+
+        :param str|None cloud_path: cloud path to store dataset at (e.g. `gs://bucket_name/path/to/dataset`)
+        :return str: cloud path for dataset
+        """
+        if not cloud_path:
+            cloud_path = translate_bucket_name_and_path_in_bucket_to_cloud_path(bucket_name, output_directory)
+
+        files_and_paths = []
+
+        for datafile in self.files:
+            datafile_path_relative_to_dataset = datafile.path.split(self.path)[-1].strip(os.path.sep).strip("/")
+            files_and_paths.append(
+                (
+                    datafile,
+                    storage.path.join(cloud_path, *datafile_path_relative_to_dataset.split(os.path.sep)),
+                )
+            )
+
+        def upload(iterable_element):
+            """Upload a datafile to the given cloud path.
+
+            :param tuple(octue.resources.datafile.Datafile, str) iterable_element:
+            :return None:
+            """
+            datafile = iterable_element[0]
+            cloud_path = iterable_element[1]
+            datafile.to_cloud(cloud_path=cloud_path)
+
+        # Use multiple threads to significantly speed up file uploads by reducing latency.
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(upload, files_and_paths)
+
+        self._cloud_path = cloud_path
+        self.path = cloud_path
+        self._upload_dataset_metadata(cloud_path)
+        return cloud_path
+
+    @property
+    def name(self):
+        """Get the name of the dataset
+
+        :return str:
+        """
+        if self._name:
+            return self._name
+
+        if self.cloud_path:
+            return storage.path.split(self.cloud_path)[-1]
+
+        return os.path.split(os.path.abspath(os.path.split(self.path)[-1]))[-1]
+
+    @property
+    def cloud_path(self):
+        """Get the cloud path of the dataset.
+
+        :return str|None:
+        """
+        return self._cloud_path
+
+    @cloud_path.setter
+    def cloud_path(self, path):
+        """Set the cloud path of the dataset.
+
+        :param str|None path:
+        :return None:
+        """
+        if path is None:
+
+            if not self.exists_locally:
+                raise CloudLocationNotSpecified(
+                    "The cloud path cannot be reset because this datafile only exists in the cloud."
+                )
+
+            self._cloud_path = None
+
+        else:
+            self.to_cloud(cloud_path=path)
+
+    @property
+    def exists_in_cloud(self):
+        """Return `True` if the dataset exists in the cloud.
+
+        :return bool:
+        """
+        return self.cloud_path is not None
+
+    @property
+    def exists_locally(self):
+        """Return `True` if the dataset exists locally.
+
+        :return bool:
+        """
+        return not storage.path.is_qualified_cloud_path(self.path)
+
+    @property
+    def all_files_are_in_cloud(self):
+        """Do all the files of the dataset exist in the cloud?
+
+        :return bool:
+        """
+        return all(file.exists_in_cloud for file in self.files)
+
+    def add(self, *args, **kwargs):
+        """Add a data/results file to the manifest.
+
+        Usage:
+            my_file = octue.DataFile(...)
+            my_manifest.add(my_file)
+
+            # or more simply
+            my_manifest.add(**{...}) which implicitly creates the datafile from the starred list of input arguments
+        """
+        if len(args) > 1:
+            # Recurse to allow addition of many files at once
+            for arg in args:
+                self.add(arg, **kwargs)
+        elif len(args) > 0:
+            if not isinstance(args[0], Datafile):
+                raise InvalidInputException(
+                    'Object "{}" must be of class Datafile to add it to a Dataset'.format(args[0])
+                )
+            self.files.add(args[0])
+
+        else:
+            # Add a single file, constructed by passing the arguments through to DataFile()
+            self.files.add(Datafile(**kwargs))
+
+    def get_file_by_label(self, label):
+        """Get a single datafile from a dataset by filtering for files with the provided label.
+
+        :param str label: the label to filter for
+        :raise octue.exceptions.UnexpectedNumberOfResultsException: if zero or more than one results satisfy the filters
+        :return octue.resources.datafile.DataFile:
+        """
+        return self.files.one(labels__contains=label)
+
+    def download_all_files(self, local_directory=None):
+        """Download all files in the dataset into the given local directory. If no path to a local directory is given,
+        the files will be downloaded to temporary locations.
+
+        :param str|None local_directory:
+        :return None:
+        """
+        local_directory = local_directory or tempfile.TemporaryDirectory().name
+
+        files_and_paths = []
+
+        for file in self.files:
+
+            if not file.exists_in_cloud:
+                continue
+
+            path_relative_to_dataset = storage.path.relpath(file.cloud_path, self.cloud_path)
+            local_path = os.path.abspath(os.path.join(local_directory, *path_relative_to_dataset.split("/")))
+            files_and_paths.append((file, local_path))
+
+        def download(iterable_element):
+            """Download a datafile to the given path.
+
+            :param tuple(octue.resources.datafile.Datafile, str) iterable_element:
+            :return str:
+            """
+            datafile = iterable_element[0]
+            local_path = iterable_element[1]
+            return datafile.download(local_path=local_path)
+
+        # Use multiple threads to significantly speed up files downloads by reducing latency.
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(download, files_and_paths)
+
+        logger.info("Downloaded %r dataset to %r.", self.name, local_directory)
+
+    @staticmethod
+    def _get_dataset_metadata(cloud_path):
+        """Get the metadata for the given dataset if a dataset metadata file has previously been uploaded.
+
+        :param str cloud_path: the path to the dataset cloud directory
+        :return dict: the dataset metadata
+        """
+        storage_client = GoogleCloudStorageClient()
+        metadata_file_path = storage.path.join(cloud_path, definitions.DATASET_METADATA_FILENAME)
+
+        if not storage_client.exists(cloud_path=metadata_file_path):
+            return {}
+
+        return json.loads(storage_client.download_as_string(cloud_path=metadata_file_path))
+
+    def _upload_dataset_metadata(self, cloud_path):
+        """Upload a metadata file representing the dataset to the given cloud location.
+
+        :param str cloud_path: the path to the dataset cloud directory
+        :return None:
+        """
+        serialised_dataset = self.to_primitive()
+        serialised_dataset["files"] = sorted(datafile.cloud_path for datafile in self.files)
+        del serialised_dataset["path"]
+
+        GoogleCloudStorageClient().upload_from_string(
+            string=json.dumps(serialised_dataset),
+            cloud_path=storage.path.join(cloud_path, definitions.DATASET_METADATA_FILENAME),
+        )
---
+++ packages/octue/octue-0.15.0.tar.gz/octue-0.15.0/tests/mixins/test_taggable.py
@@ -0,0 +1,79 @@
+from octue import exceptions
+from octue.mixins import Taggable
+from octue.resources.tag import TagDict
+
+from ..base import BaseTestCase
+
+
+class MyTaggable(Taggable):
+    pass
+
+
+class TaggableTestCase(BaseTestCase):
+    def test_instantiates(self):
+        """Ensures the class instantiates without arguments."""
+        taggable = Taggable()
+        self.assertEqual(taggable.tags, {})
+
+    def test_instantiating_with_no_tags(self):
+        """Test that instantiating a Taggable with no tags results in an empty TagDict on the tags attribute."""
+        self.assertEqual(MyTaggable().tags, TagDict())
+
+    def test_fails_to_instantiates_with_non_iterable(self):
+        """Test that instantiation with a non-iterable fails."""
+
+        class NoIter:
+            pass
+
+        with self.assertRaises(TypeError):
+            MyTaggable(tags=NoIter())
+
+    def test_instantiates_with_dict(self):
+        """Test instantiation with a dictionary works."""
+        tags = {"height": 9, "width": 8.7, "depth": 100}
+        taggable = MyTaggable(tags=tags)
+        self.assertEqual(taggable.tags, tags)
+
+    def test_instantiates_with_tag_dict(self):
+        """Test instantiation with a TagDict."""
+        taggable_1 = MyTaggable(tags={"a": 2})
+        self.assertIsInstance(taggable_1.tags, TagDict)
+        taggable_2 = MyTaggable(tags=taggable_1.tags)
+        self.assertFalse(taggable_1 is taggable_2)
+
+    def test_setting_tags_overwrites_previous_tags(self):
+        """Ensure tags can be overwritten with new ones."""
+        taggable = MyTaggable(tags={"a": 1, "b": 2})
+        taggable.tags = {"c": 3, "d": 4}
+        self.assertEqual(taggable.tags, {"c": 3, "d": 4})
+
+    def test_add_valid_tags(self):
+        """Ensures adding valid tags works."""
+        taggable = MyTaggable()
+
+        taggable.add_tags({"a_valid_tag": "blah"})
+        taggable.add_tags({"a1829tag": "blah"})
+        taggable.add_tags({"1829": "blah", "number_1829": "blah"})  # Add multiple tags at once.
+
+        self.assertEqual(
+            taggable.tags,
+            {"a_valid_tag": "blah", "a1829tag": "blah", "1829": "blah", "number_1829": "blah"},
+        )
+
+    def test_add_tags_via_kwargs(self):
+        """Test tags can be added via kwargs."""
+        taggable = MyTaggable()
+        taggable.add_tags(hello="blib", hi="glib")
+        self.assertEqual(taggable.tags, {"hello": "blib", "hi": "glib"})
+
+    def test_adding_mixture_of_valid_and_invalid_tags_fails_completely(self):
+        """Ensure that adding a variety of tags, some of which are invalid, doesn't partially add the set including the
+        invalid tags to the object.
+        """
+        taggable = MyTaggable()
+        taggable.add_tags({"first_valid_should_be_added": "hello"})
+
+        with self.assertRaises(exceptions.InvalidTagException):
+            taggable.add_tags({"second_valid_should_not_be_added_because": 1, "_the_third_is_invalid:": 2})
+
+        self.assertEqual(taggable.tags, {"first_valid_should_be_added": "hello"})
