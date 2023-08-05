a02b71b3bea04b6c653710b686d8a16efae3c35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:19 +0000

    Add package mg-file/0.1.23/mg_file-0.1.23.tar.gz

---
+++ packages/mg-file/mg_file-0.1.23.tar.gz/mg_file-0.1.23/mg_file/__init__.py
@@ -0,0 +1,12 @@
+from .file import *
+from .pcos import *
+from .sqllite_orm import *
+from .sql_raw import *
+from .castom_type import *
+
+# from .pcos import *
+# from .file import *
+# from .sqllite_orm import *
+# from .logsmal import *
+# from .sql_raw import *
+# from .castom_type import *
