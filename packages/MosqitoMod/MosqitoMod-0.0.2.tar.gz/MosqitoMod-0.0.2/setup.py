1d1edbc9daf714ee5f7bed753ee3f8f8b0f7ac6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package MosqitoMod/0.0.2/MosqitoMod-0.0.2.tar.gz

---
+++ packages/MosqitoMod/MosqitoMod-0.0.2.tar.gz/MosqitoMod-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Simplifies Python'
+LONG_DESCRIPTION = 'This package will simplify some functions of python so that you can focus on the code and not the syntax'
+
+# Setting up
+setup(
+    name="MosqitoMod",
+    version=VERSION,
+    author="MosqitoTorpedo",
+    author_email="<mosqitotorpedo@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'simple', 'code'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
