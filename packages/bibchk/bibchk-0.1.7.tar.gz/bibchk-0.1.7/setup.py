50ab38092bb8533a6802dc218551d532a901ff6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:32 +0000

    Add package bibchk/0.1.7/bibchk-0.1.7.tar.gz

---
+++ packages/bibchk/bibchk-0.1.7.tar.gz/bibchk-0.1.7/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name = "bibchk",
+        version = "0.1.7",
+        author = "Doug Keller",
+        author_email = "dg.kllr.jr@gmail.com",
+        description = "Simple command line program to return the BibTeX string of a given DOI or ISBN.",
+        long_description = long_description,
+        long_description_content_type = "text/markdown",
+        url = "https://github.com/BibTheque/bibchk",
+        entry_points={'console_scripts': ['bibchk = bibchk.bibchk:main']},
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+            "Operating System :: OS Independent",
+            ],
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src"),
+        python_requires = ">=3.9",
+        )
