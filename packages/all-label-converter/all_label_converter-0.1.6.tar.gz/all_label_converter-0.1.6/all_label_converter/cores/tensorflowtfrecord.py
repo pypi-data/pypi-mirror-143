cb587cc1a3c2e36b469dd8aa11655cc40a4345b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:58 +0000

    Add package all-label-converter/0.1.6/all_label_converter-0.1.6.tar.gz

---
+++ packages/all-label-converter/all_label_converter-0.1.6.tar.gz/all_label_converter-0.1.6/all_label_converter/cores/tensorflowtfrecord.py
@@ -0,0 +1,20 @@
+"""
+Author: Fatih Baday <bfatih27@gmail.com>
+Purpose: To import and export tensorflow csv type.
+"""
+# Importing libraries.
+import os
+import shutil
+import logging
+import pandas as pd
+import all_label_converter.commonformat as cf
+import all_label_converter.cores.utils as utils
+
+from all_label_converter.cores.config import Config
+
+
+class TFRecordExport:
+    def __init__(self, objs, folder_path: str):
+        self.objs = objs
+        self.folder_path = folder_path
+
