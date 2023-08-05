c4a47847771f7438cd316db341745941fdcf622f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:09 +0000

    Add package eva-shell/0.0.3/eva-shell-0.0.3.tar.gz

---
+++ packages/eva-shell/eva-shell-0.0.3.tar.gz/eva-shell-0.0.3/eva4_shell/sharedobj.py
@@ -0,0 +1,12 @@
+import time
+from types import SimpleNamespace
+import pytz
+
+common = SimpleNamespace(dir_eva=None,
+                         elbus_path=None,
+                         elbus_name=None,
+                         bus=None,
+                         rpc=None,
+                         cli=None,
+                         TZ=pytz.timezone(time.tzname[0]))
+current_command = SimpleNamespace(json=False, debug=False, timeout=5)
