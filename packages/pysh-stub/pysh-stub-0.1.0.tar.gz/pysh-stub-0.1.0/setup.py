269a89b7a58338c4f0205036f7ab4393e684751d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:26:53 +0000

    Add package pysh-stub/0.1.0/pysh-stub-0.1.0.tar.gz

---
+++ packages/pysh-stub/pysh-stub-0.1.0.tar.gz/pysh-stub-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pysh']
+install_requires = \
+['tomlkit>=0.10.0,<0.11.0', 'typer[all]>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['pysh = pysh:main']}
+
+setup_kwargs = {
+    'name': 'pysh-stub',
+    'version': '0.1.0',
+    'description': 'A bunch of shortcuts to simplify scripting in python',
+    'long_description': None,
+    'author': 'Stanislav Zmiev',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
