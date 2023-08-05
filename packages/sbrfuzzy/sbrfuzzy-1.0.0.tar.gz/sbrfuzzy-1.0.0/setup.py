5242def31292ba1cfd3ef3959d0ebb41a957b099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:32 +0000

    Add package sbrfuzzy/1.0.0/sbrfuzzy-1.0.0.tar.gz

---
+++ packages/sbrfuzzy/sbrfuzzy-1.0.0.tar.gz/sbrfuzzy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "sbrfuzzy",
+    version = "1.0.0",
+    long_description = 'sbrfuzzy é um módulo python para aplicações com base em sistemas baseados em regras fuzzy',
+    author = "Henrique de Lemos Braga",
+    packages = ["sbrfuzzy"],
+    install_requires = ["matplotlib","numpy"]
+)
