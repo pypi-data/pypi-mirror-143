d647d3ea1b65d9482ba7f662749ff0ade5b8f758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:20 +0000

    Add package arrlio/0.12.0/arrlio-0.12.0.tar.gz

---
+++ packages/arrlio/arrlio-0.12.0.tar.gz/arrlio-0.12.0/arrlio/serializers/base.py
@@ -0,0 +1,38 @@
+import abc
+from typing import Any, Union
+
+from arrlio.models import Event, TaskInstance, TaskResult
+
+
+class Serializer(abc.ABC):
+    @abc.abstractmethod
+    def dumps_task_instance(self, task_instance: TaskInstance, **kwds) -> Union[bytes, TaskInstance]:
+        pass
+
+    @abc.abstractmethod
+    def loads_task_instance(self, data: Union[bytes, TaskInstance]) -> TaskInstance:
+        pass
+
+    @abc.abstractmethod
+    def dumps_task_result(self, result: TaskResult, **kwds) -> Union[bytes, TaskResult]:
+        pass
+
+    @abc.abstractmethod
+    def loads_task_result(self, data: Union[bytes, TaskResult]) -> TaskResult:
+        pass
+
+    @abc.abstractmethod
+    def dumps_event(self, event: Event, **kwds) -> Union[bytes, Event]:
+        pass
+
+    @abc.abstractmethod
+    def loads_event(self, data: Union[bytes, Event]) -> Event:
+        pass
+
+    @abc.abstractmethod
+    def dumps(self, data: Any, **kwds) -> Union[bytes, Any]:
+        pass
+
+    @abc.abstractmethod
+    def loads(self, data: Union[bytes, Any]) -> Any:
+        pass
