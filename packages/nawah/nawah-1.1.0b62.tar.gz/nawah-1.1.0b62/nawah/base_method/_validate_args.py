30fca22d9b6cc16b749d1a6a0ba829acefc7ed1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:27:09 +0000

    Add package nawah/1.1.0b62/nawah-1.1.0b62.tar.gz

---
+++ packages/nawah/nawah-1.1.0b62.tar.gz/nawah-1.1.0b62/nawah/base_method/_validate_args.py
@@ -0,0 +1,99 @@
+from nawah.classes import (
+    Query,
+    ATTR,
+    InvalidCallArgsException,
+    InvalidAttrException,
+    ConvertAttrException,
+    NAWAH_ENV,
+)
+from nawah.utils import validate_attr
+
+from typing import Union, Dict, Any, List, Literal, Iterable, cast
+
+
+async def _validate_args(
+    *,
+    args: Union[Query, Dict[str, Any]],
+    args_list_label: str,
+    args_list: List[Dict[str, ATTR]],
+    env: NAWAH_ENV,
+):
+    if not args_list:
+        return
+
+    sets_check: List[Dict[str, Literal[True, 'missing', 'invalid', 'convert']]] = []
+
+    for args_set in args_list:
+        set_status = True
+        set_check = len(sets_check)
+        sets_check.append({arg: True for arg in args_set.keys()})
+
+        if args_list_label == 'query':
+            args_check: Iterable = args
+        elif args_list_label == 'doc':
+            args = cast(Dict[str, Any], args)
+            args_check = args.keys()
+
+        for arg in args_set.keys():
+            if arg not in args_check:
+                set_status = False
+                sets_check[set_check][arg] = 'missing'
+            else:
+                try:
+                    if args_list_label == 'query' and arg[0] != '$':
+                        for i in range(len(args[arg])):
+                            if ':' not in arg:
+                                args[arg][i] = await validate_attr(
+                                    mode='create',
+                                    attr_name=arg,
+                                    attr_type=args_set[arg],
+                                    attr_val=args[arg][i],
+                                    env=env,
+                                )
+                            else:
+                                attr_val = args[arg][i]
+                                if (arg_oper := arg.split(':')[1]) in ['*', '$eq']:
+                                    if (
+                                        type(args[arg][i])
+                                        and arg_oper in args[arg][i].keys()
+                                    ):
+                                        attr_val = args[arg][i][arg_oper]
+                                attr_val = await validate_attr(
+                                    mode='create',
+                                    attr_name=arg,
+                                    attr_type=args_set[arg],
+                                    attr_val=attr_val,
+                                    env=env,
+                                )
+                                if arg_oper in ['*', '$eq']:
+                                    args[arg][i] = attr_val
+                                else:
+                                    args[arg][i] = {arg_oper: attr_val}
+
+                    elif args_list_label == 'query' and arg[0] == '$':
+                        args[arg] = await validate_attr(
+                            mode='create',
+                            attr_name=arg,
+                            attr_type=args_set[arg],
+                            attr_val=args[arg],
+                            env=env,
+                        )
+                    elif args_list_label == 'doc':
+                        args[arg] = await validate_attr(
+                            mode='create',
+                            attr_name=arg,
+                            attr_type=args_set[arg],
+                            attr_val=args[arg],
+                            env=env,
+                        )
+                except InvalidAttrException:
+                    set_status = False
+                    sets_check[set_check][arg] = 'invalid'
+                except ConvertAttrException:
+                    set_status = False
+                    sets_check[set_check][arg] = 'convert'
+
+        if set_status:
+            return
+
+    raise InvalidCallArgsException(sets_check)
---
+++ packages/nawah/nawah-1.1.0b62.tar.gz/nawah-1.1.0b62/nawah/classes/_types.py
@@ -0,0 +1,131 @@
+import datetime
+from asyncio import Task
+from typing import (TYPE_CHECKING, Any, AsyncGenerator, Callable, Coroutine,
+                    Dict, List, Literal, Optional, TypedDict, Union)
+
+from aiohttp.web import WebSocketResponse
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from nawah.enums import Event
+
+NAWAH_EVENTS = List[Event]
+
+WATCH_TASK = TypedDict(
+    'WATCH_TASK',
+    {
+        'watch': Coroutine[Any, Any, None],
+        'task': Task,
+        'stream': Any,
+    },
+    total=False,
+)
+
+NAWAH_ENV_QUOTA = TypedDict(
+    'NAWAH_ENV_QUOTA',
+    {
+        'counter': int,
+        'last_check': datetime.datetime,
+    },
+)
+
+NAWAH_ENV = TypedDict(
+    'NAWAH_ENV',
+    {
+        'id': int,
+        'init': bool,
+        'conn': AsyncIOMotorClient,
+        'REMOTE_ADDR': str,
+        'HTTP_USER_AGENT': str,
+        'HTTP_ORIGIN': str,
+        'client_app': str,
+        'session': Dict,
+        'prev_session': Dict,
+        'prev_session_timeout': datetime.datetime,
+        'last_call': datetime.datetime,
+        'ws': WebSocketResponse,
+        'watch_tasks': Dict[str, WATCH_TASK],
+        'quota': NAWAH_ENV_QUOTA,
+        'args': Dict,
+    },
+    total=False,
+)
+
+NAWAH_QUERY_SPECIAL_GROUP = TypedDict(
+    'NAWAH_QUERY_SPECIAL_GROUP', {'by': str, 'count': int}
+)
+
+NAWAH_QUERY_SPECIAL_GEO_NEAR = TypedDict(
+    'NAWAH_QUERY_SPECIAL_GEO_NEAR', {'val': str, 'attr': str, 'dist': int}
+)
+
+NAWAH_QUERY_SPECIAL = TypedDict(
+    'NAWAH_QUERY_SPECIAL',
+    {
+        '$search': Optional[str],
+        '$sort': Optional[Dict[str, Literal[1, -1]]],
+        '$skip': Optional[int],
+        '$limit': Optional[int],
+        '$extn': Optional[Union[Literal[False], List[str]]],
+        '$attrs': Optional[List[str]],
+        '$group': Optional[List[NAWAH_QUERY_SPECIAL_GEO_NEAR]],
+        '$geo_near': Optional[NAWAH_QUERY_SPECIAL_GEO_NEAR],
+    },
+    total=False,
+)
+
+NAWAH_QUERY = List[  # type: ignore
+    Union[
+        'NAWAH_QUERY',  # type: ignore
+        Union[
+            Dict[
+                str,
+                Union[
+                    'NAWAH_QUERY',  # type: ignore
+                    Any,
+                    Union[
+                        Dict[Literal['$eq'], Any],
+                        Dict[Literal['$ne'], Any],
+                        Dict[Literal['$gt'], Union[int, str]],
+                        Dict[Literal['$gte'], Union[int, str]],
+                        Dict[Literal['$lt'], Union[int, str]],
+                        Dict[Literal['$lte'], Union[int, str]],
+                        Dict[Literal['$bet'], Union[List[int], List[str]]],
+                        Dict[Literal['$all'], List[Any]],
+                        Dict[Literal['$in'], List[Any]],
+                        Dict[Literal['$nin'], List[Any]],
+                        Dict[Literal['$regex'], str],
+                    ],
+                ],
+            ],
+            NAWAH_QUERY_SPECIAL,
+        ],
+    ]
+]
+
+NAWAH_DOC = Dict[
+    str,
+    Union[
+        Dict[
+            str,
+            Union[
+                Dict[Literal['$add'], int],
+                Dict[Literal['$multiply'], int],
+                Dict[Literal['$append'], Any],
+                Dict[Literal['$set_index'], Dict[int, Any]],
+                Dict[Literal['$set_index'], Dict[int, Any]],
+                Dict[Literal['$del_val'], Any],
+                Dict[Literal['$del_index'], int],
+                Any,
+            ],
+        ],
+        Any,
+    ],
+]
+
+IP_QUOTA = TypedDict(
+    'IP_QUOTA',
+    {
+        'counter': int,
+        'last_check': datetime.datetime,
+    },
+)
