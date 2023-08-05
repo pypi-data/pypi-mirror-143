05c8a9b5fa65f78296e8e20f545fbddd68a2cb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:56 +0000

    Add package keras-genetic/0.0.5/keras_genetic-0.0.5-py3-none-any.whl

---
+++ packages/keras-genetic/keras_genetic-0.0.5-py3-none-any.whl/keras_genetic/__init__.py
@@ -0,0 +1,7 @@
+from keras_genetic.breeder import Breeder
+from keras_genetic.core.individual import Individual
+from keras_genetic.initializers import Initializer
+from keras_genetic.search import search
+from keras_genetic.callbacks import Callback
+
+__version__ = "0.0.5"
