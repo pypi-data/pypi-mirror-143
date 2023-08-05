78b11ba9a7b93616d07768518bfe8c0d144c28eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:09 +0000

    Add package cloudcomponents.cdk-cloudfront-authorization/2.1.0/cloudcomponents.cdk-cloudfront-authorization-2.1.0.tar.gz

---
+++ packages/cloudcomponents.cdk-cloudfront-authorization/cloudcomponents.cdk-cloudfront-authorization-2.1.0.tar.gz/cloudcomponents.cdk-cloudfront-authorization-2.1.0/src/cloudcomponents/cdk_cloudfront_authorization/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import cloudcomponents.cdk_lambda_at_edge_pattern._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-cloudfront-authorization",
+    "2.1.0",
+    __name__[0:-6],
+    "cdk-cloudfront-authorization@2.1.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
