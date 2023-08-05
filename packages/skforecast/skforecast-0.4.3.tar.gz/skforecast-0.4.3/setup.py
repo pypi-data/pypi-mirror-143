6be07e22086a1ed4b6a8e53abe61db6da6f544de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:09 +0000

    Add package skforecast/0.4.3/skforecast-0.4.3.tar.gz

---
+++ packages/skforecast/skforecast-0.4.3.tar.gz/skforecast-0.4.3/setup.py
@@ -0,0 +1,39 @@
+################################################################################
+#                               skforecast                                     #
+#                                                                              #
+# This work by Joaquin Amat Rodrigo is licensed under a Creative Commons       #
+# Attribution 4.0 International License.                                       #
+################################################################################
+
+import setuptools
+import os
+import skforecast
+
+VERSION = skforecast.__version__
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+        
+        
+setuptools.setup(
+    name="skforecast",
+    version=VERSION,
+    author="Joaquin Amat Rodrigo",
+    author_email="j.amatrodrigo@gmail.com",
+    description="Forecasting time series with scikitlearn regressors. It also works with any regressor compatible with the scikit-learn API (pipelines, CatBoost, LightGBM, XGBoost, Ranger...).",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoaquinAmatRodrigo/skforecast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=requirements
+)
