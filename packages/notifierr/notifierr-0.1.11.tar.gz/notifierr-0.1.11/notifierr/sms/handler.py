ccf7db063e3248dbba5a4f89e770a9d4cddf98c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:15 +0000

    Add package notifierr/0.1.11/notifierr-0.1.11.tar.gz

---
+++ packages/notifierr/notifierr-0.1.11.tar.gz/notifierr-0.1.11/notifierr/sms/handler.py
@@ -0,0 +1,14 @@
+''' The primary sms handler for the application '''
+import importlib
+
+class SMSHandler():
+    ''' An agnostic interface to multiple SMS providers '''
+    def __init__(self, provider) -> None:
+        provider_module = importlib.import_module(
+            name=f'notifierr.sms.providers.{provider}'
+        )
+        self.client = provider_module.ProviderClient()
+
+    def send_message(self, receiver, message) -> None:
+        ''' Send an SMS message '''
+        self.client.send_message(receiver, message)
---
+++ packages/notifierr/notifierr-0.1.11.tar.gz/notifierr-0.1.11/setup.py
@@ -0,0 +1,44 @@
+''' The application setup.py file '''
+from setuptools import find_packages, setup
+
+SHORT_DESCRIPTION = \
+    "An API server to receive webhook events from Sonarr and Radarr and send SMS messages."
+install_requires = [
+    "flask > 2.0.0, < 3.0.0",
+    "twilio",
+]
+
+setup(
+    name="notifierr",
+    version='0.1.11',
+    description=SHORT_DESCRIPTION,
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/adamsbytes/notifierr",
+    author="adamsbytes",
+    author_email="adamsbytescode@gmail.com",
+    python_requires=">=3.6.0",
+    license="GPLv3",
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'notifierr = notifierr.cli:main',
+        ],
+    },
+    scripts=[],
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Flask",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Home Automation",
+    ],
+)
