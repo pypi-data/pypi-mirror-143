e2b34c962eb8bb15564c90977f1ec9b305af91e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:12 +0000

    Add package utils-mkmenta/0.0.1/utils_mkmenta-0.0.1-py3-none-any.whl

---
+++ packages/utils-mkmenta/utils_mkmenta-0.0.1-py3-none-any.whl/mkutils/__init__.py
@@ -0,0 +1,11 @@
+"""The mkutils package."""
+from ._multithread import *
+from ._to_pickle import *
+from ._show_images import *
+from . import stutils
+
+__name__ = 'mkutils'
+__version__ = "0.0.1"
+__author__ = 'Mikel Menta Garde'
+__url__ = "https://github.com/mkmenta/mkutils"
+__license__ = "MIT License"
