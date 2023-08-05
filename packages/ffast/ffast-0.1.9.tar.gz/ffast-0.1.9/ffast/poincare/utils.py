0bf63b00b52f3829e477d3025706263e620bc7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package ffast/0.1.9/ffast-0.1.9.tar.gz

---
+++ packages/ffast/ffast-0.1.9.tar.gz/ffast-0.1.9/ffast/poincare/utils.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+from numpy import loadtxt
+
+from ffast.preprocessor import PreprocessingPipeline
+
+PREPROCESSOR = PreprocessingPipeline()
+METAPHONES = "ABCEFHIJKLMNOPRSTUWXY0. "
+SIZE_METAPHONES = len(METAPHONES)
+PATH = "poincare/poincare.txt"
+raw_vocab = loadtxt(PATH,usecols=0,dtype=str)
+VOCABULARY = list(map(PREPROCESSOR.normalise,raw_vocab))
+VECTORS = loadtxt(PATH,usecols=range(1,101))
+
+class Poincare(Enum):
+    SIZE_VECTOR = 100
+    SIZE_VOCABULARY = len(VOCABULARY)
+    UNKNOWN = "<Unknown>"
+    SKIP = "skip"
