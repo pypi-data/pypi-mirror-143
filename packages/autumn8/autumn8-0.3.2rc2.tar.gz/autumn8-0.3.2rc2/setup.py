53f4bc261c7968582891de5e03a38d9fd43f0a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:40 +0000

    Add package autumn8/0.3.2rc2/autumn8-0.3.2rc2.tar.gz

---
+++ packages/autumn8/autumn8-0.3.2rc2.tar.gz/autumn8-0.3.2rc2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+from common._version import __version__
+
+dependencies = ["wheel>=0.31.0", "boto3>=1.20.15", "tensorflow", "click>=8.0.1", "questionary>=1.10.0", "torch", "torchvision"]
+
+if os.environ.get('AWS_ACCESS_KEY_ID') == None or os.environ.get('AWS_SECRET_ACCESS_KEY') == None:
+   raise RuntimeError('Please run the build from within a running docker server environment')
+
+os.system(f"export AWS_ACCESS_KEY_ID={os.environ['AWS_ACCESS_KEY_ID']}")
+os.system(f"export AWS_SECRET_ACCESS_KEY={os.environ['AWS_SECRET_ACCESS_KEY']}")
+
+setup(
+   name='autumn8',
+   version=__version__,
+   author='Autumn8',
+   author_email='marcink@radcode.co',
+
+   install_requires=dependencies,
+   setup_requires=dependencies,
+
+
+   packages=[
+      *find_packages(),
+   ],
+   description='Utilities to export models to autodl',
+
+   entry_points={
+      'console_scripts': [
+         'autodl-cli=cli:main',
+      ],
+   },
+)
