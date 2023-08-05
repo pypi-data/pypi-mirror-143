673bd452d65ec9c1083003f6fbd4943428a7fcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:13 +0000

    Add package algdq/0.0.6/algdq-0.0.6.tar.gz

---
+++ packages/algdq/algdq-0.0.6.tar.gz/algdq-0.0.6/algdq/__init__.py
@@ -0,0 +1,16 @@
+def dq(wjm):#读取文件，wjm=文件名
+    with open(wjm,"r",encoding="UTF-8") as file:
+        nr = file.read()
+        return nr
+def dqsc(wjm):#读取输出，wjm=文件名
+    nr = dq(wjm)
+    print(nr)
+def dqzs(wjm,bt,dx,ztdx):
+    nr = dq(wjm)
+    import tkinter as t
+    window = t.Tk()
+    window.title(bt)
+    window.geometry(dx)
+    wz = t.Label(window,text=nr,font=("kaiti",ztdx))
+    wz.pack()
+    window.mainloop()
