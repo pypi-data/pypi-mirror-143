3492fb4badc97517eb87b039d4b60280c19f09f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:16 +0000

    Add package entity-resolution/0.1.0/entity_resolution-0.1.0-py3-none-any.whl

---
+++ packages/entity-resolution/entity_resolution-0.1.0-py3-none-any.whl/resolver/__init__.py
@@ -0,0 +1,10 @@
+
+# DO NOT CHANGE ORDER
+from resolver.entlet import Entlet
+from resolver.entletmap import EntletMap
+from resolver.filter import Filter
+from resolver.strategy import Strategy
+from resolver.pipeline import Pipeline
+
+
+__all__ = ['Entlet', 'EntletMap', 'Filter', 'Pipeline', 'Strategy']
