39298d9c3b50743c3e612d3c0bc16b4adf2a9e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package skyframe/0.0.1/skyframe-0.0.1.tar.gz

---
+++ packages/skyframe/skyframe-0.0.1.tar.gz/skyframe-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='skyframe',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://rise.cs.berkeley.edu',
+    author='Sky Lab',
+    author_email='paras_jain@berkeley.edu',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
