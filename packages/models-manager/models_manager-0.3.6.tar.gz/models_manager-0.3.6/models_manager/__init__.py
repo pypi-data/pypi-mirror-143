9eb088fb27c586b19d631ee64e4e6d314a2ea50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:11 +0000

    Add package models-manager/0.3.6/models_manager-0.3.6.tar.gz

---
+++ packages/models-manager/models_manager-0.3.6.tar.gz/models_manager-0.3.6/models_manager/__init__.py
@@ -0,0 +1,11 @@
+from models_manager.connect import Connect
+from models_manager.manager.field import Field
+from models_manager.manager.model import Model
+from models_manager.providers.provider import Provider
+
+__all__ = [
+    'Field',
+    'Model',
+    'Connect',
+    'Provider'
+]
