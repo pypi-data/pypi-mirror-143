ca27b46868e60fafeb90c4aba60317547a8b2aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:14 +0000

    Add package win-console/0.0.5/win_console-0.0.5.tar.gz

---
+++ packages/win-console/win_console-0.0.5.tar.gz/win_console-0.0.5/README.md
@@ -0,0 +1,6 @@
+# Windows Console Doc V0.0.2
+Welcome to use `Windows Console on Windows` !\
+To use it, please install package with `pip install win-console`\
+Then import it: `import win_console` or `from win_console import *`\
+You can use function `stdin`,`stdout`,`backspace`, `pause`, `choices` and `getpass`.\
+Enjoy!
---
+++ packages/win-console/win_console-0.0.5.tar.gz/win_console-0.0.5/src/win_console/__init__.py
@@ -0,0 +1,14 @@
+﻿from .exception import PlatfromNotSupportError
+from sys import platform
+if not platform.startswith('win'):
+    raise PlatfromNotSupportError('This package JUST Support Running on Windows!')
+from .core import stdout, stdin, backspace, pause, choice
+from .utils import getpass
+__all__ = [
+    'stdout',
+    'stdin',
+    'backspace',
+    'pause',
+    'choice',
+    'getpass'
+]
