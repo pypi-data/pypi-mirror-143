6068d4ff727e268af17e196750c696ce8f9598ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:02 +0000

    Add package ajpuamjad/1.0/ajpuamjad-1.0.tar.gz

---
+++ packages/ajpuamjad/ajpuamjad-1.0.tar.gz/ajpuamjad-1.0/ajpuja/pdf2text.py
@@ -0,0 +1,14 @@
+""" This module provides"""
+
+
+def convert(path):
+    """
+    convert the given pdf to text.
+
+    Parameter:
+    path (str): the path to a pdf file
+
+    Returns:
+    str: The content of the pdf
+    """
+    print("pdf2text")
