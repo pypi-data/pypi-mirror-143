1cce58c3a4044390de6e1f1e287f5ef26f9b39c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:46 +0000

    Add package napari-pyclesperanto-assistant/0.16.0/napari_pyclesperanto_assistant-0.16.0.tar.gz

---
+++ packages/napari-pyclesperanto-assistant/napari_pyclesperanto_assistant-0.16.0.tar.gz/napari_pyclesperanto_assistant-0.16.0/napari_pyclesperanto_assistant/_gui/_select_gpu.py
@@ -0,0 +1,22 @@
+from magicgui import magicgui
+import pyclesperanto_prototype as cle
+from napari_tools_menu import register_function
+
+@register_function(menu="Utilities > Select GPU")
+@magicgui(Select_GPU={
+        "choices": cle.available_device_names(),
+    },call_button='Select')
+def gpu_selector(Select_GPU : str):
+    print("Selected device:", cle.select_device(Select_GPU))
+    gpu_selector.hide()
+
+def select_gpu():
+    if select_gpu.device != None:
+        print("Changing the GPU device is currently not supported.\n"
+              "See also https://github.com/clEsperanto/pyclesperanto_prototype/issues/110")
+        return
+
+    if len(cle.available_device_names()) > 1:
+        gpu_selector.show()
+
+select_gpu.device = None
