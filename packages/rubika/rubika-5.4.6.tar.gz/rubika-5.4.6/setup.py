b7f16ec358dd21bcb017dff18ff3c041a26a119d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:23:14 +0000

    Add package rubika/5.4.6/rubika-5.4.6.tar.gz

---
+++ packages/rubika/rubika-5.4.6.tar.gz/rubika-5.4.6/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+setup(
+  name = 'rubika',
+  packages = ['rubika'],
+  version = '5.4.6',
+  license='MIT', 
+  description = 'rubika library is a unofficial library for making bot in the rubika. this library works with rubika’s API',
+  long_description='docs are ready at rubikalib.ml',
+  long_description_content_type='text/html',
+  author = 'Bahman Ahmadi',
+  author_email = 'bahmanahmadi.mail@gmail.com',
+  url = 'https://github.com/Bahman-Ahmadi/rubika',
+  download_url = 'https://github.com/Bahman-Ahmadi/rubika/archive/refs/tags/v_546.tar.gz',
+  keywords = ["rubika","bot","robot","library","rubikalib","rubikalib.ml","rubikalib.ir","rubika.ir"],
+  install_requires=[
+          'requests',
+          'pycryptodome==3.10.1',
+          'urllib3',
+          'tqdm'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
