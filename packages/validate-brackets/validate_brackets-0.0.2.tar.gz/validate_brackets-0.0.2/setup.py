45406013e03a3ebf40647917695f17c4b30e472c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:56 +0000

    Add package validate-brackets/0.0.2/validate_brackets-0.0.2.tar.gz

---
+++ packages/validate-brackets/validate_brackets-0.0.2.tar.gz/validate_brackets-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="validate_brackets",                 #pip install validate_brackets
+    version="0.0.2",
+    description="validate brackets in a string",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Amit Chojar",
+    author_email="pythonchojar@gmail.com",
+    licence="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    py_modules=["ValidateBrackets"],          #name of the py file
+    package_dir={"": "src"},
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+)
