78d018cb1469cce151927f4e897dd05d7323f4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:21 +0000

    Add package eurlex2lexparency/1.1/eurlex2lexparency-1.1.tar.gz

---
+++ packages/eurlex2lexparency/eurlex2lexparency-1.1.tar.gz/eurlex2lexparency-1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(
+    name="eurlex2lexparency",
+    author="Martin Heimsoth",
+    author_email="mail@lexparency.org",
+    url="https://github.com/Lexparency/eurlex2lexparency",
+    description="Transforming documents from Eur-Lex to Lexparency",
+    version="1.1",
+    packages=['eurlex2lexparency'],
+    install_requires=[
+        'lexref',
+        'singletonmetaclasss',
+        'pandas',
+        'cachier',
+        'dataclasses',
+        'lxml',
+        'Pillow',
+        'SPARQLWrapper == 1.8.5',
+        'rdflib == 4.2.2',
+        'SQLAlchemy',
+        'urllib3',
+        'certifi',
+        'anytree',
+        'requests',
+        'dateparser',
+        'beautifulsoup4',
+        'html5lib',
+        'keepalive',
+        'numpy',
+    ],
+)
