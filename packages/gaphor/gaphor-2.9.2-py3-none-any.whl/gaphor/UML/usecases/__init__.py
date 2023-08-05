c3ffc66ff5deabca621113db676b2eb1b0fc5301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package gaphor/2.9.2/gaphor-2.9.2-py3-none-any.whl

---
+++ packages/gaphor/gaphor-2.9.2-py3-none-any.whl/gaphor/UML/usecases/__init__.py
@@ -0,0 +1,8 @@
+from gaphor.UML.usecases import usecaseconnect
+from gaphor.UML.usecases import usecasegroup
+from gaphor.UML.usecases.actor import ActorItem
+from gaphor.UML.usecases.extend import ExtendItem
+from gaphor.UML.usecases.include import IncludeItem
+from gaphor.UML.usecases.usecase import UseCaseItem
+
+__all__ = ["ActorItem", "ExtendItem", "IncludeItem", "UseCaseItem"]
