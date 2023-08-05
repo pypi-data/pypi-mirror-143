b710cdeae80779cc9a3aaf1facdf6fc2a6f20938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:10 +0000

    Add package rderp/1.0.1/rderp-1.0.1.tar.gz

---
+++ packages/rderp/rderp-1.0.1.tar.gz/rderp-1.0.1/rderp/md/material.py
@@ -0,0 +1,22 @@
+from k3cloud_webapi_sdk.main import K3CloudApiSdk
+#from rderp.rderp.main import ErpClient
+from ..main import ErpClient
+class Material(ErpClient):
+    def View(self, Number,CreateOrgId=0,Id=""):
+        self.Number = Number
+        self.CreateOrgId = CreateOrgId
+        self.Id = Id
+        '''
+        * create the data for material.View opration
+        '''
+        data = {
+    "CreateOrgId": self.CreateOrgId,
+    "Number": self.Number,
+    "Id": self.Id}
+        res = ErpClient.View(self,formid="BD_MATERIAL",data=data)
+        return(res)
+if __name__ == '__main__':
+    app = Material(acct_id='606ada0b30a9e2', user_name='胡立磊', app_id='224986_TY8p2yGs4vg9Xf0tRfSA3a/N6K3d6OlH',
+                       app_secret='e1e0cdc4e8204d178cc383557e64e959', server_url='http://8.133.163.217/k3cloud/')
+    data = app.View(Number='01.21.010')
+    print(data)
