b0b51e8b77c5665b4775bbedf91ade956e51f30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:52 +0000

    Add package django-iris/0.1.7b2/django-iris-0.1.7b2.tar.gz

---
+++ packages/django-iris/django-iris-0.1.7b2.tar.gz/django-iris-0.1.7b2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(dependency_links=[
+  'https://github.com/intersystems-community/iris-driver-distribution/raw/main/intersystems_irispython-3.2.0-py3-none-any.whl'
+])
