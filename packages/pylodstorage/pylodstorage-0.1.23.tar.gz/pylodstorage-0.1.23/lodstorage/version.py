a3ed5e2d6c09ad72b2c5784b567eee644b1f0237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:58 +0000

    Add package pylodstorage/0.1.23/pylodstorage-0.1.23.tar.gz

---
+++ packages/pylodstorage/pylodstorage-0.1.23.tar.gz/pylodstorage-0.1.23/lodstorage/version.py
@@ -0,0 +1,14 @@
+'''
+Created on 2022-03-06
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for ConferenceCorpus
+    '''
+    name="pyLodStorage"
+    version='0.1.23'
+    date = '2020-09-10'
+    updated = '2022-03-18'
