98cc2dfdf178dd92aba51e2b82bc1a1e8bbcdf00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:04 +0000

    Add package win-console/0.0.1/win_console-0.0.1-py3-none-any.whl

---
+++ packages/win-console/win_console-0.0.1-py3-none-any.whl/win_console/__init__.py
@@ -0,0 +1,12 @@
+﻿from .exception import PlatfromNotSupportError
+from sys import platform
+if not platform.startswith('win'):
+    raise PlatfromNotSupportError('This package JUST Support Running on Windows!')
+from .core import stdout, stdin, backspace
+from .utils import getpass
+__all__ = [
+    'stdout',
+    'stdin',
+    'backspace',
+    'getpass'
+]
