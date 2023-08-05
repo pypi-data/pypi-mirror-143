398d8467954e75654aac9fd84465634974721c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:30:08 +0000

    Add package ecs-dist/0.0.1/ecs-dist-0.0.1.tar.gz

---
+++ packages/ecs-dist/ecs-dist-0.0.1.tar.gz/ecs-dist-0.0.1/src/ecs-mod/__init__.py
@@ -0,0 +1,114 @@
+'''
+#### Part1 ([CDK Construct Library](https://github.com/AymanZahran/projen-cdk-tutorial-part-1))
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/AymanZahran/projen-cdk-tutorial-part-1)
+[![build](https://github.com/AymanZahran/projen-cdk-tutorial-part-1/actions/workflows/build.yml/badge.svg)](https://github.com/AymanZahran/projen-cdk-tutorial-part-1/actions/workflows/build.yml)
+[![release](https://github.com/AymanZahran/projen-cdk-tutorial-part-1/actions/workflows/release.yml/badge.svg)](https://github.com/AymanZahran/projen-cdk-tutorial-part-1/actions/workflows/release.yml)
+[![docker](https://img.shields.io/badge/docker-jsii%2Fsuperchain-brightgreen?logo=docker)](https://hub.docker.com/r/jsii/superchain)
+
+#### Part2 ([CDK App](https://github.com/AymanZahran/projen-cdk-tutorial-part-2))
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/AymanZahran/projen-cdk-tutorial-part-2)
+
+#### Part3 ([CDK Pipelines App](https://github.com/AymanZahran/projen-cdk-tutorial-part-3))
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/AymanZahran/projen-cdk-tutorial-part-3)
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import constructs
+
+
+class MyEcsConstruct(
+    constructs.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="ecs-package.MyEcsConstruct",
+):
+    def __init__(
+        self,
+        scope: constructs.Construct,
+        id: builtins.str,
+        *,
+        dockerfile_asset: builtins.str,
+        number_of_azs: jsii.Number,
+    ) -> None:
+        '''
+        :param scope: -
+        :param id: -
+        :param dockerfile_asset: 
+        :param number_of_azs: 
+        '''
+        props = MyEcsProps(
+            dockerfile_asset=dockerfile_asset, number_of_azs=number_of_azs
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+
+@jsii.data_type(
+    jsii_type="ecs-package.MyEcsProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "dockerfile_asset": "dockerfileAsset",
+        "number_of_azs": "numberOfAzs",
+    },
+)
+class MyEcsProps:
+    def __init__(
+        self,
+        *,
+        dockerfile_asset: builtins.str,
+        number_of_azs: jsii.Number,
+    ) -> None:
+        '''
+        :param dockerfile_asset: 
+        :param number_of_azs: 
+        '''
+        self._values: typing.Dict[str, typing.Any] = {
+            "dockerfile_asset": dockerfile_asset,
+            "number_of_azs": number_of_azs,
+        }
+
+    @builtins.property
+    def dockerfile_asset(self) -> builtins.str:
+        result = self._values.get("dockerfile_asset")
+        assert result is not None, "Required property 'dockerfile_asset' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def number_of_azs(self) -> jsii.Number:
+        result = self._values.get("number_of_azs")
+        assert result is not None, "Required property 'number_of_azs' is missing"
+        return typing.cast(jsii.Number, result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "MyEcsProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "MyEcsConstruct",
+    "MyEcsProps",
+]
+
+publication.publish()
