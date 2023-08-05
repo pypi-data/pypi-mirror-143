6d10f0bb70efc8c6ab0ee637941df19ce7b5ad12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:05 +0000

    Add package metaindexmanager/0.6.1/metaindexmanager-0.6.1-py3-none-any.whl

---
+++ packages/metaindexmanager/metaindexmanager-0.6.1-py3-none-any.whl/metaindexmanager/shared.py
@@ -0,0 +1,14 @@
+"""Shared definitions"""
+
+PROGRAMNAME = 'metaindexmanager'
+
+ANY_SCOPE = 'any'
+ALL_SCOPE = 'all'
+
+PREF_BORDER = 'border'
+DEFAULT_BORDER = 'full'
+BORDER_FULL = 'full'
+BORDER_MINIMAL = 'minimal'
+
+PREF_DEFAULT_PANEL = 'default-panel'
+DEFAULT_DEFAULT_PANEL = 'documents'
