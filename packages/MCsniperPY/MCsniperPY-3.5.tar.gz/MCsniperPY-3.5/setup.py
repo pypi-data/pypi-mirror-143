12e633e56a984c815c21759be62590a6edfe1c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:01 +0000

    Add package MCsniperPY/3.5/MCsniperPY-3.5.tar.gz

---
+++ packages/MCsniperPY/MCsniperPY-3.5.tar.gz/MCsniperPY-3.5/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+
+from setuptools import setup
+
+setup(
+    name="MCsniperPY",
+    version="3.5",
+    description="Minecraft name sniper written in Python",
+    long_description=(pathlib.Path(__file__).parent.resolve() / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://mcsniperpy.com",
+    author="Kqzz",
+    license="MIT",
+    packages=["mcsniperpy", "mcsniperpy.util", "mcsniperpy.util.classes"],
+    install_requires=[x.rstrip() for x in open("requirements.txt", "r")],
+    entry_points={"console_scripts": ["mcsniperpy=mcsniperpy.cli:cli"]},
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    project_urls={
+        'GitHub': 'https://github.com/MCsniperPY/MCsniperPY',
+        'Documentation': 'https://docs.mcsniperpy.com',
+    },
+)
