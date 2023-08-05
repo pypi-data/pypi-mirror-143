ed94cc4ba8ce9b26a3b8004d62e0eb853ce44b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:10 +0000

    Add package dwy/0.1/dwy-0.1-py3-none-any.whl

---
+++ packages/dwy/dwy-0.1-py3-none-any.whl/xxb/elegant/girl/information.py
@@ -0,0 +1,8 @@
+import sys
+print(f"程序包含模板：{sys.modules}")
+print(f"程序程序路径：{sys.path}")
+print(f"程序运行平台：{sys.platform}")
+print(f"程序默认编码：{sys.getdefaultencoding()}")
+
+def get_information():
+    return "ckh2.handsome.com"
