4dbf542c7652ac323a0d0e73f2fc0f9fba3069a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:18 +0000

    Add package torchx-nightly/2022.3.18/torchx_nightly-2022.3.18-py3-none-any.whl

---
+++ packages/torchx-nightly/torchx_nightly-2022.3.18-py3-none-any.whl/torchx/runtime/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+The ``torchx.runtime`` module contains libraries and utilities that you can
+use while authoring your applications. The usage of this module is not a hard
+requirement to use TorchX, but it will make it easier for you to author
+applications that are scheduler and infrastructure agnostic.
+
+
+The sub-modules of ``torchx.runtime`` are categorized by topic. For example in
+the ``torchx.runtime.hpo`` module you will find libraries that are useful
+when authoring hyper-parameter optimization jobs (applications).
+
+"""
