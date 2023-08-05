2f1dc58d71b5eb3bff6fea70a4ad4280bfee4993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:22 +0000

    Add package sniped/0.2.1/sniped-0.2.1.tar.gz

---
+++ packages/sniped/sniped-0.2.1.tar.gz/sniped-0.2.1/sniped/__main__.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import json
+import pathlib
+from urllib.request import Request, urlopen
+import sys
+
+import typer
+
+from .configs import CARBON, SNAPPIFY
+
+app = typer.Typer()
+
+CARBON_ENDPOINT = "https://carbonara-42.herokuapp.com/api/cook"
+SNAPPIFY_ENDPOINT = "https://api.snappify.io/snap/simple"
+
+
+def read_code_from_stdin():
+    return "".join([line for line in sys.stdin])
+
+
+def try_reading_from_file(possible_file_path: str) -> str:
+    """Try reading from a file with the given path or return the arg unchanged."""
+
+    possible_file = pathlib.Path(possible_file_path)
+    if possible_file.exists() and possible_file.is_file():
+        with open(possible_file, "r") as f:
+            return f.read()
+
+    return possible_file_path
+
+
+def load_config(config_file_path: str | pathlib.Path) -> dict:
+    with open(config_file_path) as f:
+        return json.load(f)
+
+
+def make_image_from_request(
+    code: str, language: str, out: str, request: Request, config: dict
+):
+
+    request.add_header("Content-Type", "application/json")
+
+    code = read_code_from_stdin() if code == "-" else try_reading_from_file(code)
+    config["code"] = code
+    config["language"] = language
+
+    with urlopen(request, data=json.dumps(config).encode()) as response:
+        image = response.read()
+
+    with open(out, "wb") as f:
+        f.write(image)
+
+
+@app.command()
+def carbon(
+    code: str,
+    language: str = typer.Argument("auto"),
+    out: str = typer.Argument("out.png"),
+):
+    make_image_from_request(
+        code,
+        language,
+        out,
+        Request(CARBON_ENDPOINT),
+        CARBON,
+    )
+
+
+@app.command()
+def snappify(code: str, key: str, language: str, out: str = typer.Argument("out.png")):
+    req = Request(SNAPPIFY_ENDPOINT)
+    key = try_reading_from_file(key)
+    req.add_header("Authorization", key)
+    make_image_from_request(
+        code,
+        language,
+        out,
+        Request(SNAPPIFY_ENDPOINT),
+        SNAPPIFY,
+    )
+
+
+if __name__ == "__main__":
+    app()
