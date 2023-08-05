a7e2d715d36dbbfcce885bf260f7b6432ccce38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:32 +0000

    Add package Sqlcarve/0.4/Sqlcarve-0.4-py3-none-any.whl

---
+++ packages/Sqlcarve/Sqlcarve-0.4-py3-none-any.whl/sqlcarve/main.py
@@ -0,0 +1,24 @@
+from src.sqlcarve.validator.validator import *
+import logging as log
+
+
+def analyse(fichier, referencefile):
+    log.info("Start of validation")
+    valid = Validator()
+    dico = {}
+    with open(fichier, "r") as f_content:
+        stmnt_list = Preprocessor.extract_stmnts(f_content)
+        for stmnt in stmnt_list:
+            dico = valid.validate(statement=stmnt, ref_statement=stmnt, ref_comments=referencefile, type="all")
+
+    print(dico)
+
+    log.info("End of validation")
+
+
+if __name__ == "__main__":
+    # referencefile = "../../resources/reference.json"
+    # archive = zipfile.ZipFile('../../resources/prese-structure-devoir.zip', 'r')
+    # analyse("../resources/prese-structure-devoir.zip", "../resources/reference.json")
+    analyse("../resources/queries/samples01/qry005.sql", "../resources/reference.json")
+    # analyse("../resources/queries/queries_tests/qryTests.sql", "../resources/reference.json")
