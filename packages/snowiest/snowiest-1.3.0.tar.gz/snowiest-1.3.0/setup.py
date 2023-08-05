3daf0ea0cd1f008683f600aaea2a14aee3cbae21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:12 +0000

    Add package snowiest/1.3.0/snowiest-1.3.0.tar.gz

---
+++ packages/snowiest/snowiest-1.3.0.tar.gz/snowiest-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'snowiest',
+    version = '1.3.0',
+    py_modules=['snowiest'],
+    author='SnowyFu',
+    author_email='fuxingxue@hotmail.com',
+    url='https://github.com/pypa/sampleproject',
+    package_dir={'snowiest': 'snowiest'},
+    package_data={'snowiest': ['*.*', 'snowiest/*']},
+)
