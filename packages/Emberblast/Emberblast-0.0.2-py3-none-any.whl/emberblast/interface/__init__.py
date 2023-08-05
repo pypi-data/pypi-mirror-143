9c23333e1068b22bab65fbd22569f03ffe6b5a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:44 +0000

    Add package Emberblast/0.0.2/Emberblast-0.0.2-py3-none-any.whl

---
+++ packages/Emberblast/Emberblast-0.0.2-py3-none-any.whl/emberblast/interface/__init__.py
@@ -0,0 +1,11 @@
+from .interface import IPlayer, IJob, IItem, IHealingItem, IRecoveryItem, IEquipmentItem, IBag, IRace, IEquipment, \
+    ISideEffect, ISkill, IMap, IGraph, IVertex, IEdge, IGame, IControlledPlayer, IBotPlayer, IAction, \
+    IGameOrchestrator, IBotDecisioning, IPlayingMode, IGameFactory, IActionsQuestioner, ISkillsQuestioner, \
+    IMovementQuestioner, IItemsQuestioner, ILevelUpQuestioner, INewGameQuestioner, ISaveLoadQuestioner, \
+    IEnemiesQuestioner
+
+__all__ = ['IPlayer', 'IJob', 'IItem', 'IHealingItem', 'IRecoveryItem', 'IEquipmentItem', 'IBag',
+           'IRace', 'IEquipment', 'ISideEffect', 'ISkill', 'IMap', 'IGraph', 'IVertex', 'IEdge', 'IGame',
+           'IControlledPlayer', 'IBotPlayer', 'IAction', 'IGameOrchestrator', 'IBotDecisioning', 'IPlayingMode',
+           'IGameFactory', 'IActionsQuestioner', 'ISkillsQuestioner', 'IMovementQuestioner',
+           'IItemsQuestioner', 'ILevelUpQuestioner', 'INewGameQuestioner', 'ISaveLoadQuestioner', 'IEnemiesQuestioner']
