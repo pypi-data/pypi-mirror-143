ec591f64e32f15bfbb62cb18929f7b538ef48630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:01 +0000

    Add package nflbdb/0.1/nflbdb-0.1-py3-none-any.whl

---
+++ packages/nflbdb/nflbdb-0.1-py3-none-any.whl/nflbdb/__init__.py
@@ -0,0 +1,4 @@
+"""My utilities for work with Big Data Bowl Datasets"""
+
+__version__ = "0.1"
+
---
+++ packages/nflbdb/nflbdb-0.1-py3-none-any.whl/nflbdb/transform.py
@@ -0,0 +1,7 @@
+""" Tools to Transform and Build various datasets """
+
+def filter_play(p):
+    # assumes that p is a valid dataframe from the play data in 2022
+    # TODO: better docs, tests, and more broadly useful for an arbitrary input
+    pass
+
