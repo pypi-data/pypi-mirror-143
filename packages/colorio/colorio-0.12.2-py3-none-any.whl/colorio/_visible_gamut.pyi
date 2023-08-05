3442feca1ea516dcc2002fde5ae4155b3770ffa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:22:04 +0000

    Add package colorio/0.12.2/colorio-0.12.2-py3-none-any.whl

---
+++ packages/colorio/colorio-0.12.2-py3-none-any.whl/colorio/_visible_gamut.pyi
@@ -0,0 +1,6 @@
+from . import observers as observers
+from ._tools import get_mono_outline_xy as get_mono_outline_xy
+from .cs import ColorCoordinates as ColorCoordinates, ColorSpace as ColorSpace, convert as convert, string_to_cs as string_to_cs
+
+def plot_visible_gamut(colorspace: Union[ColorSpace, str], observer, max_Y1, show_grid: bool = ..., h: float = ...): ...
+def plot_visible_slice(colorspace: Union[ColorSpace, str], lightness: float, outline_prec: float = ..., fill_color: str = ...): ...
