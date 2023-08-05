bbe51558926f8e07d155df94154a4f5feff077af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package laserbeamsize/1.9.4/laserbeamsize-1.9.4.tar.gz

---
+++ packages/laserbeamsize/laserbeamsize-1.9.4.tar.gz/laserbeamsize-1.9.4/laserbeamsize/__init__.py
@@ -0,0 +1,27 @@
+"""
+A package to facilitate analysis of laser beam images.
+
+Full documentation is available at <https://laserbeamsize.readthedocs.io>
+
+Local help is available for the two modules that comprise the
+laserbeamsize package. laserbeamsize.laserbeamsize has functions
+for finding the size of an beam from as single image. This
+information can be found using
+
+help(laserbeamsize.laserbeamsize)
+
+The other module has the functions that find the M² value and other
+beam parameters from a sequence of images.  This information can be
+found using
+
+help(laserbeamsize.m2)
+"""
+__version__ = '1.9.4'
+__author__ = 'Scott Prahl'
+__email__ = 'scott.prahl@oit.edu'
+__copyright__ = 'Copyright 2017-22, Scott Prahl'
+__license__ = 'MIT'
+__url__ = 'https://github.com/scottprahl/laserbeamsize.git'
+
+from .laserbeamsize import *
+from .m2 import *
