31b74c5411a35d2a4671c8fcf53da0de5478e063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:32 +0000

    Add package charmonium.freeze/0.5.3/charmonium.freeze-0.5.3-py3-none-any.whl

---
+++ packages/charmonium.freeze/charmonium.freeze-0.5.3-py3-none-any.whl/charmonium/freeze/__init__.py
@@ -0,0 +1,16 @@
+from .lib import FreezeError as FreezeError
+from .lib import FreezeRecursionError as FreezeRecursionError
+from .lib import UnfreezableTypeError as UnfreezableTypeError
+from .lib import config as config
+from .lib import freeze as freeze
+
+__version__ = "0.5.2"
+
+__all__ = [
+    "__version__",
+    "freeze",
+    "config",
+    "UnfreezableTypeError",
+    "FreezeRecursionError",
+    "FreezeError",
+]
