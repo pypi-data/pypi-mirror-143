cdc550405321a3b7ecca9da160dfc32b1a678d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package sniped/0.1.0/sniped-0.1.0.tar.gz

---
+++ packages/sniped/sniped-0.1.0.tar.gz/sniped-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sniped']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['sniped = sniped.__main__:app']}
+
+setup_kwargs = {
+    'name': 'sniped',
+    'version': '0.1.0',
+    'description': 'Turn snippets of code into beautiful images.',
+    'long_description': '# sniped\n',
+    'author': 'Rodrigo Girão Serrão',
+    'author_email': '5621605+RodrigoGiraoSerrao@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
