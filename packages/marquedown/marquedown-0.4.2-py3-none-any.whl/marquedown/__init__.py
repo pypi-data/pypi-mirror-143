a1770809f57916a9967af210067c32d0602841f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:03 +0000

    Add package marquedown/0.4.2/marquedown-0.4.2-py3-none-any.whl

---
+++ packages/marquedown/marquedown-0.4.2-py3-none-any.whl/marquedown/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '0.4.2'
+
+import markdown as md
+
+from .citation import citation
+from .video import video
+from .capitalized import capitalized
+
+
+def marquedown(document: str, **kwargs):
+    """Convert both Marquedown and Markdown into HTML."""
+
+    if kwargs.get('citation', True):
+        document = citation(document)
+
+    if kwargs.get('video', True):
+        document = video(document)
+        
+    html = md.markdown(document)
+
+    if kwargs.get('capitalized', True):
+        html = capitalized(html)
+
+    return html
