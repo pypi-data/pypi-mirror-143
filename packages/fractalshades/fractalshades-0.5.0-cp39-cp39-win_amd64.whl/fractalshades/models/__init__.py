d4f301ed9795ce7389925b5829d01dbafe7538f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:52 +0000

    Add package fractalshades/0.5.0/fractalshades-0.5.0-cp39-cp39-win_amd64.whl

---
+++ packages/fractalshades/fractalshades-0.5.0-cp39-cp39-win_amd64.whl/fractalshades/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .mandelbrot_M2 import Mandelbrot, Perturbation_mandelbrot
+from .mandelbrot_Mn import Mandelbrot_N
+from .burning_ship import Burning_ship, Perturbation_burning_ship
+from .collatz import Collatz
