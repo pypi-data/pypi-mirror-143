cd448c2b5d20a3e2e35e4724af688d1f24cd6ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:07 +0000

    Add package lhub-cli/0.0.5/lhub_cli-0.0.5.tar.gz

---
+++ packages/lhub-cli/lhub_cli-0.0.5.tar.gz/lhub_cli-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+
+setup(
+    name="lhub_cli",
+    version=VERSION,
+    author="Chad Roberts",
+    author_email="chad@logichub.com",
+    description="LogicHub CLI",
+    long_description="A Python package for interacting with LogicHub via shell commands",
+    packages=find_packages(),
+    install_requires=[
+        # ToDo Find out if these support specific versions or if requirements.txt is used for that
+        "requests", "lhub", "configobj", "dataclasses_json", "rsa", "tabulate"
+    ],
+    keywords=["python", "lhub", "LogicHub"],
+
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
+    ]
+)
