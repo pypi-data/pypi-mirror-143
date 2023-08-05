1eb525fa39cc089b4f4827a23aee55b5e3e4797b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:26:44 +0000

    Add package gpicalc/0.2.0/gpicalc-0.2.0.tar.gz

---
+++ packages/gpicalc/gpicalc-0.2.0.tar.gz/gpicalc-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gpicalc",
+    version="0.2.0",
+    author="M. Y. Chia",
+    author_email="chiamy94@gmail.com",
+    description="a package for calculating global performance indicator (GPI)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/planta94/gpicalc",
+	
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
