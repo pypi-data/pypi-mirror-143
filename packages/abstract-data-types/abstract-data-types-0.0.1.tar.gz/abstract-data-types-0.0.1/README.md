e9553e0f48ac0700d746c9f44e15b3447e157614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:59 +0000

    Add package abstract-data-types/0.0.1/abstract-data-types-0.0.1.tar.gz

---
+++ packages/abstract-data-types/abstract-data-types-0.0.1.tar.gz/abstract-data-types-0.0.1/README.md
@@ -0,0 +1,14 @@
+# Abstract data types
+
+This is a package that adds some complex data structures like arrays and graphs that are tailored to suit our needs. Over time we added methods that helped us in specific situations such as the generation of a Voronoi diagram that is included in the matrix, which is perhaps not so related to its operation, however, we added them anyway because the objective to create this library was to learn more about python and try to make as many advances from scratch
+
+## Authors
+
+* **Arcos Juan** - [ArcosJuan](https://github.com/ArcosJuan).
+* **Carazo Medley Matías** - [Matimed](https://github.com/Matimed).
+
+## License
+
+_abstract-data-types as a whole is licensed under the MIT License - Look the [LICENSE](LICENSE) file for details._
+
+
---
+++ packages/abstract-data-types/abstract-data-types-0.0.1.tar.gz/abstract-data-types-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Abstract data structures'
+
+# Setting up
+setup(
+    name="abstract-data-types",
+    version=VERSION,
+    author="Matimed (Matías Carazo Medley)",
+    author_email="<mcarazomedley@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'data', 'structures', 'graph', 'matrix'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
