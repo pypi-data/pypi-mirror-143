db0b1000d281f41a0ece1a98c80a384cb71062cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package hdroller/0.6.7/hdroller-0.6.7-py3-none-any.whl

---
+++ packages/hdroller/hdroller-0.6.7-py3-none-any.whl/hdroller/die/empty.py
@@ -0,0 +1,31 @@
+__docformat__ = 'google'
+
+import hdroller
+import hdroller.die.base
+from hdroller.collections import Weights
+
+class EmptyDie(hdroller.die.base.BaseDie):
+    """ Die with no outcomes. """
+    
+    def ndim(self):
+        return 'empty'
+    
+    def __init__(self):
+        self._data = Weights({})
+        
+    def unary_op(self, op, *args, **kwargs):
+        """ There are no outcomes, so nothing happens. """
+        return self
+    
+    def binary_op(self, other, op, *args, **kwargs):
+        """ There are no outcomes, so nothing happens. """
+        return self
+    
+    def wrap_unpack(self, func):
+        return func
+
+    def __repr__(self):
+        return type(self).__qualname__ + '()'
+    
+    def __str__(self):
+        return 'EmptyDie'
