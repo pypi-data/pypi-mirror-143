abd63a4b7ed2071dba8c3da8526f4981d36b79da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package helloworld-project/0.0.1/helloworld_project-0.0.1.tar.gz

---
+++ packages/helloworld-project/helloworld_project-0.0.1.tar.gz/helloworld_project-0.0.1/helloworld_project/__init__.py
@@ -0,0 +1,2 @@
+from core.py import get_message
+from core.py import print_message
---
+++ packages/helloworld-project/helloworld_project-0.0.1.tar.gz/helloworld_project-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='helloworld_project', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='mmorfii',
+    author_email='dasha.zueva@list.ru',
+    description='My Package hello_world',
+    packages=setuptools.find_packages(),
+)
