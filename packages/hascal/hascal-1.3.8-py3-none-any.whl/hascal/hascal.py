9e1f5b8f31f25824b3910e9a6a7309a8eef46101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:34 +0000

    Add package hascal/1.3.8/hascal-1.3.8-py3-none-any.whl

---
+++ packages/hascal/hascal-1.3.8-py3-none-any.whl/hascal/hascal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# The Hascal Application
+#
+# The Hascal Programming Language
+# Copyright 2019-2022 Hascal Development Team,
+# all rights reserved.
+
+from sys import argv 
+from hascal.core.h_builder import HascalCompiler
+import pathlib
+
+def main():
+      BASE_DIR = str(pathlib.Path(__file__).parent.resolve())
+      HascalCompiler(argv,BASE_DIR)
+
+if __name__ == "__main__":  
+      main()
---
+++ packages/hascal/hascal-1.3.8-py3-none-any.whl/hascal/hlib/libcpp/std.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdarg>
+#include <cstring>
+#include <type_traits>
+#include <typeinfo>
+#include <unordered_map>
+#include <string_view>
