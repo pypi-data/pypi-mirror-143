6d3757701cfa808f407bf8d411421c44bf5cae3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:44 +0000

    Add package Emberblast/0.0.2/Emberblast-0.0.2.tar.gz

---
+++ packages/Emberblast/Emberblast-0.0.2.tar.gz/Emberblast-0.0.2/emberblast/questions/__init__.py
@@ -0,0 +1,6 @@
+from .level_up import improve_attributes_automatically, \
+    improve_attributes_randomly
+
+from .questions import questioning_system_injector
+
+__all__ = ['improve_attributes_automatically', 'improve_attributes_randomly', 'questioning_system_injector']
