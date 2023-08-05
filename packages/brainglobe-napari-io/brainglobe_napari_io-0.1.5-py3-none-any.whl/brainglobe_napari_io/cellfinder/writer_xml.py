92b12890babb1e3f27082295cfd6f79031b5f1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:31 +0000

    Add package brainglobe-napari-io/0.1.5/brainglobe_napari_io-0.1.5-py3-none-any.whl

---
+++ packages/brainglobe-napari-io/brainglobe_napari_io-0.1.5-py3-none-any.whl/brainglobe_napari_io/cellfinder/writer_xml.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple, Dict, Any
+from imlib.IO.cells import save_cells
+from imlib.cells.cells import Cell
+
+from .utils import convert_layer_to_cells
+
+
+def cellfinder_write_xml(path, data, meta):
+    if isinstance(path, str) and path.endswith(".xml"):
+        return write_points_to_xml(path, data, meta)
+    else:
+        return None
+
+
+def write_points_to_xml(path, data, metadata):
+    cells_to_save = []
+    if metadata["metadata"]["point_type"] == Cell.CELL:
+        cells_to_save.extend(convert_layer_to_cells(data))
+    elif metadata["metadata"]["point_type"] == Cell.UNKNOWN:
+        cells_to_save.extend(convert_layer_to_cells(data, cells=False))
+
+    if cells_to_save:
+        save_cells(cells_to_save, path)
+
+    return path
---
+++ packages/brainglobe-napari-io/brainglobe_napari_io-0.1.5-py3-none-any.whl/brainglobe_napari_io/plugins.py
@@ -0,0 +1,9 @@
+from brainglobe_napari_io.cellfinder.reader_xml import cellfinder_read_xml
+from brainglobe_napari_io.cellfinder.reader_dir import cellfinder_read_dir
+
+from brainglobe_napari_io.brainreg.reader_dir import brainreg_read_dir
+
+from brainglobe_napari_io.cellfinder.writer_multiple_xml import (
+    cellfinder_write_multiple_xml,
+)
+from brainglobe_napari_io.cellfinder.writer_xml import cellfinder_write_xml
