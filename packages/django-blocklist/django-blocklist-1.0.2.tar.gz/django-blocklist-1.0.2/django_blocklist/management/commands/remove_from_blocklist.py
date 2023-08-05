2958c96131a0df843f0a6956c5a25a03a76bc69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:55 +0000

    Add package django-blocklist/1.0.2/django-blocklist-1.0.2.tar.gz

---
+++ packages/django-blocklist/django-blocklist-1.0.2.tar.gz/django-blocklist-1.0.2/django_blocklist/management/commands/remove_from_blocklist.py
@@ -0,0 +1,27 @@
+"""Remove given IPs from django_blocklist."""
+import datetime
+import logging
+import sys
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from django_blocklist.models import BlockedIP
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("ips", nargs="+", type=str, help="IPs (space-separated) to remove")
+
+    help = __doc__
+
+    def handle(self, *args, **options):
+        ips = options.get("ips")
+        matches = BlockedIP.objects.filter(ip__in=ips)
+        count = matches.count()
+        print(f"Found {count} of {len(ips)}.")
+        if count:
+            matches.delete()
+            print("Deleted.")
