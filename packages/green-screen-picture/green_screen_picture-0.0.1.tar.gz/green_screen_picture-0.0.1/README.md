ac26d4a5abd176820029f16844ca29409819c1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:56 +0000

    Add package green-screen-picture/0.0.1/green_screen_picture-0.0.1.tar.gz

---
+++ packages/green-screen-picture/green_screen_picture-0.0.1.tar.gz/green_screen_picture-0.0.1/README.md
@@ -0,0 +1,26 @@
+# green_screen_picture
+
+The package green_screen_picture is used to merge background to green screen picture
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install green_screen_picture
+
+```bash
+pip install green_screen_picture
+```
+
+## Usage
+
+```python
+from green_screen_picture.combination import combine
+combine(picture, background)
+```
+
+## Author
+
+mrcs22
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/green-screen-picture/green_screen_picture-0.0.1.tar.gz/green_screen_picture-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt", "rb") as f:
+    encoded_requirements = f.read()
+    decoded_requirements = encoded_requirements.decode(
+        'utf-16le', 'ignore').splitlines()
+
+    requirements = [
+        req for req in decoded_requirements if req.find("==") >= 0]
+    print(requirements)
+
+
+setup(
+    name="green_screen_picture",
+    version="0.0.1",
+    author="mrcs22",
+    author_email="marcusmoraes2010@hotmail.com",
+    description="simple package to add background images to green screen",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrcs22/green_screen_picture",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
