ca1ef98b924a04bae7b965ef44208fa8e35749fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:25 +0000

    Add package d3m-simon/1.2.5/d3m-simon-1.2.5.tar.gz

---
+++ packages/d3m-simon/d3m-simon-1.2.5.tar.gz/d3m-simon-1.2.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='d3m-simon',
+    version='1.2.5',
+    description='Character-level CNN+LSTM model for text classification',
+    packages=['Simon', 'Simon.penny'],
+    license='MIT',
+    url='https://gitlab.com/datadrivendiscovery/contrib/simon',
+    install_requires=['Faker >= 0.7.7',
+        'scikit-learn==0.22.2.post1',
+        'python-dateutil>=2.8.1',
+        'pandas==1.1.3',
+        'scipy==1.4.1',
+        'tensorflow-gpu==2.2.0',
+        'h5py >= 2.7.0'],
+    include_package_data=True,
+)
