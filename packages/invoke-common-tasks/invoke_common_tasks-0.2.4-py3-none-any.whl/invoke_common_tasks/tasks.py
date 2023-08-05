83ed61f0933abca20a523216c27ac7de6c1decf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:22:23 +0000

    Add package invoke-common-tasks/0.2.4/invoke_common_tasks-0.2.4-py3-none-any.whl

---
+++ packages/invoke-common-tasks/invoke_common_tasks-0.2.4-py3-none-any.whl/invoke_common_tasks/tasks.py
@@ -0,0 +1,49 @@
+# Standard Library
+import shutil
+
+# Third Party
+from invoke import task
+
+# NOTE: Invoke tasks files don't support mypy typechecking for the forseeable future
+# They were looking at addressing it after Python2 EOL 01-01-2020 but there was a global pandemic.
+# https://github.com/pyinvoke/invoke/issues/357
+
+
+@task
+def format(c):
+    """Autoformat code for code style."""
+    c.run("black .")
+    c.run("isort .")
+
+
+@task
+def lint(c):
+    """Linting and style checking."""
+    c.run("black --check .")
+    c.run("isort --check .")
+    c.run("flake8 .")
+
+
+@task
+def typecheck(c):
+    """Run typechecking tooling."""
+    c.run("mypy .")
+
+
+@task
+def test(c):
+    """Run test suite."""
+    c.run("python3 -m pytest")
+
+
+@task
+def build(c):
+    """Build wheel."""
+    shutil.rmtree("dist/", ignore_errors=True)
+    c.run("poetry build -f wheel")
+
+
+@task(pre=[lint, typecheck, test])
+def ci(c):
+    """Run linting and test suite for Continuous Integration."""
+    ...
