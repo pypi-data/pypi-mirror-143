2b4bf98d95062a829fa453ff9dce17d0d4d49f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package foledol-django/0.0.20/foledol-django-0.0.20.tar.gz

---
+++ packages/foledol-django/foledol-django-0.0.20.tar.gz/foledol-django-0.0.20/src/foledol/django/tools/filter.py
@@ -0,0 +1,18 @@
+class Filter:
+    code = 'default'
+    name = "default"
+    title = None
+
+    def __str__(self):
+        return self.name
+
+    def filter(self, source):
+        return source
+
+    def all(self):
+        pass
+
+
+def filters_dictionary(filters):
+    return {item.code: item for item in filters}
+
