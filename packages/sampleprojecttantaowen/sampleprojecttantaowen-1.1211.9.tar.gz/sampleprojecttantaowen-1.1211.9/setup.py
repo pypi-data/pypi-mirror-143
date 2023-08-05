7fe33c562d1f0905a5b04dade1f5b4c1d672e9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:46 +0000

    Add package sampleprojecttantaowen/1.1211.9/sampleprojecttantaowen-1.1211.9.tar.gz

---
+++ packages/sampleprojecttantaowen/sampleprojecttantaowen-1.1211.9.tar.gz/sampleprojecttantaowen-1.1211.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sampleprojecttantaowen", # Replace with your own username
+    version="1.1211.9",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleprojecttantaowen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
