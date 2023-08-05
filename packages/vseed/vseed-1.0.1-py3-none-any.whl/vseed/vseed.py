1de1a5e0b4e68cb4b5b2cae0c43b060b63e7e72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package vseed/1.0.1/vseed-1.0.1-py3-none-any.whl

---
+++ packages/vseed/vseed-1.0.1-py3-none-any.whl/vseed/vseed.py
@@ -0,0 +1,9 @@
+def iacp():
+    import os
+    os.system("pip list")
+    os.system("pip install easygui")
+    os.system("pip install pywin32")
+def importall():
+    import os,time,easygui
+    import win32api as hhg
+    import win32con as hhu
