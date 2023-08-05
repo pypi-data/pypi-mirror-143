4a8d954ed3c74ae3799e07bc48c8f09f37478b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:11 +0000

    Add package rivals-top8-results/1.5.3/rivals-top8-results-1.5.3.tar.gz

---
+++ packages/rivals-top8-results/rivals-top8-results-1.5.3.tar.gz/rivals-top8-results-1.5.3/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+from glob import glob
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+resources_dir = r"C:/Users/Impasse/My Drive/Linguaggi/Python/Rivals Top 8 Results/src/rivals_top8_results/"
+
+setuptools.setup(
+    name="rivals-top8-results",
+    version="1.5.3",
+    author="Impasse52",
+    author_email="giuseppe.termerissa@gmail.com",
+    description="Small Pillow library useful in creating automatic Rivals of Aether Top 8 Results screens ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Impasse52/rivals-top8-results",
+    project_urls={
+        "Bug Tracker": "https://github.com/Impasse52/rivals-top8-results/issues",
+    },
+    package_dir={"": "src"},
+    include_package_data=True,
+    data_files=[
+        (
+            "Resources_Main",
+            glob(r"./src/rivals_top8_results/Resources/Characters/Main/**/**.png"),
+        ),
+        (
+            "Resources_Secondary",
+            glob(r"./src/rivals_top8_results/Resources/Characters/Secondary/**"),
+        ),
+        (
+            "Resources_Custom",
+            glob(r"./src/rivals_top8_results/Resources/Characters/Main/Custom/**/**.png"),
+        ),
+        (
+            "Resources_Backgrounds",
+            glob(r"./src/rivals_top8_results/Resources/Backgrounds/**/**"),
+        ),
+        (
+            "Resources_Layout",
+            glob(r"./src/rivals_top8_results/Resources/Layout/**"),
+        ),
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
