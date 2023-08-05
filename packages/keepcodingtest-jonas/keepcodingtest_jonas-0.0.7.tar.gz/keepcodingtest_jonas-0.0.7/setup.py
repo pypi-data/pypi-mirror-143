2a711d84dbe6b05ffc8b464d99cd7d5c9211566e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:28 +0000

    Add package keepcodingtest-jonas/0.0.7/keepcodingtest_jonas-0.0.7.tar.gz

---
+++ packages/keepcodingtest-jonas/keepcodingtest_jonas-0.0.7.tar.gz/keepcodingtest_jonas-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="keepcodingtest_jonas",
+    version="0.0.7",
+    author="jonasmarquez",
+    author_email="jonas@jonasmarquez.com",
+    description="Simple Example",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=[
+       'build',
+       'twine',
+       'coloredlogs'
+   ],
+)
