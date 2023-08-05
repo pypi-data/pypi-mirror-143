6a38c877d583e25ace5df0d1e6609cfe26af7af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:25 +0000

    Add package django-history-triggers/3.1.1/django_history_triggers-3.1.1-py3-none-any.whl

---
+++ packages/django-history-triggers/django_history_triggers-3.1.1-py3-none-any.whl/history/__init__.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+from .backends import get_backend  # noqa
+from .utils import get_history_model  # noqa
+
+__version__ = "3.1.1"
+__version_info__ = tuple(int(num) for num in __version__.split(".") if num.isdigit())
+
+
+class _Configuration:
+    def __init__(self, prefix=None, **defaults):
+        self.prefix = prefix or self.__module__.split(".")[0]
+        self.defaults = defaults
+
+    def __getattr__(self, name):
+        if name not in self.defaults:
+            raise AttributeError(name)
+        setting_name = "{}_{}".format(self.prefix, name).upper()
+        return getattr(settings, setting_name, self.defaults[name])
+
+
+conf = _Configuration(
+    IGNORE_APPS=["admin", "contenttypes", "sessions"],
+    MIDDLEWARE_IGNORE=[],
+    FILTER="history.utils.default_filter",
+    REQUEST_CONTEXT="history.utils.get_request_context",
+    ADMIN_ENABLED=True,
+    SNAPSHOTS=True,
+)
