77aeb30a3c744951c0380143d7d88c8cba2800ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:22 +0000

    Add package extract-video-ppt/0.1/extract-video-ppt-0.1.tar.gz

---
+++ packages/extract-video-ppt/extract-video-ppt-0.1.tar.gz/extract-video-ppt-0.1/README.md
@@ -0,0 +1,11 @@
+## extract ppt from a video
+
+### useage
+``` shell
+# help info
+evp --help
+# example
+evp --degree 0.6 --pdfname yeah.pdf ./ ./test.mp4
+# degree: if frame is same with last frame over degree , this frame will be dropped
+# pdfname: the name for export pdf 
+```
---
+++ packages/extract-video-ppt/extract-video-ppt-0.1.tar.gz/extract-video-ppt-0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+    name='extract-video-ppt',
+    version='0.1',
+    author="wudu",
+    author_email="296525335@qq.com",
+    description="support export ppt from a video",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=['evp'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'fpdf',
+        'matplotlib',
+        'opencv-python',
+        'numpy'
+    ],
+    entry_points='''
+        [console_scripts]
+        evp=video2ppt:main
+    ''',
+)
