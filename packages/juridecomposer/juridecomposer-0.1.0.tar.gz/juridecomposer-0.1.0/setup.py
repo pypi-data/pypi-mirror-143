b6895a55e250f44cbdc602770417ceb4bb144bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:31 +0000

    Add package juridecomposer/0.1.0/juridecomposer-0.1.0.tar.gz

---
+++ packages/juridecomposer/juridecomposer-0.1.0.tar.gz/juridecomposer-0.1.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['juridecomposer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nltk>=3.5,<4.0',
+ 'pandas>=1.0.5,<2.0.0',
+ 'pattern>=3.6,<4.0',
+ 'xmltodict>=0.12.0,<0.13.0']
+
+entry_points = \
+{'console_scripts': ['my-script = '
+                     'src.juridecomposer.juridecomposer:xml_to_dataframe',
+                     "packages = [{'include': 'juridecomposer', 'from': "
+                     "'src'}]"]}
+
+setup_kwargs = {
+    'name': 'juridecomposer',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Roos Bakker',
+    'author_email': 'roos.bakker@tno.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.1,<3.7',
+}
+
+
+setup(**setup_kwargs)
