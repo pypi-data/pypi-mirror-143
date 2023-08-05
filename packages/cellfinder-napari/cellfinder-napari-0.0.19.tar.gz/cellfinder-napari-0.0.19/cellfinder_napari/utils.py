afa7c5eb11a6e72fa64f207d2bf7103d8cda97f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package cellfinder-napari/0.0.19/cellfinder-napari-0.0.19.tar.gz

---
+++ packages/cellfinder-napari/cellfinder-napari-0.0.19.tar.gz/cellfinder-napari-0.0.19/cellfinder_napari/utils.py
@@ -0,0 +1,148 @@
+import pandas as pd
+from imlib.cells.cells import Cell
+from pkg_resources import resource_filename
+from qtpy.QtWidgets import QComboBox, QLabel, QMessageBox, QPushButton
+
+brainglobe_logo = resource_filename(
+    "cellfinder_napari", "images/brainglobe.png"
+)
+
+
+def html_label_widget(label: str, tag: str = "b") -> dict:
+    return dict(
+        widget_type="Label",
+        label=f"<{tag}>{label}</{tag}>",
+    )
+
+
+def add_layers(points, viewer) -> None:
+    """
+    Adds classified cell candidates as two separate point layers to the napari viewer.
+    """
+    detected, rejected = cells_to_array(points)
+
+    viewer.add_points(
+        rejected,
+        name="Rejected",
+        size=15,
+        n_dimensional=True,
+        opacity=0.6,
+        symbol="ring",
+        face_color="lightskyblue",
+        visible=False,
+        metadata=dict(point_type=Cell.UNKNOWN),
+    )
+    viewer.add_points(
+        detected,
+        name="Detected",
+        size=15,
+        n_dimensional=True,
+        opacity=0.6,
+        symbol="ring",
+        face_color="lightgoldenrodyellow",
+        metadata=dict(point_type=Cell.CELL),
+    )
+
+
+def cells_df_as_np(cells_df, new_order=[2, 1, 0], type_column="type"):
+    cells_df = cells_df.drop(columns=[type_column])
+    cells = cells_df[cells_df.columns[new_order]]
+    cells = cells.to_numpy()
+    return cells
+
+
+def cells_to_array(cells):
+    df = pd.DataFrame([c.to_dict() for c in cells])
+    points = cells_df_as_np(df[df["type"] == Cell.CELL])
+    rejected = cells_df_as_np(df[df["type"] == Cell.UNKNOWN])
+    return points, rejected
+
+
+def add_combobox(
+    layout,
+    label,
+    items,
+    row,
+    column=0,
+    label_stack=False,
+    callback=None,
+    width=150,
+):
+    if label_stack:
+        combobox_row = row + 1
+        combobox_column = column
+    else:
+        combobox_row = row
+        combobox_column = column + 1
+    combobox = QComboBox()
+    combobox.addItems(items)
+    if callback:
+        combobox.currentIndexChanged.connect(callback)
+    combobox.setMaximumWidth = width
+
+    if label is not None:
+        combobox_label = QLabel(label)
+        combobox_label.setMaximumWidth = width
+        layout.addWidget(combobox_label, row, column)
+    else:
+        combobox_label = None
+
+    layout.addWidget(combobox, combobox_row, combobox_column)
+    return combobox, combobox_label
+
+
+def add_button(
+    label,
+    layout,
+    connected_function,
+    row,
+    column=0,
+    visibility=True,
+    minimum_width=0,
+    alignment="center",
+) -> QPushButton:
+    button = QPushButton(label)
+    if alignment == "center":
+        pass
+    elif alignment == "left":
+        button.setStyleSheet("QPushButton { text-align: left; }")
+    elif alignment == "right":
+        button.setStyleSheet("QPushButton { text-align: right; }")
+
+    button.setVisible(visibility)
+    button.setMinimumWidth(minimum_width)
+    layout.addWidget(button, row, column)
+    button.clicked.connect(connected_function)
+    return button
+
+
+def display_info(widget, title, message):
+    """
+    Display a warning in a pop up that informs
+    about overwriting files
+    """
+    QMessageBox.information(widget, title, message, QMessageBox.Ok)
+
+
+def display_error_box(message):
+    msg = QMessageBox()
+    msg.setWindowTitle("Error")
+    msg.setText(message)
+    msg.exec()
+
+
+def display_question(widget, title, message):
+    """
+    Display a warning in a pop up that informs
+    about overwriting files
+    """
+    message_reply = QMessageBox.question(
+        widget,
+        title,
+        message,
+        QMessageBox.Yes | QMessageBox.Cancel,
+    )
+    if message_reply == QMessageBox.Yes:
+        return True
+    else:
+        return False
---
+++ packages/cellfinder-napari/cellfinder-napari-0.0.19.tar.gz/cellfinder-napari-0.0.19/setup.py
@@ -0,0 +1,69 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = [
+    "napari",
+    "napari-plugin-engine >= 0.1.4",
+    "napari-ndtiffs",
+    "brainglobe-napari-io",
+    "cellfinder-core>=0.2.4",
+]
+
+setup(
+    name="cellfinder-napari",
+    version="0.0.19",
+    author="Adam Tyson",
+    author_email="code@adamltyson.com",
+    license="BSD-3-Clause",
+    description="Efficient cell detection in large images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage>=5.0.3",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+            "check-manifest",
+        ]
+    },
+    url="https://brainglobe.info/cellfinder",
+    project_urls={
+        "Source Code": "https://github.com/brainglobe/cellfinder-napari",
+        "Bug Tracker": "https://github.com/brainglobe/cellfinder-napari/issues",
+        "Documentation": "https://docs.brainglobe.info/cellfinder-napari/",
+        "User Support": "https://forum.image.sc/tag/brainglobe",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: napari",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "napari.manifest": [
+            "cellfinder-napari = cellfinder_napari:napari.yaml",
+        ],
+    },
+    package_data={"cellfinder_napari": ["napari.yaml"]},
+)
