e57201fd376a55a801e166b8ba165847f5c92dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:07 +0000

    Add package cloudcomponents.cdk-temp-stack/2.1.0/cloudcomponents.cdk-temp-stack-2.1.0.tar.gz

---
+++ packages/cloudcomponents.cdk-temp-stack/cloudcomponents.cdk-temp-stack-2.1.0.tar.gz/cloudcomponents.cdk-temp-stack-2.1.0/setup.py
@@ -0,0 +1,63 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-temp-stack",
+    "version": "2.1.0",
+    "description": "A stack that destroys itself after a given time (ttl)",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_temp_stack",
+        "cloudcomponents.cdk_temp_stack._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_temp_stack._jsii": [
+            "cdk-temp-stack@2.1.0.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_temp_stack": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk-lib>=2.8.0, <3.0.0",
+        "constructs>=10.0.41, <11.0.0",
+        "jsii>=1.52.1, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
