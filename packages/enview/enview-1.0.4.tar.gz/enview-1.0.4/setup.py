50203d469ab029c502053a71086e24590cffe047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:48 +0000

    Add package enview/1.0.4/enview-1.0.4.tar.gz

---
+++ packages/enview/enview-1.0.4.tar.gz/enview-1.0.4/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="enview",
+    version="1.0.4",
+    author="Chivier Humber",
+    install_requires=[
+        'click==8.0.3',
+        'fuzzywuzzy==0.18.0',
+        'jellyfish==0.9.0',
+        'mypy-extensions==0.4.3',
+        'pathspec==0.9.0',
+        'platformdirs==2.4.1',
+        'prettytable==3.0.0',
+        'prompt-toolkit==3.0.26',
+        'Pygments==2.11.2',
+        'pyparsing==3.0.7',
+        'python-nubia==0.2b5',
+        'readchar==3.0.5',
+        'termcolor==1.1.0',
+        'tomli==1.2.3',
+        'typing-inspect==0.7.1',
+        'typing_extensions==4.0.1',
+        'wcwidth==0.2.5'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chivier/enview",
+    author_email="chivier.humber@outlook.com",
+    entry_points={
+        "console_scripts": ["enview=enview.enview:main"],
+    },
+    license="Apache-2.0 License",
+    keywords="translator",
+    packages=find_packages(),
+)
