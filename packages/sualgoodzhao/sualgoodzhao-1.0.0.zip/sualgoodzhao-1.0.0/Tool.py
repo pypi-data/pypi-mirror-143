8eb98416ba6fc925b7467b9abffb5422f8e9835f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:21:27 +0000

    Add package sualgoodzhao/1.0.0/sualgoodzhao-1.0.0.zip

---
+++ packages/sualgoodzhao/sualgoodzhao-1.0.0.zip/sualgoodzhao-1.0.0/Tool.py
@@ -0,0 +1,2 @@
+import sualgoodzhao
+sualgoodzhao.testone.run()
---
+++ packages/sualgoodzhao/sualgoodzhao-1.0.0.zip/sualgoodzhao-1.0.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='sualgoodzhao',version='1.0.0',description='this is a very amaziong package',author='qingquanzhao',
+      author_email='1259567750@qq.com',packages=['sualgoodzhao'],py_modules=['Tool'])
