d17fd1f2a310c236f58aa444fbe2152a00126506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package pladif/1.3/pladif-1.3.tar.gz

---
+++ packages/pladif/pladif-1.3.tar.gz/pladif-1.3/pladif/PLADIF.py
@@ -0,0 +1,215 @@
+""""This file is part of PLADIF.
+
+	MIT License
+
+	Copyright (c) 2022 - Thibault Hilaire
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+
+PLADIF is a simple tool that plots attrakdiff graphs from CSV files (like those from Usabilla).
+It is written by Thibault Hilaire
+
+File: PLADIF.py
+Date: Feb 2022
+
+	main file with the interactions
+"""
+import pandas as pd
+import streamlit as st
+from tempfile import TemporaryDirectory
+from os.path import join, splitext
+from locale import getdefaultlocale
+import matplotlib.pyplot as plt
+from pladif.attrakdiff import plotWordPair, plotAttrakdiff, plotAverageValues
+from pladif.naming import langOption, plt_pair, plt_attr, plt_avrg, order_long, pairs, summary_title, summary_info
+from importlib.metadata import version
+from pladif.data import DataAttrakdiff, removeStar
+
+
+
+def updateFileList():
+	"""update the list of files
+	(do not load already load file, remove from the dict the deleted files
+	we need to do that to deal with the lack of information given by on_change that call that function"""
+	# TODO: we can do the same with cache (I guess)!
+	# check the file(s) that are not yet in the dict, and load them
+	newfiles = [f for f in st.session_state.csvFile if f.name not in st.session_state.data]
+	cols = list(st.session_state.data.values())[0].columns if st.session_state.data else []
+	if newfiles:
+		for f in newfiles:
+			try:
+				# load the data
+				data = DataAttrakdiff(f)
+				st.session_state.data[splitext(f.name)[0]] = data
+				# compare with previous columns
+				if len(cols) > 0 and (set(data.columns) != set(cols)):
+					st.error("""**Error loading the file `%s`.**
+					 
+					 The file should have the same columns as the other files""", f.name)
+			except ValueError as e:
+				st.error("""**Error loading the file `%s`.**
+				
+				""" % f.name + str(e))
+
+	# check the file(s) that are not anymore in the dict, and del them
+	delfilenames = [
+		name for name in st.session_state.data.keys() if name not in
+		[splitext(f.name)[0] for f in st.session_state.csvFile]
+	]
+	if delfilenames:
+		for name in delfilenames:
+			del st.session_state.data[name]
+
+
+def figure(fct, imgFormat, **kwargs):
+	"""Plot a figure (by calling `fct` with the data)
+	and add a `Download image` button"""
+	# call the function to draw the plot
+	fig, ax = plt.subplots()
+	ret = fct(ax, {name: data.df for name, data in st.session_state.data.items()}, **kwargs)
+	st.pyplot(fig)
+	# save it to the file
+	imgFilename = join(tmpFolder.name, fct.__name__ + "." + imgFormat)
+	plt.savefig(imgFilename, format=imgFormat, dpi=dpi, bbox_inches='tight')
+	# give it to the download image button
+	with open(imgFilename, 'rb') as temp:
+		st.download_button(label="Download image", data=temp, file_name=fct.__name__ + "." + imgFormat, mime="image")
+	return ret
+
+
+
+# create a temporary folder, to put the image files
+tmpFolder = TemporaryDirectory()
+
+
+# st.session_state.data stores all the data: {filename: DataFrame}
+if 'data' not in st.session_state:
+	st.session_state.data = {}
+
+# determine the default lang according to the locale
+langs = [lang for lang in langOption.keys() if lang in getdefaultlocale()[0].lower()]
+lang = langs[0] if langs else 'en'
+
+
+# ====== Page ======
+
+st.set_page_config(layout="wide")
+
+
+# sidebar (to upload files)
+with st.sidebar:
+
+	# file uploader
+	st.markdown("<h1 style='text-align: center;'>" + "Add here your CSV files" + "</h1>", unsafe_allow_html=True)
+
+	# file uploader
+	files = st.file_uploader("", type=['csv', 'xls', 'xlsx', 'xlsm', 'xlsb', 'odf', 'ods', 'odt'], accept_multiple_files=True,
+	    help="The file must be a CSV file, with tab delimiter and UTF-16 encoding (as produced by Usabilla) or an excel file.",
+	    on_change=updateFileList, key='csvFile'
+	)
+
+	# spacing
+	for i in range(4):
+		st.write("")
+
+	# plot options
+	with st.expander("Plot options"):
+		# language
+		lang = st.selectbox("Language", langOption.keys(), format_func=lambda x: langOption.get(x),
+			index=list(langOption).index(lang),
+			help="Change the language used in the plots.")
+		# interval confidence
+		stdOption = {0: "No", 0.68: "Yes at 68%", 0.95: "Yes at 95%", 0.997: "Yes at 99.7%"}
+		std = st.selectbox("Plot confidence interval ?", stdOption.keys(), format_func=lambda x: stdOption.get(x),
+			help="Display in the graph the confidence interval (at 67%, 90% or 95%) or not.", index=1, disabled=False)
+		# save option
+		backendTypes = plt.gcf().canvas.get_supported_filetypes()
+		imageFormatList = list(set(backendTypes.keys()) & {'jpg', 'pdf', 'tif', 'svg', 'png'})
+
+		helpTypes = ", ".join(['%s (%s)' % ty for ty in backendTypes.items() if ty[0] in imageFormatList])
+		imageFormat = st.selectbox("Image format", imageFormatList,
+			help="Chosse the file format used to download the image. The possible formats are:\n" + helpTypes,
+			index=imageFormatList.index('jpg')
+		)
+		resolution = [72, 200, 400]
+		dpi = st.selectbox("Resolution (dpi)", resolution, format_func=lambda x: "%d dpi" % x,
+			help="Larger resolution (in dot per inch) produces larger image file (and better quality)",
+			index = len(resolution)//2
+		)
+
+
+
+# title
+st.markdown("<h1 style='text-align:center;'>PLADIF: Plot Attrakdiff graphs from CSV files<h1/>", unsafe_allow_html=True)
+
+
+# plot the graphs and data tables
+if st.session_state.data:
+	# create summary table and remove empty lines
+	st.subheader(summary_title[lang])
+	sumup = pd.DataFrame(
+		[a.summary(order_long, lang) for a in st.session_state.data.values()],
+		index=st.session_state.data.keys(),
+		columns=summary_info[lang]+['%s-%s (%s)' % (*pairs[removeStar(p)][lang], removeStar(p)) for p in order_long]
+	).T
+	sumup.replace("", float("NaN"), inplace=True)
+	sumup.dropna(subset=sumup.columns, inplace=True)
+	sumup.astype(str)
+	st.table(sumup)
+	# average values QP, QHI, QHS, ATT
+	st.subheader(plt_avrg[lang])
+	col1, col2 = st.columns((3, 1))
+	with col1:
+		mv = figure(plotAverageValues, imageFormat, alpha=std, lang=lang)
+	with col2:
+		st.table(mv)
+
+	# pair words plot
+	st.subheader(plt_pair[lang])
+	col1, col2 = st.columns((3, 1))
+	with col1:
+		pw = figure(plotWordPair, imageFormat, alpha=std, lang=lang)
+	with col2:
+		st.table(pw)
+
+	# attrakdiff
+	st.subheader(plt_attr[lang])
+	col1, col2 = st.columns((3, 1))
+	with col1:
+		attrakdiff = figure(plotAttrakdiff, imageFormat, alpha=std, lang=lang)
+	with col2:
+		st.table(attrakdiff)
+
+
+
+# footer
+footer = """<style> .footer {
+position: fixed; left: 0; bottom: 0; width: 100%; background-color: white; color: black; text-align: center;
+}
+</style>
+<div class="footer">
+<p><a href="https://github.com/thilaire/PLADIF">PLADIF</a> (v""" + version('pladif') + \
+""") is a small open source tool to draw attrakdiff plots from CSV files. &nbsp;&nbsp;&nbsp; ©️ T. Hilaire, 2022.</p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
+
+
+
---
+++ packages/pladif/pladif-1.3.tar.gz/pladif-1.3/pladif/attrakdiff.py
@@ -0,0 +1,188 @@
+""""This file is part of PLADIF.
+
+	MIT License
+
+	Copyright (c) 2022 - Thibault Hilaire
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+
+PLADIF is a simple tool that plots attrakdiff graphs from CSV files (like those from Usabilla).
+It is written by Thibault Hilaire
+
+File: attrakdiff.py
+Date: Feb 2022
+
+	Plot functions
+"""
+
+
+from typing import Dict, List
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle, FancyBboxPatch
+from scipy import stats
+import pandas as pd
+
+
+from pladif.naming import categories, titles, order_long, pairs
+from pladif.naming import i18n_dim, i18n_average, QPQH, plt_avrg, plt_pair, plt_attr
+from pladif.data import DataAttrakdiff, removeStar
+
+
+def interval(data, alpha):
+	"""Apply Student's t-distribution to get the confidence interval around the mean
+	according to alpha (alpha=0.05 for a 95% confidence interval)
+	returns the mean (center of the interval) and the interval (center of the interval)
+	"""
+	# We use Student's t-distribution to compute the confidence interval
+	# see https://en.wikipedia.org/wiki/Student%27s_t-distribution
+	mean = data.mean()
+	inter = stats.t.interval(alpha, len(data) - 1, loc=mean, scale=stats.sem(data))
+	return mean, inter[0], inter[1]
+
+
+
+def cat2dict(data: DataFrame) -> Dict[str, List[str]]:
+	"""Returns the dictionary of the categories used
+	Ex: {'QP': ['QP1','QP2'], 'ATT': ['ATT1']} """
+	# groups categories
+	return {name: [col for col in data.columns if name in col] for name in categories}
+
+
+def plotAverageValues(ax: plt.Axes, datas: Dict[str, DataFrame], alpha: float, lang: str):
+	"""Plot the diagrame of average values
+	and returns the associated dataFrame"""
+	cat = cat2dict(datas[next(iter(datas))])
+	data = DataFrame.from_dict(
+		{name: {name: list(interval(dF[cat].stack(), alpha)) for name, cat in cat.items()} for name, dF in datas.items()}
+	)
+	data = data.reindex(cat.keys())
+
+	for name, d in data.items():
+		T = DataFrame.from_dict({c: v for c, v in d.items()})
+		plt.plot(T.loc[0], marker='o', label=name)
+		plt.fill_between(range(len(T.columns)), T.loc[1], T.loc[2], alpha=0.1)
+
+	plt.xlabel(i18n_dim[lang])
+	plt.ylabel(i18n_average[lang])
+	plt.ylim(-3, 3)
+	plt.setp(ax.get_xticklabels(), y=0.5)
+	plt.grid()
+	plt.title(plt_avrg[lang])
+	plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=False)
+	return data.applymap(lambda x: "%.2f: [%.2f;%.2f]" % tuple(x)) if alpha else data.applymap(lambda x: x[0])
+
+
+
+def plotWordPair(ax: plt.Axes, datas: Dict[str, DataFrame], alpha: float, lang: str):
+	"""Draw the diagram of word-pairs
+	and return the associated dataframe"""
+	columns = list(datas[next(iter(datas))].columns)
+	plt.plot([0, 0], [len(columns) + 0.5, 0.5], 'k')
+	# plot each line
+	for name, data in datas.items():
+		T = data.apply(lambda x: interval(x, alpha))
+		plt.plot(T.loc[0], range(1, len(T.columns)+1), 's-', label=name)
+		plt.fill_betweenx(range(1, len(T.columns)+1), T.loc[1], T.loc[2], alpha=0.1)
+	# legend
+	plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), fancybox=True, shadow=False)
+	# add rectangle for each category
+	y = 1
+	xpos = {'en': -5.3, 'fr': -5.8, 'de': -4}        # TODO: computes it automatically (get minimum position of all labels)
+	length = {'en': 11, 'fr': 11.3, 'de': 8}
+	for cat, color in zip(categories, ['skyblue', 'orchid', 'pink', 'palegreen']):
+		size = len([x for x in columns if cat in x])
+		ax.add_patch(FancyBboxPatch((xpos[lang], y), length[lang], size-1, fill=True, alpha=0.2, clip_on=False, color=color))
+		plt.text(xpos[lang]-0.2, y+size/2-0.5, cat, clip_on=False, rotation='vertical', verticalalignment='center')
+		y += size
+
+	# set axes, and pair words as left/right labels
+	labelsL = [""] + [pairs[col][lang][0] for col in datas[next(iter(datas))].T.index]
+	labelsR = [""] + [pairs[col][lang][1] for col in datas[next(iter(datas))].T.index]
+	ax.set_yticks(range(len(labelsL)), labels=labelsL)
+	ax.set_ylim(len(columns)+0.5, 0.5)
+	axR = ax.twinx()
+	axR.set_yticks(range(len(labelsR)), labels=labelsR)
+	axR.set_ylim(len(columns)+0.5, 0.5)
+	plt.xlim([-3, 3])
+
+	ax.grid(visible=True)
+	ax.set_box_aspect(1.25)
+	plt.title(plt_pair[lang])
+
+
+	# return data in good shape TODO: just use apply method
+	dd = DataFrame.from_dict({name: {col: interval(dF[col], alpha) for col in columns} for name, dF in datas.items()})
+	return dd.applymap(lambda x: "%.2f: [%.2f;%.2f]" % tuple(x)) if alpha else dd.applymap(lambda x: x[0])
+
+
+
+def plotAttrakdiff(ax: plt.Axes, datas: Dict[str, DataFrame], alpha: float, lang: str):
+	"""Plot the Attrakdiff portfolio
+	and return the associated dataframe"""
+	plt.xlim([-3, 3])
+	plt.ylim([-3, 3])
+	ax.xaxis.set_ticks([-3, -1, 1, 3])
+	ax.yaxis.set_ticks([-3, -1, 1, 3])
+	plt.grid()
+	for i in [-2, 0, 2]:
+		for j in [-2, 0, 2]:
+			if (i, j) in QPQH:
+				plt.text(i, j, QPQH[i, j][lang], alpha=0.5, ha='center', va='center', zorder=-5)
+	plt.xlabel(titles["QP"][lang])
+	plt.ylabel(titles["QH"][lang])
+
+	cat = cat2dict(datas[next(iter(datas))])
+	attr = {}
+	for name, data in datas.items():
+		# get QH and QP
+		QH = data[cat["QHI"]+cat["QHS"]]
+		QP = data[cat["QP"]]
+		x, ixm, ixp = interval(QP.stack(), alpha)
+		y, iym, iyp = interval(QH.stack(), alpha)
+		# plot point
+		p = plt.plot(x, y, 'o', label=name)
+		# plot interval
+		if alpha:
+			ax.add_patch(Rectangle((ixm, iym), ixp-ixm, iyp-iym, fill=True, alpha=0.2, color=p[0].get_color()))
+
+		if alpha:
+			attr[name] = {"QP": "%.2f: [%.2f;%.2f]" % (x, ixm, ixp), "QH": "%.2f: [%.2f;%.2f]" % (y, iym, iyp)}
+		else:
+			attr[name] = {"QP": x, "QH": y}
+
+	plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=False)
+	plt.title(plt_attr[lang])
+	return pd.DataFrame.from_dict(attr)
+
+
+# ONLY USED, FOR INTERN TESTING
+if __name__ == '__main__':
+	X = DataAttrakdiff("../resources/test2.xlsx")
+	dd = pd.DataFrame(
+		X.summary(order_long, 'en'),
+		index=['file name', 'nb rows', 'filesize']+[p + ': %s-%s' % pairs[removeStar(p)]['fr'] for p in order_long]
+	)
+	print(dd)
+	# fig, ax = plt.subplots()
+	# plotWordPair({'toto': X})
+	f, a = plt.subplots()
+	print(plotAttrakdiff(a, {'toto': X.df}, 0.95, 'fr'))
+	plt.show()
