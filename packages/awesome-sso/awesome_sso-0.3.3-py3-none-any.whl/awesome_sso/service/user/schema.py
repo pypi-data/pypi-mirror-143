4c82412c45e1e6f4005e88bb3f5791859e2f25ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:38 +0000

    Add package awesome-sso/0.3.3/awesome_sso-0.3.3-py3-none-any.whl

---
+++ packages/awesome-sso/awesome_sso-0.3.3-py3-none-any.whl/awesome_sso/service/user/schema.py
@@ -0,0 +1,84 @@
+from enum import Enum
+from typing import Any, Dict, List, Optional, Type, TypeVar
+
+from beanie import Document, PydanticObjectId
+from pydantic import AnyHttpUrl, BaseModel, EmailStr
+
+
+class ConfigType(str, Enum):
+    STRING = "str"
+    INT = "int"
+    BOOLEAN = "bool"
+
+
+class ConfigConstraint(BaseModel):
+    min: Optional[int] = None
+    max: Optional[int] = None
+    options: Optional[List[str]] = None
+
+
+class ConfigOption(BaseModel):
+    name: str
+    description: str
+    type: ConfigType
+    constraint: ConfigConstraint
+    default: Any
+
+
+class ServiceStatus(str, Enum):
+    HEALTHY = "healthy"
+    DOWN = "down"
+    END_OF_LIFE = "eof"
+
+
+class Service(BaseModel):
+    name: str
+    internal_domain: AnyHttpUrl
+    external_domain: AnyHttpUrl
+    user_register_endpoint: str
+    user_unregister_endpoint: str
+    user_login_endpoint: str
+    status: ServiceStatus = ServiceStatus.DOWN
+    mem_percent: float
+    cpu_percent: float
+    config_options: List[ConfigOption] = []
+
+
+class AccessToken(BaseModel):
+    access_token: str
+
+
+class RegisterModel(BaseModel):
+    name: str
+    email: EmailStr
+    sso_user_id: PydanticObjectId
+
+
+AwesomeUserType = TypeVar("AwesomeUserType", bound="AwesomeUser")
+
+
+class AwesomeUser(Document):
+    name: str
+    email: EmailStr
+    sso_user_id: PydanticObjectId
+    settings: Dict[str, Any] = {}
+    line_linked: bool = False
+
+    class Collection:
+        name = "user"
+
+    @classmethod
+    async def register(
+        cls: Type[AwesomeUserType], args: RegisterModel
+    ) -> AwesomeUserType:
+        extra_params = await cls.extra_constructor_params(args)
+        return await cls(**args.dict(), **extra_params).create()
+
+    @classmethod
+    async def extra_constructor_params(
+        cls: Type[AwesomeUserType], args: RegisterModel
+    ) -> dict:
+        return {}
+
+    async def delete_data(self):
+        pass
