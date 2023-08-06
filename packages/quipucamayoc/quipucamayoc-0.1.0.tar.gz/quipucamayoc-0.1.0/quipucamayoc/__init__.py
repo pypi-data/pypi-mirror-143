8e5bada0ee2b0aa124dadc588d9842c16c3f1343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:49 +0000

    Add package quipucamayoc/0.1.0/quipucamayoc-0.1.0.tar.gz

---
+++ packages/quipucamayoc/quipucamayoc-0.1.0.tar.gz/quipucamayoc-0.1.0/quipucamayoc/__init__.py
@@ -0,0 +1,11 @@
+"""
+Quipucamayoc: tools for digitizing historical data
+====================================
+
+Quipucamayoc is a Python package that provides
+a set of building blocks to implement large-scale
+digitization of historical structured data,
+such as balance sheets or price lists.
+"""
+
+from .version import __version__
