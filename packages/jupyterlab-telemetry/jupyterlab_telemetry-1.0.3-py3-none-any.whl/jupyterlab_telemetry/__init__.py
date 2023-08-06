197bd4255eb94b76bf2301de3ab1a4a57ddb16f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:05 +0000

    Add package jupyterlab-telemetry/1.0.3/jupyterlab_telemetry-1.0.3-py3-none-any.whl

---
+++ packages/jupyterlab-telemetry/jupyterlab_telemetry-1.0.3-py3-none-any.whl/jupyterlab_telemetry/__init__.py
@@ -0,0 +1,33 @@
+import json
+from pathlib import Path
+
+from ._version import __version__
+from .handlers import setup_handlers
+
+
+
+HERE = Path(__file__).parent.resolve()
+
+
+def _jupyter_server_extension_points():
+    return [{
+        "module": "jupyterlab_telemetry"
+    }]
+
+
+def _load_jupyter_server_extension(server_app):
+    """Registers the API handler to receive HTTP requests from the frontend extension.
+
+    Parameters
+    ----------
+    server_app: jupyterlab.labapp.LabApp
+        JupyterLab application instance
+    """
+    name = "jupyterlab_telemetry"
+    setup_handlers(server_app.web_app)
+    server_app.log.info("Registered {name} server extension")
+
+
+# For backward compatibility with notebook server - useful for Binder/JupyterHub
+load_jupyter_server_extension = _load_jupyter_server_extension
+
