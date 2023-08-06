b3edd30f2e0c481da28f544203f8a78174cfa30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:53 +0000

    Add package cpg-scpi/0.0.0/cpg_scpi-0.0.0-py3-none-any.whl

---
+++ packages/cpg-scpi/cpg_scpi-0.0.0-py3-none-any.whl/cpg_scpi/__init__.py
@@ -0,0 +1,10 @@
+"""CPG SCPI
+
+Educational client library to use Adafruit Circuit Playground via SCPI protocol in Python3.
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Georg Braun'
+
+class CircuitPlayground:
+    pass
