b1261b11a2af03ce73b83db06308b77de9c78ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package pysenha/2.0.0/pysenha-2.0.0.tar.gz

---
+++ packages/pysenha/pysenha-2.0.0.tar.gz/pysenha-2.0.0/README.md
@@ -0,0 +1,16 @@
+
+<h1 align="center">
+<img src="https://img.shields.io/static/v1?label=PYSENHA%20POR&message=Bates&color=7159c1&style=flat-square&logo=ghost"/>
+<h3> <p align="center">PYSENHA </p> </h3>
+<h3> <p align="center"> ================= </p> </h3>
+>> <h3> Resume </h3>
+<p> gerador de senha </p>
+>> <h3> How install </h3>
+```
+pip install pysenha
+```
+>> <h3> How Works </h3>
+```
+pysenha(velhasenha)
+```
+    
---
+++ packages/pysenha/pysenha-2.0.0.tar.gz/pysenha-2.0.0/pysenha/pysenha.py
@@ -0,0 +1,19 @@
+
+#############################################################################################################################
+#   filename:pysenha.py                                                       
+#   created: 2022-03-23                                                              
+#   import your librarys below                                                    
+#############################################################################################################################
+import random
+
+def pysenha(old_password):
+  
+    old_password = old_password.replace("", "-")
+    old_password = old_password.split('-')[1:17]
+    new = []
+    for i in range(len(old_password)):
+        a = random.choice(old_password)
+        new.append(a)
+    
+    return print(''.join(new))
+    
