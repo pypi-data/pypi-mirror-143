5d215cb7fd84f4107d0a282b9a291b119ed8c6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:27 +0000

    Add package sleepecg/0.5.0/sleepecg-0.5.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/sleepecg/sleepecg-0.5.0-cp310-cp310-macosx_10_9_x86_64.whl/sleepecg/_heartbeat_detection.pyi
@@ -0,0 +1,17 @@
+# Authors: Florian Hofer
+#
+# License: BSD (3-clause)
+
+import numpy as np
+
+def _squared_moving_integration(
+    x: np.ndarray,
+    window_length: int,
+) -> np.ndarray: ...
+
+
+def _thresholding(
+    filtered_ecg: np.ndarray,
+    integrated_ecg: np.ndarray,
+    fs: float,
+) -> np.ndarray: ...
