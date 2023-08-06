5ec24ef96eb2cd533c58de589748dc128e076264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:48:36 +0000

    Add package OpenFisca-France/116.0.0/OpenFisca-France-116.0.0.tar.gz

---
+++ packages/OpenFisca-France/OpenFisca-France-116.0.0.tar.gz/OpenFisca-France-116.0.0/openfisca_france/model/prestations/pass_culture.py
@@ -0,0 +1,15 @@
+from openfisca_france.model.base import *  # noqa analysis:ignore
+
+
+class pass_culture(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = "Montant du pass culture"
+    set_input = set_input_divide_by_period
+    reference = ['https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000043518870', 'https://www.service-public.fr/particuliers/vosdroits/F34959']
+
+    def formula(individu, period, parameters):
+        montant = parameters(period).prestations_sociales.aides_jeunes.pass_culture.montant
+        a_18ans = individu('age', period.offset(1, 'month')) == 18
+        return a_18ans * montant
