17c13ed6aeb3126c9c92d90c3cdc6ff71e9c41e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:26:48 +0000

    Add package eRepublik/0.30.0.3/eRepublik-0.30.0.3.tar.gz

---
+++ packages/eRepublik/eRepublik-0.30.0.3.tar.gz/eRepublik-0.30.0.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read()
+    requirements = requirements.split()
+
+setup_requirements = []
+
+with open("requirements-tests.txt") as test_req_file:
+    test_requirements = test_req_file.read()
+    test_requirements = [
+        line.strip() for line in test_requirements.split() if line.strip()[:2].strip() not in ("#", "-r")
+    ]
+
+setup(
+    author="Eriks Karls",
+    author_email="eriks@72.lv",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="Python package for automated eRepublik playing",
+    entry_points={},
+    install_requires=requirements,
+    license="GPLv3",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="erepublik",
+    name="eRepublik",
+    packages=find_packages(include=["erepublik"]),
+    python_requires=">=3.8, <4",
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/eeriks/erepublik/",
+    version="0.30.0.3",
+    zip_safe=False,
+)
