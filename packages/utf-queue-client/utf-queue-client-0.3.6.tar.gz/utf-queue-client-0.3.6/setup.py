4bf7f35f2d03e3313b3d79536a6602cf087b5ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:00 +0000

    Add package utf-queue-client/0.3.6/utf-queue-client-0.3.6.tar.gz

---
+++ packages/utf-queue-client/utf-queue-client-0.3.6.tar.gz/utf-queue-client-0.3.6/setup.py
@@ -0,0 +1,43 @@
+"""
+
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "utf-queue-client"
+VERSION = "0.3.6"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["pika >= 1.2.0", "jsonschema >= 3.2.0", "msgpack >= 1.0.2", "pydantic >= 1.8.2", "regex", "typing_extensions; python_version < '3.8'"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="",
+    author="Joe Savage",
+    author_email="joe.savage@silabs.com",
+    url="",
+    keywords=["UTF", ""],
+    python_requires=">=3.6",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    package_data={
+      "utf_queue_client": ["models/schemas/utf_queue_models/models/python/*.py"]
+    },
+    data_files=[
+        (
+            "utf_queue_client/models/schemas/utf_queue_models/schema",
+            ["utf_queue_client/models/schemas/utf_queue_models/schema/queue_message.json"],
+        )
+    ],
+    include_package_data=True,
+    long_description="""\
+    No description provided
+    """,
+)
