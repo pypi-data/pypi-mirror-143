070f0f3e7a0c1ef37d04ff043bd781a86fc70996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:36 +0000

    Add package indicpy/0.0.4/indicpy-0.0.4.tar.gz

---
+++ packages/indicpy/indicpy-0.0.4.tar.gz/indicpy-0.0.4/indicpy/__init__.py
@@ -0,0 +1,10 @@
+from main import get_executable_code
+from main import get_token_lines
+from main import fix_name
+from main import replace_tokens
+from main import get_runnable_script
+from main import get_language_map
+from main import run_cli
+
+if __name__ == "__main__":
+	run_cli()
