aa0256b2ff836744186e32ca1fdc08d40f9ed807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:05 +0000

    Add package aiotapioca-yandex-metrika/2022.3.26/aiotapioca_yandex_metrika-2022.3.26-py3-none-any.whl

---
+++ packages/aiotapioca-yandex-metrika/aiotapioca_yandex_metrika-2022.3.26-py3-none-any.whl/aiotapioca_yandex_metrika/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Pavel Maksimov, Andrey Ilin"
+__email__ = "andreyilin@fastmail.com"
+__version__ = "2022.03.26"
+
+from .resource_mapping import *
+from .yandex_metrika import (
+    YandexMetrikaStats,
+    YandexMetrikaLogsAPI,
+    YandexMetrikaManagement,
+)
