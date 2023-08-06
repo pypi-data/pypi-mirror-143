2e96b4eda5e70317d4f6059c9237808d43a6676f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:59 +0000

    Add package decOM/0.0.7/decOM-0.0.7.tar.gz

---
+++ packages/decOM/decOM-0.0.7.tar.gz/decOM-0.0.7/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="decOM",
+    version="0.0.7",
+    author="Camila Duitama González",
+    author_email="cduitama@pasteur.fr",
+    description="decOM: K-mer method for aOral metagenome decontamination",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CamilaDuitama/decOM",
+    project_urls={
+        "Bug Tracker": "https://github.com/CamilaDuitama/decOM/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'numpy>=1.22.1',
+        'plotly>=5.5.0',
+        'pandas>=1.3.5',
+        'importlib_resources>=5.4.0',
+        'dask>=2021.12.0',
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    extras_require={
+        'dev':[],
+        'test':[],
+        },
+    package_data={'': ['data/*']},
+    entry_points={
+        'console_scripts': [
+            'decOM=decOM.__main__:main',
+        ],
+    },
+)
