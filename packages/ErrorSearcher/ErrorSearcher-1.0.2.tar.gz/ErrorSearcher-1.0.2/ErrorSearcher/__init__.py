da495f4507a007971750cc2d5cf79e1ae12fa48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:08 +0000

    Add package ErrorSearcher/1.0.2/ErrorSearcher-1.0.2.tar.gz

---
+++ packages/ErrorSearcher/ErrorSearcher-1.0.2.tar.gz/ErrorSearcher-1.0.2/ErrorSearcher/__init__.py
@@ -0,0 +1,14 @@
+import webbrowser
+def detect(path):
+    try:
+        if __file__!=path:
+            exec(open(path).read())
+            
+    except Exception as e:
+        a = type(e).__name__
+        error = f"{str(a)}: {e}"
+        error =f"https://www.google.com/search?q={error} stack overflow"
+        webbrowser.open(error)
+    finally :
+        if __file__!=path:
+            print("Error not Found!!!!")
