e776b18bd7eeedfdaed5ef03884d5117de2ab349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:02 +0000

    Add package to-cei/0.1.5/to_cei-0.1.5-py3-none-any.whl

---
+++ packages/to-cei/to_cei-0.1.5-py3-none-any.whl/to_cei/charter_group.py
@@ -0,0 +1,52 @@
+from typing import List, Optional
+
+from lxml import etree
+
+from to_cei.charter import Charter
+from to_cei.config import CEI
+from to_cei.xml_assembler import XmlAssembler
+
+
+class CharterGroup(XmlAssembler):
+    _charters: List[Charter] = []
+    _name: str = ""
+
+    def __init__(self, name: str, charters: List[Charter] = []):
+        """Creates a new charter group object.
+
+        Args:
+            name (str): The name of the charter group. Is not allowed to be empty
+            charters(List[Charter] = []): An optional list of Charter objects
+        """
+        self.name = name
+        self.charters = charters
+
+    @property
+    def charters(self):
+        return self._charters
+
+    @charters.setter
+    def charters(self, value: List[Charter]):
+        self._charters = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) == 0:
+            raise ValueError("Group name cannot be empty")
+        self._name = value
+
+    def to_xml(self) -> etree._Element:
+        xml = CEI.cei(
+            CEI.teiHeader(CEI.fileDesc(CEI.titleStmt(CEI.title(self.name)))),
+            CEI.text(CEI.group(*[charter.to_xml() for charter in self.charters])),
+        )
+        return xml
+
+    def to_file(self, folder: Optional[str] = None):
+        return super(CharterGroup, self).to_file(
+            self.name.lower().replace(" ", "_") + ".cei.group", folder=folder
+        )
---
+++ packages/to-cei/to_cei-0.1.5-py3-none-any.whl/to_cei/xml_assembler.py
@@ -0,0 +1,48 @@
+import os
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List, Optional
+
+from lxml import etree
+
+from to_cei.config import CEI_PREFIX
+
+
+class XmlAssembler(ABC):
+    @abstractmethod
+    def to_xml(self) -> Optional[etree._Element]:
+        pass
+
+    def to_string(self) -> str:
+        xml = self.to_xml()
+        return (
+            ""
+            if xml is None
+            else str(etree.tostring(xml, encoding="unicode", pretty_print=True))
+        )
+
+    def to_file(
+        self,
+        name: str,
+        folder: Optional[str | Path] = None,
+        inclusive_ns_prefixes: List[str] = [],
+    ):
+        xml = self.to_xml()
+        if xml is None:
+            raise Exception("Failed to read xml")
+        folder = (
+            folder
+            if isinstance(folder, str)
+            else (
+                str(folder.absolute())
+                if isinstance(folder, Path)
+                else os.path.abspath(os.getcwd())
+            )
+        )
+        os.makedirs(folder, exist_ok=True)
+        etree.ElementTree(xml).write(
+            os.path.join(folder, name + ".xml"),
+            encoding="UTF-8",
+            pretty_print=True,
+            inclusive_ns_prefixes=[CEI_PREFIX] + inclusive_ns_prefixes,
+        )
