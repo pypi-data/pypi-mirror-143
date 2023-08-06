3050707d272a6d50971ce0214884d7af4a487336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:36 +0000

    Add package bioimageio.spec/0.4.5.post6/bioimageio.spec-0.4.5.post6-py3-none-any.whl

---
+++ packages/bioimageio.spec/bioimageio.spec-0.4.5.post6-py3-none-any.whl/bioimageio/spec/commands.py
@@ -0,0 +1,122 @@
+import os
+import traceback
+import warnings
+from pathlib import Path
+from typing import Any, Dict, IO, Union
+
+from marshmallow import ValidationError
+
+from .collection.v0_2.utils import resolve_collection_entries
+from .io_ import load_raw_resource_description, resolve_rdf_source, save_raw_resource_description
+from .shared.common import ValidationWarning, nested_default_dict_as_nested_dict
+from .v import __version__
+
+
+def update_format(
+    rdf_source: Union[dict, os.PathLike, IO, str, bytes],
+    path: Union[os.PathLike, str],
+    update_to_format: str = "latest",
+):
+    """Update a BioImage.IO resource"""
+    raw = load_raw_resource_description(rdf_source, update_to_format=update_to_format)
+    save_raw_resource_description(raw, Path(path))
+
+
+def validate(
+    rdf_source: Union[dict, os.PathLike, IO, str, bytes],
+    update_format: bool = False,
+    update_format_inner: bool = None,
+    verbose: bool = "deprecated",  # type: ignore
+) -> dict:
+    """Validate a BioImage.IO Resource Description File (RDF).
+
+    Args:
+        rdf_source: resource description as path, url or bytes of an RDF or packaged resource, or as yaml string or dict
+        update_format: weather or not to apply auto-conversion to the latest format version before validation
+        update_format_inner: (applicable to `collections` resources only) `update_format` for nested resources
+        verbose: deprecated
+
+    Returns:
+        A summary dict with keys: status, error, warnings, traceback, nested_errors, bioimageio_spec_version
+    """
+    if verbose != "deprecated":
+        warnings.warn("'verbose' flag is deprecated")
+
+    if update_format_inner is None:
+        update_format_inner = update_format
+
+    error: Union[None, str, Dict[str, Any]] = None
+    tb = None
+    nested_errors: Dict[str, dict] = {}
+    try:
+        rdf_source, source_name, root = resolve_rdf_source(rdf_source)
+    except Exception as e:
+        error = str(e)
+        tb = traceback.format_tb(e.__traceback__)
+        try:
+            source_name = str(rdf_source)
+        except Exception as e:
+            source_name = str(e)
+    else:
+        if not isinstance(rdf_source, dict):
+            error = f"expected loaded resource to be a dictionary, but got type {type(rdf_source)}: {str(rdf_source)}"
+
+    raw_rd = None
+    format_version = ""
+    resource_type = ""
+    if error:
+        validation_warnings = []
+    else:
+        with warnings.catch_warnings(record=True) as all_warnings:
+            try:
+                raw_rd = load_raw_resource_description(rdf_source, update_to_format="latest" if update_format else None)
+            except ValidationError as e:
+                error = nested_default_dict_as_nested_dict(e.normalized_messages())
+            except Exception as e:
+                error = str(e)
+                tb = traceback.format_tb(e.__traceback__)
+
+            if raw_rd is not None:
+                format_version = raw_rd.format_version
+                resource_type = "general" if raw_rd.type == "rdf" else raw_rd.type
+
+            if raw_rd is not None and raw_rd.type == "collection":
+                assert hasattr(raw_rd, "collection")
+                for idx, (rdf_data, entry_error) in enumerate(resolve_collection_entries(raw_rd)):  # type: ignore
+                    if entry_error:
+                        entry_summary = {"error": entry_error}
+                    else:
+                        entry_summary = validate(
+                            rdf_data, update_format=update_format, update_format_inner=update_format_inner
+                        )
+
+                        wrns: Union[str, dict] = entry_summary.get("warnings", {})
+                        assert isinstance(wrns, dict)
+                        id_info = f"(id={rdf_data['id']}) " if "id" in rdf_data else ""
+                        for k, v in wrns.items():
+                            warnings.warn(f"collection[{idx}]:{k}: {id_info}{v}", category=ValidationWarning)
+
+                    if entry_summary["error"]:
+                        if "collection" not in nested_errors:
+                            nested_errors["collection"] = {}
+
+                        nested_errors["collection"][idx] = entry_summary["error"]
+
+                if nested_errors:
+                    error = nested_errors
+
+        validation_warnings = [w for w in all_warnings if issubclass(w.category, ValidationWarning)]
+
+    return {
+        "bioimageio_spec_version": __version__,
+        "error": error,
+        "name": (
+            f"bioimageio.spec static validation of {resource_type} RDF {format_version}"
+            f"{' with update to latest format version' if update_format else ''}"
+        ),
+        "nested_errors": nested_errors,
+        "source_name": source_name,
+        "status": "passed" if error is None else "failed",
+        "traceback": tb,
+        "warnings": ValidationWarning.get_warning_summary(validation_warnings),
+    }
