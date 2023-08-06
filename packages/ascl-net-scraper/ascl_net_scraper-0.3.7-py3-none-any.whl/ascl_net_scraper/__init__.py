4ae7e137b7726ed2574010f5b5ac6228997716ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:01 +0000

    Add package ascl-net-scraper/0.3.7/ascl_net_scraper-0.3.7-py3-none-any.whl

---
+++ packages/ascl-net-scraper/ascl_net_scraper-0.3.7-py3-none-any.whl/ascl_net_scraper/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.3.7"
+from .lib import CodeRecord as CodeRecord
+from .lib import DetailedCodeRecord as DetailedCodeRecord
+from .lib import scrape_details as scrape_details
+from .lib import scrape_index as scrape_index
