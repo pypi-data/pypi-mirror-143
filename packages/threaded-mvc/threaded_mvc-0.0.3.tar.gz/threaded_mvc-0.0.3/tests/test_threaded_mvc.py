e4e031fc92bb799a8b0215fd1171fb1f6d12a6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:20 +0000

    Add package threaded-mvc/0.0.3/threaded_mvc-0.0.3.tar.gz

---
+++ packages/threaded-mvc/threaded_mvc-0.0.3.tar.gz/threaded_mvc-0.0.3/tests/test_threaded_mvc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Tests for `threaded_mvc` package."""
+
+import pytest
+
+from threaded_mvc import Model, View, Controller
+
+def test_model_class_is_abstract():
+    with pytest.raises(TypeError):
+        mvcmodel = Model()
+
+def test_view_class_is_abstract():
+    with pytest.raises(TypeError):
+        mvcview = View()
+
+def test_controller_class_is_abstract():
+    with pytest.raises(TypeError):
+        mvccontroller = Controller()
