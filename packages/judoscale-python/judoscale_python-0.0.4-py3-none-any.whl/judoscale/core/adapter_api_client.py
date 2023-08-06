9e91c4c53c2036d15fac2dbb5dd367b2b7b8ae8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:28 +0000

    Add package judoscale-python/0.0.4/judoscale_python-0.0.4-py3-none-any.whl

---
+++ packages/judoscale-python/judoscale_python-0.0.4-py3-none-any.whl/judoscale/core/adapter_api_client.py
@@ -0,0 +1,19 @@
+from judoscale.core.config import config
+import logging
+import requests
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class AdapterApiClient:
+    def post_report(self, report):
+        try:
+            logger.debug("Posting metrics to Judoscale adapter API")
+            payload = json.dumps(report)
+            requests.post(config.api_base_url + "/v1/metrics", timeout=5, json=payload)
+        except requests.RequestException as e:
+            logger.warn("Adapter API request failed - {}".format(e))
+
+
+api_client = AdapterApiClient()
