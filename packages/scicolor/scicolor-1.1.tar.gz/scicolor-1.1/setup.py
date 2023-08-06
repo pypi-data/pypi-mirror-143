7180c52d909ec7f0987d79649d7ba799ce43dd1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:39 +0000

    Add package scicolor/1.1/scicolor-1.1.tar.gz

---
+++ packages/scicolor/scicolor-1.1.tar.gz/scicolor-1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from distutils.core import setup
+
+setup(name='scicolor',
+      version='1.1',
+      description = 'Collections of color maps for scientific visualizations',
+      author = 'Kaicheng Yang',
+      author_email = 'yangkc@iu.edu',
+      url="https://github.com/yangkcatiu/scicolor",
+      license = 'MIT',
+      include_package_data=True,
+      install_requires=[
+          'matplotlib',
+          'numpy',
+          'pandas'
+      ],
+      packages = ['scicolor']
+)
