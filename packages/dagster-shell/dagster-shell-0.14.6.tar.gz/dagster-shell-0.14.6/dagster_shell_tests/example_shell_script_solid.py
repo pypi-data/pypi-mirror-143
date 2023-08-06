99cb8d79771345e10bb1ee5be11cfd48eb7eab05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package dagster-shell/0.14.6/dagster-shell-0.14.6.tar.gz

---
+++ packages/dagster-shell/dagster-shell-0.14.6.tar.gz/dagster-shell-0.14.6/dagster_shell_tests/example_shell_script_solid.py
@@ -0,0 +1,10 @@
+# pylint: disable=no-value-for-parameter
+from dagster_shell import create_shell_script_solid
+
+from dagster import file_relative_path, pipeline
+
+
+@pipeline
+def pipe():
+    a = create_shell_script_solid(file_relative_path(__file__, "hello_world.sh"), name="a")
+    a()
