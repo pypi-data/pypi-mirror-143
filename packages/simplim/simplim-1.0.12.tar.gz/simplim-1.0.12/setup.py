1dc890de0599cf7bcbceeaea5f46128c85942729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:11 +0000

    Add package simplim/1.0.12/simplim-1.0.12.tar.gz

---
+++ packages/simplim/simplim-1.0.12.tar.gz/simplim-1.0.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+	name = 'simplim',
+	version = '1.0.12',
+	packages = find_packages(),
+	url='',
+	author='hardrelice',
+	author_email='122527455@qq.com'
+)
