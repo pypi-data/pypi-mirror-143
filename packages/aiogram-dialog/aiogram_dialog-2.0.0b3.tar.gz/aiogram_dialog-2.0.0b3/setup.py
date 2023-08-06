3ae70a3e74fd59e45fb60c5d3ccc1aefa0121374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:40 +0000

    Add package aiogram-dialog/2.0.0b3/aiogram_dialog-2.0.0b3.tar.gz

---
+++ packages/aiogram-dialog/aiogram_dialog-2.0.0b3.tar.gz/aiogram_dialog-2.0.0b3/setup.py
@@ -0,0 +1,41 @@
+  #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from os import path
+
+from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='aiogram_dialog',
+    description='Mini-framework for dialogs on top of aiogram',
+    version='2.0.0b3',
+    url='https://github.com/tishka17/aiogram_dialog',
+    author='A. Tikhonov',
+    author_email='17@itishka.org',
+    license='Apache2',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages(include=['aiogram_dialog', 'aiogram_dialog.*']),
+    install_requires=[
+        'aiogram>=3.0.0b1,<4.0.0',
+        'jinja2',
+        'cachetools==4.*',
+        'magic_filter',
+    ],
+    extras_require={
+        "tools": [
+            "diagrams"
+        ]
+    },
+    package_data={
+        'aiogram_dialog.tools': ['calculator.png'],
+        'aiogram_dialog.tools.templates': ['message.html'],
+    },
+    python_requires=">=3.8",
+)
