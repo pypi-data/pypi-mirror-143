c3df5f27280de3e883d435e2ce071a8b288b5b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:20 +0000

    Add package rath/0.1.29/rath-0.1.29-py3-none-any.whl

---
+++ packages/rath/rath-0.1.29-py3-none-any.whl/rath/links/split.py
@@ -0,0 +1,65 @@
+from typing import Callable
+
+from pydantic import Field
+from rath.operation import Operation
+from rath.links.base import TerminatingLink
+
+
+class SplitLink(TerminatingLink):
+    left: TerminatingLink
+    right: TerminatingLink
+    split: Callable[[Operation], bool] = Field(exclude=True)
+
+    async def aquery(self, operation: Operation, **kwargs) -> Operation:
+        future = (
+            self.left.aquery(operation, **kwargs)
+            if self.split(operation)
+            else self.right.aquery(operation, **kwargs)
+        )
+        return await future
+
+    async def asubscribe(self, operation: Operation, **kwargs) -> Operation:
+        iterator = (
+            self.left.asubscribe(operation, **kwargs)
+            if self.split(operation)
+            else self.right.asubscribe(operation, **kwargs)
+        )
+
+        async for res in iterator:
+            yield res
+
+    def query(self, operation: Operation, **kwargs) -> Operation:
+        future = (
+            self.left.query(operation, **kwargs)
+            if self.split(operation)
+            else self.right.query(operation, **kwargs)
+        )
+
+        return future
+
+    def subscribe(self, operation: Operation, **kwargs) -> Operation:
+        iterator = (
+            self.left.subscribe(operation, **kwargs)
+            if self.split(operation)
+            else self.right.subscribe(operation, **kwargs)
+        )
+
+        return iterator
+
+    async def __aenter__(self):
+        await self.left.__aenter__()
+        await self.right.__aenter__()
+
+    async def __aexit__(self, *args, **kwargs):
+        await self.left.__aexit__(*args, **kwargs)
+        await self.right.__aexit__(*args, **kwargs)
+
+
+def split(
+    left: TerminatingLink, right: TerminatingLink, split: Callable[[Operation], bool]
+):
+    """
+    Splits a Link into two paths. Acording to a predicate function. If predicate returns
+    true, the operation is sent to the left path, otherwise to the right path.
+    """
+    return SplitLink(left=left, right=right, split=split)
