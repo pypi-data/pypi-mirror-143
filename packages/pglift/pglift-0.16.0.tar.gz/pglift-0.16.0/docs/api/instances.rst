b30a60d6e00ed9d7ca3e6ac72fd7d7f409dcc8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:55 +0000

    Add package pglift/0.16.0/pglift-0.16.0.tar.gz

---
+++ packages/pglift/pglift-0.16.0.tar.gz/pglift-0.16.0/docs/api/instances.rst
@@ -0,0 +1,27 @@
+Instances
+=========
+
+.. currentmodule:: pglift.instance
+
+Module :mod:`pglift.instance` exposes the following API to manipulate
+instances:
+
+.. autofunction:: apply
+.. autofunction:: describe
+.. autofunction:: exists
+.. autofunction:: list
+.. autofunction:: drop
+.. autofunction:: init
+.. autofunction:: configure
+.. autofunction:: start
+.. autofunction:: stop
+.. autofunction:: restart
+.. autofunction:: reload
+.. autofunction:: status
+.. autofunction:: check_status
+.. autofunction:: running
+.. autofunction:: stopped
+.. autofunction:: promote
+.. autofunction:: upgrade
+.. autofunction:: settings
+.. autofunction:: logs
---
+++ packages/pglift/pglift-0.16.0.tar.gz/pglift-0.16.0/tests/func/test_20_backup.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pglift import backup
+from pglift import instance as instance_mod
+from pglift import systemd
+from pglift.ctx import Context
+from pglift.models import system
+
+
+def test_systemd_backup_job(ctx: Context, instance: system.Instance) -> None:
+    scheduler = ctx.settings.scheduler
+    if scheduler != "systemd":
+        pytest.skip(f"not applicable for scheduler method '{scheduler}'")
+
+    assert systemd.is_enabled(ctx, backup.systemd_timer(instance))
+
+    assert not systemd.is_active(ctx, backup.systemd_timer(instance))
+    with instance_mod.running(ctx, instance, run_hooks=True):
+        assert systemd.is_active(ctx, backup.systemd_timer(instance))
+    assert not systemd.is_active(ctx, backup.systemd_timer(instance))
