5eed78043a2ff1969eb38e862dcbfba54c9f0f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package diana-3/0.1/diana-3-0.1.tar.gz

---
+++ packages/diana-3/diana-3-0.1.tar.gz/diana-3-0.1/setup.py
@@ -0,0 +1,12 @@
+from gettext import install
+from setuptools import setup
+
+setup(name='diana-3',
+      version='0.1',
+      description='This is the Python implementation of DIANA Clustering Algorithm',
+      author='HPC-ML',
+      author_email='flcus@example.com',
+      license='MIT',
+      packages=['Diana-11'],
+      install_requires=['numpy','pandas','scipy'],
+      zip_safe=False)
