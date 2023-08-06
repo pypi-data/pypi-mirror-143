4fe4c23cec805825f5c30b4b504b0ebdf8e912c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:51:24 +0000

    Add package kasushi/0.0.8/kasushi-0.0.8.tar.gz

---
+++ packages/kasushi/kasushi-0.0.8.tar.gz/kasushi-0.0.8/release.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+export COLOR_NC='\033[0m' # No Color
+export COLOR_RED='\033[0;31m'
+
+set -eo pipefail
+
+echo "==> Changing directory"
+cd "$(dirname "$0")"
+
+PREVIOUS_GIT_TAG=$(git describe --abbrev=0 --tags)
+
+
+# Ensure working tree is clean or ask to commit
+# Don't continue while working tree isn't clean
+
+while true; do
+  if [[ -n $(git status --porcelain) ]]; then
+    echo "==> ! Working tree is not clean. Please commit or stash changes before releasing. Press enter when done."
+    # Wait for key press
+    read -r
+  else
+    break
+  fi
+done
+
+echo -e "==> Previous version was v$COLOR_RED$PREVIOUS_GIT_TAG$COLOR_NC"
+
+# Ask for input
+echo "==> ! Enter the new version number (e.g. 1.2.3)> "
+read -r NEW_GIT_TAG
+
+# Create tag for new release
+git tag "$NEW_GIT_TAG"
+echo "==> Created tag $NEW_GIT_TAG"
+
+# Push tag to github
+git push origin "$NEW_GIT_TAG"
+echo "==> Pushed tag $NEW_GIT_TAG"
+
+# Run build script
+echo "==> Running build script"
+./build.sh
+
+echo "==> Checking build status"
+twine check dist/*
+
+echo "==> Uploading to PyPI"
+twine upload dist/*
+
+echo "==> Deleting temporary files"
+rm -R dist
+
+
