6fd713f0a486520e862ed3e6bcacc38efde47c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:45 +0000

    Add package bert-summarizer/0.2.5/bert_summarizer-0.2.5-py3-none-any.whl

---
+++ packages/bert-summarizer/bert_summarizer-0.2.5-py3-none-any.whl/bert_summarizer/data/__init__.py
@@ -0,0 +1,5 @@
+from .data_collator import (
+    DataCollatorWithPaddingWithAdditionalFeatures,
+    EncoderDecoderDataCollatorWithPadding,
+)
+from .datasets import *
---
+++ packages/bert-summarizer/bert_summarizer-0.2.5-py3-none-any.whl/bert_summarizer/models/__init__.py
@@ -0,0 +1,1 @@
+from .bertsum import BertSumAbs, BertSumExt
