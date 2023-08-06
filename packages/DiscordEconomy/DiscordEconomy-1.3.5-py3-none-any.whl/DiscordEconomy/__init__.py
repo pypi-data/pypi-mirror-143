8a8d0108964263a60e9a3f7eed55c7bc869be401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package DiscordEconomy/1.3.5/DiscordEconomy-1.3.5-py3-none-any.whl

---
+++ packages/DiscordEconomy/DiscordEconomy-1.3.5-py3-none-any.whl/DiscordEconomy/__init__.py
@@ -0,0 +1,11 @@
+from .economy import *
+
+__title__ = "DiscordEconomy"
+__summary__ = "Discord.py, other libs, and forks(pycord, nextcord etc.) extension to create economy easily."
+__uri__ = "https://github.com/Nohet/DiscordEconomy"
+__version__ = "1.3.5"
+__author__ = "Nohet"
+__email__ = "igorczupryniak503@gmail.com"
+__license__ = "MIT License"
+__copyright__ = f"Copyright 2021 {__author__}"
+
