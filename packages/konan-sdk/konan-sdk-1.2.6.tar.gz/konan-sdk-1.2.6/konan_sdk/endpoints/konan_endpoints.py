33b8139d5d2910e003b0caef7ee827079d60c964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:23 +0000

    Add package konan-sdk/1.2.6/konan-sdk-1.2.6.tar.gz

---
+++ packages/konan-sdk/konan-sdk-1.2.6.tar.gz/konan-sdk-1.2.6/konan_sdk/endpoints/konan_endpoints.py
@@ -0,0 +1,283 @@
+import datetime
+from typing import Any, Dict, List, Union
+
+from konan_sdk.endpoints.base_endpoint import (
+    KonanBaseEndpoint,
+    KonanBaseGenericDeploymentsEndpoint,
+    KonanBaseDeploymentEndpoint,
+    KonanEndpointOperationEnum,
+)
+from konan_sdk.endpoints.interfaces import (
+    KonanEndpointRequest,
+    KonanEndpointResponse,
+)
+from konan_sdk.konan_metrics import (
+    KonanBaseMetric,
+    KonanCustomMetric,
+    KONAN_PREDEFINED_METRICS,
+)
+from konan_sdk.konan_types import (
+    KonanCredentials,
+    KonanDeployment,
+    KonanDeploymentCreationRequest,
+    KonanDeploymentCreationResponse,
+    KonanDeploymentError,
+    KonanDeploymentErrorType,
+    KonanTokens,
+    KonanPrediction,
+    KonanFeedbackSubmission, KonanFeedbackStatus, KonanFeedbacksResult,
+    KonanTimeWindow,
+)
+
+
+class LoginEndpoint(KonanBaseEndpoint[KonanCredentials, KonanTokens]):
+    @property
+    def name(self) -> str:
+        return 'login'
+
+    @property
+    def endpoint_path(self) -> str:
+        return '/api/auth/login/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(
+        self, request_object: KonanCredentials
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest(json={
+            'email': request_object.email,
+            'password': request_object.password
+        })
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> KonanTokens:
+        return KonanTokens(
+            endpoint_response.json['access'],
+            endpoint_response.json['refresh']
+        )
+
+
+class RefreshTokenEndpoint(KonanBaseEndpoint[str, str]):
+    @property
+    def name(self) -> str:
+        return 'refresh_token'
+
+    @property
+    def endpoint_path(self) -> str:
+        return '/api/auth/token/refresh/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(self, request_object: str) -> KonanEndpointRequest:
+        return KonanEndpointRequest(json={'refresh': request_object})
+
+    def process_response(self, endpoint_response: KonanEndpointResponse) -> str:
+        return endpoint_response.json['access']
+
+
+class PredictionEndpoint(
+    KonanBaseDeploymentEndpoint[Union[Dict, str], KonanPrediction]
+):
+    @property
+    def name(self) -> str:
+        return 'predict'
+
+    @property
+    def endpoint_path(self) -> str:
+        return super().endpoint_path + '/predict/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(
+        self, request_object: Union[Dict, str]
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest(json=request_object)
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> KonanPrediction:
+        return KonanPrediction(
+            endpoint_response.json['prediction_uuid'],
+            endpoint_response.json['output']
+        )
+
+
+class EvaluateEndpoint(
+    KonanBaseDeploymentEndpoint[KonanTimeWindow, List[KonanBaseMetric]]
+):
+    @property
+    def name(self) -> str:
+        return 'evaluate'
+
+    @property
+    def endpoint_path(self) -> str:
+        return super().endpoint_path + '/evaluate/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(
+        self, request_object: KonanTimeWindow
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest(json={
+            'start_time': request_object.start_time.isoformat(),
+            'end_time': request_object.end_time.isoformat(),
+        })
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> List[KonanBaseMetric]:
+        predfined_metrics_dict: Dict[str, Any] = endpoint_response.json['metrics'].get(
+            "predefined", dict()
+        )
+        custom_metrics_list: List[Dict] = endpoint_response.json['metrics'].get(
+            "custom", list()
+        )
+
+        metrics: List[KonanBaseMetric] = [
+            KONAN_PREDEFINED_METRICS.get(metric_name, KonanCustomMetric)(
+                metric_value, name=metric_name
+            ) for metric_name, metric_value in predfined_metrics_dict.items() if metric_value is not None
+        ] + [
+            KonanCustomMetric(
+                metric_dict["metric_value"],
+                name=metric_dict["metric_name"]
+            )
+            for metric_dict in custom_metrics_list
+        ]
+
+        return metrics
+
+
+class FeedbackEndpoint(
+    KonanBaseDeploymentEndpoint[
+        List[KonanFeedbackSubmission], KonanFeedbacksResult
+    ]
+):
+    @property
+    def name(self) -> str:
+        return 'feedback'
+
+    @property
+    def endpoint_path(self) -> str:
+        return super().endpoint_path + '/predictions/feedback/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(
+        self, request_object: List[KonanFeedbackSubmission]
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest(
+            json={
+                "feedback": [
+                    {
+                        "prediction_uuid": feedback.prediction_uuid,
+                        "target": feedback.target,
+                    } for feedback in request_object
+                ]
+            }
+        )
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> KonanFeedbacksResult:
+        return KonanFeedbacksResult(
+            [
+                KonanFeedbackStatus(
+                    feedback_status['prediction_uuid'],
+                    feedback_status['status'],
+                    feedback_status['message'],
+                ) for feedback_status in endpoint_response.json["data"]
+            ],
+            endpoint_response.json['success'],
+            endpoint_response.json['failure'],
+            endpoint_response.json['total']
+        )
+
+
+class CreateDeploymentEndpoint(
+    KonanBaseGenericDeploymentsEndpoint[
+        KonanDeploymentCreationRequest,
+        KonanDeploymentCreationResponse
+    ]
+):
+    @property
+    def name(self) -> str:
+        return 'create-deployment'
+
+    @property
+    def endpoint_path(self) -> str:
+        return super().endpoint_path + '/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.POST
+
+    def prepare_request(
+        self, request_object: KonanDeploymentCreationRequest
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest(json={
+            'name': request_object.name,
+            'docker_username': request_object.docker_credentials.username,
+            'docker_password': request_object.docker_credentials.password,
+            'image_url': request_object.docker_image.url,
+            'exposed_port': request_object.docker_image.exposed_port,
+        })
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> KonanDeploymentCreationResponse:
+        return KonanDeploymentCreationResponse(
+            KonanDeployment(
+                endpoint_response.json['deployment']['uuid'],
+                endpoint_response.json['deployment']['name'],
+                datetime.datetime.fromisoformat(
+                    endpoint_response.json['deployment']['created_at'],
+                )
+            ),
+            [
+                KonanDeploymentError(
+                    KonanDeploymentErrorType(error['field']),
+                    error['message'],
+                ) for error in endpoint_response.json['errors']
+            ],
+            endpoint_response.json['container_logs']
+        )
+
+
+class DeleteDeployment(
+    KonanBaseDeploymentEndpoint[
+        None, bool
+    ]
+):
+    @property
+    def name(self) -> str:
+        return 'delete-deployment'
+
+    @property
+    def endpoint_path(self) -> str:
+        return super().endpoint_path + '/'
+
+    @property
+    def endpoint_operation(self) -> KonanEndpointOperationEnum:
+        return KonanEndpointOperationEnum.DELETE
+
+    def prepare_request(
+        self, request_object: None
+    ) -> KonanEndpointRequest:
+        return KonanEndpointRequest()
+
+    def process_response(
+        self, endpoint_response: KonanEndpointResponse
+    ) -> bool:
+        return True
---
+++ packages/konan-sdk/konan-sdk-1.2.6.tar.gz/konan-sdk-1.2.6/konan_sdk/sdk.py
@@ -0,0 +1,184 @@
+import datetime
+import sys
+from loguru import logger
+from typing import Dict, List, Optional, Tuple, Union
+
+from konan_sdk.auth import KonanAuth
+from konan_sdk.endpoints.konan_endpoints import (
+    CreateDeploymentEndpoint,
+    DeleteDeployment,
+    PredictionEndpoint,
+    EvaluateEndpoint,
+    FeedbackEndpoint,
+)
+from konan_sdk.konan_metrics import KonanBaseMetric
+from konan_sdk.konan_types import (
+    KonanDeploymentCreationRequest,
+    KonanDeploymentCreationResponse,
+    KonanDockerCredentials,
+    KonanDockerImage,
+    KonanFeedbackSubmission,
+    KonanFeedbacksResult,
+    KonanTimeWindow,
+)
+
+
+class KonanSDK:
+    """konan-sdk's main class for API integration.
+    """
+    def __init__(
+        self, auth_url="https://auth.konan.ai", api_url="https://api.konan.ai",
+        verbose=False
+    ):
+        self.auth_url = auth_url
+        self.api_url = api_url
+
+        self.auth: Optional[KonanAuth] = None
+
+        if not verbose:
+            logger.remove()
+            logger.add(sys.stderr, level="INFO")
+
+    def login(self, email: str, password: str) -> None:
+        """Login to Konan with user credentials
+
+        :param email: email of registered user
+        :type email: str
+        :param password: password of registered user
+        :type password: str
+        """
+        self.auth = KonanAuth(self.auth_url, email, password)
+        self.auth.login()
+
+    def create_deployment(
+        self,
+        name: str,
+        docker_credentials: KonanDockerCredentials,
+        docker_image: KonanDockerImage
+    ) -> KonanDeploymentCreationResponse:
+        """Call the create deployment function
+
+        :param name: name of the deployment to create
+        :type name: str
+        :param docker_credentials: credentials for the docker registry to use
+        :type docker_credentials: KonanDockerCredentials
+        :param docker_image: docker image information
+        :type docker_image: KonanDockerImage
+        :return: konan_deployment_creation_response
+        :rtype: KonanDeploymentCreationResponse
+        """
+        # check user performed login
+        self.auth._post_login_checks()
+
+        # Check if access token is valid and retrieve a new one if needed
+        self.auth.auto_refresh_token()
+
+        deployment_creation_response = CreateDeploymentEndpoint(
+            self.api_url, user=self.auth.user
+        ).request(KonanDeploymentCreationRequest(
+            name, docker_credentials, docker_image
+        ))
+        return deployment_creation_response
+
+    def predict(
+        self,
+        deployment_uuid: str, input_data: Union[Dict, str]
+    ) -> Tuple[str, Dict]:
+        """Call the predict function for a given deployment
+
+        :param deployment_uuid: uuid of deployment to use for prediction
+        :type deployment_uuid: str
+        :param input_data: data to pass to the model
+        :type input_data: Union[Dict, str]
+        :return: A tuple of prediction uuid and the prediction output
+        :rtype: Tuple[str, Dict]
+        """
+        # check user performed login
+        self.auth._post_login_checks()
+
+        # Check if access token is valid and retrieve a new one if needed
+        self.auth.auto_refresh_token()
+
+        prediction = PredictionEndpoint(
+            self.api_url,
+            deployment_uuid=deployment_uuid, user=self.auth.user
+        ).request(input_data)
+        return prediction.uuid, prediction.output
+
+    def evaluate(
+        self, deployment_uuid: str,
+        start_time: datetime.datetime, end_time: datetime.datetime
+    ) -> List[KonanBaseMetric]:
+        """Call the evaluate function for a given deployment
+
+        :param deployment_uuid: uuid of deployment to use for evaluation
+        :type deployment_uuid: str
+        :param start_time: use predictins made at or after this time
+        :type start_time: datetime.datetime
+        :param end_time: use predictins made before or at this time
+        :type end_time: datetime.datetime
+        :return: A model evaluation object
+        :rtype: EvaluateEndpoint.ResponseObject
+        """
+        # check user performed login
+        self.auth._post_login_checks()
+
+        # Check if access token is valid and retrieve a new one if needed
+        self.auth.auto_refresh_token()
+
+        model_metrics = EvaluateEndpoint(
+            self.api_url,
+            deployment_uuid=deployment_uuid, user=self.auth.user
+        ).request(KonanTimeWindow(start_time, end_time))
+
+        return model_metrics
+
+    def feedback(
+        self, deployment_uuid: str,
+        feedbacks: List[KonanFeedbackSubmission]
+    ) -> KonanFeedbacksResult:
+        """Call the feedback function for a given deployment
+
+        :param deployment_uuid: uuid of deployment to use for prediction
+        :type deployment_uuid: str
+        :param feedbacks: feedback objects to register with the deployment
+        :type feedbacks: List[KonanFeedbackSubmission]
+        :return: feedback result
+        :rtype: KonanFeedbacksResult
+        """
+        # check user performed login
+        self.auth._post_login_checks()
+
+        # Check if access token is valid and retrieve a new one if needed
+        self.auth.auto_refresh_token()
+
+        feedbacks_result = FeedbackEndpoint(
+            self.api_url,
+            deployment_uuid=deployment_uuid, user=self.auth.user
+        ).request(feedbacks)
+        return feedbacks_result
+
+    def delete_deployment(
+        self,
+        deployment_uuid: str,
+    ) -> bool:
+        """Call the delete function for a given deployment
+        WARNING: Using this method with a valid deployment_uuid will DELETE it!!
+        :param deployment_uuid: uuid of deployment to delete
+        :type deployment_uuid: str
+        :return: success
+        :rtype: bool
+        """
+
+        # check user performed login
+        self.auth._post_login_checks()
+
+        # Check if access token is valid and retrieve a new one if needed
+        self.auth.auto_refresh_token()
+
+        delete_deployment_result = DeleteDeployment(
+            self.api_url,
+            deployment_uuid=deployment_uuid,
+            user=self.auth.user
+        ).request(None)
+        return delete_deployment_result
