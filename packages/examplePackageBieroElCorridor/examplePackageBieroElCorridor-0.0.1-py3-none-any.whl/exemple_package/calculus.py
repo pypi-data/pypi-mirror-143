4f743ebb948b5df75cf41332663b3fdc336e6717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package examplePackageBieroElCorridor/0.0.1/examplePackageBieroElCorridor-0.0.1-py3-none-any.whl

---
+++ packages/examplePackageBieroElCorridor/examplePackageBieroElCorridor-0.0.1-py3-none-any.whl/exemple_package/calculus.py
@@ -0,0 +1,8 @@
+def add_one(number): 
+    return number + 1
+
+def mult_by_two(number): 
+    return number * 2
+
+def split_number_in_alf(number): 
+    return number / 2
---
+++ packages/examplePackageBieroElCorridor/examplePackageBieroElCorridor-0.0.1-py3-none-any.whl/exemple_package/demulus.py
@@ -0,0 +1,5 @@
+def demulus(): 
+    print("va et retro satanas")
+
+def satanas(): 
+    print("cubine mére des rue des binastral")    
