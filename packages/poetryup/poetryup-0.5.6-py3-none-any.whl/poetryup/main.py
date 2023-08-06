a58ccfdecfae9325a99d75d3dc51d54fdea250a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:35 +0000

    Add package poetryup/0.5.6/poetryup-0.5.6-py3-none-any.whl

---
+++ packages/poetryup/poetryup-0.5.6-py3-none-any.whl/poetryup/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import logging
+import os
+from pathlib import Path
+
+import typer
+
+from poetryup.pyproject import Pyproject
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO").upper())
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def poetryup(
+    latest: bool = typer.Option(
+        default=False,
+        help="Whether to update dependencies to their latest version.",
+    ),
+):
+    """Update dependencies and bump their version in pyproject.toml file"""
+
+    try:
+        pyproject_str = Path("pyproject.toml").read_text()
+    except FileNotFoundError:
+        raise Exception(
+            "poetryup couldn't find a pyproject.toml file in current directory"
+        )
+
+    pyproject = Pyproject(pyproject_str)
+    pyproject.update_dependencies(latest)
+    Path("pyproject.toml").write_text(pyproject.dumps())
+
+
+if __name__ == "__main__":
+    app()
