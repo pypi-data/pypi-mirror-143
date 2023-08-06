c1dccd08ff2b658e0b01141bbf67a20326f1e713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:06 +0000

    Add package CohesionIntegration/0.0.4/CohesionIntegration-0.0.4.tar.gz

---
+++ packages/CohesionIntegration/CohesionIntegration-0.0.4.tar.gz/CohesionIntegration-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import pkutils
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = list(pkutils.parse_requirements('CohesionIntegration/requirements.txt'))
+
+setup(
+    name='CohesionIntegration',
+    version='0.0.4',
+    packages=find_packages(),
+    url='http://cohesion.regione.marche.it/cohesioninformativo',
+    license='MIT License',
+    author='Saverio Delpriori',
+    author_email='saveriodelpriori@gmail.com',
+    description='Cohesion integration package'
+)
