6a50fb97a2f2e2d04b62fd74f6a034bb5405ba44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package xiaoTangPypi/0.0.1/xiaoTangPypi-0.0.1-py3-none-any.whl

---
+++ packages/xiaoTangPypi/xiaoTangPypi-0.0.1-py3-none-any.whl/xiaoTangPypi/xiaoTangTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@File    ：xiaoTangTest.py
+@Author  ：唐维康
+@Date    ：2022/3/25 10:52 
+@功能：
+@用法：
+'''
+def xiaoTang():
+    print("**********hello xiaoTang!!!*********")
