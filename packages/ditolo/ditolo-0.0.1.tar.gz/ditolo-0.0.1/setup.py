85150ac8213eaaaf40a49224f3059dcf0f8aa39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:38 +0000

    Add package ditolo/0.0.1/ditolo-0.0.1.tar.gz

---
+++ packages/ditolo/ditolo-0.0.1.tar.gz/ditolo-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ditolo',
+    description="discord extension package ditolo",
+    version='0.0.1',
+    packages=find_packages(),
+    author="Za",
+    url="https://github.com/zearakun/discord-token-login-packege",
+    license='MIT'
+)
