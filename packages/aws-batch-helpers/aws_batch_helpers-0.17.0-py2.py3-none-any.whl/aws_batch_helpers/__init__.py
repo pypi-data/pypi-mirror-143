442264fb819103d26b157962cd5427ab48e37e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:45 +0000

    Add package aws-batch-helpers/0.17.0/aws_batch_helpers-0.17.0-py2.py3-none-any.whl

---
+++ packages/aws-batch-helpers/aws_batch_helpers-0.17.0-py2.py3-none-any.whl/aws_batch_helpers/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for AWS Batch Helpers."""
+
+__author__ = """Jillian Rowe"""
+__email__ = 'jillian.e.rowe@gmail.com'
+
+from . import _version
+__version__ = _version.get_versions()['version']
