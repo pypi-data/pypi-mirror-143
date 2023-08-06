b0b53ff703ecb99f353983e7d202e4d506e2b038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:43 +0000

    Add package aggregationslib/0.0.1/aggregationslib-0.0.1.tar.gz

---
+++ packages/aggregationslib/aggregationslib-0.0.1.tar.gz/aggregationslib-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='aggregationslib',
+    packages=find_packages(include=['aggregationslib']),
+    version='0.0.1',
+    description='Python implementation of Arithmetic, quasi arithmetic and other aggregating functions',
+    author='wgalka',
+    license='TO DOO',
+
+    url='https://github.com/wgalka/Means',
+    download_url='https://github.com/wgalka/Means/archive/refs/tags/v0.0.1-alpha.tar.gz',
+
+    install_requires=['numpy==1.22.2', 'pynverse~=0.1.4.4', 'Deprecated~=1.2.13'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
