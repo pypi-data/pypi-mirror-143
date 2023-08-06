50f9f96ff0e28f539d73200a729a93451403a217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:06 +0000

    Add package hangman-marking-aicore/1.0.1/hangman-marking-aicore-1.0.1.tar.gz

---
+++ packages/hangman-marking-aicore/hangman-marking-aicore-1.0.1.tar.gz/hangman-marking-aicore-1.0.1/hangman_test/verify_milestone4.py
@@ -0,0 +1,24 @@
+from .verify import get_errors_fails, mark_incomplete, mark_complete
+
+errors = get_errors_fails('milestone_4.txt')
+task1_id = '6abbafb7-59a3-481a-9b1f-837e37b5826c' # Putting it all together
+task2_id = '33a3d741-121f-4a7a-aa2c-b0eeae1c0c0a' # Take it even further! (Bonus Points)
+task3_id = 'f9464de6-74a0-43f7-aa62-91f2677b267f' # Document your experience
+
+# test_play_lose 
+# test_play_win 
+# test_presence_readme
+
+if 'test_play_lose' in errors or 'test_play_win' in errors:
+    mark_incomplete(task1_id)
+    mark_incomplete(task2_id)
+else:
+    mark_complete(task1_id)
+
+if 'test_presence_readme' in errors:
+    mark_incomplete(task3_id)
+else:
+    mark_complete(task3_id)
+
+if len(errors) == 0:
+    mark_complete(task2_id)
