6853e340d5d2dd851aaff204be9dceb5719df3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package cohere/1.3.2/cohere-1.3.2.tar.gz

---
+++ packages/cohere/cohere-1.3.2.tar.gz/cohere-1.3.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cohere',
+    version='1.3.2',
+    author='kipply',
+    author_email='carol@cohere.ai',
+    description='A Python library for the Cohere API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cohere-ai/cohere-python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
