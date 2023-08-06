ca16941e48522fec45d36085e737483b8ebbde31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:06 +0000

    Add package pygifmaker/1.0.0/pygifmaker-1.0.0.tar.gz

---
+++ packages/pygifmaker/pygifmaker-1.0.0.tar.gz/pygifmaker-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pygifmaker",
+    version="1.0.0",
+    description="Test package for distribution",
+    author="Eunki7",
+    author_email="outsider7224@gmail.com",
+    url="",
+    download_url="",
+    install_requires=["pillow"],
+    include_package_data=True,
+    packages=find_packages(),
+    keywords=["GIFMaker", "gifmaker"],
+    python_requires=">=3",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
