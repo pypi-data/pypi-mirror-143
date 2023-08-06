903e62fb19e4510e473cb3e740a892162ed0cbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package bloxs/0.0.1/bloxs-0.0.1.tar.gz

---
+++ packages/bloxs/bloxs-0.0.1.tar.gz/bloxs-0.0.1/bloxs/bloxs.py
@@ -0,0 +1,14 @@
+
+class Blox:
+
+    def __init__(self):
+        pass
+
+    def _repr_html_(self):
+        return """
+<div style="text-align: center; width: 30%; border: 1px solid #e5e5e5; margin: 10px; padding-top: 40px; background: white; border-radius:5px">
+      <span style="font-size: 4em; color: #5a5a5a; font-family: monospace;">1,286 </span>
+      <span style="font-size: 2em; color: gray; display: block; padding-top: 20px; font-family: monospace;">GitHub Stars </span>
+
+<div>
+        """
