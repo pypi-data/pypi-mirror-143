8f5d89f132b58a4728f8ff560e4f6938e8dd37e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package model-log/1.1.8/model-log-1.1.8.tar.gz

---
+++ packages/model-log/model-log-1.1.8.tar.gz/model-log-1.1.8/README.md
@@ -0,0 +1,4 @@
+![imgs](https://gitee.com/kkweishe/images1/raw/master/ML/wechat/model_log_logo.png)
+
+
+
---
+++ packages/model-log/model-log-1.1.8.tar.gz/model-log-1.1.8/setup.py
@@ -0,0 +1,34 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+"""
+打包的用的setup必须引入，
+"""
+
+
+VERSION = '1.1.8'
+
+with open('README.md', encoding='utf-8') as fp:
+    readme = fp.read()
+
+setup(name='model-log',
+      version=VERSION,
+      description="test description",
+      long_description=readme,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python ML DL model log',
+      author='mantch',
+      author_email='mantchs@163.com',
+      url='https://github.com/NLP-LOVE',
+      license='MIT',
+      packages=['model_log'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'flask >= 0.11'
+      ],
+      entry_points={
+        'console_scripts':[
+            'model-log = model_log.modellog_web:main'
+        ]
+      }
+)
