fb670d499ee99a67dab8728e5fbc90a98a20826c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:37 +0000

    Add package quick-time/1.0/quick_time-1.0.tar.gz

---
+++ packages/quick-time/quick_time-1.0.tar.gz/quick_time-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='quick_time',
+      version='1.0',
+      description="quick time",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/keegang6705/instant-time',
+      author='keegang_6705',
+      author_email='darunphobwi@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires= ['datetime','pytz'],
+      python_requires='>=3.0',
+      zip_safe=False)
