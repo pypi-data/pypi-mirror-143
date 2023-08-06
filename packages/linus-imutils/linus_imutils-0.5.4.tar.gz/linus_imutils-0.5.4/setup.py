e6a10ac7f921a244afaefe0263b98311e5f9126e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:55 +0000

    Add package linus-imutils/0.5.4/linus_imutils-0.5.4.tar.gz

---
+++ packages/linus-imutils/linus_imutils-0.5.4.tar.gz/linus_imutils-0.5.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='linus_imutils',
+    packages=['imutils', 'imutils.video', 'imutils.io', 'imutils.feature', 'imutils.face_utils'],
+    version='0.5.4',
+    description='A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and both Python 2.7 and Python 3.',
+    author='Adrian Rosebrock',
+    author_email='adrian@pyimagesearch.com',
+    url='https://github.com/lamhoangtung/imutils',
+    keywords=['computer vision', 'image processing', 'opencv', 'matplotlib'],
+    classifiers=[],
+    scripts=['bin/range-detector'],
+    install_requires=['numpy', 'opencv-python', 'scipy'],
+)
