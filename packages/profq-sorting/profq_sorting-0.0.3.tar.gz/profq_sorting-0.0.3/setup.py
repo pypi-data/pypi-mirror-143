7263d6941b966ab411d703e755caf17c6d0b9c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:17 +0000

    Add package profq-sorting/0.0.3/profq_sorting-0.0.3.tar.gz

---
+++ packages/profq-sorting/profq_sorting-0.0.3.tar.gz/profq_sorting-0.0.3/setup.py
@@ -0,0 +1,25 @@
+# sourcery skip: path-read
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A library for sorting algorithms.'
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="profq_sorting",
+    version=VERSION,
+    author="ProfessorQu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'sorting', 'algorithms'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
