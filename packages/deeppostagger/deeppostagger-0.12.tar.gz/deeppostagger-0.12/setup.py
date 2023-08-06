5a9c57ed76fd81106828b98c218d7d99e57b6769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:31 +0000

    Add package deeppostagger/0.12/deeppostagger-0.12.tar.gz

---
+++ packages/deeppostagger/deeppostagger-0.12.tar.gz/deeppostagger-0.12/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+pkgs = find_packages('src')
+
+def long_description():
+    with open('README.md', 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
+print(pkgs)
+
+install_requires = [
+    "tensorflow>=2.3.0",
+    "tokenizers>=0.11.4"
+    ]
+print(install_requires)
+setup(
+    name = "deeppostagger",
+    version = "0.12",
+    long_description = long_description(),
+    long_description_content_type='text/markdown',
+    author="sun9sun9",
+    author_email="sun9sun9@gmail.com",
+    packages = pkgs,
+    install_requires=install_requires,
+    setup_requires=install_requires,
+    package_dir = {
+        '': 'src',
+    },
+    package_data = {
+        '': ['res/sejong_nikl/model.p', 'res/sejong_nikl/config.json', 'res/sejong_nikl/tokenizer.json']
+    }
+    , exclude_package_data={"": ["README.md"]}
+)
