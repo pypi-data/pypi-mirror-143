470e54d87199d35fa5736cedea5cfb233637fbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:21 +0000

    Add package ringity/0.2a3/ringity-0.2a3.tar.gz

---
+++ packages/ringity/ringity-0.2a3.tar.gz/ringity-0.2a3/ringity/tests/RUNME.py
@@ -0,0 +1,9 @@
+from test_conversions import *
+from test_centralities import *
+from test_diagram_function import *
+from test_classes import *
+from test_score import *
+from test_methods import *
+
+if __name__ == '__main__':
+    unittest.main()
