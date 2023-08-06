df552f6c38ea7a8414748faaf163f14446e0a848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:49 +0000

    Add package ObserverAPI/1.1.5/ObserverAPI-1.1.5.tar.gz

---
+++ packages/ObserverAPI/ObserverAPI-1.1.5.tar.gz/ObserverAPI-1.1.5/ObserverAPI/objects/leaderboards.py
@@ -0,0 +1,14 @@
+class Leaderboards:
+    def __init__(self, data):
+        self.data = data
+        self.leaderboard = data.get("leaderboards", {})
+
+    def get_index(self, index:int):
+        return Index(self.leaderboard, index-1)
+
+class Index:
+    def __init__(self, data, index):
+        self.rank = data[index].get("rank", 0)
+        self.id = data[index].get("id", 0)
+        self.value = data[index].get("value", 0)
+        self.formatted = data[index].get("formatted", "") 
