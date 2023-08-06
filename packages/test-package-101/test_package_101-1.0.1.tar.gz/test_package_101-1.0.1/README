2400ff489040750449c985e6344667bcae1c9649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package test-package-101/1.0.1/test_package_101-1.0.1.tar.gz

---
+++ packages/test-package-101/test_package_101-1.0.1.tar.gz/test_package_101-1.0.1/README
@@ -0,0 +1,1 @@
+dowolna tresc
---
+++ packages/test-package-101/test_package_101-1.0.1.tar.gz/test_package_101-1.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='test_package_101',
+      version='1.0.1',
+      description='Python Distribution Utilities',
+      author='Tom Jerry',
+      author_email='tomjerry@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+     )
