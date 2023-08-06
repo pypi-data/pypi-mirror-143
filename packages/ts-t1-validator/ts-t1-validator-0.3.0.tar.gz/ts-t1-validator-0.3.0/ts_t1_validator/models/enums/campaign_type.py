2b3726723f570e1db6372c5ac2af403b43c66c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:47 +0000

    Add package ts-t1-validator/0.3.0/ts-t1-validator-0.3.0.tar.gz

---
+++ packages/ts-t1-validator/ts-t1-validator-0.3.0.tar.gz/ts-t1-validator-0.3.0/ts_t1_validator/models/enums/campaign_type.py
@@ -0,0 +1,7 @@
+from ts_t1_validator.models.enums.abstract_enum import AbstractEnum
+
+
+class CampaignTypeEnum(AbstractEnum):
+    PMP = "PMP"
+    PG = "PG"
+    UNDEFINED = None
---
+++ packages/ts-t1-validator/ts-t1-validator-0.3.0.tar.gz/ts-t1-validator-0.3.0/ts_t1_validator/models/enums/cap_type.py
@@ -0,0 +1,12 @@
+from ts_t1_validator.models.enums.abstract_enum import AbstractEnum
+
+
+class CapTypeEnum(AbstractEnum):
+    EVEN = "even"
+    ASAP = "asap"
+    NO_LIMIT = "no-limit"
+    UNDEFINED = None
+
+    @classmethod
+    def list(cls) -> str:
+        return ", ".join("'{0}'".format(x.value) for x in cls if x.value is not None)
