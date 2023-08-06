65843ba5de5271ce03a8ae479ecdf3fafae020bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:19 +0000

    Add package granary/4.0/granary-4.0.tar.gz

---
+++ packages/granary/granary-4.0.tar.gz/granary-4.0/granary/as2.py
@@ -0,0 +1,236 @@
+"""Convert between ActivityStreams 1 and 2.
+
+AS2: http://www.w3.org/TR/activitystreams-core/
+
+AS1: http://activitystrea.ms/specs/json/1.0/
+     http://activitystrea.ms/specs/json/schema/activity-schema.html
+"""
+import copy
+import datetime
+import logging
+
+from oauth_dropins.webutil import util
+
+logger = logging.getLogger(__name__)
+
+# ActivityPub Content-Type details:
+# https://www.w3.org/TR/activitypub/#retrieving-objects
+CONTENT_TYPE = 'application/activity+json'
+CONTENT_TYPE_LD = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+CONNEG_HEADERS = {
+    'Accept': f'{CONTENT_TYPE}; q=0.9, {CONTENT_TYPE_LD}; q=0.8',
+}
+CONTEXT = 'https://www.w3.org/ns/activitystreams'
+
+
+def _invert(d):
+  return {v: k for k, v in d.items()}
+
+OBJECT_TYPE_TO_TYPE = {
+  'article': 'Article',
+  'audio': 'Audio',
+  'collection': 'Collection',
+  'comment': 'Note',
+  'event': 'Event',
+  'hashtag': 'Tag',  # not in AS2 spec; needed for correct round trip conversion
+  'image': 'Image',
+  # not in AS1 spec; needed to identify mentions in eg Bridgy Fed
+  'mention': 'Mention',
+  'note': 'Note',
+  'person': 'Person',
+  'place': 'Place',
+  'video': 'Video',
+}
+TYPE_TO_OBJECT_TYPE = _invert(OBJECT_TYPE_TO_TYPE)
+TYPE_TO_OBJECT_TYPE['Note'] = 'note'  # disambiguate
+
+VERB_TO_TYPE = {
+  'favorite': 'Like',
+  'follow': 'Follow',
+  'invite': 'Invite',
+  'like': 'Like',
+  'post': 'Create',
+  'rsvp-maybe': 'TentativeAccept',
+  'rsvp-no': 'Reject',
+  'rsvp-yes': 'Accept',
+  'share': 'Announce',
+  'tag': 'Add',
+  'update': 'Update',
+}
+TYPE_TO_VERB = _invert(VERB_TO_TYPE)
+TYPE_TO_VERB['Like'] = 'like'  # disambiguate
+
+
+def from_as1(obj, type=None, context=CONTEXT):
+  """Converts an ActivityStreams 1 activity or object to ActivityStreams 2.
+
+  Args:
+    obj: dict, AS1 activity or object
+    type: string, default type if type inference can't determine a type.
+    context: string, included as @context
+
+  Returns: dict, AS2 activity or object
+  """
+  if not obj:
+    return {}
+  elif not isinstance(obj, dict):
+    raise ValueError(f'Expected dict, got {obj!r}')
+
+  obj = copy.deepcopy(obj)
+
+  verb = obj.pop('verb', None)
+  obj_type = obj.pop('objectType', None)
+  type = (OBJECT_TYPE_TO_TYPE.get(verb or obj_type) or
+          VERB_TO_TYPE.get(verb or obj_type) or
+          type)
+
+  if context:
+    obj['@context'] = context
+
+  def all_from_as1(field, type=None):
+    return [from_as1(elem, type=type, context=None)
+            for elem in util.pop_list(obj, field)]
+
+  images = all_from_as1('image', type='Image')
+  inner_objs = all_from_as1('object')
+  if len(inner_objs) == 1:
+    inner_objs = inner_objs[0]
+
+  obj.update({
+    'type': type,
+    'name': obj.pop('displayName', None),
+    'actor': from_as1(obj.get('actor'), context=None),
+    'attachment': all_from_as1('attachments'),
+    'attributedTo': all_from_as1('author', type='Person'),
+    'image': images,
+    'inReplyTo': util.trim_nulls([orig.get('id') or orig.get('url')
+                                  for orig in obj.get('inReplyTo', [])]),
+    'object': inner_objs,
+    'tag': all_from_as1('tags'),
+    'preferredUsername': obj.pop('username', None),
+  })
+
+  if obj_type == 'person':
+    # TODO: something better. (we don't know aspect ratio though.)
+    obj['icon'] = images
+  elif obj_type == 'mention':
+    obj['href'] = obj.pop('url', None)
+  elif obj_type in ('audio', 'video'):
+    stream = util.pop_list(obj, 'stream')
+    if stream:
+      obj.update({
+        'url': stream[0].get('url'),
+        # file size in bytes. nonstandard, not in AS2 proper
+        'size': stream[0].get('size'),
+      })
+      duration = stream[0].get('duration')
+      if duration:
+        try:
+          # ISO 8601 duration
+          # https://www.w3.org/TR/activitystreams-vocabulary/#dfn-duration
+          # https://en.wikipedia.org/wiki/ISO_8601#Durations
+          obj['duration'] = util.to_iso8601_duration(
+            datetime.timedelta(seconds=duration))
+        except TypeError:
+          logger.warning(f'Dropping unexpected duration {duration!r}; expected int, is {duration.__class__}')
+
+  loc = obj.get('location')
+  if loc:
+    obj['location'] = from_as1(loc, type='Place', context=None)
+
+  obj = util.trim_nulls(obj)
+  if list(obj.keys()) == ['url']:
+    return obj['url']
+
+  return obj
+
+
+def to_as1(obj, use_type=True):
+  """Converts an ActivityStreams 2 activity or object to ActivityStreams 1.
+
+  Args:
+    obj: dict, AS2 activity or object
+    use_type: boolean, whether to include objectType and verb
+
+  Returns: dict, AS1 activity or object
+  """
+  if not obj:
+    return {}
+  elif isinstance(obj, str):
+    return {'url': obj}
+  elif not isinstance(obj, dict):
+    raise ValueError(f'Expected dict, got {obj!r}')
+
+  obj = copy.deepcopy(obj)
+  obj.pop('@context', None)
+
+  type = obj.pop('type', None)
+  if use_type:
+    if type and not isinstance(type, str):
+      raise ValueError(f'Expected type to be string, got {type!r}')
+    obj['objectType'] = TYPE_TO_OBJECT_TYPE.get(type)
+    obj['verb'] = TYPE_TO_VERB.get(type)
+    if obj.get('inReplyTo') and obj['objectType'] in ('note', 'article'):
+      obj['objectType'] = 'comment'
+    elif obj['verb'] and not obj['objectType']:
+      obj['objectType'] = 'activity'
+
+  def url_or_as1(val):
+    return {'url': val} if isinstance(val, str) else to_as1(val)
+
+  def all_to_as1(field):
+    return [to_as1(elem) for elem in util.pop_list(obj, field)]
+
+  images = []
+  # icon first since e.g. Mastodon uses icon for profile picture,
+  # image for featured photo.
+  for as2_img in util.pop_list(obj, 'icon') + util.pop_list(obj, 'image'):
+    as1_img = to_as1(as2_img, use_type=False)
+    if as1_img not in images:
+      images.append(as1_img)
+
+  # inner objects
+  inner_objs = all_to_as1('object')
+  actor = to_as1(obj.get('actor', {}))
+
+  if type == 'Create':
+    for inner_obj in inner_objs:
+      inner_obj.setdefault('author', {}).update(actor)
+
+  if len(inner_objs) == 1:
+    inner_objs = inner_objs[0]
+
+  obj.update({
+    'displayName': obj.pop('name', None),
+    'username': obj.pop('preferredUsername', None),
+    'actor': actor,
+    'attachments': all_to_as1('attachment'),
+    'image': images,
+    'inReplyTo': [url_or_as1(orig) for orig in util.get_list(obj, 'inReplyTo')],
+    'location': url_or_as1(obj.get('location')),
+    'object': inner_objs,
+    'tags': all_to_as1('tag'),
+  })
+
+  # media
+  if type in ('Audio', 'Video'):
+    duration = util.parse_iso8601_duration(obj.pop('duration', None))
+    if duration:
+      duration = duration.total_seconds()
+    obj['stream'] = {
+      'url': obj.pop('url', None),
+      # file size in bytes. nonstandard, not in AS1 proper
+      'size': obj.pop('size', None),
+      'duration': duration or None,
+    }
+  elif type == 'Mention':
+    obj['url'] = obj.pop('href', None)
+
+  # object author
+  attrib = util.pop_list(obj, 'attributedTo')
+  if attrib:
+    if len(attrib) > 1:
+      logger.warning(f'ActivityStreams 1 only supports single author; dropping extra attributedTo values: {attrib[1:]}')
+    obj.setdefault('author', {}).update(to_as1(attrib[0]))
+
+  return util.trim_nulls(obj)
---
+++ packages/granary/granary-4.0.tar.gz/granary-4.0/granary/tests/test_reddit.py
@@ -0,0 +1,276 @@
+# coding=utf-8
+"""Unit tests for reddit.py.
+"""
+import copy
+
+from mox3 import mox
+from oauth_dropins.webutil import testutil
+from oauth_dropins.webutil import util
+
+from granary import reddit
+
+import praw
+import praw.models
+from praw.models.comment_forest import CommentForest
+from prawcore.exceptions import NotFound
+
+
+class FakeUserSubreddit():
+  """ to mock https://praw.readthedocs.io/en/latest/code_overview/other/usersubreddit.html
+  """
+  id = 'abc123'
+  display_name = 'u_bonkerfield'
+  name = 'Human readable?'
+  description = 'foo bar'
+  public_description = 'https://bonkerfield.org https://viewfoil.bonkerfield.org'
+
+
+class FakeRedditor():
+  """ to mock https://praw.readthedocs.io/en/latest/code_overview/models/redditor.html
+  """
+  id = '59ucsixw'
+  name = 'bonkerfield'
+  subreddit = FakeUserSubreddit()
+  icon_img = 'https://styles.redditmedia.com/t5_2az095/styles/profileIcon_ek6onop1xbf41.png'
+  created_utc = 1576950011.0
+
+
+class FakeMissingRedditor():
+  """ to mock https://praw.readthedocs.io/en/latest/code_overview/models/redditor.html
+  """
+  name = 'ms_missing'
+
+  @property
+  def subreddit(self):
+    class FakeResponse():
+      status_code = '404'
+    raise NotFound(FakeResponse())
+
+
+class FakeSubmission():
+  """ to mock https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
+  """
+  id = 'ezv3f2'
+  permalink = '/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/'
+  created_utc = 1581007919.0
+  title = '[P] GPT-2 + BERT reddit replier'
+  selftext = '<!-- SC_OFF --><div class="md"><p>I was trying to make a reddit reply bot with GPT-2 to see if it could pass as a human on reddit. I wrote up a <a href="https://www.bonkerfield.org/2020/02/combining-gpt-2-and-bert/">results overview</a> and a <a href="https://www.bonkerfield.org/2020/02/reddit-bot-gpt2-bert/">tutorial post</a> to explain how it works.'
+
+  def __init__(self, fake_redditor):
+    self.author = fake_redditor
+
+
+class FakeComment():
+  """ to mock https://praw.readthedocs.io/en/latest/code_overview/models/comment.html
+  """
+  id = 'fgpvzfw'
+  permalink = '/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/fgpvzfw/'
+  created_utc = 1581013069.0
+  body_html = '<div class="md"><p>The ultimate purpose of Reddit will be the testing ground for passing the Turing test. Then we can all quit the internet.</p>\n</div>'
+
+  def __init__(self, fake_submission, fake_redditor):
+    self._parent = fake_submission
+    self.author = fake_redditor
+
+  def parent(self):
+    return self._parent
+
+
+ACTOR = {
+  'description': 'https://bonkerfield.org https://viewfoil.bonkerfield.org',
+  'displayName': 'bonkerfield',
+  'id': 'tag:reddit.com:bonkerfield',
+  'image': {'url': 'https://styles.redditmedia.com/t5_2az095/styles/profileIcon_ek6onop1xbf41.png'},
+  'numeric_id': '59ucsixw',
+  'objectType': 'person',
+  'published': '2019-12-21T17:40:11Z',
+  'url': 'https://reddit.com/user/bonkerfield/',
+  'urls': [
+    {'value': 'https://reddit.com/user/bonkerfield/'},
+    {'value': 'https://bonkerfield.org'},
+    {'value': 'https://viewfoil.bonkerfield.org'}
+  ],
+  'username': 'bonkerfield'
+}
+
+ACTIVITY_WITH_SELFTEXT = {
+  'verb': 'post',
+  'id': 'tag:reddit.com:ezv3f2',
+  'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/',
+  'title': '[P] GPT-2 + BERT reddit replier',
+  'actor': ACTOR,
+  'object': {
+    'objectType': 'note',
+    'id': 'tag:reddit.com:ezv3f2',
+    'author': ACTOR,
+    'displayName': '[P] GPT-2 + BERT reddit replier',
+    'content': '<!-- SC_OFF --><div class="md"><p>I was trying to make a reddit reply bot with GPT-2 to see if it could pass as a human on reddit. I wrote up a <a href="https://www.bonkerfield.org/2020/02/combining-gpt-2-and-bert/">results overview</a> and a <a href="https://www.bonkerfield.org/2020/02/reddit-bot-gpt2-bert/">tutorial post</a> to explain how it works.',
+    'published': '2020-02-06T16:51:59Z',
+    'tags': [{
+      'displayName': 'https://www.bonkerfield.org/2020/02/combining-gpt-2-and-bert/',
+      'objectType': 'article',
+      'url': 'https://www.bonkerfield.org/2020/02/combining-gpt-2-and-bert/',
+    }, {
+      'displayName': 'https://www.bonkerfield.org/2020/02/reddit-bot-gpt2-bert/',
+      'objectType': 'article',
+      'url': 'https://www.bonkerfield.org/2020/02/reddit-bot-gpt2-bert/',
+    }],
+    'to': [{'alias': '@public', 'objectType': 'group'}],
+    'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/',
+  },
+}
+
+ACTIVITY_WITH_LINK ={
+  'verb': 'post',
+  'id': 'tag:reddit.com:ezv3f2',
+  'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/',
+  'title': '[P] GPT-2 + BERT reddit replier',
+  'actor': ACTOR,
+  'object': {
+    'objectType': 'bookmark',
+    'id': 'tag:reddit.com:ezv3f2',
+    'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/',
+    'author': ACTOR,
+    'displayName': '[P] GPT-2 + BERT reddit replier',
+    'targetUrl': 'https://reddit.com/ezv3f2',
+    'published': '2020-02-06T16:51:59Z',
+    'to': [{'alias': '@public', 'objectType': 'group'}],
+  },
+}
+
+COMMENT_OBJECT = {
+  'objectType': 'comment',
+  'id': 'tag:reddit.com:fgpvzfw',
+  'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/fgpvzfw/',
+  'author': ACTOR,
+  'content': '<div class="md"><p>The ultimate purpose of Reddit will be the testing ground for passing the Turing test. Then we can all quit the internet.</p>\n</div>',
+  'inReplyTo': [{
+    'id': 'tag:reddit.com:ezv3f2',
+    'url': 'https://reddit.com/r/MachineLearning/comments/ezv3f2/p_gpt2_bert_reddit_replier_i_built_a_system_that/'
+  }],
+  'published': '2020-02-06T18:17:49Z',
+  'to': [{'alias': '@public', 'objectType': 'group'}],
+}
+
+ACTIVITY_WITH_COMMENT = copy.deepcopy(ACTIVITY_WITH_SELFTEXT)
+ACTIVITY_WITH_COMMENT['object']['replies'] = {
+  'items': [COMMENT_OBJECT],
+  'totalItems': 1,
+}
+
+MISSING_OBJECT = {}
+
+
+class RedditTest(testutil.TestCase):
+
+  def setUp(self):
+    super(RedditTest, self).setUp()
+    self.reddit = reddit.Reddit('token-here')
+    self.api = self.reddit.reddit_api = self.mox.CreateMockAnything(praw.Reddit)
+
+    self.submission_selftext = FakeSubmission(FakeRedditor())
+    self.comment = FakeComment(self.submission_selftext, FakeRedditor())
+
+    self.submission_link = FakeSubmission(FakeRedditor())
+    self.submission_link.selftext = ''
+    self.submission_link.url = 'https://reddit.com/ezv3f2'
+
+    reddit.user_cache.clear()
+
+  def test_user_url(self):
+    self.assert_equals('https://reddit.com/user/foo', self.reddit.user_url('foo'))
+
+  def test_missing_user_to_actor(self):
+    self.assert_equals(MISSING_OBJECT,
+                       self.reddit.praw_to_actor(FakeMissingRedditor()))
+
+  def test_suspended_user_to_actor(self):
+    suspended = util.Struct(name='mr_suspended', is_suspended=True)
+    self.assert_equals(MISSING_OBJECT, self.reddit.praw_to_actor(suspended))
+
+  def test_praw_to_actor(self):
+    self.assert_equals(ACTOR, self.reddit.praw_to_actor(FakeRedditor()))
+
+  def test_broken_praw_to_actor(self):
+    self.assert_equals(MISSING_OBJECT, self.reddit.praw_to_actor(util.Struct()))
+
+  def test_praw_to_actor_no_subreddit(self):
+    redditor = FakeRedditor()
+    redditor.subreddit = None
+    expected = copy.deepcopy(ACTOR)
+    del expected['description']
+    del expected['urls']
+    expected['url'] = 'https://reddit.com/user/bonkerfield/'
+    self.assert_equals(expected, self.reddit.praw_to_actor(redditor))
+
+  def test_praw_to_comment(self):
+    self.assert_equals(COMMENT_OBJECT,
+                       self.reddit.praw_to_object(self.comment, 'comment'))
+
+  def test_broken_praw_to_comment(self):
+    self.assert_equals(MISSING_OBJECT,
+                       self.reddit.praw_to_object(util.Struct(), 'comment'))
+
+  def test_submission_to_activity_with_link(self):
+    self.assert_equals(
+      ACTIVITY_WITH_LINK,
+      self.reddit.praw_to_activity(self.submission_link, type='submission'))
+
+  def test_submission_to_activity_with_selftext(self):
+    self.assert_equals(
+      ACTIVITY_WITH_SELFTEXT,
+      self.reddit.praw_to_activity(self.submission_selftext, type='submission'))
+
+  def test_broken_submission_to_object(self):
+    self.assert_equals(MISSING_OBJECT,
+                       self.reddit.praw_to_activity(util.Struct(), type='submission'))
+
+  def test_post_id(self):
+    self.assert_equals(
+      'lhzukq',
+      self.reddit.post_id('https://www.reddit.com/r/xyz/comments/lhzukq/abc/'))
+
+  # TODO
+  # def test_get_activities_default_user(self):
+  #   self.api.user.me().submissions.new(limit=None).AndReturn(
+  #     [self.submission_selftext, self.submission_link])
+  #   self.mox.ReplayAll()
+
+  #   self.assert_equals([ACTIVITY_WITH_SELFTEXT, ACTIVITY_WITH_LINK],
+  #                      self.reddit.get_activities())
+
+  def test_get_activities_activity_id(self):
+    self.api.submission(id='abc').AndReturn(self.submission_selftext)
+    self.mox.ReplayAll()
+
+    self.assert_equals([ACTIVITY_WITH_SELFTEXT],
+                       self.reddit.get_activities(activity_id='abc'))
+
+  def test_get_activities_search_query(self):
+    subreddit = self.mox.CreateMock(praw.models.Subreddit)
+    self.mox.StubOutWithMock(self.api, 'subreddit')
+    self.api.subreddit('all').AndReturn(subreddit)
+    subreddit.search('foo bar', sort='new', limit=None).AndReturn(
+      [self.submission_selftext, self.submission_link])
+    self.mox.ReplayAll()
+
+    self.assert_equals([ACTIVITY_WITH_SELFTEXT, ACTIVITY_WITH_LINK],
+                       self.reddit.get_activities(search_query='foo bar'))
+
+  def test_get_activities_fetch_replies(self):
+    self.api.submission(id='ezv3f2').MultipleTimes().AndReturn(self.submission_selftext)
+    self.submission_selftext.comments = CommentForest(self.submission_selftext,
+                                                      comments=[self.comment])
+    self.mox.StubOutWithMock(self.submission_selftext.comments, 'replace_more')
+    self.submission_selftext.comments.replace_more()
+    self.mox.ReplayAll()
+
+    self.assert_equals(
+      [ACTIVITY_WITH_COMMENT],
+      self.reddit.get_activities(activity_id='ezv3f2', fetch_replies=True))
+
+  def test_get_comment(self):
+    self.api.comment(id='xyz').AndReturn(self.comment)
+    self.mox.ReplayAll()
+
+    self.assert_equals(COMMENT_OBJECT, self.reddit.get_comment('xyz'))
