6d186f033f4e6af976d20abcedb46916717e7e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:02 +0000

    Add package multiviewdata/0.2.0/multiviewdata-0.2.0.tar.gz

---
+++ packages/multiviewdata/multiviewdata-0.2.0.tar.gz/multiviewdata-0.2.0/multiviewdata/torchdatasets/__init__.py
@@ -0,0 +1,7 @@
+from .cars3d import CarsDataset
+from .generic import GenericDataset
+from .mnist import NoisyMNISTDataset, SplitMNISTDataset, TangledMNISTDataset
+from .wiw import WIWDataset
+from .xrmb import XRMBDataset
+from .mfeat import MFeatDataset
+from .twitter import TwitterDataset
