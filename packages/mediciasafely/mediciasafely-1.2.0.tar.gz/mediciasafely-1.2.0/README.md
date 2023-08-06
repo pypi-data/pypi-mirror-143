ac744ffb10933104fca705c723357b30ac64ab00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:09 +0000

    Add package mediciasafely/1.2.0/mediciasafely-1.2.0.tar.gz

---
+++ packages/mediciasafely/mediciasafely-1.2.0.tar.gz/mediciasafely-1.2.0/README.md
@@ -0,0 +1,23 @@
+# MediciaSAFELY Command Line Interface
+
+This tool is part of the [MediciaSAFELY](https://www.medicia.ai/)
+project.
+
+To use it you will need to install:
+ * [Python](https://www.python.org/downloads/) (at least version 3.8, but the more recent the better)
+ * [git](https://git-scm.com/downloads)
+ * [Docker](https://docs.docker.com/get-docker/)
+
+Once these are installed you can install the tool itself with:
+```
+pip install mediciasafely
+```
+
+To see available commands run
+```
+mediciasafely --help
+```
+
+## Developer docs
+
+Please see [the additional information](DEVELOPERS.md).
---
+++ packages/mediciasafely/mediciasafely-1.2.0.tar.gz/mediciasafely-1.2.0/setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import find_namespace_packages, setup
+
+
+with open(os.path.join("mediciasafely", "VERSION")) as f:
+    version = f.read().strip()
+
+setup(
+    name="mediciasafely",
+    version=version,
+    packages=find_namespace_packages(exclude=["tests"]),
+    include_package_data=True,
+    url="https://github.com/mediciaai/mediciasafely-cli",
+    description="Command line tool for running MediciaSAFELY studies locally.",
+    license="GPLv3",
+    author="MediciaSAFELY",
+    author_email="frankie@medicia.ai",
+    python_requires=">=3.8",
+    entry_points={"console_scripts": ["mediciasafely=mediciasafely:main"]},
+    classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
+    project_urls={
+        "Homepage": "https://www.medicia.ai",
+        "Documentation": "https://docs.opensafely.org",
+    },
+)
