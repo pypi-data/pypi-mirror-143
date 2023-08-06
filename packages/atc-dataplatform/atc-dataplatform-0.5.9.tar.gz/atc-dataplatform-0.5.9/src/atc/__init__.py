cc9c2ff5c523631288e4961eae0fcd8ada9e274d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:01 +0000

    Add package atc-dataplatform/0.5.9/atc-dataplatform-0.5.9.tar.gz

---
+++ packages/atc-dataplatform/atc-dataplatform-0.5.9.tar.gz/atc-dataplatform-0.5.9/src/atc/__init__.py
@@ -0,0 +1,12 @@
+"""
+A common set of python libraries for DataBricks.
+See https://github.com/atc-net/atc-dataplatform for details
+"""
+
+__version__ = "0.5.9"
+
+from atc import spark
+from atc import sql
+from atc.config_master.config_master import ConfigMaster
+from atc import functions
+from atc import etl
