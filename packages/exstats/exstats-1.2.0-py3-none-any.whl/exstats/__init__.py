3fa2fc0388c7c60e9b805124cc9905c5a9ece494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:05 +0000

    Add package exstats/1.2.0/exstats-1.2.0-py3-none-any.whl

---
+++ packages/exstats/exstats-1.2.0-py3-none-any.whl/exstats/__init__.py
@@ -0,0 +1,42 @@
+# Checking if numbers are odd.
+def odd(number):
+    if number % 2 == 1:
+        return True
+    else:
+        return False
+
+# Getting the median (Middle most item in a sorted list, also works for strings)
+def median(ls):
+    if odd(len(ls)):
+        return ls[len(ls)//2]
+    else:
+        return (ls[len(ls)//2-1] + ls[len(ls)//2])/2
+
+def quartileList(ls):
+    if odd(len(ls)):
+        medianPlace = (len(ls) + 1) / 2
+        q1List = ls[:int(medianPlace -1)]
+        q3List = ls[-int(medianPlace -1):]
+    else:
+        q1List = ls[:len(ls)//2]
+        q3List = ls[len(ls)//2:]
+    
+    return q1List, q3List
+
+def quartiles(ls):
+    q1 = median(quartileList(ls)[0])
+    q3 = median(quartileList(ls)[1])
+    
+    return q1, q3
+def iqr(ls):
+    iqr = quartiles(ls)[1] - quartiles(ls)[0]
+    return iqr
+
+def mode(ls):
+    mode = max(set(ls), key = ls.count)
+    return mode
+
+def mean(ls):
+    mean = sum(ls) / len(ls)
+    return mean
+
