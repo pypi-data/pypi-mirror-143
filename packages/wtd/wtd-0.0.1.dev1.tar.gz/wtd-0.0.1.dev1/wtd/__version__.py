93003ea7628368016dbec36175b74f3c26d42b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:05 +0000

    Add package wtd/0.0.1.dev1/wtd-0.0.1.dev1.tar.gz

---
+++ packages/wtd/wtd-0.0.1.dev1.tar.gz/wtd-0.0.1.dev1/wtd/__version__.py
@@ -0,0 +1,15 @@
+"""Package version and meta information."""
+from semantic_version import Version
+
+__title__ = 'wtd'
+__description__ = ''  # TODO
+__version__ = Version('0.0.1-dev1')
+
+__docs_url__ = 'https://github.com/ddnomad/wtd'  # TODO: Change to RTD
+__source_url__ = 'https://github.com/ddnomad/wtd'
+__home_url__ = __docs_url__
+
+__author__ = 'ddnomad'
+__author_email__ = 'self@ddnomad.io'
+__copyright__ = ''  # TODO
+__license__ = ''  # TODO
