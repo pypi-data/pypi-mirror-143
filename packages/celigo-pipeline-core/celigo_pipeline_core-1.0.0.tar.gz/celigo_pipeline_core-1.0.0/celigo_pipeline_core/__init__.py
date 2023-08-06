3804fb2ab83a0a028d37ae5d367e097a229006da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:45 +0000

    Add package celigo-pipeline-core/1.0.0/celigo_pipeline_core-1.0.0.tar.gz

---
+++ packages/celigo-pipeline-core/celigo_pipeline_core-1.0.0.tar.gz/celigo_pipeline_core-1.0.0/celigo_pipeline_core/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "AICS"
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "1.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .celigo_single_image_core import (
+    CeligoSingleImageCore,
+)
+
+__all__ = "CeligoSingleImageCore"
