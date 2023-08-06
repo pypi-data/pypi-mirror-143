84eb8bcfe79b8b50548a09f3ba5f8eafebfecb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:15 +0000

    Add package cysimdjson/21.11/cysimdjson-21.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cysimdjson/cysimdjson-21.11-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cysimdjson/__init__.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+# If the environment variable CYSIMDJSON_GLOBAL_DL is set to "1"
+# then the compiled cysimdjson library is imported into a global symbol table
+# It is needed to enable third party component to access cysimdjson C API
+prev = sys.getdlopenflags()
+try:
+	if os.environ.get("CYSIMDJSON_GLOBAL_DL") == "1":
+		sys.setdlopenflags(os.RTLD_GLOBAL | os.RTLD_NOW)
+
+	from .cysimdjson import (
+		JSONParser,
+		JSONObject,
+		JSONArray,
+		JSONElement,
+		addr_to_element,
+		SIMDJSON_VERSION,
+		MAXSIZE_BYTES,
+		PADDING,
+	)
+finally:
+	sys.setdlopenflags(prev)
+
+__all__ = [
+	"JSONParser",
+	"JSONObject",
+	"JSONArray",
+	"JSONElement",
+	"addr_to_element",
+	"SIMDJSON_VERSION",
+	"MAXSIZE_BYTES",
+	"PADDING",
+]
