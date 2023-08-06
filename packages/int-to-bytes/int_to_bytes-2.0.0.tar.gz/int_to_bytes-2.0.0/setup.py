06ddd065585da90bf40a1ef0f363a2203f197a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:17 +0000

    Add package int-to-bytes/2.0.0/int_to_bytes-2.0.0.tar.gz

---
+++ packages/int-to-bytes/int_to_bytes-2.0.0.tar.gz/int_to_bytes-2.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="int_to_bytes",
+    version="2.0.0",
+    license='MIT',
+    author="Go Dae Hyeon",
+    author_email="castar2001kr@naver.com",
+    description="transformate integet number to binary",
+    long_description=open('README.md').read(),
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
