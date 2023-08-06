3fe75f1412e10cd43fcfd9c63fe3b2924eeb142f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:22 +0000

    Add package swmm-api/0.3a1/swmm-api-0.3a1.tar.gz

---
+++ packages/swmm-api/swmm-api-0.3a1.tar.gz/swmm-api-0.3a1/swmm_api/input_file/sections/_identifiers.py
@@ -0,0 +1,15 @@
+class IDENTIFIERS:
+    """predefined to identify the objects in an input file section and also used in the tags section"""
+    Name = 'Name'
+
+    Node = 'Node'
+    Subcatch = 'Subcatch'
+    Link = 'Link'
+
+    Gage = 'Gage'
+
+    Constituent = 'Constituent'
+
+    Pollutant = 'pollutant'
+
+    Landuse = 'landuse'
---
+++ packages/swmm-api/swmm-api-0.3a1.tar.gz/swmm-api-0.3a1/swmm_api/output_file/__init__.py
@@ -0,0 +1,2 @@
+from .out import read_out_file, SwmmOutput, out2frame
+from .definitions import VARIABLES, OBJECTS
