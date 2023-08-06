fe45d6a13e1436237281fefea38ee80630d3e856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:03 +0000

    Add package codeplateau/1.0.4/codeplateau-1.0.4.tar.gz

---
+++ packages/codeplateau/codeplateau-1.0.4.tar.gz/codeplateau-1.0.4/main.py
@@ -0,0 +1,15 @@
+from pydoc import cli
+import click
+import colorama
+
+
+from scripts import shell
+
+
+@click.command()
+def start():
+    click.echo(shell.egg())
+
+
+if __name__ == '__main__':
+    start()
