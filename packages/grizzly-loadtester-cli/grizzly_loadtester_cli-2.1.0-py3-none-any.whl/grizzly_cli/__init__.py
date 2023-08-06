d0f627edeffcd6eb74d20e5e424b7f0d3415bce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:50:05 +0000

    Add package grizzly-loadtester-cli/2.1.0/grizzly_loadtester_cli-2.1.0-py3-none-any.whl

---
+++ packages/grizzly-loadtester-cli/grizzly_loadtester_cli-2.1.0-py3-none-any.whl/grizzly_cli/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from typing import Set
+
+from behave.model import Scenario
+
+
+__version__ = '2.1.0'
+
+EXECUTION_CONTEXT = os.getcwd()
+
+STATIC_CONTEXT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
+
+MOUNT_CONTEXT = os.environ.get('GRIZZLY_MOUNT_CONTEXT', EXECUTION_CONTEXT)
+
+PROJECT_NAME = os.path.basename(EXECUTION_CONTEXT)
+
+SCENARIOS: Set[Scenario] = set()
