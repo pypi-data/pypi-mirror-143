3dabe377d90fcab14ca577131de0be999d050ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:49 +0000

    Add package join-asvbins/0.0.5/join_asvbins-0.0.5-py3-none-any.whl

---
+++ packages/join-asvbins/join_asvbins-0.0.5-py3-none-any.whl/join_asvbins/snake_functions.py
@@ -0,0 +1,173 @@
+"""These functions are used directly by the snakemake pipline"""
+import os
+import pandas as pd
+from join_asvbins.utils import df_to_fasta, fasta_to_df, read_mbstats, \
+    filter_mdstats, mbstats_reformat,  process_mbdata, process_barfasta, \
+    barstats_reformat, combine_fasta, filter_fasta_from_headers, read_gff, \
+    get_stage1_mbstats_fasta
+
+CANDIDATE_16S_SEQS_PATH = 'candidate_sequences.fna'
+
+
+def resolve_dup_gene_locs(mbstats:str, bs_name:str, bs_start:str,
+                          bs_end:str, values:str, ascending:bool):
+    gene_locs = {}
+    def check_overlap(line:pd.Series):
+        line_name = line[bs_name]
+        line_start = min(line[bs_start], line[bs_end])
+        line_end = max(line[bs_start], line[bs_end])
+        if line_name in gene_locs:
+            match_start = gene_locs[line_name]['start']
+            match_end = gene_locs[line_name]['end']
+            if match_start <= line_start < match_end:
+                return False
+            if match_start <= line_end < match_end:
+                return False
+        gene_locs[line_name] = {
+            'start': line_start,
+            'end': line_end,
+        }
+        return True
+    mbstats.sort_values(values, inplace=True, ascending=False)
+    mbstats = mbstats[mbstats.apply(check_overlap, axis=1)]
+    return mbstats
+
+def combine_mbstats_barrnap(mbstats_fasta_path:str, mbstats_stats_path:str,
+                            barrnap_fasta_path:str, out_fasta_path:str,
+                            out_stats_path:str, barrnap_stats_path:str,
+                            search_tool:str, allow_empty:bool=False,
+                            **filter_kargs) -> None:
+    """
+    Combine the statistics from mmseqs or blast with  barrnap.
+
+    This function is a monster
+
+    :param mbstats_fasta_path: Path to mmseqs or blast fasta
+    :param mbstats_stats_path: Path to mmseqs or blast statistics, see docs for format requirements
+    :param barrnap_fasta_path: Path to barrnap fasta
+    :param out_fasta_path: Path for the combined fasta file
+    :param out_stats_path: Path for the combined statistics file
+    :param barrnap_stats_path: Path for the barrnap stats gff
+    :param min_pct_id: A filter for the pct identity, only for the non barrnap output
+    :param min_length: A filter for min_length, only for the non barrnap output
+    :param search_tool: The name of the search_tool that is not barrnap
+    :param allow_empty: If true the program will continue if only one search_tool gives results
+    :raises ValueError:
+    """
+    # TODO add checks that these functions return empty dfs if given empty
+    barfasta = fasta_to_df(barrnap_fasta_path)
+    mbstats_raw = read_mbstats(mbstats_stats_path)
+    mbstats_raw.drop_duplicates(inplace=True)
+    mbstats = filter_mdstats(mbstats_raw, **filter_kargs)
+    if barfasta.empty and mbstats.empty:
+        raise ValueError(f"There are no hits from barrnap or {search_tool},"
+                          " this is most likely caused by some irregularity in"
+                          " the input files. The program can not continue.")
+    if mbstats.empty:
+        if not allow_empty:
+            raise ValueError(f"There are no hits from {search_tool}, this is"
+                             " may be caused by some irregularity the input"
+                             " files. Consider if your bins are few and small,"
+                             " or you could use a larger, more appropriate set"
+                             " of generic 16S sequences. If you are confident"
+                             " in your data you can continue by passing"
+                             " --allow_empty to skip this search tool and use "
+                             " only barrnap. Consider using --no_clean also to"
+                             " save time.")
+        else:
+            data = process_barfasta(barfasta)
+            # save_barnap_stats(barfasta, out_barstats_path)
+            df_to_fasta(data, out_fasta_path)
+            make_stage1_statistics(out_stats_path, search_tool, barfasta=data,
+                                   barrnap_stats_path=barrnap_stats_path)
+            return
+    if barfasta.empty:
+        if not allow_empty:
+            raise ValueError("There are no hits from barrnap, this is may"
+                             " be caused by some irregularity the input files."
+                             " If you are confident in your data you can"
+                             " continue by passing --allow_empty to skip this"
+                             f" search tool and use only {search_tool}."
+                             " Consider using --no_clean also to save time.")
+        else:
+            data = get_stage1_mbstats_fasta(mbstats, mbstats_fasta_path)
+            df_to_fasta(data, out_fasta_path)
+            make_stage1_statistics(out_stats_path, search_tool, mbstats=data)
+            return
+    mbdata = get_stage1_mbstats_fasta(mbstats, mbstats_fasta_path)
+    barfasta = process_barfasta(barfasta)
+    # save_barnap_stats(barfasta, out_barstats_path)
+    data = combine_fasta(mbdata, barfasta, search_tool)
+    df_to_fasta(data, out_fasta_path)
+    make_stage1_statistics(out_stats_path, search_tool, mbstats=mbstats,
+                           barfasta=barfasta,
+                           barrnap_stats_path=barrnap_stats_path)
+
+
+def set_program_output(bins_path:str=None, asv_seqs_path:str=None,  qiime_out:bool=False):
+    search1_output = ["candidate_statistics.tsv", CANDIDATE_16S_SEQS_PATH]
+    search2_output = ["match_statistics.tsv", "match_sequences.fna"]
+    qiime_output = ["match_sequences.qza"]
+    program_output = []
+    if bins_path is not None:
+        program_output += search1_output
+    if asv_seqs_path is not None:
+        program_output += search2_output
+    if qiime_out:
+        program_output += qiime_out
+    if len(program_output) < 1:
+        raise AttributeError(
+            "There are no tasks for join_asvbins to do."
+            " Check that all arguments are logical. For example, if you provided"
+            " 16S from bins but not asvs then the progam has nothing to do."
+        )
+    return program_output
+
+
+def make_stage1_statistics(output_path:str, search_tool:str,
+                           mbstats:pd.DataFrame=None,
+                           barfasta:pd.DataFrame=None,
+                           barrnap_stats_path:str=None):
+    if barfasta is not None:
+        barstats = read_gff(barrnap_stats_path)
+        barstats_corrected = barfasta[['header', 'start', 'stop']]
+        barstats = barstats_reformat(barstats_corrected, barstats, 'bin')
+        barstats = resolve_dup_gene_locs(barstats, bs_name='name',
+                                         bs_start='bin_scaffold_start',
+                                         bs_end='bin_scaffold_end',
+                                         values='barrnap_e-value',
+                                         ascending=True)
+        if mbstats is None:
+            barstats.to_csv(output_path, sep='\t', index=False, na_rep='NA')
+            return
+    if mbstats is not None:
+        mbstats = resolve_dup_gene_locs(mbstats,
+                                        bs_name='sseqid',
+                                        bs_start='send', bs_end='sstart',
+                                        values='bitscore', ascending=False)
+        mbstats = mbstats_reformat(mbstats, search_tool, '16S')
+        if barstats is None:
+            mbstats.to_csv(output_path, sep='\t', index=False, na_rep='NA')
+            return
+    stats = pd.concat([mbstats, barstats])
+    stats.to_csv(output_path, sep='\t', index=False, na_rep='NA')
+
+
+def filter_from_mbstats(stats_file_in:str, fasta_file_in:str,
+                        fasta_file_out:str,
+                        stats_file_out:str, search_tool:str,  **filter_kargs):
+        mbstats = read_mbstats(stats_file_in)
+        mbstats = filter_mdstats(mbstats, **filter_kargs)
+        mbstats = mbstats_reformat(mbstats, search_tool, 'ASV')
+        mbstats.to_csv(stats_file_out, sep='\t', index=False, na_rep='NA')
+        filter_fasta_from_headers(fasta_file_in,
+                                  fasta_file_out,
+                                  mbstats['bin_scaffold_header'].values)
+
+
+def pullseqs_header_name_from_tab(in_fasta_path:str, out_fasta_path:str,
+                                  tab_file_path:str,
+                                  header_column:str='sseqid'):
+       mbstats = read_mbstats(tab_file_path)
+       headers = set(mbstats[header_column].values)
+       filter_fasta_from_headers(in_fasta_path, out_fasta_path, headers)
---
+++ packages/join-asvbins/join_asvbins-0.0.5-py3-none-any.whl/join_asvbins/utils.py
@@ -0,0 +1,346 @@
+"""Tools for extract 16S from scaffolds"""
+import os
+import pandas as pd
+import numpy as np
+from skbio import write as write_fa
+from skbio import read as read_fa
+from skbio import Sequence
+import warnings
+
+# This is the header format for blast and mmseqs stats
+MBSTATS_NAMES=[
+    "qseqid", "sseqid", "pident", "length", "mismatch", "gapopen",
+    "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qlen",
+    "slen"]
+
+
+def fasta_to_df(path, headers=None):
+    """
+    Convert a fasta to a dataframe
+
+    :param path: A path to a fasta
+    :param headers: Optional, headers to read from fasta
+    :returns: A dataframe
+    """
+    if os.stat(path).st_size == 0:
+        return pd.DataFrame()
+    try:
+        if headers is not None:
+            dafr = pd.DataFrame({seq.metadata['id']:[seq.values]
+                               for seq in read_fa(path, format='fasta')
+                               if seq.metadata['id'] in headers})
+        else:
+            dafr = pd.DataFrame({seq.metadata['id']:[seq.values]
+                               for seq in read_fa(path, format='fasta')})
+    except ValueError:
+        warnings.warn('Some fasta file was not read, posbly it is the wrong'
+                      'format.')
+        return pd.DataFrame()
+    dafr = dafr.T
+    dafr.reset_index(inplace=True)
+    dafr.columns = ['header', 'seq']
+    return dafr
+
+
+def df_to_fasta(dafr:pd.DataFrame, path:str):
+    """
+    Convert a dataframe to a fasta
+
+    :param dafr: A dataframe containing 'seq', 'note' and 'header' fields
+    :param path: A path to a fasta
+    """
+    seqs = (Sequence(x['seq'],
+                     metadata={"id": x["header"],
+                               'description': x["note"]}
+                     )
+            for _, x in dafr.iterrows())
+    write_fa(seqs, 'fasta', path)
+
+
+def merge_duplicate_seqs(data:pd.DataFrame) -> pd.DataFrame:
+    data.sort_values('start')
+    joined = data.iloc[0]
+    if len(data) <= 1:
+        return joined
+    for _, to_join in data.iloc[1:].iterrows():
+        if to_join['start'] > joined['stop']:
+            # raise Warning("Found non-overlapping duplicate in barrnap data")
+            continue
+        trim_point = joined['stop'] - to_join['start']
+        joined['seq'] = np.concatenate([joined['seq'],
+                                        to_join['seq'][trim_point:]])
+        joined['stop'] = to_join['stop']
+        joined['barrnap_header'] = f"{joined['header']}:{joined['start']}-{joined['stop']}"
+    return joined
+
+
+def get_stage1_mbstats_fasta(mbstats, mbstats_fasta_path):
+    mbseqs = fasta_to_df(mbstats_fasta_path,
+                         mbstats['sseqid'].values)
+    mbdata = pd.merge(mbseqs, mbstats, right_on='sseqid',
+                      left_on='header',
+                        how='inner')
+    mbdata = process_mbdata(mbdata)
+    return mbdata
+
+
+# def save_barnap_stats(barfasta, out_barstats_path):
+#     """
+#     save barrnap stats, run process_barfasta first!
+#
+#     :param barfasta:
+#     :param out_barstats_path:
+#     """
+#     barfasta[['header', 'start', 'stop']].to_csv(out_barstats_path,
+#                                              sep='\t', index=False)
+
+
+def process_barfasta(data:pd.DataFrame) -> pd.DataFrame:
+    data.rename(columns={'header': 'barrnap_header'}, inplace=True)
+    data[['header', 'start']] = \
+        data['barrnap_header'].str.split(':', expand=True)
+    data[['start', 'stop']] = data['start'].str.split('-', expand=True)
+    data[['start', 'stop']] = data[['start', 'stop']].astype(int)
+    data = data.groupby('header').apply(merge_duplicate_seqs).\
+        reset_index(drop=True)
+    # extra assert statement to cover by back
+    assert sum(
+        (data['stop'] - data['start']) != data['seq'].apply(len)) == 0, \
+        "The length of the sequence dose not match the size from the indexes."
+    return data
+
+def read_mbstats(stats_path:str) -> pd.DataFrame:
+    """
+    Read the tab delimited mmseqs or blast file with its very specific format.
+
+    :param stats_path: The path to the formatted statistics
+    :returns: A dataframe with proper format
+    """
+    stats = pd.read_csv(stats_path, header=None, sep='\t',
+                        names=MBSTATS_NAMES)
+    return stats
+
+
+def get_mbstats_dups(data):
+    """
+    Check mmseqs or blast stats data for dups
+
+    :param data:
+    :returns:
+    """
+    dups = data[data['sseqid'].duplicated(keep=False)]
+    dups = dups[[i for i in dups.columns
+                 if i not in ['seq', 'sseqid', 'qseqid']]]
+    return dups
+
+
+def process_mbdata(data:pd.DataFrame, headers=None) -> pd.DataFrame:
+    data = data.sort_values('length', ascending=False).\
+        drop_duplicates('sseqid')
+    # Keep only elements of blast_fasta_list that are in headers object
+    if headers is not None:
+        data = pd.merge(data, pd.DataFrame({
+            'sseqid': list(headers)}), on='sseqid')
+    data['seq'] = data.apply(
+        lambda x: x["seq"][x["sstart"] - 1:x["send"]]
+        if x["sstart"] < x["send"]
+        else x["seq"][::-1][x["send"] - 1:x["sstart"]], axis=1)
+    return data
+
+
+def combine_fasta(mbstats:pd.DataFrame, barrnap:pd.DataFrame,
+                  search_tool:str) -> pd.DataFrame:
+    barseqs= barrnap[['header', 'seq']].copy()
+    mbseqs = mbstats[['header', 'seq']].copy()
+    mbseqs['mbseqs'] = True
+    barseqs['barseqs'] = True
+    # NOTE we have a choice for merge, we can convert the arays to tuples or
+    # we can fix the problem after. I opt for option 2, it gives more control.
+    data = pd.merge(barseqs, mbseqs, on='header',
+                    how='outer')
+    data[['mbseqs', 'barseqs']] = data[['mbseqs', 'barseqs']].fillna(False)
+    data.rename(columns={'seq_x': 'seq_bar', 'seq_y': 'seq_other'},
+                inplace=True)
+    def select_and_describe_seq(x):
+        if x['barseqs'] and not x['mbseqs']:
+            return x['seq_bar'], 'Barnnap'
+        elif x['mbseqs'] and not x['barseqs']:
+            return x['seq_other'], search_tool
+        elif len(x['seq_bar']) > len(x['seq_other']):
+            return x['seq_bar'], f'Barnnap>{search_tool}'
+        elif len(x['seq_bar']) < len(x['seq_other']):
+            return x['seq_bar'], f'{search_tool}>Barnnap'
+        elif len(x['seq_bar']) == len(x['seq_other']):
+            return x['seq_bar'], f'{search_tool}=Barnnap'
+        else:
+            raise Exception("Non equal duplicates in barseqs, and mbseqs")
+
+    data[['seq', 'note']] = data.apply(select_and_describe_seq, axis=1, result_type='expand')
+    print("After Merge: \n"
+         f" There are {sum(data['barseqs'])} Sequences found by Barrnap.\n"
+         f" There are {sum(data['mbseqs'])} Sequences found by"
+          " MMseqs/BLAST.\n"
+         f" There are {sum(data['mbseqs'] & data['barseqs'])}"
+          " Sequences found by both Barrnap and MMseqs/BLAST.\n"
+          )
+    return data[['header', 'seq', 'note']]
+
+
+
+
+def filter_mdstats(data, min_pct_id:float=None, min_length:int=None,
+                   min_len_pct:float=None, max_gaps:int=None,
+                   max_missmatch:int=None, min_len_with_overlap:int=None,
+                   min_len_pct_no_overlap:float=None, end_buffer_length:int=5):
+    """
+    Creates and then applies a filter for mmseqs or blast statistics
+
+    :param data: Data to be filter must be in a computable blast style format
+    :param min_pct_id: Optional filter
+    :param min_length: Optional filter
+    :param min_len_pct: Optional filter
+    :param max_gaps: Optional filter
+    :param max_missmatch: Optional filter
+    :returns: Filtered data
+    #TODO look more at annotat_vgfs get_gene order
+    """
+    def check_overlap(x:pd.Series) -> bool:
+        """
+        Conditions for True
+        qseq reversed      qstart included sstart included
+                           qend included   send included
+        qseq not reversed qstart included  send included
+                          qend included    sstart included
+
+        NOTE sseq cant be reversed
+        """
+        ebl = end_buffer_length
+        # NOTE is this exceptionable
+        if abs(x['length']- x['qlen']) <= 2*ebl or \
+            abs(x['length']- x['slen']) <= 2*ebl:
+            return True
+        if x['qlen'] == x['slen']:
+            return True
+        if x['sstart'] > x['send']:
+            breakpoint()
+            raise ValueError("The search sequences cant be reversed."
+                             " Your data may be corrupt")
+        if x['qstart'] > x['qend']:# q is reversed
+            if (abs(x['qstart'] - x['qlen']) <= ebl) \
+                and (abs(x['send'] - x['slen']) <= ebl):
+                return True
+            if (x['qend'] <= ebl) and (x['sstart'] <= ebl):
+                return True
+        else:# q is not reversed
+            if (x['qstart'] <= ebl) and (abs(x['send'] - x['slen']) <= ebl):
+                return True
+            if (abs(x['qend'] - x['qlen']) <= ebl) and (x['sstart'] <= ebl):
+                return True
+        return False
+    data_checks = []
+    if max_gaps is not None:
+        data_checks.append(lambda x: x['gapopen'] <= max_gaps)
+    if max_missmatch is not None:
+        data_checks.append(lambda x: x['mismatch'] <= max_missmatch)
+    if min_length is not None:
+        data_checks.append(lambda x: x['length'] >= min_length)
+    if min_pct_id is not None:
+        data_checks.append(lambda x: x['pident'] >= min_pct_id)
+    if min_len_pct is not None:
+        data_checks.append(lambda x:
+    # TODO check that qlen should not be slen
+            ((x['length'] / x['qlen']) * 100) >= min_len_pct)
+    # NOTE MIN_SLEN_LENGTH = 1000
+    if min_len_with_overlap is not None and \
+       min_len_pct_no_overlap is not None:
+        data_checks.append(lambda x:
+            (check_overlap(x) & \
+             (x['length'] >= min_len_with_overlap)) | \
+            ((x['pident'] >= min_len_pct_no_overlap) & \
+             (x['length'] >= min_len_with_overlap)))
+    return data[data.apply(
+        lambda x: all([l(x) for l in data_checks]),
+        axis=1)]
+
+
+def barstats_reformat(barstats_corrected:pd.DataFrame,
+                      barstats_raw:pd.DataFrame,
+                      qname:str)->pd.DataFrame:
+    output_colums = {
+        "seqname":   "bin_scaffold_header",
+        "start":     "bin_scaffold_start",
+        "end":       "bin_scaffold_end",
+        "source":    "search_tool",
+        "score":     "barrnap_e-value",
+        "attribute": "barrnap_attribute"
+    }
+    barstats_raw = barstats_raw[[i for i in output_colums]].copy()
+    barstats_raw['name'] = barstats_raw['seqname'].\
+       str.split(':', expand=True)[0]
+    barstats_corrected = barstats_corrected[['header']]
+    barstats_corrected.rename(columns = {"header": "name"},
+                              inplace=True)
+    barstats_out = pd.merge(barstats_corrected, barstats_raw, on='name',
+                            how='outer')
+    barstats_out.rename(columns=output_colums, inplace=True)
+    return barstats_out
+
+
+def read_gff(gff_path:str) -> pd.DataFrame:
+    out_df = pd.read_csv(gff_path, sep='\t',
+                         names=["seqname", "source", "feature", "start",
+                                "end", "score", "strand", "frame",
+                                "attribute"])
+    return out_df
+
+
+def mbstats_reformat(mbstats_in:pd.DataFrame, search_tool:str, qname:str):
+    output_colums = {
+        "qseqid":   f"{qname}_header",
+        "sseqid":    "bin_scaffold_header",
+        "pident":    "pident",
+        "length":    "length",
+        "mismatch":  "mismatch",
+        "gapopen":   "gapopen",
+        "qstart":   f"{qname}_start",
+        "qend":     f"{qname}_end",
+        "sstart":    "bin_scaffold_start",
+        "send":      "bin_scaffold_end",
+        "evalue":    "evalue",
+        "bitscore":  "bitscore"
+    }
+    mbstats_out = mbstats_in[output_colums.keys()].copy()
+    mbstats_out.rename(columns=output_colums, inplace=True)
+    if search_tool == 'mmseqs':
+        mbstats_out['search_tool'] = 'MMseqs2'
+    elif search_tool == 'blast':
+        mbstats_out['search_tool'] = 'BLAST'
+    else:
+        raise ValueError(f"The provided search tool name {search_tool}"
+                          " is not recognized.")
+    return mbstats_out
+
+
+def load_barrnap_gtf(gtf_path):
+    gtf_columns = ["seqname", "source", "feature", "start", "end", "score",
+                   "strand", "frame", "attribute"]
+    barstats = pd.read_csv(gtf_path, delimiter="\t", names=gtf_columns)
+    return barstats
+
+
+# TODO Look into how you type hint the headers
+# TODO handel empty files
+def filter_fasta_from_headers(in_fasta_path:str, out_fasta_path:str, headers):
+    """
+
+    Filter a fast to a list of headers
+
+    :param in_fasta_path: Path to unfilterd fasta
+    :param out_fasta_path: Path to filtered fast
+    :param headers: Headers to filter by
+    """
+    headers = set(headers)
+    write_fa((seq for seq in read_fa(in_fasta_path, format='fasta')
+              if seq.metadata['id'] in headers),
+             'fasta', out_fasta_path)
+
