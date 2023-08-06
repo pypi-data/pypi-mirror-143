d4c4f70cd6815bb9583a563837c3b3dd1f9325d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:31 +0000

    Add package ipyslides/1.4.5/ipyslides-1.4.5-py3-none-any.whl

---
+++ packages/ipyslides/ipyslides-1.4.5-py3-none-any.whl/ipyslides/__init__.py
@@ -0,0 +1,11 @@
+from .__version__ import __version__
+__version__ = __version__
+
+from .core import LiveSlides
+
+__all__ = ['LiveSlides']
+      
+        
+if __name__ == '__main__':
+    print('Use this package in Jupyter notebook!')
+    
