34d6bcb7330e40207e05ee32cbae5a0be511e8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package basedmypy/1.3.0b2/basedmypy-1.3.0b2-py3-none-any.whl

---
+++ packages/basedmypy/basedmypy-1.3.0b2-py3-none-any.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "0.941"
+based_version_info = VersionInfo(major=1, minor=3, patch=0, release_level='beta', iteration=2, mypy_version='0.941', mypy_release_level='final')
+__based_version__ = "1.3.0b2"
