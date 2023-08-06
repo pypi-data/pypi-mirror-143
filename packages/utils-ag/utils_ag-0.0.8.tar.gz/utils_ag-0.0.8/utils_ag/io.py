ba4bd53fdb7e4465d8cb3e827189190975ea0024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package utils-ag/0.0.8/utils_ag-0.0.8.tar.gz

---
+++ packages/utils-ag/utils_ag-0.0.8.tar.gz/utils_ag-0.0.8/utils_ag/io.py
@@ -0,0 +1,8 @@
+
+def select_from_list(options):
+    from pyfzf.pyfzf import FzfPrompt
+    fzf = FzfPrompt()
+    answer = fzf.prompt(options, fzf_options="--cycle --layout reverse")
+    if len(answer) == 0:
+        return None
+    return answer[0]
