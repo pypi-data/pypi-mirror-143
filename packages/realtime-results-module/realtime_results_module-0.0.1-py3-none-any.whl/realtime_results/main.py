ab23fc6787ed56f51fc20ddb70075f2d5d2ca380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:28 +0000

    Add package realtime-results-module/0.0.1/realtime_results_module-0.0.1-py3-none-any.whl

---
+++ packages/realtime-results-module/realtime_results_module-0.0.1-py3-none-any.whl/realtime_results/main.py
@@ -0,0 +1,12 @@
+
+
+class RealtimeResults:
+    """This class will be as entry point"""
+
+    def __init__(self):
+        pass
+
+    def foo(self):
+        """Fix me
+        :return:
+        """
