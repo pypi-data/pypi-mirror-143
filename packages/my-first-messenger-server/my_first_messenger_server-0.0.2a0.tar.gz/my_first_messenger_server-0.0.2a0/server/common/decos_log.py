77d65c826a91798bb818fec2fbc2fef5b129d9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:56 +0000

    Add package my-first-messenger-server/0.0.2a0/my_first_messenger_server-0.0.2a0.tar.gz

---
+++ packages/my-first-messenger-server/my_first_messenger_server-0.0.2a0.tar.gz/my_first_messenger_server-0.0.2a0/server/common/decos_log.py
@@ -0,0 +1,28 @@
+import sys
+import logging
+sys.path.append('../')
+import logs.client_log_config
+import logs.server_log_config
+
+if sys.argv[0].find('client') == -1:
+    LOGGER = logging.getLogger('server')
+else:
+    LOGGER = logging.getLogger('client')
+
+
+def log(func_to_log):
+    """
+    Декоратор, выполняющий логирование вызовов функций.
+    Сохраняет события типа debug, содержащие
+    информацию о имени вызываемой функиции, параметры с которыми
+    вызывается функция, и модуль, вызывающий функцию.
+    """
+
+    def log_saver(*args, **kwargs):
+        LOGGER.debug(
+            f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. '
+            f'Вызов из модуля {func_to_log.__module__}')
+        ret = func_to_log(*args, **kwargs)
+        return ret
+
+    return log_saver
---
+++ packages/my-first-messenger-server/my_first_messenger_server-0.0.2a0.tar.gz/my_first_messenger_server-0.0.2a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="my_first_messenger_server",
+      version="0.0.2a",
+      description="Messenger Server",
+      author="Aleksandr",
+      author_email="aleksander.17047665@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=[r'server\server_run'],
+      )
