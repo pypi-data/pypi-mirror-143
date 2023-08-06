71a9892126099aa88e9d18a24c4636a348bd6906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:32 +0000

    Add package getpathpkg/0.0.2/getpathpkg-0.0.2.tar.gz

---
+++ packages/getpathpkg/getpathpkg-0.0.2.tar.gz/getpathpkg-0.0.2/getpath/__init__.py
@@ -0,0 +1,1 @@
+from .getpath import getpath
---
+++ packages/getpathpkg/getpathpkg-0.0.2.tar.gz/getpathpkg-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'GetPath'
+LONG_DESCRIPTION = 'GetPath for Tests.'
+
+# Setting up
+setup(
+    name="getpathpkg",
+    version=VERSION,
+    author="Juan Carlos Juárez",
+    author_email="<jc.juarezgarcia@outlook.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['Python', 'CLI', 'Tool'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
