e9e12dc35ce2f4300d61a1807617263bbd8c573f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:18 +0000

    Add package sklearn-transformer-extensions/0.2.1/sklearn_transformer_extensions-0.2.1-py3-none-any.whl

---
+++ packages/sklearn-transformer-extensions/sklearn_transformer_extensions-0.2.1-py3-none-any.whl/sklearn_transformer_extensions/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.2.1'
+
+from .xyadapter import XyAdapter, XyAdapterBase, XyData
+
+__all__ = [
+    "compose",
+    "preprocessing",
+    # Non-modules:
+    "XyAdapter",
+    "XyAdapterBase",
+    "XyData",
+]
