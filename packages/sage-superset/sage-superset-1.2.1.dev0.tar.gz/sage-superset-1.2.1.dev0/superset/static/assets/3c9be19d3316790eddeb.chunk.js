f194b64f0d0a57569c26d0d2bb66dbb1c7ccf72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:41 +0000

    Add package sage-superset/1.2.1.dev0/sage-superset-1.2.1.dev0.tar.gz

---
+++ packages/sage-superset/sage-superset-1.2.1.dev0.tar.gz/sage-superset-1.2.1.dev0/superset/static/assets/3c9be19d3316790eddeb.chunk.js
@@ -0,0 +1,23 @@
+"use strict";(globalThis.webpackChunksuperset=globalThis.webpackChunksuperset||[]).push([[8174],{88174:(t,a,s)=>{s.r(a),s.d(a,{default:()=>p}),s(67294);var e=s(45697),o=s.n(e),i=s(51995),r=s(11965);const l=({className:t})=>(0,r.tZ)("div",{className:t},(0,r.tZ)("iframe",{title:t,scrolling:"no",src:"http://127.0.0.1:8081/static/link/china/index.html"}),(0,r.tZ)("video",{width:"auto",height:"auto",src:"http://127.0.0.1:8081/static/link/world.mp4",autoPlay:"autoplay",loop:"loop"},(0,r.tZ)("track",{src:"",kind:"captions",srcLang:"en",label:"english_captions"})));l.defaultProps={otherProps:{}},l.propTypes={className:o().string.isRequired,otherProps:o().objectOf(o().any)};const p=(0,i.iK)(l)`
+.superset-legacy-chart-calendar
+{
+  padding: 10
+  px;
+  position: static
+  !important;
+  overflow: auto
+  !important;
+}
+
+.superset-legacy-chart-calendar .ch-tooltip
+{
+  margin - left
+:
+  20
+  px;
+  margin - top
+:
+  5
+  px;
+}
+`}}]);
