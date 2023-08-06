4bef0de251858671d6cf99b5960d294ba933c816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:35 +0000

    Add package pylodstorage/0.1.29/pylodstorage-0.1.29.tar.gz

---
+++ packages/pylodstorage/pylodstorage-0.1.29.tar.gz/pylodstorage-0.1.29/lodstorage/version.py
@@ -0,0 +1,14 @@
+'''
+Created on 2022-03-06
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyLoDStorage
+    '''
+    name="pyLodStorage"
+    version='0.1.29'
+    date = '2020-09-10'
+    updated = '2022-03-21'
