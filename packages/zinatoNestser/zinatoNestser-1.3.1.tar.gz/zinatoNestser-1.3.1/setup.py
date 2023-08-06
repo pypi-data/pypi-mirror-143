f9f6b1dc7f605fc69272195a0d5b2fea831a25dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:14 +0000

    Add package zinatoNestser/1.3.1/zinatoNestser-1.3.1.tar.gz

---
+++ packages/zinatoNestser/zinatoNestser-1.3.1.tar.gz/zinatoNestser-1.3.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+	name = 'zinatoNestser',
+	version = '1.3.1',
+	py_module = ['zinatoNestser'],
+	author = 'zinato',
+	author_email = "zzz@ndfd.com",
+	url = "http://wwww.headfirst.com",
+	description = "A simple printer of nested lists-TEST"
+
+	)
