59b0a28b2086db1abeffe70e3b20745a93b9efb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:17 +0000

    Add package algoritmika-quiz-package/0.0.2/algoritmika_quiz_package-0.0.2.tar.gz

---
+++ packages/algoritmika-quiz-package/algoritmika_quiz_package-0.0.2.tar.gz/algoritmika_quiz_package-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='algoritmika_quiz_package',
+    version='0.0.2',
+    author='Buynichenko Mikhail',
+    description="My unique package with unique functionality, which wasn't on https://pypi.org/",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    py_modules=['algoritmika_quiz_package'],
+    install_requires=[
+    ]
+)
