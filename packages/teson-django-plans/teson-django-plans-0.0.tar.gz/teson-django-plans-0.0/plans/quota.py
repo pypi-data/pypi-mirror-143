a19ed5f4a55d61fcd6adcdf957659c2c60116d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:27 +0000

    Add package teson-django-plans/0.0/teson-django-plans-0.0.tar.gz

---
+++ packages/teson-django-plans/teson-django-plans-0.0.tar.gz/teson-django-plans-0.0/plans/quota.py
@@ -0,0 +1,8 @@
+def get_user_quota(user):
+    """
+    Tiny helper for getting quota dict for user
+    If user has expired plan, return default plan or None
+    """
+    from .models import Plan
+    plan = Plan.get_current_plan(user)
+    return plan.get_quota_dict()
