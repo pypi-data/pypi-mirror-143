ad826113484946dcbb87bd8ce0d817bd3cbefa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:51:21 +0000

    Add package eRepublik/0.30.0/eRepublik-0.30.0-py2.py3-none-any.whl

---
+++ packages/eRepublik/eRepublik-0.30.0-py2.py3-none-any.whl/erepublik/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for eRepublik script."""
+
+__author__ = """Eriks Karls"""
+__email__ = "eriks@72.lv"
+__version__ = "0.30.0"
+
+from erepublik.citizen import Citizen
+
+__all__ = ["Citizen", "__version__"]
