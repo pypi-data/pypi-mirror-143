3fc6158eea0c96d07576e39c5b4a6c29e827e41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package ezbee/0.1.0/ezbee-0.1.0.tar.gz

---
+++ packages/ezbee/ezbee-0.1.0.tar.gz/ezbee-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ezbee']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['icecream>=2.1.1,<3.0.0', 'install>=1.3.5,<2.0.0', 'logzero>=1.7.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ezbee',
+    'version': '0.1.0',
+    'description': 'pack_name descr ',
+    'long_description': '# ezbee\n[![pytest](https://github.com/ffreemt/ezbee/actions/workflows/routine-tests.yml/badge.svg)](https://github.com/ffreemt/ezbee/actions)[![python](https://img.shields.io/static/v1?label=python+&message=3.7%2B&color=blue)](https://www.python.org/downloads/)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/ezbee.svg)](https://badge.fury.io/py/ezbee)\n\nezbee descr\n\n## Install it\n\n```shell\npip install git+https://github.com/ffreemt/ezbee\n# poetry add git+https://github.com/ffreemt/ezbee\n# git clone https://github.com/ffreemt/ezbee && cd ezbee\n```\n\n## Use it\n```python\nfrom ezbee import ezbee\n\n```\n',
+    'author': 'ffreemt',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ffreemt/ezbee',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
