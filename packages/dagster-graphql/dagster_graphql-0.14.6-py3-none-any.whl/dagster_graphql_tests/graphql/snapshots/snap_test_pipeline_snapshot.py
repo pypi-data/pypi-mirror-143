df8ccbb119ed926672e56e9924c16a613cfe95e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package dagster-graphql/0.14.6/dagster_graphql-0.14.6-py3-none-any.whl

---
+++ packages/dagster-graphql/dagster_graphql-0.14.6-py3-none-any.whl/dagster_graphql_tests/graphql/snapshots/snap_test_pipeline_snapshot.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_fetch_snapshot_or_error_by_active_pipeline_name_not_found 1'] = '''{
+  "pipelineSnapshotOrError": {
+    "__typename": "PipelineNotFoundError"
+  }
+}'''
+
+snapshots['test_fetch_snapshot_or_error_by_active_pipeline_name_success 1'] = '''{
+  "pipelineSnapshotOrError": {
+    "__typename": "PipelineSnapshot",
+    "dagsterTypes": [
+      {
+        "key": "Any"
+      },
+      {
+        "key": "Bool"
+      },
+      {
+        "key": "Float"
+      },
+      {
+        "key": "Int"
+      },
+      {
+        "key": "Nothing"
+      },
+      {
+        "key": "PoorMansDataFrame"
+      },
+      {
+        "key": "String"
+      }
+    ],
+    "description": null,
+    "modes": [
+      {
+        "name": "default"
+      }
+    ],
+    "name": "csv_hello_world",
+    "pipelineSnapshotId": "49f7e87ba417c7b1511056aaecf9255df9844a81",
+    "solidHandles": [
+      {
+        "handleID": "sum_solid"
+      },
+      {
+        "handleID": "sum_sq_solid"
+      }
+    ],
+    "solids": [
+      {
+        "name": "sum_solid"
+      },
+      {
+        "name": "sum_sq_solid"
+      }
+    ],
+    "tags": []
+  }
+}'''
+
+snapshots['test_fetch_snapshot_or_error_by_snapshot_id_snapshot_not_found 1'] = '''{
+  "pipelineSnapshotOrError": {
+    "__typename": "PipelineSnapshotNotFoundError",
+    "snapshotId": "notthere"
+  }
+}'''
+
+snapshots['test_fetch_snapshot_or_error_by_snapshot_id_success 1'] = '''{
+  "pipelineSnapshotOrError": {
+    "__typename": "PipelineSnapshot",
+    "dagsterTypes": [
+      {
+        "key": "Any"
+      },
+      {
+        "key": "Bool"
+      },
+      {
+        "key": "Float"
+      },
+      {
+        "key": "Int"
+      },
+      {
+        "key": "Nothing"
+      },
+      {
+        "key": "String"
+      }
+    ],
+    "description": null,
+    "modes": [
+      {
+        "name": "default"
+      }
+    ],
+    "name": "noop_pipeline",
+    "pipelineSnapshotId": "7ffd65ba8633d4c172a7b15dfee5927bed301724",
+    "solidHandles": [
+      {
+        "handleID": "noop_solid"
+      }
+    ],
+    "solids": [
+      {
+        "name": "noop_solid"
+      }
+    ],
+    "tags": []
+  }
+}'''
