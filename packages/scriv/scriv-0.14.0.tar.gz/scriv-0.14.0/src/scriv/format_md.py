9e9df27dccd03855fb50c7d5d4cf39654e4f4c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:34 +0000

    Add package scriv/0.14.0/scriv-0.14.0.tar.gz

---
+++ packages/scriv/scriv-0.14.0.tar.gz/scriv-0.14.0/src/scriv/format_md.py
@@ -0,0 +1,81 @@
+"""Markdown text knowledge for scriv."""
+
+import re
+from typing import Optional
+
+from .format import FormatTools, SectionDict
+
+
+class MdTools(FormatTools):
+    """Specifics about how to work with Markdown."""
+
+    def parse_text(
+        self, text
+    ) -> SectionDict:  # noqa: D102 (inherited docstring)
+        sections = {}  # type: SectionDict
+        lines = text.splitlines()
+        in_comment = False
+        paragraphs = None
+
+        for line in lines:
+            line = line.rstrip()
+            if in_comment:
+                if re.search(r"-->$", line):
+                    in_comment = False
+            else:
+                if re.search(r"^\s*<!--.*-->$", line):
+                    # A one-line comment, skip it.
+                    continue
+                if re.search(r"^\s*<!--", line):
+                    in_comment = True
+                    continue
+                if re.search(r"^#+ ", line):
+                    section_title = line.split(maxsplit=1)[1]
+                    paragraphs = sections.setdefault(section_title, [])
+                    paragraphs.append("")
+                    continue
+
+                if not line:
+                    if paragraphs is not None:
+                        paragraphs.append("")
+                    continue
+
+                if paragraphs is None:
+                    paragraphs = sections.setdefault(None, [])
+                    paragraphs.append("")
+
+                paragraphs[-1] += line + "\n"
+
+        # Trim out all empty paragraphs.
+        sections = {
+            section: [par.rstrip() for par in paragraphs if par]
+            for section, paragraphs in sections.items()
+            if paragraphs
+        }
+        return sections
+
+    def format_header(
+        self, text: str, anchor: Optional[str] = None
+    ) -> str:  # noqa: D102 (inherited docstring)
+        num = int(self.config.md_header_level)
+        header = "\n"
+        if anchor:
+            header += f"<a id='{anchor}'></a>\n"
+        header += "#" * num + " " + text + "\n"
+        return header
+
+    def format_sections(
+        self, sections: SectionDict
+    ) -> str:  # noqa: D102 (inherited docstring)
+        lines = []
+        for section, paragraphs in sections.items():
+            if section:
+                lines.append("")
+                lines.append(
+                    "#" * (int(self.config.md_header_level) + 1) + " " + section
+                )
+            for paragraph in paragraphs:
+                lines.append("")
+                lines.append(paragraph)
+
+        return "\n".join(lines) + "\n"
