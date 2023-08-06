eb860770cb0e5488a600898a8b9ad26e94cac9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:38 +0000

    Add package mosaicml-cli/0.0.7a1/mosaicml-cli-0.0.7a1.tar.gz

---
+++ packages/mosaicml-cli/mosaicml-cli-0.0.7a1.tar.gz/mosaicml-cli-0.0.7a1/mcli/version.py
@@ -0,0 +1,6 @@
+""" MCLI Versioning """
+__version_major__ = 0
+__version_minor__ = 0
+__version_patch__ = 7
+__version_extras__ = 'a1'
+__version__ = f'v{__version_major__}.{__version_minor__}.{__version_patch__}{__version_extras__}'
