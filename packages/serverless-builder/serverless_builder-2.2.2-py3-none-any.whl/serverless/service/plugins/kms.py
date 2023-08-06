9f012847f334ec42a99ee43b5dff06fc2b20ac1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:46 +0000

    Add package serverless-builder/2.2.2/serverless_builder-2.2.2-py3-none-any.whl

---
+++ packages/serverless-builder/serverless_builder-2.2.2-py3-none-any.whl/serverless/service/plugins/kms.py
@@ -0,0 +1,28 @@
+from serverless.aws.resources.kms import EncryptableResource
+from serverless.service.plugins.generic import Generic
+from serverless.service.plugins.iam_roles import IAMRoles
+from troposphere import Ref
+
+
+class KMSGrant(Generic, EncryptableResource):
+    def __init__(self, **kwds):
+        super().__init__("serverless-kms-grants")
+
+    def enable(self, service):
+        super().enable(service)
+        service.custom.kmsGrants = []
+
+    def pre_render(self, service):
+        if service.plugins.get(IAMRoles):
+            for fn in service.functions.all():
+                service.custom["kmsGrants"].append(
+                    dict(
+                        kmsKeyId="alias/${self:service}-${sls:stage}",
+                        roleName=fn.iam.role,
+                    )
+                )
+
+        else:
+            service.custom["kmsGrants"].append(
+                dict(kmsKeyId="alias/${self:service}-${sls:stage}", roleName=service.provider.iam.role)
+            )
