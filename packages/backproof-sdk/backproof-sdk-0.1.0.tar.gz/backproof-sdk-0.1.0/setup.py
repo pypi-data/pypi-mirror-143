c202095fcce68d4af227c709bea269d2abbf6e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:29 +0000

    Add package backproof-sdk/0.1.0/backproof-sdk-0.1.0.tar.gz

---
+++ packages/backproof-sdk/backproof-sdk-0.1.0.tar.gz/backproof-sdk-0.1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="backproof-sdk",
+    version="0.1.0",
+    author="Thomas Nicollet <thomas.nicollet@nebulis.io>",
+    description="Python SDK for Backproof",
+    packages=["backproof_sdk"]
+)
