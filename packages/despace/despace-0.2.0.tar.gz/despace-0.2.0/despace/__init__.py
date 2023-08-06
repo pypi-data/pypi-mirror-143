4f18d28d05a7500d8b4599a9b67404385f5e6dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:33 +0000

    Add package despace/0.2.0/despace-0.2.0.tar.gz

---
+++ packages/despace/despace-0.2.0.tar.gz/despace-0.2.0/despace/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Intialization of the package
+#
+# ----------------------------
+
+from despace.spatial_sort import SortND
+from .version import __version__
+
+__all__ = ['SortND', __version__]
