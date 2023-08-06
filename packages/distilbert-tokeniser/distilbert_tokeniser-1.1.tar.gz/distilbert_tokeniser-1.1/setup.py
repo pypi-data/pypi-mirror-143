7df5dd998200d477d11877ad64d7ce2fe3d9114a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:22 +0000

    Add package distilbert-tokeniser/1.1/distilbert_tokeniser-1.1.tar.gz

---
+++ packages/distilbert-tokeniser/distilbert_tokeniser-1.1.tar.gz/distilbert_tokeniser-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='distilbert_tokeniser',
+    version='1.1',
+    license='',
+    author="",
+    platforms=["any"],
+    author_email='email@example.com',
+    url='',
+    packages=find_packages(exclude=["test"]),
+    keywords='distilbert_tokeniser',
+    install_requires=[ ],
+
+)
