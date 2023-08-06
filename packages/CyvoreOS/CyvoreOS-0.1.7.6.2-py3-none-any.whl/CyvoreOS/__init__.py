9df176ff3630f7c21b6537b40491fec75d93d1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:14 +0000

    Add package CyvoreOS/0.1.7.6.2/CyvoreOS-0.1.7.6.2-py3-none-any.whl

---
+++ packages/CyvoreOS/CyvoreOS-0.1.7.6.2-py3-none-any.whl/CyvoreOS/__init__.py
@@ -0,0 +1,6 @@
+from CyvoreOS.Plugins.virusTotal_plugin import run_check
+from CyvoreOS.Plugins.abuseIPDB_plugin import run_check
+from CyvoreOS.Plugins.whois_plugin import run_check
+from CyvoreOS.Plugins.cryptoWalletValidator_plugin import run_check
+from CyvoreOS import checkObject
+from CyvoreOS.Scan import scanUrl, scan_one_string
