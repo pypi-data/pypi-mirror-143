56d01c877cbd61b081f2873d40b6583d60a97651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:32 +0000

    Add package regression-model-template/0.0.1/regression-model-template-0.0.1.tar.gz

---
+++ packages/regression-model-template/regression-model-template-0.0.1.tar.gz/regression-model-template-0.0.1/regression_model/predict.py
@@ -0,0 +1,35 @@
+import typing as t
+
+import numpy as np
+import pandas as pd
+
+from regression_model import __version__ as _version
+from regression_model.config.core import config
+from regression_model.processing.data_manager import load_pipeline
+from regression_model.processing.validation import validate_inputs
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
+_price_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(*, input_data: t.Union[pd.DataFrame, dict]) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    data = pd.DataFrame(input_data)
+    predictions, errors = None, None
+
+    try:
+        validated_data = validate_inputs(input_data=data)
+        X = validated_data[config.model_config.features]  # type: ignore
+        predictions = [np.exp(y) for y in _price_pipe.predict(X=X)]
+
+    except Exception as e:
+        errors = e
+
+    results = {
+        "predictions": predictions,
+        "version": _version,
+        "errors": errors,
+    }
+
+    return results
---
+++ packages/regression-model-template/regression-model-template-0.0.1.tar.gz/regression-model-template-0.0.1/regression_model/processing/validation.py
@@ -0,0 +1,40 @@
+from typing import Optional, Tuple
+
+import pandas as pd
+
+from regression_model.config.core import config
+from regression_model.processing.schemas import HouseDataInputSchema
+
+
+def drop_na_inputs(*, input_data: pd.DataFrame) -> pd.DataFrame:
+    """Check model inputs for na values and filter."""
+
+    # Columns in train data with missing values
+    train_vars_with_na = (
+        config.model_config.categorical_vars_with_na_frequent
+        + config.model_config.categorical_vars_with_na_missing
+        + config.model_config.numerical_vars_with_na
+    )
+
+    # At least one example in column var is missing
+    new_vars_with_na = [
+        var
+        for var in config.model_config.features
+        if var not in train_vars_with_na and input_data[var].isnull().sum() > 0
+    ]
+
+    # Drop rows
+    return input_data.dropna(axis=0, subset=new_vars_with_na)
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    """Check model inputs for unprocessable values."""
+
+    selected_features = config.model_config.features
+    input_data = input_data.rename(columns=config.model_config.variables_to_rename)
+    input_data = input_data[selected_features].copy()
+    input_data = drop_na_inputs(input_data=input_data)
+
+    schema = HouseDataInputSchema.select_columns(selected_features)
+    validated_data = schema.validate(input_data)
+    return validated_data
