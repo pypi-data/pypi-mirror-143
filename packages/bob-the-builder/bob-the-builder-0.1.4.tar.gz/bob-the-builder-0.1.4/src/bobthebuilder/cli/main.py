c05e6c49969a6781ba2a09991b5e93d7633f01e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:51 +0000

    Add package bob-the-builder/0.1.4/bob-the-builder-0.1.4.tar.gz

---
+++ packages/bob-the-builder/bob-the-builder-0.1.4.tar.gz/bob-the-builder-0.1.4/src/bobthebuilder/cli/main.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+
+from bobthebuilder.builder import BobBuilder
+
+
+def build_cli():
+    args = argparse.ArgumentParser(description="Сборщик проектов на defold")
+    args.add_argument('-p', choices=['js-web'], dest='platform', help='Build platform.', required=True)
+    args.add_argument('-a', choices=['js-web', 'js-web,wasm-web'], dest='arch', help='Build Architecture', required=True)
+    args.add_argument('-d', help='Game project directory')
+    args.add_argument('-o', help='Compiled artifact directory', dest='out_dir', required=True)
+    args.add_argument('--variant', help="Bob build variant", choices=['debug', 'release'], default='release')
+    parsed = args.parse_args()
+    project_dir = parsed.d if parsed.d else os.getcwd()
+    bob = BobBuilder(project_dir)
+    bob.resolve_deps()
+    bob.build(parsed.platform, parsed.arch)
+    bob.bundle(parsed.out_dir, platform=parsed.platform, arch=parsed.arch, variant=parsed.variant)
