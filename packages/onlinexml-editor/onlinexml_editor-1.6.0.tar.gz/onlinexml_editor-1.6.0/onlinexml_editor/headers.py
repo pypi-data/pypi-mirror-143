8472b10ad5fefb3fc03fb96e815a9b9283b5ecb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:22 +0000

    Add package onlinexml-editor/1.6.0/onlinexml_editor-1.6.0.tar.gz

---
+++ packages/onlinexml-editor/onlinexml_editor-1.6.0.tar.gz/onlinexml_editor-1.6.0/onlinexml_editor/headers.py
@@ -0,0 +1,5 @@
+# Created by matveyev at 23.02.2021
+
+device_headers = ('name', 'active', 'device', 'tag', 'sardananame', 'type', 'module', 'control', 'hostname')
+online_headers = ('name', 'active', 'device', 'tag', 'sardananame', 'comment')
+possible_headers = []
