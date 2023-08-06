f561e356f5ec8ea268c376aa9b7df4242904f5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:08 +0000

    Add package dagster-mysql/0.14.6rc0/dagster_mysql-0.14.6rc0-py3-none-any.whl

---
+++ packages/dagster-mysql/dagster_mysql-0.14.6rc0-py3-none-any.whl/dagster_mysql_tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from dagster.utils import file_relative_path
+from dagster.utils.test.mysql_instance import TestMySQLInstance
+
+
+@pytest.fixture(scope="session")
+def hostname(conn_string):  # pylint: disable=redefined-outer-name, unused-argument
+    return TestMySQLInstance.get_hostname()
+
+
+@pytest.fixture(scope="session")
+def conn_string():  # pylint: disable=redefined-outer-name, unused-argument
+    with TestMySQLInstance.docker_service_up_or_skip(
+        file_relative_path(__file__, "docker-compose.yml"), "test-mysql-db"
+    ) as conn_str:
+        yield conn_str
