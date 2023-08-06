3331c0d2f1cb2776b76169b9cbcecd9648c8f0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:38 +0000

    Add package AppZoo/2022.3.23.16.27.57/appzoo-2022.3.23.16.27.57-py3-none-any.whl

---
+++ packages/AppZoo/appzoo-2022.3.23.16.27.57-py3-none-any.whl/appzoo/测试多个apps/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : nesc.
+# @File         : demo.py
+# @Time         : 2021/11/11 下午3:20
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  :
+
+from appzoo import App
+from meutils.pipe import *
+
+app = App(False)
+app_ = app.app
+
+app.add_apps()
+
+
+#########################个性化#########################
+@app.get('/xx')
+def func():
+    return 'OK'
+
+
+print('xxx')
+
+#########################个性化#########################
+
+
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=8501)
+
+    import uvicorn
+
+    uvicorn.config.LOGGING_CONFIG['formatters']['access'][
+        'fmt'] = '%(asctime)s - %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
+
+    # debug
+    app.run(f"{app.app_file_name(__file__)}:app_", port=9000, debug= not is_open())
