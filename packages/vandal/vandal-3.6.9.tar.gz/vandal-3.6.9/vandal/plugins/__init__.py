e9a820361cde8c4231ec05df795e27b09593778a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:24 +0000

    Add package vandal/3.6.9/vandal-3.6.9.tar.gz

---
+++ packages/vandal/vandal-3.6.9.tar.gz/vandal-3.6.9/vandal/plugins/__init__.py
@@ -0,0 +1,27 @@
+# imports relevant contents.
+from vandal.plugins.metaclass import Meta
+from vandal.plugins.types import (
+    Vector,
+    StringDictionary,
+    NumberDictionary,
+    OrderedDictionary,
+    NumType,
+)
+
+# all relevant contents.
+__all__ = [
+    Vector,
+    StringDictionary,
+    NumberDictionary,
+    OrderedDictionary,
+    NumType,
+]
+
+# available types.
+__types__ = [
+    Vector,
+    StringDictionary,
+    NumberDictionary,
+    OrderedDictionary,
+    NumType,
+]
---
+++ packages/vandal/vandal-3.6.9.tar.gz/vandal-3.6.9/vandal/plugins/types.py
@@ -0,0 +1,7 @@
+from typing import Dict, List, Union
+
+Vector = List[float]
+StringDictionary = Dict[str, str]
+NumberDictionary = Dict[str, float]
+OrderedDictionary = Dict[int, str]
+NumType = Union[Vector, NumberDictionary, int, float, list]
