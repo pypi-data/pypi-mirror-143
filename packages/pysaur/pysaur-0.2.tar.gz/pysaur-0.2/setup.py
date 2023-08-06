e8653264d9807520755cf1b1fd8f8e1c0ef95de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:41 +0000

    Add package pysaur/0.2/pysaur-0.2.tar.gz

---
+++ packages/pysaur/pysaur-0.2.tar.gz/pysaur-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pysaur',
+      version='0.2',
+      description='pysaur module',
+      packages=['pysaur'],
+      author_email='oleksiy.saurin@gmail.com',
+      zip_safe=False)
