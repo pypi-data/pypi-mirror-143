d37d0980ecfddeaa167adfdc85f36e6cfcd01562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:25 +0000

    Add package selenium-shortcuts/1.2/selenium_shortcuts-1.2.tar.gz

---
+++ packages/selenium-shortcuts/selenium_shortcuts-1.2.tar.gz/selenium_shortcuts-1.2/selenium_shortcuts/__init__.py
@@ -0,0 +1,38 @@
+class setup:
+    ''' '''
+    def __init__(self, driver):
+        self.driver = driver
+        
+    def find(self, css_selector, attribute=None, parent=None):
+        if not parent:
+            parent = self.driver
+        element = parent.find_element_by_css_selector(css_selector)
+        if attribute:
+            attribute = element.get_attribute(attribute)
+            return element, attribute
+        return element
+
+    def finds(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
+        elements = parent.find_elements_by_css_selector(css_selector)
+        return elements
+
+    def click(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
+        element = parent.find_element_by_css_selector(css_selector)
+        element.click()
+
+    def text(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
+        element = parent.find_element_by_css_selector(css_selector)
+        return element.text
+
+    def send(self, css_selector, text, clear=True, parent=None):
+        if not parent:
+            parent = self.driver
+        element = parent.find_element_by_css_selector(css_selector)
+        element.clear()
+        element.send_keys(text)
