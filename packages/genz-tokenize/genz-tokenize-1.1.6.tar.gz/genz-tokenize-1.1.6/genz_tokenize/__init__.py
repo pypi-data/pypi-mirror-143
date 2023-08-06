789a08e4777374ebddb4fb92133d504fe262858e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:17 +0000

    Add package genz-tokenize/1.1.6/genz-tokenize-1.1.6.tar.gz

---
+++ packages/genz-tokenize/genz-tokenize-1.1.6.tar.gz/genz-tokenize-1.1.6/genz_tokenize/__init__.py
@@ -0,0 +1,6 @@
+from genz_tokenize.tokenize import Tokenize
+from genz_tokenize.tokenize import TokenizeForBert
+from genz_tokenize.tokenize import get_embedding_matrix
+from genz_tokenize import preprocess
+import genz_tokenize.models
+# from genz_tokenize.models import *
