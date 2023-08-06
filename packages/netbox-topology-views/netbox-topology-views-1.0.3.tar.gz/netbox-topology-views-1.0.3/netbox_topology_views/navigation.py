256a26afe11336d9f8eb0cf6fe0a13adb96b7f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:19 +0000

    Add package netbox-topology-views/1.0.3/netbox-topology-views-1.0.3.tar.gz

---
+++ packages/netbox-topology-views/netbox-topology-views-1.0.3.tar.gz/netbox-topology-views-1.0.3/netbox_topology_views/navigation.py
@@ -0,0 +1,17 @@
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_topology_views:home',
+        link_text='Topology',
+        buttons=(
+               PluginMenuButton(
+                link='plugins:netbox_topology_views:home',
+                title='Topology View',
+                icon_class='mdi mdi-plus-thick',
+                permissions=[],
+            ),
+        )
+    ),
+)
