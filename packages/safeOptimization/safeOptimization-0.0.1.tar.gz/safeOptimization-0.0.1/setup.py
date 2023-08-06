ac4330c8b127343d8803319c2831df347536163b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package safeOptimization/0.0.1/safeOptimization-0.0.1.tar.gz

---
+++ packages/safeOptimization/safeOptimization-0.0.1.tar.gz/safeOptimization-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='safeOptimization',
+   version='0.0.1',
+   description='Safe Optimization algorithms',
+   author='Mohammad Amin Roohi',
+   author_email='m5a0r7@gmail.com',
+   package_dir={'': 'src'},
+   py_modules=['logBarrier']
+)
