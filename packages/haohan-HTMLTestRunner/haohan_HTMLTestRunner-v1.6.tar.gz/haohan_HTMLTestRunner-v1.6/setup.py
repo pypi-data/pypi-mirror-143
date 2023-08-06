07ae02f18d527705d8adb90a17ca01f7e6e0a849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:14 +0000

    Add package haohan-HTMLTestRunner/1.6/haohan_HTMLTestRunner-v1.6.tar.gz

---
+++ packages/haohan-HTMLTestRunner/haohan_HTMLTestRunner-v1.6.tar.gz/haohan_HTMLTestRunner-v1.6/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name = 'haohan_HTMLTestRunner',#需要打包的名字
+    version = 'v1.6',#版本
+    author = 'Javen', # 作者
+    py_modules = ['haohan_HTMLTestRunner']#需要打包的模块
+)
