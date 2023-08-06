1298b495f223e0b52c57e7a638c3ddb91f985d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:47:19 +0000

    Add package Algo-Vi/0.1.0/Algo_Vi-0.1.0.tar.gz

---
+++ packages/Algo-Vi/Algo_Vi-0.1.0.tar.gz/Algo_Vi-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="Algo_Vi",
+    version="0.1.0",
+    description="It visual the Search Algorithm",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/punithanae/Python_library",
+    author="Punithan",
+    author_email="punithanae@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    package_dir={"__main__":"Search-Vi"},
+    include_package_data=True,
+    install_requires=[],
+    keyword=['Search']
+
+)
