45c06598af541ed34eff19baf332bcced8236e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:46 +0000

    Add package mcquic/0.1.9/mcquic-0.1.9.tar.gz

---
+++ packages/mcquic/mcquic-0.1.9.tar.gz/mcquic-0.1.9/mcquic/utils/registry.py
@@ -0,0 +1,32 @@
+from typing import Callable, Tuple, Type
+
+from vlutils.base import Registry
+
+from mcquic.baseClass import ValueTuner
+
+__all__ = [
+    "ModuleRegistry",
+    "ValueTunerRegistry",
+    "LrSchedulerRegistry",
+    "OptimizerRegistry",
+    "EntrypointRegistry",
+    "LossRegistry"
+]
+
+class ModuleRegistry(Registry[Type["torch.nn.Module"]]):
+    pass
+
+class ValueTunerRegistry(Registry[Type[ValueTuner]]):
+    pass
+
+class LrSchedulerRegistry(Registry[Type["torch.optim.lr_scheduler._LRScheduler"]]):
+    pass
+
+class OptimizerRegistry(Registry[Type["torch.optim.optimizer.Optimizer"]]):
+    pass
+
+class EntrypointRegistry(Registry[Callable[[Tuple[str], ], int]]):
+    pass
+
+class LossRegistry(Registry[Type["torch.nn.Module"]]):
+    pass
