fcedc65ccec5c4fafe5157f3143d44b749878a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:16 +0000

    Add package nanian/1.0.2/nanian-1.0.2.tar.gz

---
+++ packages/nanian/nanian-1.0.2.tar.gz/nanian-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name="nanian",
+    version="1.0.2",
+    author="那年",
+    author_email="1009689491@qq.com",
+    description="This group is composed of one student that is author,黄伟松",
+)
