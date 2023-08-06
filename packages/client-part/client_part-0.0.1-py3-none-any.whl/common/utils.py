2ebe6600694994c5600a0977014358c3693460db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:46 +0000

    Add package client-part/0.0.1/client_part-0.0.1-py3-none-any.whl

---
+++ packages/client-part/client_part-0.0.1-py3-none-any.whl/common/utils.py
@@ -0,0 +1,35 @@
+import json
+import sys
+from common.variables import MAX_PACKAGE_LENGTH, FORMAT
+from common.errors import IncorrectDataRecivedError, NonDictInputError
+from common.decoration import log
+sys.path.append('../')
+
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку
+# значения
+
+
+@log
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(FORMAT)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        else:
+            raise IncorrectDataRecivedError
+    else:
+        raise IncorrectDataRecivedError
+
+
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
+@log
+def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(FORMAT)
+    sock.send(encoded_message)
