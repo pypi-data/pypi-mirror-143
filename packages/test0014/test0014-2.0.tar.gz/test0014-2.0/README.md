1282efa4d28d19ac0ec685765b7be715877bbb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:51 +0000

    Add package test0014/2.0/test0014-2.0.tar.gz

---
+++ packages/test0014/test0014-2.0.tar.gz/test0014-2.0/README.md
@@ -0,0 +1,1 @@
+This package is to test upload time.
---
+++ packages/test0014/test0014-2.0.tar.gz/test0014-2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test0014',
+    version='2.0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages(),
+    install_requires=[
+        'Abhi-pdf==1.1',
+    ]
+)
+
