73a057a782796a52eb02168fd988f90ba6811c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:53 +0000

    Add package battlenet-client/1.2.2/battlenet_client-1.2.2.tar.gz

---
+++ packages/battlenet-client/battlenet_client-1.2.2.tar.gz/battlenet_client-1.2.2/src/battlenet_client/exceptions.py
@@ -0,0 +1,29 @@
+"""Defines the exceptions for the package
+
+.. moduleauthor: David "Gahd" Couples <gahdania@gahd.io>
+"""
+
+
+class BNetError(Exception):
+    """Base Error class for BattleNet Client"""
+    pass
+
+
+class BNetRegionNotFoundError(BNetError):
+    """Error raised when an invalid region code is detected"""
+    pass
+
+
+class BNetDataNotFoundError(BNetError):
+    """Error raised when the Battle.net API returns a 404"""
+    pass
+
+
+class BNetAccessForbiddenError(BNetError):
+    """Error raised with the Battle.net API returns a 403"""
+    pass
+
+
+class BNetClientError(BNetError):
+    """Error raised if there is a mismatch with the client and API endpoints"""
+    pass
