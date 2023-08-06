153bf37e386324d877c87b435db696da788682b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:59 +0000

    Add package robolson/0.3.45/robolson-0.3.45.tar.gz

---
+++ packages/robolson/robolson-0.3.45.tar.gz/robolson-0.3.45/help.py
@@ -0,0 +1,32 @@
+import glob
+
+from pathlib import Path
+
+import toml, rich
+
+
+_EXCLUSIONS = ["__init__"]
+
+
+def main():
+
+    tom = toml.load(Path(__file__).parent / "pyproject.toml")
+    version = tom["tool"]["poetry"]["version"]
+    package_name = tom["tool"]["poetry"]["name"]
+
+    rich.print(
+        f"[red]Rob's Utility Scripts[/red]\n[green]Version: {version}[/green]\n\nAvailable scripts (pass -h for CLI usage):"
+    )
+    print(
+        "\n".join(
+            [
+                "rob." + Path(elem).stem
+                for elem in glob.glob(f"{Path(__file__).parent}/*.py")
+                if Path(elem).stem not in _EXCLUSIONS
+            ]
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
