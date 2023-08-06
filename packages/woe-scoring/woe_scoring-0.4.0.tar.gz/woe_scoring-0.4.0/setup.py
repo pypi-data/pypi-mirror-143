f0f9883daeea65ed0a9af57659af0c1f5e70a2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:51:17 +0000

    Add package woe-scoring/0.4.0/woe_scoring-0.4.0.tar.gz

---
+++ packages/woe-scoring/woe_scoring-0.4.0.tar.gz/woe_scoring-0.4.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['woe_scoring',
+ 'woe_scoring.core',
+ 'woe_scoring.core.binning',
+ 'woe_scoring.core.model']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lxml>=4.8.0',
+ 'numpy>=1.19.5',
+ 'pandas>=1.2.2',
+ 'scikit-learn>=0.24.1',
+ 'scipy>=1.6.1',
+ 'statsmodels>=0.12.2']
+
+setup_kwargs = {
+    'name': 'woe-scoring',
+    'version': '0.4.0',
+    'description': 'Weight Of Evidence Transformer and LogisticRegression model with scikit-learn API',
+    'long_description': None,
+    'author': 'Stroganov Kirill',
+    'author_email': 'kiraplenkin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
