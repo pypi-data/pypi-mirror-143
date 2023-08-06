1effa13185e6446b321145b1d18b8770672b6ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:39 +0000

    Add package paper-uploads/0.8.2/paper-uploads-0.8.2.tar.gz

---
+++ packages/paper-uploads/paper-uploads-0.8.2.tar.gz/paper-uploads-0.8.2/paper_uploads/admin/__init__.py
@@ -0,0 +1,4 @@
+from .base import ResourceAdminBase  # noqa: F401
+from .collection import CollectionAdminBase  # noqa: F401
+from .file import UploadedFileAdmin  # noqa: F401
+from .image import UploadedImageAdmin  # noqa: F401
