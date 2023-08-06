a880a2fc861bc1c8842a5b1209fabf6a3d23b479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:34 +0000

    Add package num2rank/1.0.1/num2rank-1.0.1.tar.gz

---
+++ packages/num2rank/num2rank-1.0.1.tar.gz/num2rank-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Converts integers (1, 2, 3) into ranks (1st, 2nd, 3rd)'
+LONG_DESCRIPTION = "Description:\n\nA package that allows the conversion between integers, for example 1, 2 or 3, to ranks, for example 1st, 2nd or 3rd.\n\nExample Usage:\n\n `print(num2rank(5))`\n\n"
+
+# Setting up
+setup(
+    name="num2rank",
+    version=VERSION,
+    author="AnnoyingRains",
+    author_email="<annoyingrain5@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', "number", "1st", "2nd", "3rd", "1 to 1st", "number converter", "number suffix converter", "number suffix"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
