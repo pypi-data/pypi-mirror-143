8ce61ac981204e78a21a4f84f44e3e616b2102e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:46 +0000

    Add package wagtail-localize-rws-languagecloud/0.7.2/wagtail-localize-rws-languagecloud-0.7.2.tar.gz

---
+++ packages/wagtail-localize-rws-languagecloud/wagtail-localize-rws-languagecloud-0.7.2.tar.gz/wagtail-localize-rws-languagecloud-0.7.2/wagtail_localize_rws_languagecloud/__init__.py
@@ -0,0 +1,7 @@
+default_app_config = (
+    "wagtail_localize_rws_languagecloud.apps.WagtailLocalizeRwsLanguageCloudAppConfig"
+)
+
+
+VERSION = (0, 7, 2)
+__version__ = ".".join(map(str, VERSION))
---
+++ packages/wagtail-localize-rws-languagecloud/wagtail-localize-rws-languagecloud-0.7.2.tar.gz/wagtail-localize-rws-languagecloud-0.7.2/wagtail_localize_rws_languagecloud/test/local_settings.py
@@ -0,0 +1,3 @@
+from .settings import DATABASES
+
+DATABASES["default"]["TEST"] = {"NAME": "test.db"}
