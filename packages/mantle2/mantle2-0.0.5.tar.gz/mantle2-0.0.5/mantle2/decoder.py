5fef7957efc0671bd3d40549d309a88832dd5fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package mantle2/0.0.5/mantle2-0.0.5.tar.gz

---
+++ packages/mantle2/mantle2-0.0.5.tar.gz/mantle2-0.0.5/mantle2/decoder.py
@@ -0,0 +1,11 @@
+import magma as m
+
+
+class Decoder(m.Generator2):
+    """
+    Input `n` bits
+    Output `2**n` bits one-hot encoding the integer value of `n`
+    """
+    def __init__(self, n: int):
+        self.io = m.IO(I=m.In(m.Bits[n]), O=m.Out(m.Bits[2**n]))
+        self.io.O @= m.bits(1, 2**n) << m.zext_to(self.io.I, 2**n)
