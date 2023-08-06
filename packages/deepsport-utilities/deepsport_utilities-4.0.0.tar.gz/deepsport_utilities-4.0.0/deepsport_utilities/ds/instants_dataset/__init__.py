5c53fc5d72d4630daabc1af7217bf5f15a827640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:21 +0000

    Add package deepsport-utilities/4.0.0/deepsport_utilities-4.0.0.tar.gz

---
+++ packages/deepsport-utilities/deepsport_utilities-4.0.0.tar.gz/deepsport_utilities-4.0.0/deepsport_utilities/ds/instants_dataset/__init__.py
@@ -0,0 +1,13 @@
+from .instants_dataset import InstantsDataset, Instant, InstantKey, DownloadFlags, PlayerAnnotation, BallAnnotation
+from .instants_transforms import GammaCorrectionTransform
+from .views_dataset import ViewsDataset, ViewKey, View, BuildBallViews, BuildCameraViews, \
+    BuildHeadsViews, BuildCourtViews, BuildPlayersViews, BuildThumbnailViews
+from .views_transforms import AddBallAnnotation, UndistortTransform, RectifyTransform, \
+    RectifyUndistortTransform, ComputeDiff, GameGammaColorTransform, GameRGBColorTransform, \
+    BayeringTransform, ViewRandomCropperTransform, AddCalibFactory, AddCourtFactory, AddDiffFactory
+from .dataset_splitters import DeepSportDatasetSplitter, KFoldsArenaLabelsTestingDatasetSplitter, TestingArenaLabelsDatasetSplitter
+
+try:
+    from .views_transforms import AddBallDistance
+except ImportError:
+    pass
