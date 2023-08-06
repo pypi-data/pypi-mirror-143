d739f40428a86bedcc28ff60e5df2fcc3c5d8fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:28 +0000

    Add package Farsa/0.1/Farsa-0.1.tar.gz

---
+++ packages/Farsa/Farsa-0.1.tar.gz/Farsa-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Farsa',
+    version='0.1',
+    packages=['farsa'],
+    install_requires=['pefile~=2021.9.3', ],
+    url='https://github.com/nyaoouo/Farsa',
+    license='GPLv3',
+    author='Nyaoouo',
+    author_email='x@y.z',
+)
