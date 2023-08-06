7ec58fbb69c8bda4ae750224401e3cf8921230de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:59 +0000

    Add package IMAPY-music/0.1.1a0/IMAPY-music-0.1.1a0.tar.gz

---
+++ packages/IMAPY-music/IMAPY-music-0.1.1a0.tar.gz/IMAPY-music-0.1.1a0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='IMAPY-music',
+    version='0.1.1-alpha',
+    packages=['imapy_music',],
+
+    long_description=open('README.md').read(),
+
+    url='https://github.com/NadiaCarvalho/IMAPY',
+    author='Nádia Carvalho',
+    author_email='nadiacarvalho118@gmail.com',
+    license='GNU Lesser General Public License v3.0',
+)
