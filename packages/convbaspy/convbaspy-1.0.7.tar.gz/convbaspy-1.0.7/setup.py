2b3cf1a0d7c627fb77c2ae7069b0dcd022e088d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:06 +0000

    Add package convbaspy/1.0.7/convbaspy-1.0.7.tar.gz

---
+++ packages/convbaspy/convbaspy-1.0.7.tar.gz/convbaspy-1.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r', encoding='utf-8') as fh:
+    long_description= fh.read()
+
+setup(
+    name='convbaspy',
+    version='1.0.7',
+    license='MIT',
+    author="D Penilla",
+    author_email='dpenillac@correo.usbcali.edu.co',
+    packages=find_packages('src'),
+    description='Convertidor de Bases IEEE754',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    url='https://github.com/etensor/baseconvpy',
+    keywords='convertidor bases converter hex oct bin ieee754 32bit 64bit',
+    install_requires=[
+          'rich',
+          'textual',
+          'pyfiglet',
+          'pyperclip'
+      ],
+    entry_ponts={
+        'console_scripts': [
+            'convbaspy=convbaspy:runApp'
+        ]
+    }
+
+)
