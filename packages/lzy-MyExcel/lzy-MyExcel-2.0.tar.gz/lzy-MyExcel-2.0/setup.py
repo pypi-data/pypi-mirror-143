5276d12b1508e9753df2123efebcc43b699e9bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:01 +0000

    Add package lzy-MyExcel/2.0/lzy-MyExcel-2.0.tar.gz

---
+++ packages/lzy-MyExcel/lzy-MyExcel-2.0.tar.gz/lzy-MyExcel-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='lzy-MyExcel',
+      version='2.0',
+      author_email='mvli@qq.com',
+      py_modules=['MyExcel']
+      )
+
+# python setup.py sdist
+# twine upload dist/*
