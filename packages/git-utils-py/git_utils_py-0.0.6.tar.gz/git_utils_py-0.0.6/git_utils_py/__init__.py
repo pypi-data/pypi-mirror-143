678737cf29be7eee79ab47826ea803a46e3f8151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:41 +0000

    Add package git-utils-py/0.0.6/git_utils_py-0.0.6.tar.gz

---
+++ packages/git-utils-py/git_utils_py-0.0.6.tar.gz/git_utils_py-0.0.6/git_utils_py/__init__.py
@@ -0,0 +1,2 @@
+from .File import download_file
+from .Artifacts import download_artifacts
---
+++ packages/git-utils-py/git_utils_py-0.0.6.tar.gz/git_utils_py-0.0.6/setup.py
@@ -0,0 +1,51 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+with open(HERE / "README.rst", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+VERSION = '0.0.6'
+PACKAGE_NAME = 'git_utils_py'
+AUTHOR = 'Juan José Macanás Beteta'
+AUTHOR_EMAIL = 'juanjo.macanas@gmail.com'
+URL = 'https://bitbucket.org/informaticaljd'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Library to download files from gitlab'
+LONG_DESCRIPTION = readme
+LONG_DESC_TYPE = "text/x-rst"
+
+PYTHON_VERSION = ">=3.7.0"
+
+INSTALL_REQUIRES = [
+    'python-gitlab'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=PYTHON_VERSION,
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
