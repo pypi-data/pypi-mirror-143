8bb8080ed411228d52e38640d140c9e631be1c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:54 +0000

    Add package medaimodels/0.0.7/medaimodels-0.0.7.tar.gz

---
+++ packages/medaimodels/medaimodels-0.0.7.tar.gz/medaimodels-0.0.7/medaimodels/__init__.py
@@ -0,0 +1,60 @@
+from typing import List
+import uuid
+import skimage
+
+
+__version__ = "0.1.0"
+
+class ModelOutput():
+    """Output for a single study.
+    Args:
+        image (str): A path to dicom images.
+        class_probabilities (dict): Dictionary of key value pairs of diagnoses and probability that this diagnosis matches the image.
+        mask (str): A path to a numpy mask.
+        display (str): A summary of the outpuer
+
+    Attributes:
+        image (str): A path to dicom images.
+        class_probabilities (dict): Dictionary of key value pairs of diagnoses and probability that this diagnosis matches the image.
+        mask (str): A path to a numpy mask.
+        display (str): A summary of the outpuer
+    """ 
+
+
+
+    
+    def __init__(self, image=None, class_probabilities=None, mask=None, display=''):
+        self.image = image
+        self.class_probabilities = class_probabilities
+        self.mask = mask
+        self.display = display
+
+
+    def __repr__(self):
+        """Repr
+        print function will return display attributes
+        """
+
+        return self.display
+
+
+class ModelMain():
+    """ Class for the main.py file in med ai models
+    Args:
+
+    Attributes:
+
+    """
+    model = None
+
+    def evaluate_model(self, files: List[str]) -> List[ModelOutput]:
+        pass
+
+    def load_model(self):
+        pass
+
+def save_image(image):
+    parent_path = "/opt/images/"
+    path =  '{path}.png'.format(path=uuid.uuid4())
+    skimage.io.imsave(parent_path + path, image)
+    return path
