6b1bfeda785025f54d8293a9c2036d42c53a3e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package AOMUSE/0.0.6/AOMUSE-0.0.6.tar.gz

---
+++ packages/AOMUSE/AOMUSE-0.0.6.tar.gz/AOMUSE-0.0.6/AOMUSE/__init__.py
@@ -0,0 +1,13 @@
+"""
+Init file for the AO Muse library.
+"""
+
+version_info = (0, 0, 6)
+__version__ = ".".join([str(x) for x in version_info])
+
+__all__ = [
+    "ao"
+    "db"
+    "process"
+    "utils"
+]
