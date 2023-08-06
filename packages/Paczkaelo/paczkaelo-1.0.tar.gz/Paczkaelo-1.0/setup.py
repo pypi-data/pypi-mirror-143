db003b0769bcd9bd72e24af75721d5195a0cbcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package Paczkaelo/1.0/paczkaelo-1.0.tar.gz

---
+++ packages/Paczkaelo/paczkaelo-1.0.tar.gz/Paczkaelo-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Paczkaelo',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Karolina Leszczyńska',
+      author_email='karolina.leszczynska05@gmai.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+     )
