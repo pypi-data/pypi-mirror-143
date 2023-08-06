c66ff405f2c6526bfe74121df24e1abcdfecdf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:34 +0000

    Add package iecasdmx/0.0.0/iecasdmx-0.0.0.tar.gz

---
+++ packages/iecasdmx/iecasdmx-0.0.0.tar.gz/iecasdmx-0.0.0/iecasdmx/__init__.py
@@ -0,0 +1,7 @@
+#flake8: noqa
+from iecasdmx.consulta import Consulta
+from iecasdmx.jerarquia import Jerarquia
+from iecasdmx.datos import Datos
+
+__version__ = '1.1.0'
+__author__ = 'frapercan <frapercan1@alum.us.es>'
