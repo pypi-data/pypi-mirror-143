b139bb1e146b49431fff3dde2912e139113accdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:52 +0000

    Add package pykostalpiko/0.1.0/pykostalpiko-0.1.0.tar.gz

---
+++ packages/pykostalpiko/pykostalpiko-0.1.0.tar.gz/pykostalpiko-0.1.0/pykostalpiko/__main__.py
@@ -0,0 +1,17 @@
+import sys
+
+from pykostalpiko.dxs import entries
+from pykostalpiko.dxs.entries import Entries
+from pykostalpiko.inverter import Inverter
+
+
+def main() -> None:
+    inv = Inverter(ip=sys.argv[1])
+
+    values = inv.fetch(*Entries.__members__.values())
+
+    print(values)
+
+
+if __name__ == "__main__":
+    main()
