9e720212c8b754c2fab30b6283f8b9c465dd6ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:06 +0000

    Add package hiword/0.5.3/hiword-0.5.3.tar.gz

---
+++ packages/hiword/hiword-0.5.3.tar.gz/hiword-0.5.3/hiword/__init__.py
@@ -0,0 +1,9 @@
+from hiword.extractor import KeywordsExtractor
+from hiword.extractor import extract_keywords
+
+__version__ = '0.5.3'
+
+__all__ = [
+    'KeywordsExtractor',
+    'extract_keywords',
+]
