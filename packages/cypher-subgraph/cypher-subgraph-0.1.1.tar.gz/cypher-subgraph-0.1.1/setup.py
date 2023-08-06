11ad07b7f4f58386b996d4f9e8de6b755afc84ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:17 +0000

    Add package cypher-subgraph/0.1.1/cypher-subgraph-0.1.1.tar.gz

---
+++ packages/cypher-subgraph/cypher-subgraph-0.1.1.tar.gz/cypher-subgraph-0.1.1/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+def setup() -> None:
+    with open('README.md') as readme_file:
+        readme = readme_file.read()
+
+    with open('LICENSE') as license_file:
+        license = license_file.read()
+
+    setuptools.setup(
+        name='cypher-subgraph',
+        version='0.1.1',
+        description='A python library and CLI tool that rewrites and '
+                    'generates cypher queries for supporting sub-graph.',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        author='Alireza Roshanzamir',
+        author_email='a.roshanzamir1996@gmail.com',
+        url='https://github.com/AlirezaRoshanzamir/'
+            'cypher-subgraph/tree/main/src/cypher_subgraph',
+        license=license,
+        include_package_data=True,
+        install_requires=[
+            'antlr4-python3-runtime==4.7.2',
+            'click>=8.0.4,<9'
+        ],
+        entry_points={
+            'console_scripts': [
+                'cypher-subgraph = cypher_subgraph.cli:cypher_subgraph'
+            ]
+        },
+        package_dir={'': 'src'},
+        packages=setuptools.find_packages('src'),
+        python_requires='>=3.7',
+    )
+
+
+if __name__ == '__main__':
+    setup()
---
+++ packages/cypher-subgraph/cypher-subgraph-0.1.1.tar.gz/cypher-subgraph-0.1.1/src/cypher_subgraph/cli.py
@@ -0,0 +1,56 @@
+import sys
+
+import click
+
+from cypher_subgraph.add_to_subgraph import add_to_subgraph
+from cypher_subgraph.canonize_subgraph_id import canonize_subgraph_id
+from cypher_subgraph.delete_subgraph import delete_subgraph
+from cypher_subgraph.rewrite_for_subgraph import rewrite_for_subgraph
+
+
+@click.group()
+def cypher_subgraph() -> None:
+    """Rewrite or generate query for supporting sub-graph feature."""
+    pass
+
+
+@cypher_subgraph.command(
+    short_help='Rewrite the query read from standard input that the query '
+    'returned values become the member of the specified "subgraph" argument.'
+)
+@click.argument('subgraph', type=str)
+def add_to(subgraph: str) -> None:
+    query = read_query_from_stdin()
+    canonical_subgraph_id = canonize_subgraph_id(subgraph)
+    adding_query = add_to_subgraph(query, canonical_subgraph_id)
+    click.echo(adding_query)
+
+
+@cypher_subgraph.command(
+    short_help='Rewrite the query read from standard input that the query is '
+    'executed only in the specified "subgraph" argument.'
+)
+@click.argument('subgraph', type=str)
+def rewrite_for(subgraph: str) -> None:
+    query = read_query_from_stdin()
+    canonical_subgraph_id = canonize_subgraph_id(subgraph)
+    rewritten_query = rewrite_for_subgraph(query, canonical_subgraph_id)
+    click.echo(rewritten_query)
+
+
+@cypher_subgraph.command(
+    short_help='Generate a query for deleting specfied "subgraph" argument.'
+)
+@click.argument('subgraph', type=str)
+def delete(subgraph: str) -> None:
+    canonical_subgraph_id = canonize_subgraph_id(subgraph)
+    delete_query = delete_subgraph(canonical_subgraph_id)
+    click.echo(delete_query)
+
+
+def read_query_from_stdin() -> str:
+    return ''.join(line for line in sys.stdin)
+
+
+if __name__ == '__main__':
+    cypher_subgraph()
