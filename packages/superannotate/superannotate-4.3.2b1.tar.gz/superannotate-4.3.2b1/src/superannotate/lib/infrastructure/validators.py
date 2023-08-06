f793b850247dc69e33ad2131d27188789b492384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:25 +0000

    Add package superannotate/4.3.2b1/superannotate-4.3.2b1.tar.gz

---
+++ packages/superannotate/superannotate-4.3.2b1.tar.gz/superannotate-4.3.2b1/src/superannotate/lib/infrastructure/validators.py
@@ -0,0 +1,52 @@
+import os
+from collections import defaultdict
+
+from lib.core.entities import PixelAnnotation
+from lib.core.validators import BaseValidator
+from pydantic import ValidationError
+
+
+def get_tabulation() -> int:
+    try:
+        return int(os.get_terminal_size().columns / 2)
+    except OSError:
+        return 48
+
+
+def wrap_error(e: ValidationError) -> str:
+    tabulation = get_tabulation()
+    error_messages = defaultdict(list)
+    for error in e.errors():
+        errors_list = list(error["loc"])
+        if "__root__" in errors_list:
+            errors_list.remove("__root__")
+        errors_list[1::] = [
+            f"[{i}]" if isinstance(i, int) else f".{i}" for i in errors_list[1::]
+        ]
+        error_messages["".join([str(item) for item in errors_list])].append(
+            error["msg"]
+        )
+    texts = ["\n"]
+    for field, text in error_messages.items():
+        texts.append(
+            "{} {}{}".format(
+                field,
+                " " * (tabulation - len(field)),
+                f"\n {' ' * tabulation}".join(text),
+            )
+        )
+    return "\n".join(texts)
+
+
+class BaseSchemaValidator(BaseValidator):
+    MODEL = PixelAnnotation
+
+    def is_valid(self) -> bool:
+        try:
+            self._validate()
+        except ValidationError as e:
+            self._validation_output = e
+        return not bool(self._validation_output)
+
+    def generate_report(self) -> str:
+        return wrap_error(self._validation_output)
