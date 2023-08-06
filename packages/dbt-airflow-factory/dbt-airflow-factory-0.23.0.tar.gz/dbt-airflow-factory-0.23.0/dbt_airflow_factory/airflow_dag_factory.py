668816bb13b91a531a832a1f2af69ccc1cd1dd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:12 +0000

    Add package dbt-airflow-factory/0.23.0/dbt-airflow-factory-0.23.0.tar.gz

---
+++ packages/dbt-airflow-factory/dbt-airflow-factory-0.23.0.tar.gz/dbt-airflow-factory-0.23.0/dbt_airflow_factory/airflow_dag_factory.py
@@ -0,0 +1,128 @@
+"""Factory creating Airflow DAG."""
+
+import os
+
+import airflow
+from airflow import DAG
+from airflow.models import BaseOperator
+
+if airflow.__version__.startswith("1."):
+    from airflow.operators.dummy_operator import DummyOperator
+else:
+    from airflow.operators.dummy import DummyOperator
+
+from pytimeparse import parse
+
+from dbt_airflow_factory.builder import DbtAirflowTasksBuilder
+from dbt_airflow_factory.builder_factory import DbtAirflowTasksBuilderFactory
+from dbt_airflow_factory.config_utils import read_config
+from dbt_airflow_factory.notifications.handler import NotificationHandlersFactory
+
+
+class AirflowDagFactory:
+    """
+    Factory creating Airflow DAG.
+
+    :param dag_path: path to ``manifest.json`` file.
+    :type dag_path: str
+    :param env: name of the environment.
+    :type env: str
+    :param dbt_config_file_name: name of the DBT config file.
+        If not specified, default value is ``dbt.yml``.
+    :type dbt_config_file_name: str
+    :param execution_env_config_file_name: name of the execution env config file.
+        If not specified, default value is ``execution_env.yml``.
+    :type execution_env_config_file_name: str
+    :param airflow_config_file_name: name of the Airflow config file.
+        If not specified, default value is ``airflow.yml``.
+    :type airflow_config_file_name: str
+    """
+
+    _builder: DbtAirflowTasksBuilder
+    dag_path: str
+    """path to ``manifest.json`` file."""
+    env: str
+    """name of the environment."""
+    airflow_config_file_name: str
+    """name of the Airflow config file (default: ``airflow.yml``)."""
+
+    def __init__(
+        self,
+        dag_path: str,
+        env: str,
+        dbt_config_file_name: str = "dbt.yml",
+        execution_env_config_file_name: str = "execution_env.yml",
+        airflow_config_file_name: str = "airflow.yml",
+    ):
+        self._builder = DbtAirflowTasksBuilderFactory(
+            dag_path,
+            env,
+            dbt_config_file_name,
+            execution_env_config_file_name,
+        ).create()
+        self._notifications_handlers_builder = NotificationHandlersFactory()
+        self.dag_path = dag_path
+        self.env = env
+        self.airflow_config_file_name = airflow_config_file_name
+
+    def create(self) -> DAG:
+        """
+        Parse ``manifest.json`` and create tasks based on the data contained there.
+
+        :return: Generated DAG.
+        :rtype: airflow.models.dag.DAG
+        """
+        config = self.read_config()
+        with DAG(default_args=config["default_args"], **config["dag"]) as dag:
+            self.create_tasks(config)
+        return dag
+
+    def create_tasks(self, config: dict) -> None:
+        """
+        Parse ``manifest.json`` and create tasks based on the data contained there.
+
+        :param config: Dictionary representing ``airflow.yml``.
+        :type config: dict
+        """
+        start = self._create_starting_task(config)
+        end = DummyOperator(task_id="end")
+        tasks = self._builder.parse_manifest_into_tasks(
+            self._manifest_file_path(config),
+            config.get("use_task_group", False),
+            config.get("show_ephemeral_models", True),
+        )
+        for starting_task in tasks.get_starting_tasks():
+            start >> starting_task.get_start_task()
+        for ending_task in tasks.get_ending_tasks():
+            ending_task.get_end_task() >> end
+
+    def _create_starting_task(self, config: dict) -> BaseOperator:
+        if config.get("seed_task", True):
+            return self._builder.create_seed_task()
+        else:
+            return DummyOperator(task_id="start")
+
+    def _manifest_file_path(self, config: dict) -> str:
+        file_dir = config.get("manifest_dir_path", self.dag_path)
+        return os.path.join(file_dir, config.get("manifest_file_name", "manifest.json"))
+
+    def read_config(self) -> dict:
+        """
+        Read ``airflow.yml`` from ``config`` directory into a dictionary.
+
+        :return: Dictionary representing ``airflow.yml``.
+        :rtype: dict
+        :raises KeyError: No ``default_args`` key in ``airflow.yml``.
+        """
+        config = read_config(
+            self.dag_path, self.env, self.airflow_config_file_name, replace_jinja=True
+        )
+        if "retry_delay" in config["default_args"]:
+            config["default_args"]["retry_delay"] = parse(config["default_args"]["retry_delay"])
+        if "failure_handlers" in config:
+            config["default_args"][
+                "on_failure_callback"
+            ] = self._notifications_handlers_builder.create_failure_handler(
+                config["failure_handlers"]
+            )
+        return config
---
+++ packages/dbt-airflow-factory/dbt-airflow-factory-0.23.0.tar.gz/dbt-airflow-factory-0.23.0/dbt_airflow_factory/builder_factory.py
@@ -0,0 +1,92 @@
+"""Factory creating Airflow tasks."""
+
+from dbt_airflow_factory.builder import DbtAirflowTasksBuilder
+from dbt_airflow_factory.config_utils import read_config
+from dbt_airflow_factory.dbt_parameters import DbtExecutionEnvironmentParameters
+from dbt_airflow_factory.ecs.ecs_operator import EcsPodOperatorBuilder
+from dbt_airflow_factory.ecs.ecs_parameters_loader import EcsExecutionParametersLoader
+from dbt_airflow_factory.k8s.k8s_operator import KubernetesPodOperatorBuilder
+from dbt_airflow_factory.k8s.k8s_parameters_loader import (
+    KubernetesExecutionParametersLoader,
+)
+from dbt_airflow_factory.operator import DbtRunOperatorBuilder
+
+
+class DbtAirflowTasksBuilderFactory:
+    """
+    Factory creating Airflow tasks.
+
+    :param dag_path: path to ``manifest.json`` file.
+    :type dag_path: str
+    :param env: name of the environment.
+    :type env: str
+    :param dbt_config_file_name: name of the DBT config file.
+        If not specified, default value is ``dbt.yml``.
+    :type dbt_config_file_name: str
+    :param execution_env_config_file_name: name of the execution environment config file.
+        If not specified, default value is ``execution_env.yml``.
+    :type execution_env_config_file_name: str
+    """
+
+    base_config_name: str
+    """Name of the ``base`` environment (default: ``base``)."""
+    dag_path: str
+    """path to ``manifest.json`` file."""
+    env: str
+    """name of the environment."""
+    dbt_config_file_name: str
+    """name of the DBT config file (default: ``dbt.yml``)."""
+    execution_env_config_file_name: str
+    """name of the execution env config file (default: ``execution_env.yml``)."""
+
+    def __init__(
+        self,
+        dag_path: str,
+        env: str,
+        dbt_config_file_name: str = "dbt.yml",
+        execution_env_config_file_name: str = "execution_env.yml",
+    ):
+        self.base_config_name = "base"
+        self.dag_path = dag_path
+        self.env = env
+        self.dbt_config_file_name = dbt_config_file_name
+        self.execution_env_config_file_name = execution_env_config_file_name
+
+    def create(self) -> DbtAirflowTasksBuilder:
+        """
+        Create :class:`.DbtAirflowTasksBuilder` to use.
+
+        :return: Instance of :class:`.DbtAirflowTasksBuilder`.
+        :rtype: DbtAirflowTasksBuilder
+        """
+        dbt_params = self._create_dbt_config()
+        execution_env_type = self._read_execution_env_type()
+        return DbtAirflowTasksBuilder(self._create_operator_builder(execution_env_type, dbt_params))
+
+    def _create_operator_builder(
+        self, type: str, dbt_params: DbtExecutionEnvironmentParameters
+    ) -> DbtRunOperatorBuilder:
+        if type == "k8s":
+            return KubernetesPodOperatorBuilder(
+                dbt_params,
+                KubernetesExecutionParametersLoader.create_config(
+                    self.dag_path, self.env, self.execution_env_config_file_name
+                ),
+            )
+        elif type == "ecs":
+            return EcsPodOperatorBuilder(
+                dbt_params,
+                EcsExecutionParametersLoader.create_config(
+                    self.dag_path, self.env, self.execution_env_config_file_name
+                ),
+            )
+        else:
+            raise TypeError(f"Unsupported env type {type}")
+
+    def _create_dbt_config(self) -> DbtExecutionEnvironmentParameters:
+        return DbtExecutionEnvironmentParameters(
+            **read_config(self.dag_path, self.env, self.dbt_config_file_name)
+        )
+
+    def _read_execution_env_type(self) -> str:
+        return read_config(self.dag_path, self.env, self.execution_env_config_file_name)["type"]
