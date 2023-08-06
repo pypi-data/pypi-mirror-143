1fb9666fc720fc46c9bb230cbdc5dce5d359eea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:53 +0000

    Add package wxc-sdk/0.4.0/wxc_sdk-0.4.0.tar.gz

---
+++ packages/wxc-sdk/wxc_sdk-0.4.0.tar.gz/wxc_sdk-0.4.0/wxc_sdk/telephony/__init__.py
@@ -0,0 +1,37 @@
+"""
+Telephony types and API
+"""
+
+from .calls import CallsApi
+from .schedules import ScheduleAPI
+from .paging import PagingAPI
+from .huntgroup import HuntGroupAPI
+from .callqueue import CallQueueAPI
+from .autoattendant import AutoAttendantAPI
+from ..api_child import ApiChild
+from ..rest import RestSession
+
+__all__ = ['TelephonyApi']
+
+
+class TelephonyApi(ApiChild, base='telephony'):
+    """
+    The telephony API. Child of :class:`WebexSimpleApi`
+
+    :ivar auto_attendant: :class:`autoattendant.AutoAttendantAPI`
+    :ivar calls: :class:`calls.CallsApi`
+    :ivar schedules: :class:`schedules.ScheduleAPI`
+    :ivar paging: :class:`paging.PagingAPI`
+    :ivar huntgroup: :class:`huntgroup.HuntGroupAPI`
+    :ivar callqueue: :class:`callqueue.CallQueueAPI`
+
+    """
+
+    def __init__(self, session: RestSession):
+        super().__init__(session=session)
+        self.auto_attendant = AutoAttendantAPI(session=session)
+        self.calls = CallsApi(session=session)
+        self.schedules = ScheduleAPI(session=session)
+        self.paging = PagingAPI(session=session)
+        self.huntgroup = HuntGroupAPI(session=session)
+        self.callqueue = CallQueueAPI(session=session)
