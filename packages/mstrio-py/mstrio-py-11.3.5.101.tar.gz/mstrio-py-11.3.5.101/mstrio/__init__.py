4b6af6df2c5382e0518f2b0e7bfa1bc3f0524b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:40 +0000

    Add package mstrio-py/11.3.5.101/mstrio-py-11.3.5.101.tar.gz

---
+++ packages/mstrio-py/mstrio-py-11.3.5.101.tar.gz/mstrio-py-11.3.5.101/mstrio/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "mstrio-py"
+__version__ = "11.3.5.101"  # NOSONAR
+__dev_version__ = 1075
+__license__ = "Apache License 2.0"
+__description__ = "Python interface for the MicroStrategy REST API"
+__author__ = "MicroStrategy"
+__author_email__ = "pkowal@microstrategy.com"
