89a6372dafd8f59b6e60271b39ecac076c0b4e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:51 +0000

    Add package autumn8/0.4.0rc8/autumn8-0.4.0rc8.tar.gz

---
+++ packages/autumn8/autumn8-0.4.0rc8.tar.gz/autumn8-0.4.0rc8/lib/examples/model.py
@@ -0,0 +1,22 @@
+from convblock import ConvBlock
+import torch
+import numpy as np
+import autumn8
+from torch import nn
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super(TestModel, self).__init__()
+        self.conv = torch.nn.Conv2d(3, 32, 3, 1)
+        self.block1 = ConvBlock()
+        self.block2 = ConvBlock()
+
+    def forward(self, x):
+        return self.block2(self.block1(self.conv(x)))
+
+dummy_input = torch.randn(1, 3, 28, 28)
+#just for testing prediction on server
+np.save("inp.npy", dummy_input.numpy())
+model = TestModel()
+
+autumn8.attach_model(model, dummy_input)
