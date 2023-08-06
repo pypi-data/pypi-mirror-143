737bd9d51948e228f225ee5116dda1833b817c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:36 +0000

    Add package ddd-objects/0.0.12/ddd_objects-0.0.12.tar.gz

---
+++ packages/ddd-objects/ddd_objects-0.0.12.tar.gz/ddd_objects-0.0.12/src/ddd_objects/domain/exception.py
@@ -0,0 +1,14 @@
+class IDNotExistError(Exception):
+    pass
+
+class UpdateError(Exception):
+    pass
+
+class OverLimitError(Exception):
+    pass
+
+class KeyExistError(Exception):
+    pass
+
+class ResultFormatError(Exception):
+    pass
