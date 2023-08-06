d2269f20dc6be89fe6804cc874322223325c42ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:01 +0000

    Add package codefree/0.1.3/codefree-0.1.3.tar.gz

---
+++ packages/codefree/codefree-0.1.3.tar.gz/codefree-0.1.3/codefree/codefree.py
@@ -0,0 +1,27 @@
+'''
+Function:
+    Codefree: Make no code a reality.
+Author:
+    Zhenchao Jin
+WeChat public account:
+    Charles_pikachu
+'''
+from .sources import StackOverflow
+
+
+'''Codefree'''
+class CodeFree():
+    def __init__(self, keyword, source='stackoverflow', proxies={}, **kwargs):
+        self.source = source.lower()
+        self.supported_sources = {
+            'stackoverflow': StackOverflow
+        }
+        assert source in self.supported_sources, f'Source {self.source} has not been supported'
+        self.selected_source = self.supported_sources[self.source](keyword=keyword, proxies=proxies, **kwargs)
+        print(self)
+    '''call'''
+    def __call__(self, **kwargs):
+        return self.selected_source(**kwargs)
+    '''str'''
+    def __str__(self):
+        return 'Welcome to use codefree!\nYou can visit https://github.com/CharlesPikachu/codefree for more details.'
