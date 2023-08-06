5f9209123a030c59a9ddecec7c7750f9a344be95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:31 +0000

    Add package comun-config/0.3/comun_config-0.3.tar.gz

---
+++ packages/comun-config/comun_config-0.3.tar.gz/comun_config-0.3/comun_config.py
@@ -0,0 +1,30 @@
+import psycopg
+from psycopg.rows import namedtuple_row, dict_row
+
+
+def get_config_params(connection_string, id_project):
+
+    conn = psycopg.connect(connection_string, row_factory=namedtuple_row)
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT key, value FROM params WHERE project_id=%s OR project_id=0', [id_project])
+    rows = cursor.fetchall()
+    conn.close()
+
+    db_config = {}
+    for row in rows:
+        db_config[row[0]] = row[1]
+
+    return db_config
+
+
+def get_deploy_params(connection_string, id_project):
+
+    conn = psycopg.connect(connection_string, row_factory=dict_row)
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT server, project_path, deploy_user FROM projects WHERE project_id=%s', [id_project])
+    row = cursor.fetchone()
+    conn.close()
+
+    return row
---
+++ packages/comun-config/comun_config-0.3.tar.gz/comun_config-0.3/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='comun_config',
+      version='0.3',
+      py_modules=['comun_config'],
+      install_requires=['psycopg'])
