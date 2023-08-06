cb4ca5e7a6a027533b955e6508166cbf8d12fb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:16 +0000

    Add package aiotapioca-wrapper/3.0.0/aiotapioca-wrapper-3.0.0.tar.gz

---
+++ packages/aiotapioca-wrapper/aiotapioca-wrapper-3.0.0.tar.gz/aiotapioca-wrapper-3.0.0/aiotapioca/__init__.py
@@ -0,0 +1,12 @@
+
+__author__ = "Filipe Ximenes, Andrey Ilin"
+__email__ = "andreyilin@fastmail.com"
+__version__ = "3.0.0"
+
+
+from .adapters import (
+    generate_wrapper_from_adapter,
+    TapiocaAdapter,
+    FormAdapterMixin,
+    JSONAdapterMixin,
+)
