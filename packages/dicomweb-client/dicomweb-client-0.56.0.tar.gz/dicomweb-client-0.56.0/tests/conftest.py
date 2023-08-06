ae8ccf3319d8eb7f27f2d27b647a9e7aea293319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:09 +0000

    Add package dicomweb-client/0.56.0/dicomweb-client-0.56.0.tar.gz

---
+++ packages/dicomweb-client/dicomweb-client-0.56.0.tar.gz/dicomweb-client-0.56.0/tests/conftest.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pytest
+from pydicom.data.data_manager import DATA_ROOT
+
+from dicomweb_client.cli import _get_parser
+from dicomweb_client.api import DICOMwebClient
+from dicomweb_client.file import DICOMfileClient
+
+
+@pytest.fixture
+def parser():
+    '''Instance of `argparse.Argparser`.'''
+    return _get_parser()
+
+
+@pytest.fixture
+def cache_dir():
+    '''Directory where responses are cached.'''
+    return Path(__file__).parent.parent.joinpath('data')
+
+
+@pytest.fixture
+def client(httpserver):
+    '''Instance of `dicomweb_client.api.DICOMwebClient`.'''
+    return DICOMwebClient(httpserver.url)
+
+
+@pytest.fixture
+def file_client():
+    '''Instance of `dicomweb_client.api.DICOMwebClient`.'''
+    base_dir = Path(DATA_ROOT)
+    return DICOMfileClient(base_dir, recreate_db=True, in_memory=True)
