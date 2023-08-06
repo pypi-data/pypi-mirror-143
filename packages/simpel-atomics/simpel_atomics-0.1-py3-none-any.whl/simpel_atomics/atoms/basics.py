cf1c5becee4352def29692f3ed9333aed9fa93c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package simpel-atomics/0.1/simpel_atomics-0.1-py3-none-any.whl

---
+++ packages/simpel-atomics/simpel_atomics-0.1-py3-none-any.whl/simpel_atomics/atoms/basics.py
@@ -0,0 +1,71 @@
+from ..base import Component
+
+
+class NoTag(Component):
+    template_string = """{{ childs }}"""
+
+
+class Tag(Component):
+    template_string = """<{{ tag }}>{{ text|safe }}</{{ tag }}>"""
+
+
+class Content(Component):
+    template_string = """{{ value|safe }}"""
+
+
+class Paragraph(Component):
+    template_string = """<p>{{ text|safe }}</p>"""
+
+
+class Icon(Component):
+    template_string = """<i {{ attrs }} class="{{ classes }}" href="{{ url }}"></i> """
+
+
+class MaterialIcon(Icon):
+    template_string = """<i {{ attrs }} class="mdi mdi-{{ name }} {{ classes}}""></i> """
+
+
+class BoostrapIcon(Icon):
+    template_string = """<i {{ attrs }} class="bi bi-{{ name }} {{ classes}}"></i> """
+
+
+class FontAwesomeIcon(Icon):
+    template_string = """<i {{ attrs }} class="fa fa-{{ name }} {{ classes}}"></i> """
+
+
+class Link(Component):
+    template_string = """
+    <a {{ attrs }}
+         {% if style %}style="{{ style }}"{% endif %}
+         {% if classes %}class="{{ classes }}"{% endif %}
+         href="{{ url }}"
+        >{{ childs }}{{ label }}</a>
+    """
+
+
+class Button(Component):
+    template_string = """
+    <button {{ attrs }} class="{{ classes}}"
+            type="{{ type }}" {% if action %}formaction="{{ action }}"{% endif %}>
+            {{ label }}
+    </button>
+    """
+
+
+class ButtonLink(Component):
+    template_string = """
+    <a href="{{ url }}" class="btn btn{{ variant }}" target="{{ target }}" >{{ text }}</a>
+    """
+
+
+__all__ = [
+    "Content",
+    "Paragraph",
+    "Icon",
+    "MaterialIcon",
+    "BoostrapIcon",
+    "FontAwesomeIcon",
+    "Link",
+    "Button",
+    "ButtonLink",
+]
---
+++ packages/simpel-atomics/simpel_atomics-0.1-py3-none-any.whl/simpel_atomics/atoms/containers.py
@@ -0,0 +1,83 @@
+from ..base import Component
+
+
+class Div(Component):
+    template_string = """
+    <div {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</div>
+    """
+
+
+class Nav(Component):
+    template_string = """
+    <nav {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</nav>
+    """
+
+
+class Header(Component):
+    template_string = """
+    <header {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</header>
+    """
+
+
+class Footer(Component):
+    template_string = """
+    <footer {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</footer>
+    """
+
+
+class Article(Component):
+    template_string = """
+    <article {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</article>
+    """
+
+
+class Section(Component):
+    template_string = """
+    <section {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+       >{{ childs }}
+    </section>
+    """
+
+
+class UnOrderedList(Component):
+    template_string = """
+    <ul {% if attrs %}{{ attrs }}{% endif %}
+        {% if style %}style="{{ style }}"{% endif %}
+        {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</ul>
+    """
+
+
+class OrderedList(Component):
+    template_string = """
+    <ol {% if attrs %}{{ attrs }}{% endif %}
+         {% if style %}style="{{ style }}"{% endif %}
+         {% if classes %}class="{{ classes }}"{% endif %}
+         >{{ childs }}</ol>
+    """
+
+
+class ListItem(Component):
+    template_string = """
+    <li {{ attrs }}
+         {% if style %}style="{{ style }}"{% endif %}
+         {% if classes %}class="{{ classes }}"{% endif %}
+        >{{ childs }}</li>
+    """
