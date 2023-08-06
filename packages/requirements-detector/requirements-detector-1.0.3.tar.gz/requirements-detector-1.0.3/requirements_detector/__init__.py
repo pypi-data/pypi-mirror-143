181cc5f88b82bef57421ba22c631edafa0d4d9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:10 +0000

    Add package requirements-detector/1.0.3/requirements-detector-1.0.3.tar.gz

---
+++ packages/requirements-detector/requirements-detector-1.0.3.tar.gz/requirements-detector-1.0.3/requirements_detector/__init__.py
@@ -0,0 +1,7 @@
+from requirements_detector.detect import (  # from_setup_py,
+    CouldNotParseRequirements,
+    RequirementsNotFound,
+    find_requirements,
+    from_requirements_dir,
+    from_requirements_txt,
+)
