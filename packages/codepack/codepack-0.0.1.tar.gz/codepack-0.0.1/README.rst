e2f5a86aa89a76159a0506db178990db9d53461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:14 +0000

    Add package codepack/0.0.1/codepack-0.0.1.tar.gz

---
+++ packages/codepack/codepack-0.0.1.tar.gz/codepack-0.0.1/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://github.com/ihnokim/codepack/workflows/Test/badge.svg?event=push&branch=master
+  :target: https://github.com/ihnokim/codepack/actions?query=workflow%3ATest+event%3Apush+branch%3Amaster
+  :alt: Test Status
+.. image:: https://coveralls.io/repos/github/ihnokim/codepack/badge.svg
+  :target: https://coveralls.io/github/ihnokim/codepack
+  :alt: Code Coverage
+
+CodePack
+-----
---
+++ packages/codepack/codepack-0.0.1.tar.gz/codepack-0.0.1/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="codepack",
+    version="0.0.1",
+    author="ihnokim",
+    author_email="ihnokim58@gmail.com",
+    description="Codepack is the package for making workflows with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ihnokim/codepack",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "dill>=0.3.4",
+        "pymongo>=3.12.1",
+        "numpy",
+        "pandas",
+        "sshtunnel>=0.4.0",
+        "PyMySQL>=1.0.2",
+        "pymssql>=2.2.2",
+        "boto3>=1.19.6",
+        "cx-Oracle>=8.2.1",
+        "parse>=1.19.0",
+        "APScheduler>=3.8.1",
+        "kafka-python>=2.0.2",
+        "docker>=5.0.3",
+        "requests>=2.26.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
