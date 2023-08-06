d9cc8e262a3fe32fe90224582b0b04ffb1a4dc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:36 +0000

    Add package rio-tiler/3.1.2/rio-tiler-3.1.2.tar.gz

---
+++ packages/rio-tiler/rio-tiler-3.1.2.tar.gz/rio-tiler-3.1.2/rio_tiler/__init__.py
@@ -0,0 +1,16 @@
+"""rio-tiler."""
+
+from . import (  # noqa
+    colormap,
+    constants,
+    errors,
+    expression,
+    io,
+    mosaic,
+    profiles,
+    reader,
+    tasks,
+    utils,
+)
+
+__version__ = "3.1.2"
