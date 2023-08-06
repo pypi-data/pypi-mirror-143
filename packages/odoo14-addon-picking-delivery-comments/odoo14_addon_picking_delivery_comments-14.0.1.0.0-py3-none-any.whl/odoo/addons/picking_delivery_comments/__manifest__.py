d4a07ee252ad6c5f7f061199f8b6abedfad7f986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:23 +0000

    Add package odoo14-addon-picking-delivery-comments/14.0.1.0.0/odoo14_addon_picking_delivery_comments-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-picking-delivery-comments/odoo14_addon_picking_delivery_comments-14.0.1.0.0-py3-none-any.whl/odoo/addons/picking_delivery_comments/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Picking Delivery Comments",
+    "version": "14.0.1.0.0",
+    "depends": ["sale_stock"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Hidden",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Print comments from delivery contact in Delivery Stock Picking
+    """,
+    "data": ["report/stock_report_deliveryslip.xml"],
+    "installable": True,
+}
