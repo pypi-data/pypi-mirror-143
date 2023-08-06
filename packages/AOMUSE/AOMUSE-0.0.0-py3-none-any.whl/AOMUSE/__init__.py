d2a2be78ede50c3e6587597557eefa9ff95c4843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package AOMUSE/0.0.0/AOMUSE-0.0.0-py3-none-any.whl

---
+++ packages/AOMUSE/AOMUSE-0.0.0-py3-none-any.whl/AOMUSE/__init__.py
@@ -0,0 +1,13 @@
+"""
+Init file for the SpatialCluster library.
+"""
+
+version_info = (0, 0, 0)
+__version__ = ".".join([str(x) for x in version_info])
+
+__all__ = [
+    "ao"
+    "db"
+    "process"
+    "utils"
+]
