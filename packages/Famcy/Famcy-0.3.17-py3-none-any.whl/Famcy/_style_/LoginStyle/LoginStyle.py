e189582dfd2983b140461af4b00cd60131476651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:54 +0000

    Add package Famcy/0.3.17/Famcy-0.3.17-py3-none-any.whl

---
+++ packages/Famcy/Famcy-0.3.17-py3-none-any.whl/Famcy/_style_/LoginStyle/LoginStyle.py
@@ -0,0 +1,31 @@
+import Famcy
+from flask import render_template
+
+class LoginStyle(Famcy.FamcyStyle):
+	def __init__(self):
+		super(LoginStyle, self).__init__()
+
+		# set default value
+		self.title = ""
+		self.desc = ""
+		self.font_theme = Famcy.FamcyFontTheme()
+		self.loader = Famcy.FamcyStyleLoader()
+		self.color_theme = Famcy.FamcyColorTheme()
+
+
+	def setHeadScript(self, title=None, desc=None):
+		if title:
+			self.title = title
+		if desc:
+			self.desc = desc
+
+	def render(self, extra_script, content, background_flag=False, route="", time=5000, form_init_js=None, end_script="", **kwargs):
+
+		html_header = self.setDashboardHTMLHeader()
+		end_js = end_script+self.setDashboardJavaScript()
+		color_theme = self.color_theme.render()
+		font_theme = self.font_theme.render()
+		load_spinner = self.loader.render()
+
+		body_on_load = "background_loop('" + self.main_url + str(route) + "/bgloop" + "', '" + str(route) + "', " + str(time) + ");console.log('start!')" if background_flag else ""
+		return render_template("login.html", event_source=str(self.main_url)+"/"+str(route)+"/event_source", font_theme=font_theme, form_init_js=form_init_js, load_spinner=load_spinner, color_theme=color_theme, html_header=html_header, content=content, extra_script=extra_script, end_js=end_js, body_on_load=body_on_load)
