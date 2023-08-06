10bcc6db0dd38dd5001235981953ef166660395f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:20 +0000

    Add package red-manager/0.0.2/red_manager-0.0.2.tar.gz

---
+++ packages/red-manager/red_manager-0.0.2.tar.gz/red_manager-0.0.2/src/__main__.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from upload import uploader 
+
+
+
+
+file_path=sys.argv[1]
+
+
+
+if sys.argv[1]=='twine':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('python -m twine upload dist/*')
+else : 
+    if len(sys.argv)!=2:
+        print("Insufficient arguments")
+        sys.exit()
+
+    f=open("README.md",'w')
+    f.close()
+    f=open("setup.py",'w')
+    f.close()
+    f=open("LICENSE",'w')
+    f.close()
+        
+    print("note the follows")
+
+    upload_1=uploader()
+    upload_1.up()
+    upload_1.load()
+    
+
+    os.system('python setup.py sdist bdist_wheel')
