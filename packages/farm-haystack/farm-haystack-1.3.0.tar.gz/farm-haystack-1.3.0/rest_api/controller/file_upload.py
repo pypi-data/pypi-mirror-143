ea2e449a010602fc0bbd7786d74714e9145eae6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:30 +0000

    Add package farm-haystack/1.3.0/farm-haystack-1.3.0.tar.gz

---
+++ packages/farm-haystack/farm-haystack-1.3.0.tar.gz/farm-haystack-1.3.0/rest_api/controller/file_upload.py
@@ -0,0 +1,125 @@
+from typing import Optional, List, Union
+
+import json
+import logging
+import os
+import shutil
+import uuid
+from pathlib import Path
+
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException, Depends
+from pydantic import BaseModel
+
+from haystack.pipelines.base import Pipeline
+from haystack.errors import PipelineConfigError
+from haystack.pipelines.config import (
+    get_component_definitions,
+    get_pipeline_definition,
+    read_pipeline_config_from_yaml,
+    validate_config,
+)
+from rest_api.config import PIPELINE_YAML_PATH, FILE_UPLOAD_PATH, INDEXING_PIPELINE_NAME
+from rest_api.controller.utils import as_form
+
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+try:
+    pipeline_config = read_pipeline_config_from_yaml(Path(PIPELINE_YAML_PATH))
+    validate_config(pipeline_config)
+    pipeline_definition = get_pipeline_definition(pipeline_config=pipeline_config, pipeline_name=INDEXING_PIPELINE_NAME)
+    component_definitions = get_component_definitions(
+        pipeline_config=pipeline_config, overwrite_with_env_variables=True
+    )
+    # Since each instance of FAISSDocumentStore creates an in-memory FAISS index, the Indexing & Query Pipelines would
+    # end up with different indices. The same applies for InMemoryDocumentStore. The check below prevents creation of
+    # Indexing Pipelines with FAISSDocumentStore or InMemoryDocumentStore.
+    is_faiss_or_inmemory_present = False
+    for node in pipeline_definition["nodes"]:
+        if (
+            component_definitions[node["name"]]["type"] == "FAISSDocumentStore"
+            or component_definitions[node["name"]]["type"] == "InMemoryDocumentStore"
+        ):
+            is_faiss_or_inmemory_present = True
+            break
+    if is_faiss_or_inmemory_present:
+        logger.warning(
+            "Indexing Pipeline with FAISSDocumentStore or InMemoryDocumentStore is not supported with the REST APIs."
+        )
+        INDEXING_PIPELINE = None
+    else:
+        INDEXING_PIPELINE = Pipeline.load_from_yaml(Path(PIPELINE_YAML_PATH), pipeline_name=INDEXING_PIPELINE_NAME)
+
+except PipelineConfigError as e:
+    INDEXING_PIPELINE = None
+    logger.error(f"{e.message}. File Upload API will not be available.")
+
+# create directory for uploading files
+os.makedirs(FILE_UPLOAD_PATH, exist_ok=True)
+
+
+@as_form
+class FileConverterParams(BaseModel):
+    remove_numeric_tables: Optional[bool] = None
+    valid_languages: Optional[List[str]] = None
+
+
+@as_form
+class PreprocessorParams(BaseModel):
+    clean_whitespace: Optional[bool] = None
+    clean_empty_lines: Optional[bool] = None
+    clean_header_footer: Optional[bool] = None
+    split_by: Optional[str] = None
+    split_length: Optional[int] = None
+    split_overlap: Optional[int] = None
+    split_respect_sentence_boundary: Optional[bool] = None
+
+
+class Response(BaseModel):
+    file_id: str
+
+
+@router.post("/file-upload")
+def upload_file(
+    files: List[UploadFile] = File(...),
+    # JSON serialized string
+    meta: Optional[str] = Form("null"),  # type: ignore
+    fileconverter_params: FileConverterParams = Depends(FileConverterParams.as_form),  # type: ignore
+    preprocessor_params: PreprocessorParams = Depends(PreprocessorParams.as_form),  # type: ignore
+):
+    """
+    You can use this endpoint to upload a file for indexing
+    (see https://haystack.deepset.ai/guides/rest-api#indexing-documents-in-the-haystack-rest-api-document-store).
+    """
+    if not INDEXING_PIPELINE:
+        raise HTTPException(status_code=501, detail="Indexing Pipeline is not configured.")
+
+    file_paths: list = []
+    file_metas: list = []
+
+    meta_form = json.loads(meta) or {}  # type: ignore
+    if not isinstance(meta_form, dict):
+        raise HTTPException(status_code=500, detail=f"The meta field must be a dict or None, not {type(meta_form)}")
+
+    for file in files:
+        try:
+            file_path = Path(FILE_UPLOAD_PATH) / f"{uuid.uuid4().hex}_{file.filename}"
+            with file_path.open("wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
+
+            file_paths.append(file_path)
+            meta_form["name"] = file.filename
+            file_metas.append(meta_form)
+        finally:
+            file.file.close()
+
+    INDEXING_PIPELINE.run(
+        file_paths=file_paths,
+        meta=file_metas,
+        params={
+            "TextFileConverter": fileconverter_params.dict(),
+            "PDFFileConverter": fileconverter_params.dict(),
+            "Preprocessor": preprocessor_params.dict(),
+        },
+    )
---
+++ packages/farm-haystack/farm-haystack-1.3.0.tar.gz/farm-haystack-1.3.0/rest_api/controller/search.py
@@ -0,0 +1,131 @@
+from typing import Dict, Any
+
+import logging
+import time
+import json
+from pathlib import Path
+from numpy import ndarray
+
+from fastapi import APIRouter
+
+import haystack
+from haystack.pipelines.base import Pipeline
+from haystack.telemetry import send_event_if_public_demo
+from rest_api.config import PIPELINE_YAML_PATH, QUERY_PIPELINE_NAME
+from rest_api.config import LOG_LEVEL, CONCURRENT_REQUEST_PER_WORKER
+from rest_api.schema import QueryRequest, QueryResponse
+from rest_api.controller.utils import RequestLimiter
+
+
+logging.getLogger("haystack").setLevel(LOG_LEVEL)
+logger = logging.getLogger("haystack")
+
+from pydantic import BaseConfig
+
+BaseConfig.arbitrary_types_allowed = True
+
+router = APIRouter()
+
+
+PIPELINE = Pipeline.load_from_yaml(Path(PIPELINE_YAML_PATH), pipeline_name=QUERY_PIPELINE_NAME)
+DOCUMENT_STORE = PIPELINE.get_document_store()
+logging.info(f"Loaded pipeline nodes: {PIPELINE.graph.nodes.keys()}")
+
+concurrency_limiter = RequestLimiter(CONCURRENT_REQUEST_PER_WORKER)
+logging.info("Concurrent requests per worker: {CONCURRENT_REQUEST_PER_WORKER}")
+
+
+@router.get("/initialized")
+def check_status():
+    """
+    This endpoint can be used during startup to understand if the
+    server is ready to take any requests, or is still loading.
+
+    The recommended approach is to call this endpoint with a short timeout,
+    like 500ms, and in case of no reply, consider the server busy.
+    """
+    return True
+
+
+@router.get("/hs_version")
+def haystack_version():
+    """
+    Get the running Haystack version.
+    """
+    return {"hs_version": haystack.__version__}
+
+
+@router.post("/query", response_model=QueryResponse, response_model_exclude_none=True)
+def query(request: QueryRequest):
+    """
+    This endpoint receives the question as a string and allows the requester to set
+    additional parameters that will be passed on to the Haystack pipeline.
+    """
+    with concurrency_limiter.run():
+        result = _process_request(PIPELINE, request)
+        return result
+
+
+@send_event_if_public_demo
+def _process_request(pipeline, request) -> Dict[str, Any]:
+    start_time = time.time()
+
+    params = request.params or {}
+
+    # format global, top-level filters (e.g. "params": {"filters": {"name": ["some"]}})
+    if "filters" in params.keys():
+        params["filters"] = _format_filters(params["filters"])
+
+    # format targeted node filters (e.g. "params": {"Retriever": {"filters": {"value"}}})
+    for key in params.keys():
+        if "filters" in params[key].keys():
+            params[key]["filters"] = _format_filters(params[key]["filters"])
+
+    result = pipeline.run(query=request.query, params=params, debug=request.debug)
+
+    # Ensure answers and documents exist, even if they're empty lists
+    if not "documents" in result:
+        result["documents"] = []
+    if not "answers" in result:
+        result["answers"] = []
+
+    # if any of the documents contains an embedding as an ndarray the latter needs to be converted to list of float
+    for document in result["documents"]:
+        if isinstance(document.embedding, ndarray):
+            document.embedding = document.embedding.tolist()
+
+    logger.info(
+        json.dumps({"request": request, "response": result, "time": f"{(time.time() - start_time):.2f}"}, default=str)
+    )
+    return result
+
+
+def _format_filters(filters):
+    """
+    Adjust filters to compliant format:
+    Put filter values into a list and remove filters with null value.
+    """
+    new_filters = {}
+    if filters is None:
+        logger.warning(
+            f"Request with deprecated filter format ('\"filters\": null'). "
+            f"Remove empty filters from params to be compliant with future versions"
+        )
+    else:
+        for key, values in filters.items():
+            if values is None:
+                logger.warning(
+                    f"Request with deprecated filter format ('{key}: null'). "
+                    f"Remove null values from filters to be compliant with future versions"
+                )
+                continue
+
+            if not isinstance(values, list):
+                logger.warning(
+                    f"Request with deprecated filter format ('{key}': {values}). "
+                    f"Change to '{key}':[{values}]' to be compliant with future versions"
+                )
+                values = [values]
+
+            new_filters[key] = values
+    return new_filters
