762f674a3d61f209e7a11d526b9d573a7e25e76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:07 +0000

    Add package sqleyes/0.3.0/sqleyes-0.3.0.tar.gz

---
+++ packages/sqleyes/sqleyes-0.3.0.tar.gz/sqleyes-0.3.0/sqleyes/detector/antipatterns/fear_of_the_unknown.py
@@ -0,0 +1,29 @@
+"""Fear of the Unknown anti-pattern detector class"""
+import re
+from sqleyes.detector.antipatterns.abstract_base_class import AbstractDetector
+from sqleyes.definitions.definitions import DEFINITIONS
+from sqleyes.detector.detector_output import DetectorOutput
+
+
+class FearOfTheUnknownDetector(AbstractDetector):
+
+    filename = DEFINITIONS["anti_patterns"]["fear_of_the_unknown"]["filename"]
+    type = DEFINITIONS["anti_patterns"]["fear_of_the_unknown"]["type"]
+    title = DEFINITIONS["anti_patterns"]["fear_of_the_unknown"]["title"]
+
+    def __init__(self, query):
+        super().__init__(query)
+
+    def check(self):
+        patterns = [re.compile(r'<>\s*NULL', re.IGNORECASE),
+                    re.compile(r'!=\s*NULL', re.IGNORECASE),
+                    re.compile(r'=\s*NULL', re.IGNORECASE)]
+        for pattern in patterns:
+            if pattern.search(self.query):
+                return DetectorOutput(certainty="high",
+                                      description=super().get_description(),
+                                      detector_type=self.detector_type,
+                                      title=self.title,
+                                      type=self.type)
+
+        return None
