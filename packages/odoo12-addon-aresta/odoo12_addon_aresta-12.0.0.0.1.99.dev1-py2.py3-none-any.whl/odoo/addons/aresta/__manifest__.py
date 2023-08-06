09d997aae1010b468ad4816b6a005f7b42daf333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:13 +0000

    Add package odoo12-addon-aresta/12.0.0.0.1.99.dev1/odoo12_addon_aresta-12.0.0.0.1.99.dev1-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-aresta/odoo12_addon_aresta-12.0.0.0.1.99.dev1-py2.py3-none-any.whl/odoo/addons/aresta/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Odoo customizations for Aresta",
+    "version": "12.0.0.0.1",
+    "depends": [
+        "delivery",
+        "pos_order_to_sale_order",
+        "pos_sale",
+        "sale"
+    ],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Project Management",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Odoo customizations for Aresta.
+    """,
+    "data": [
+        "views/sale_order.xml",
+        "views/sale_report.xml"
+    ],
+    "installable": True,
+}
