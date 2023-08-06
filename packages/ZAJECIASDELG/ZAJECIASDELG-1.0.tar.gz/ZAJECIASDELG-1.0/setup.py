e2e1b341a1a954fa0165003a1b07df1c23d16fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:13 +0000

    Add package ZAJECIASDELG/1.0/ZAJECIASDELG-1.0.tar.gz

---
+++ packages/ZAJECIASDELG/ZAJECIASDELG-1.0.tar.gz/ZAJECIASDELG-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ZAJECIASDELG',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Santiago_Delgado',
+      author_email='abder@poczta.onet.pl',
+      url='https://www.python.org/sigs/distutils-sig/',
+     )
