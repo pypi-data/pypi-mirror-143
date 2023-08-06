c7be4884f260498b34ca031379ffa15d1ffa147b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:55 +0000

    Add package passpredict/0.5.0/passpredict-0.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/passpredict/passpredict-0.5.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/passpredict/exceptions.py
@@ -0,0 +1,10 @@
+class NotReachable(Exception):
+    """Raised when a pass is not reachable on a time window"""
+
+
+class PropagationError(Exception):
+    """Raised when a calculation issue is found"""
+
+
+class CelestrakError(Exception):
+    """ Raised when TLEs can't be downloaded from Celestrak """
