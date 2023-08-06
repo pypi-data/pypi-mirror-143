74f73c78ca7c338a086100be2369dea8db921781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:12 +0000

    Add package pycapacity/1.2.8/pycapacity-1.2.8.tar.gz

---
+++ packages/pycapacity/pycapacity-1.2.8.tar.gz/pycapacity-1.2.8/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pycapacity',
+    version='1.2.8',
+    author='Antun Skuric',
+    author_email='antun.skuric@inria.fr',
+    description='A real-time task space capacity calculation module for robotic manipulators and human musculoskeletal models',
+    long_description=long_description, #'A Real-time capable robot capacity calculation module', 
+    long_description_content_type="text/markdown",
+    url='https://gitlab.inria.fr/auctus-team/people/antunskuric/pycapacity',
+    license='MIT',
+    #package_dir = {'pycapacity': 'pycapacity'}, 
+    packages = ['pycapacity'],
+    #py_modules=['pycapacity.robot','pycapacity.human'],
+    install_requires=['numpy','scipy','cvxopt>=1.2.6','matplotlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Robot Framework",
+        "Framework :: Robot Framework :: Library",
+        "Framework :: Robot Framework :: Tool"
+    ],
+)
