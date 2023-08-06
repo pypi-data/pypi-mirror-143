1cba809ee3533c64e663df7f28680d0bed9f6298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:41 +0000

    Add package dacon/0.1.1/dacon-0.1.1.tar.gz

---
+++ packages/dacon/dacon-0.1.1.tar.gz/dacon-0.1.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dacon",
+    version="0.1.1",
+    author="dacon",
+    author_email="dacon@dacon.io",
+    description="dacon-open-api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dacon-ds2/dacon-open-api", # project link
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={'': ['dacon/datasets/data/*.csv']},
+)
+"""
+1. 패키지 빌드
+python3 setup.py sdist bdist_wheel
+
+2. 패키지 업로드
+python3 -m twine upload dist/*
+"""
