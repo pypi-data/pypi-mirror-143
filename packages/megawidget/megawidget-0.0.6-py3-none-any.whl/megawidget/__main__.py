e80c1bd292993e14dd64848c69a6e31297d8b7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:29 +0000

    Add package megawidget/0.0.6/megawidget-0.0.6-py3-none-any.whl

---
+++ packages/megawidget/megawidget-0.0.6-py3-none-any.whl/megawidget/__main__.py
@@ -0,0 +1,16 @@
+text = """
+Collection of megawidgets to build GUIs with Python.
+https://github.com/pyrustic/megawidget
+
+This project is part of the Pyrustic Open Ecosystem.
+https://pyrustic.github.io
+
+"""
+
+
+def main():
+    print(text)
+
+
+if __name__ == "__main__":
+    main()
