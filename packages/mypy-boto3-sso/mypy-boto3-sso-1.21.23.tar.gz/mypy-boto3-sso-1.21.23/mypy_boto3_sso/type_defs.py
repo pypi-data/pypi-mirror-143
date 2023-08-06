2895afc6bbacddaadccfe2f1c37c7b74c3ea6b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package mypy-boto3-sso/1.21.23/mypy-boto3-sso-1.21.23.tar.gz

---
+++ packages/mypy-boto3-sso/mypy-boto3-sso-1.21.23.tar.gz/mypy-boto3-sso-1.21.23/mypy_boto3_sso/type_defs.py
@@ -0,0 +1,146 @@
+"""
+Type annotations for sso service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sso/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_sso.type_defs import AccountInfoTypeDef
+
+    data: AccountInfoTypeDef = {...}
+    ```
+"""
+import sys
+from typing import Dict, List
+
+from typing_extensions import NotRequired
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "AccountInfoTypeDef",
+    "GetRoleCredentialsRequestRequestTypeDef",
+    "GetRoleCredentialsResponseTypeDef",
+    "ListAccountRolesRequestRequestTypeDef",
+    "ListAccountRolesResponseTypeDef",
+    "ListAccountsRequestRequestTypeDef",
+    "ListAccountsResponseTypeDef",
+    "LogoutRequestRequestTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+    "RoleCredentialsTypeDef",
+    "RoleInfoTypeDef",
+)
+
+AccountInfoTypeDef = TypedDict(
+    "AccountInfoTypeDef",
+    {
+        "accountId": NotRequired[str],
+        "accountName": NotRequired[str],
+        "emailAddress": NotRequired[str],
+    },
+)
+
+GetRoleCredentialsRequestRequestTypeDef = TypedDict(
+    "GetRoleCredentialsRequestRequestTypeDef",
+    {
+        "roleName": str,
+        "accountId": str,
+        "accessToken": str,
+    },
+)
+
+GetRoleCredentialsResponseTypeDef = TypedDict(
+    "GetRoleCredentialsResponseTypeDef",
+    {
+        "roleCredentials": "RoleCredentialsTypeDef",
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListAccountRolesRequestRequestTypeDef = TypedDict(
+    "ListAccountRolesRequestRequestTypeDef",
+    {
+        "accessToken": str,
+        "accountId": str,
+        "nextToken": NotRequired[str],
+        "maxResults": NotRequired[int],
+    },
+)
+
+ListAccountRolesResponseTypeDef = TypedDict(
+    "ListAccountRolesResponseTypeDef",
+    {
+        "nextToken": str,
+        "roleList": List["RoleInfoTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+ListAccountsRequestRequestTypeDef = TypedDict(
+    "ListAccountsRequestRequestTypeDef",
+    {
+        "accessToken": str,
+        "nextToken": NotRequired[str],
+        "maxResults": NotRequired[int],
+    },
+)
+
+ListAccountsResponseTypeDef = TypedDict(
+    "ListAccountsResponseTypeDef",
+    {
+        "nextToken": str,
+        "accountList": List["AccountInfoTypeDef"],
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+LogoutRequestRequestTypeDef = TypedDict(
+    "LogoutRequestRequestTypeDef",
+    {
+        "accessToken": str,
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+RoleCredentialsTypeDef = TypedDict(
+    "RoleCredentialsTypeDef",
+    {
+        "accessKeyId": NotRequired[str],
+        "secretAccessKey": NotRequired[str],
+        "sessionToken": NotRequired[str],
+        "expiration": NotRequired[int],
+    },
+)
+
+RoleInfoTypeDef = TypedDict(
+    "RoleInfoTypeDef",
+    {
+        "roleName": NotRequired[str],
+        "accountId": NotRequired[str],
+    },
+)
