7ba30da0f9961ce4f5b57c9c7547c2119a8cd60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:24 +0000

    Add package vtu-aiml/0.1.1/vtu_aiml-0.1.1.tar.gz

---
+++ packages/vtu-aiml/vtu_aiml-0.1.1.tar.gz/vtu_aiml-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vtu_aiml',
+    version='0.1.1',    
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Hrithikesh',
+    author_email='hrithikesh@gmail.com',
+    license='BSD 2-clause',
+    packages=['vtu_aiml'],
+    install_requires=[]
+)
