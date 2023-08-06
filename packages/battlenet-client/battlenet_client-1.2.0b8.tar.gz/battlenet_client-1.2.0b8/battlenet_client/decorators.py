11cd0498bb735ce1716fd1d053d404533b8748e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:52 +0000

    Add package battlenet-client/1.2.0b8/battlenet_client-1.2.0b8.tar.gz

---
+++ packages/battlenet-client/battlenet_client-1.2.0b8.tar.gz/battlenet_client-1.2.0b8/battlenet_client/decorators.py
@@ -0,0 +1,20 @@
+from battlenet_client.util import localize
+
+
+def params_headers(function):
+    def wrapper(*args, **kwargs):
+
+        kwargs['params'] = {'locale': localize(args[2])}
+
+        if 'headers' not in kwargs.keys():
+            kwargs['headers'] = {'Battlenet-Namespace': getattr(args[0], f"{args[3]}")}
+        else:
+            kwargs['headers']['Battlenet-Namespace'] = getattr(args[0], f"{args[3]}")
+
+        if 'fields' in kwargs.keys():
+            fields = ','.join([f"{key}={value}" for key, value in kwargs['fields'].items()])
+            kwargs['params'].update({'fields': fields})
+            kwargs.pop('fields', None)
+
+        return function(*args, **kwargs)
+    return wrapper
