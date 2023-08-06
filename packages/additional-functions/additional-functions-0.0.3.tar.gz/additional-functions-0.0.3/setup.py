6a287d5e0b4b5ab1a97fdee3f6206965fb17bea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:59 +0000

    Add package additional-functions/0.0.3/additional-functions-0.0.3.tar.gz

---
+++ packages/additional-functions/additional-functions-0.0.3.tar.gz/additional-functions-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="additional-functions",
+    version="0.0.3",
+    author="s0urce",
+    author_email="boyarkin.gleb@gmail.com",
+    description="Additional functions",
+    long_description='',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
