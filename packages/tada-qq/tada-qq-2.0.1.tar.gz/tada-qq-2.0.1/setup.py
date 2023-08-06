d2546a4c53cd6d524a6af6e61f3ce8a5280ff5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:02 +0000

    Add package tada-qq/2.0.1/tada-qq-2.0.1.tar.gz

---
+++ packages/tada-qq/tada-qq-2.0.1.tar.gz/tada-qq-2.0.1/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+with open("README.md") as f:
+    README = f.read()
+    lines = README.split('\n')
+    desc_lines = [line for line in lines if line[:2] != "[!"]
+    README = "\n".join(desc_lines)
+# This call to setup() does all the work
+setup(
+    name="tada-qq",
+    version="2.0.1",
+    description="Quantile Quantile Plot with Linear Approximation and Semantic Labelling of Numeric Columns",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oeg-upm/tada-qq",
+    author="Ahmad Alobaid",
+    author_email="aalobaid@fi.upm.es",
+    license="Apache2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["tadaqq.qq", "tadaqq.slabel"],
+    include_package_data=True,
+    install_requires=["pandas", "easysparql", "pcake"]
+)
