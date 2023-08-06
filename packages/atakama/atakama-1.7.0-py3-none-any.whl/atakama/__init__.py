fbfc4b31544041433a6309be99fe55f8263c420b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:27 +0000

    Add package atakama/1.7.0/atakama-1.7.0-py3-none-any.whl

---
+++ packages/atakama/atakama-1.7.0-py3-none-any.whl/atakama/__init__.py
@@ -0,0 +1,7 @@
+"""Atakama sdk."""
+
+# only import stuff here that has no co-deps, external-lib deps, etc.
+# otherwise, the user should import the from a specific submodule
+
+from .plugin_base import *
+from .rule_engine import *
