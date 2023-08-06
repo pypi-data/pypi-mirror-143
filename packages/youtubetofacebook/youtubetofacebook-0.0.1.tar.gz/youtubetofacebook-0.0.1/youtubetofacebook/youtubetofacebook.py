e841af8e5bb3bd22d438aae5ff6369848f1466ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:14 +0000

    Add package youtubetofacebook/0.0.1/youtubetofacebook-0.0.1.tar.gz

---
+++ packages/youtubetofacebook/youtubetofacebook-0.0.1.tar.gz/youtubetofacebook-0.0.1/youtubetofacebook/youtubetofacebook.py
@@ -0,0 +1,12 @@
+class youtubetofb:
+  def __init__(self, youtubeLink, facebookApi):
+    self.name = youtubeLink
+    self.age = facebookApi
+
+  def haveNewVideo(self):
+    """check if any new video available"""
+    pass
+
+  def upload(self):
+    """upload new video to facebook"""
+    pass
