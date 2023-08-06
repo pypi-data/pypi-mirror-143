812057619f5877af695ce59552d3b046ac5f8f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:46 +0000

    Add package gmtools/0.0.5/gmtools-0.0.5-py3-none-any.whl

---
+++ packages/gmtools/gmtools-0.0.5-py3-none-any.whl/gmtool/util.py
@@ -0,0 +1,45 @@
+# coding:utf-8
+'''
+@File    : util.py
+@Author  : guomin
+@Desc    : 工具方法
+'''
+
+import yagmail
+
+def send_mail_util(from_user,pwd,host,to_user,subject,content):
+    """
+    :param from_user:发件人
+    :param pwd:发件人密码
+    :param host:发件主机
+    :param to_user:接收人
+    :param subject:邮件主题
+    :param content:邮件内容
+    :return :
+    """
+
+    try:
+        with yagmail.SMTP(user=from_user,password=pwd,host=host) as yag:
+            yag.send(to_user,subject,content)
+    except Exception as e:
+        print("发生错误:{}".format(e))
+
+def json_util(data):
+    """
+    字符串转字典或者字典转字符串
+    :param data:待转换的数据
+    :return
+    """
+    if type(data)==str and data.startswith("{") and data.endswith("}"):
+        return eval(data)
+    elif type(data)==dict:
+        return str(data)
+    else:
+        print( "数据不适用!!")
+
+
+# 验证方法
+if __name__=="__main__":
+    # send_mail_util("tech-tm-qa@pin-dao.cn","Tm-qa-888888","smtp.mxhichina.com","guomin1@pin-dao.cn","主题","你好呀内容")
+    data='{"key":1,"key2":"1256463","key3":"8923hfds"}'
+    # print(type(json_util(data)))
