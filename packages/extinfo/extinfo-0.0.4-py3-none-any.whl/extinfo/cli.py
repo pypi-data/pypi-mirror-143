9ca5f5fdc818a41e4b616e94e15df2f179f66af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:49 +0000

    Add package extinfo/0.0.4/extinfo-0.0.4-py3-none-any.whl

---
+++ packages/extinfo/extinfo-0.0.4-py3-none-any.whl/extinfo/cli.py
@@ -0,0 +1,43 @@
+import sys
+
+import click
+import deal
+
+from extinfo.extractors import fileinfo_com, filesuffix_com
+
+from .exceptions import ExtensionNotFoundError
+from .utils import Extractor
+
+deal.activate()
+
+__EXTRACTORS__ = [
+    Extractor(filesuffix_com),
+    Extractor(fileinfo_com),
+]
+
+
+@click.command(
+    no_args_is_help=True,
+)
+@click.version_option()
+@click.argument("extension", type=str)
+def cli(extension: str) -> None:
+    for extractor in __EXTRACTORS__:
+        try:
+            results = extractor.extract(extension)
+        except ExtensionNotFoundError as e:
+            print(str(e))
+        else:
+            print(f"From {extractor.site}\n")
+            for report in results:
+                print(f"# {report.description_short}")
+                print("")
+                if report.description_long:
+                    print(report.description_long)
+                    print("")
+                if report.how_to_open:
+                    print(report.how_to_open)
+                    print("")
+            sys.exit(0)
+
+    sys.exit(1)
