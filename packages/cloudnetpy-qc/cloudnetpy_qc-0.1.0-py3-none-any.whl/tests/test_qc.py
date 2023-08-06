44424eeb05dbdc1b59a426cb135ea7cbc23c8bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:59 +0000

    Add package cloudnetpy-qc/0.1.0/cloudnetpy_qc-0.1.0-py3-none-any.whl

---
+++ packages/cloudnetpy-qc/cloudnetpy_qc-0.1.0-py3-none-any.whl/tests/test_qc.py
@@ -0,0 +1,31 @@
+from os import path
+from cloudnetpy_qc import Quality
+
+SCRIPT_PATH = path.dirname(path.realpath(__file__))
+
+
+def test_valid_file():
+    filename = f'{SCRIPT_PATH}/data/20211129_juelich_hatpro.nc'
+    check = Check(filename)
+    check.check_metadata()
+    check.check_data()
+
+
+def test_invalid_lwp():
+    filename = f'{SCRIPT_PATH}/data/20220215_schneefernerhaus_hatpro.nc'
+    check = Check(filename)
+    check.check_metadata()
+    check.check_data(1)
+
+
+class Check:
+    def __init__(self, filename: str):
+        self.quality = Quality(filename)
+
+    def check_metadata(self, n_failures: int = 0):
+        self.quality.check_metadata()
+        assert self.quality.n_metadata_test_failures == n_failures
+
+    def check_data(self, n_failures: int = 0):
+        self.quality.check_data()
+        assert self.quality.n_data_test_failures == n_failures
