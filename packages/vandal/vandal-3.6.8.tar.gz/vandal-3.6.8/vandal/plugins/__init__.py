8b6316db2cd9f671fbc94d699dce21c52fe4a72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:41 +0000

    Add package vandal/3.6.8/vandal-3.6.8.tar.gz

---
+++ packages/vandal/vandal-3.6.8.tar.gz/vandal-3.6.8/vandal/plugins/__init__.py
@@ -0,0 +1,13 @@
+# imports relevant contents.
+from vandal.plugins.metaclass import Meta
+from vandal.plugins.types import (
+    Vector,
+    Dictionary,
+)
+
+# all relevant contents.
+__all__ = [
+    Meta,
+    Vector,
+    Dictionary,
+]
