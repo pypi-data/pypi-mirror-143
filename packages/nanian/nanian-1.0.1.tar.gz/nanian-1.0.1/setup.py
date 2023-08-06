12e340b60c2cadcb2ea9f3f47f55357005b8310f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:22 +0000

    Add package nanian/1.0.1/nanian-1.0.1.tar.gz

---
+++ packages/nanian/nanian-1.0.1.tar.gz/nanian-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+with open("haha.rst","r") as f:
+    long_description = f.read()
+
+setup(
+    name='nanian',
+    version='1.0.1',
+    description='This group is composed of one student who is author',
+    author='那年',
+    author_email='1009689491@qq.com',
+)
