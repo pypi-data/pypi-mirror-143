63aa1cca3018d366a97e81db0e75a90ca133e76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:08 +0000

    Add package rastless-cli/0.2/rastless_cli-0.2-py3-none-any.whl

---
+++ packages/rastless-cli/rastless_cli-0.2-py3-none-any.whl/rastless/cli/colormaps.py
@@ -0,0 +1,30 @@
+import click
+import os
+
+from rastless.config import Cfg
+from rastless.core.colormap import create_colormap
+
+
+@click.command()
+@click.argument('sld_file', type=click.Path(exists=True))
+@click.option("-n", "--name", help="Name of the colormap, otherwise take the filename")
+@click.option("-d", "--description", help="Add description")
+@click.option("-l", "--legend-image", help="Filepath to png legend image")
+@click.pass_obj
+def add_colormap(cfg: Cfg, sld_file, name, description, legend_image):
+    """Add a SLD file"""
+    if not name:
+        name = os.path.basename(sld_file.split(".")[0])
+    try:
+        color_map = create_colormap(name, sld_file, description, legend_image)
+        cfg.db.add_color_map(color_map)
+    except Exception as e:
+        click.echo(f"SLD File could not be converted. Reason: {e}")
+
+
+@click.command()
+@click.option("-n", "--name", help="Name of the colormap", required=True)
+@click.pass_obj
+def delete_colormap(cfg: Cfg, name):
+    """Remove a SLD file"""
+    cfg.db.delete_color_map(name)
