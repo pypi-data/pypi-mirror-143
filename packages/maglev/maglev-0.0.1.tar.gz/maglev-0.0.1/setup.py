62ec201bce6975daeed2daf8599181e5885ff5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package maglev/0.0.1/maglev-0.0.1.tar.gz

---
+++ packages/maglev/maglev-0.0.1.tar.gz/maglev-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['maglev']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'maglev',
+    'version': '0.0.1',
+    'description': "Python utilities for working with NVIDIA's MLOps infrastructure.",
+    'long_description': None,
+    'author': 'Ryan Miller',
+    'author_email': 'rjmill@nvidia.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
