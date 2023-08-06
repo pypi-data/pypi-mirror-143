9abba10f6d450212f9397c45ce01c51f3ea76452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:28 +0000

    Add package git-substatus/0.2.10/git_substatus-0.2.10-py3-none-any.whl

---
+++ packages/git-substatus/git_substatus-0.2.10-py3-none-any.whl/git_substatus/__init__.py
@@ -0,0 +1,5 @@
+# package version:
+__version__ = "0.2.10"
+
+# min default supported python version:
+__py_version__ = "3.8"
---
+++ packages/git-substatus/git_substatus-0.2.10-py3-none-any.whl/git_substatus/base.py
@@ -0,0 +1,16 @@
+# Base file - global settings & modules for the entire git_substatus module.
+
+import os
+import subprocess
+import sys
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterator,
+    List,
+    Optional,
+    Tuple,
+    TypedDict,
+    Union,
+)
