4f93b3b1c4b1025660d62bd86a9a90c36ba9e444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:20 +0000

    Add package boostpi/0.1.1/boostpi-0.1.1.tar.gz

---
+++ packages/boostpi/boostpi-0.1.1.tar.gz/boostpi-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['boostpi']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['boostpi = boostpi.main:main']}
+
+setup_kwargs = {
+    'name': 'boostpi',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Alexander Swensen',
+    'author_email': 'alex.swensen@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
