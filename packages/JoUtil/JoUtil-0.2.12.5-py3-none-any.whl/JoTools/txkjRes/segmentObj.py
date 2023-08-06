8f52669769fe5e446726ceca1c8a34cc00ec73b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:26 +0000

    Add package JoUtil/0.2.12.5/JoUtil-0.2.12.5-py3-none-any.whl

---
+++ packages/JoUtil/JoUtil-0.2.12.5-py3-none-any.whl/JoTools/txkjRes/segmentObj.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8  -*-
+# -*- author: jokker -*-
+
+import math
+
+
+class SegmentObj(object):
+    """一个分割对象"""
+
+    def __init__(self, label="", points=None, shape_type="polygon", mask=None, mask_value=None):
+        self.label = label
+        self.points = [] if points is None else points
+        self.shape_type = shape_type
+        self.flags = ""
+        self.line_color = []
+        self.fill_color = []
+        self.mask = mask
+        self.mask_value = mask_value            # 当前要素在 mask 中的值（mask 中相同的 label 不同的对象有不同的值）
+        # 根据 points 得到 box
+        if self.points:
+            x_min, y_min, x_max, y_max = math.inf, math.inf, -math.inf, -math.inf
+            for each_point in self.points:
+                each_x, each_y = each_point
+                if each_x < x_min:
+                    x_min = each_x
+                if each_x > x_max:
+                    x_max = each_x
+                if each_y < y_min:
+                    y_min = each_y
+                if each_y > y_max:
+                    y_max = each_y
+            #
+            self.box = [x_min, y_min, x_max, y_max]
+
+    def get_format_list(self):
+        """获得格式化的输出"""
+        return [self.label, len(self.points), self.shape_type]
+
+    def do_print(self):
+        """打印"""
+
+        print("label : {0}".format(self.label))
+        print("points count : {0}".format(len(self.points)))
+        print("shape_type : {0}".format(self.shape_type))
+        # print("flags : {0}".format(self.flags))
+        # print("line_color : {0}".format(self.line_color))
+        # print("fill_color : {0}".format(self.fill_color))
+
+
