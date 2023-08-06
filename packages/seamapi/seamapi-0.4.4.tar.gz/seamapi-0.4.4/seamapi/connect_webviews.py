4e16a2b752fc7a9ef6488d1a34ee558c14a6e87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:34 +0000

    Add package seamapi/0.4.4/seamapi-0.4.4.tar.gz

---
+++ packages/seamapi/seamapi-0.4.4.tar.gz/seamapi-0.4.4/seamapi/connect_webviews.py
@@ -0,0 +1,70 @@
+from seamapi.types import (
+    AbstractConnectWebviews,
+    ConnectWebview,
+    AbstractSeam as Seam,
+    AcceptedProvider,
+)
+import requests
+from typing import List, Optional
+
+
+class ConnectWebviews(AbstractConnectWebviews):
+    seam: Seam
+
+    def __init__(self, seam: Seam):
+        self.seam = seam
+
+    def list(self) -> List[ConnectWebview]:
+        res = requests.post(
+            f"{self.seam.api_url}/connect_webviews/list",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_webviews = res.json()["connect_webviews"]
+        return [
+            ConnectWebview(
+                connect_webview_id=json_webview["connect_webview_id"],
+                status=json_webview["status"],
+                url=json_webview["url"],
+                login_successful=json_webview["login_successful"],
+                connected_account_id=json_webview["connected_account_id"],
+            )
+            for json_webview in json_webviews
+        ]
+
+    def get(self, connect_webview_id: str) -> ConnectWebview:
+        res = requests.get(
+            f"{self.seam.api_url}/connect_webviews/get",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            params={"connect_webview_id": connect_webview_id},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_webview = res.json()["connect_webview"]
+        return ConnectWebview(
+            connect_webview_id=json_webview["connect_webview_id"],
+            status=json_webview["status"],
+            url=json_webview["url"],
+            login_successful=json_webview["login_successful"],
+            connected_account_id=json_webview["connected_account_id"],
+        )
+
+    def create(
+        self, accepted_providers: Optional[List[AcceptedProvider]] = []
+    ) -> ConnectWebview:
+        res = requests.post(
+            f"{self.seam.api_url}/connect_webviews/create",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            json={"accepted_providers": accepted_providers},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_webview = res.json()["connect_webview"]
+        return ConnectWebview(
+            connect_webview_id=json_webview["connect_webview_id"],
+            status=json_webview["status"],
+            url=json_webview["url"],
+            login_successful=json_webview["login_successful"],
+            connected_account_id=None,
+        )
---
+++ packages/seamapi/seamapi-0.4.4.tar.gz/seamapi-0.4.4/seamapi/locks.py
@@ -0,0 +1,90 @@
+from seamapi.types import (
+    AbstractLocks,
+    ActionAttempt,
+    Device,
+    DeviceId,
+    AbstractSeam as Seam,
+)
+import time
+from typing import List, Union, Optional, cast
+import requests
+
+
+def to_device_id(device: Union[DeviceId, Device]) -> str:
+    if isinstance(device, str):
+        return device
+    return device.device_id
+
+
+class Locks(AbstractLocks):
+    seam: Seam
+
+    def __init__(self, seam: Seam):
+        self.seam = seam
+
+    def list(
+        self,
+        connected_account: Optional[str] = None,
+        connect_webview: Optional[str] = None,
+    ) -> List[Device]:
+        params = {}
+        if connected_account:
+            params["connected_account_id"] = connected_account
+        if connect_webview:
+            params["connect_webview_id"] = connect_webview
+
+        res = requests.post(
+            f"{self.seam.api_url}/locks/list",
+            params=params,
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_locks = res.json()["devices"]
+        return [Device.from_dict(d) for d in json_locks]
+
+    def get(
+        self,
+        device: Optional[Union[DeviceId, Device]] = None,
+        name: Optional[str] = None,
+    ) -> Device:
+        params = {}
+        if device:
+            params["device_id"] = to_device_id(device)
+        if name:
+            params["name"] = name
+        res = requests.post(
+            f"{self.seam.api_url}/locks/get",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            params=params,
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        json_lock = res.json()["device"]
+        return Device.from_dict(json_lock)
+
+    def lock_door(self, device: Union[DeviceId, Device]) -> ActionAttempt:
+        device_id = to_device_id(device)
+        res = requests.post(
+            f"{self.seam.api_url}/locks/lock_door",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            json={"device_id": device_id},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        return self.seam.action_attempts.poll_until_ready(
+            res.json()["action_attempt"]["action_attempt_id"]
+        )
+
+    def unlock_door(self, device: Union[DeviceId, Device]) -> ActionAttempt:
+        device_id = to_device_id(device)
+        res = requests.post(
+            f"{self.seam.api_url}/locks/unlock_door",
+            headers={"Authorization": f"Bearer {self.seam.api_key}"},
+            json={"device_id": device_id},
+        )
+        if not res.ok:
+            raise Exception(res.text)
+        return self.seam.action_attempts.poll_until_ready(
+            res.json()["action_attempt"]["action_attempt_id"]
+        )
