23ba25d4de502dd93da6a1900327a9f8b783530b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:51:29 +0000

    Add package fief-server/0.4.7/fief-server-0.4.7.tar.gz

---
+++ packages/fief-server/fief-server-0.4.7.tar.gz/fief-server-0.4.7/fief/models/authorization_code.py
@@ -0,0 +1,32 @@
+import secrets
+from typing import List
+
+from pydantic import UUID4
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import JSON, String
+
+from fief.models.base import WorkspaceBase
+from fief.models.client import Client
+from fief.models.generics import GUID, CreatedUpdatedAt, UUIDModel
+from fief.models.user import User
+
+
+class AuthorizationCode(UUIDModel, CreatedUpdatedAt, WorkspaceBase):
+    __tablename__ = "authorization_codes"
+
+    code: str = Column(
+        String(length=255),
+        default=secrets.token_urlsafe,
+        nullable=False,
+        index=True,
+        unique=True,
+    )
+    redirect_uri: str = Column(String(length=2048), nullable=False)
+    scope: List[str] = Column(JSON, nullable=False, default=list)
+
+    user_id: UUID4 = Column(GUID, ForeignKey(User.id, ondelete="CASCADE"), nullable=False)  # type: ignore
+    user: User = relationship("User")
+
+    client_id: UUID4 = Column(GUID, ForeignKey(Client.id, ondelete="CASCADE"), nullable=False)  # type: ignore
+    client: Client = relationship("Client", lazy="joined")
