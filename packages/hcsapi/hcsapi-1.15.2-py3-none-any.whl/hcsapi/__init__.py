d9bdf808d2ed068a0d7a9d2822a3d34ec75e36aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:20 +0000

    Add package hcsapi/1.15.2/hcsapi-1.15.2-py3-none-any.whl

---
+++ packages/hcsapi/hcsapi-1.15.2-py3-none-any.whl/hcsapi/__init__.py
@@ -0,0 +1,14 @@
+from .hcs import *
+
+__version__ = "1.15.2"
+
+__all__ = [
+    "asyncSelfCehck",
+    "asyncUserLogin",
+    "asyncTokenSelfCheck",
+    "asyncGenerateToken",
+    "selfcheck",
+    "userlogin",
+    "tokenselfcheck",
+    "generatetoken",
+]
