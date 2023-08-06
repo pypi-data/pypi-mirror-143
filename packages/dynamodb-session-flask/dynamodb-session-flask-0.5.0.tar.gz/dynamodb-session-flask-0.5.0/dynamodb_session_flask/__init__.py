88da6cf9622f8f8ed14760cab11d144493150378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:52 +0000

    Add package dynamodb-session-flask/0.5.0/dynamodb-session-flask-0.5.0.tar.gz

---
+++ packages/dynamodb-session-flask/dynamodb-session-flask-0.5.0.tar.gz/dynamodb-session-flask-0.5.0/dynamodb_session_flask/__init__.py
@@ -0,0 +1,7 @@
+from ._session import DynamoDbSession, DynamoDbSessionInstance, FlaskNullSessionInstance
+
+__all__ = [
+    'DynamoDbSession',
+    'DynamoDbSessionInstance',
+    'FlaskNullSessionInstance',
+]
