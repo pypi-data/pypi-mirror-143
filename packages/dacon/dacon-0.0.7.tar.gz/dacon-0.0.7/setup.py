8b84eef386a02d43305af2aee023a23d1f90fc4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:27 +0000

    Add package dacon/0.0.7/dacon-0.0.7.tar.gz

---
+++ packages/dacon/dacon-0.0.7.tar.gz/dacon-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dacon",
+    version="0.0.7",
+    author="dacon",
+    author_email="dacon@dacon.io",
+    description="dacon-open-api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dacon-ds2/dacon-open-api", # project link
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+# 패키지 빌드 : python3 setup.py sdist bdist_wheel
+# 패키지 업로드 : python3 -m twine upload dist/*
