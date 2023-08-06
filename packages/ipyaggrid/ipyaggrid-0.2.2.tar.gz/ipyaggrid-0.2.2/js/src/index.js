8b23da3207ec9bac3813fa198539f677053e7327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:15 +0000

    Add package ipyaggrid/0.2.2/ipyaggrid-0.2.2.tar.gz

---
+++ packages/ipyaggrid/ipyaggrid-0.2.2.tar.gz/ipyaggrid-0.2.2/js/src/index.js
@@ -0,0 +1,4 @@
+// Entry point for the notebook bundle containing custom model definitions.
+// Export widget models and views, and the npm package version number.
+export * from './widget';
+export { version } from '../package.json';
