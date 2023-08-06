d9b47ad73f8200ced9da856662e7d97818d7c0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:22 +0000

    Add package poc-dependency-gob/0.1.1/poc_dependency_gob-0.1.1.tar.gz

---
+++ packages/poc-dependency-gob/poc_dependency_gob-0.1.1.tar.gz/poc_dependency_gob-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poc_dependency_gob',
+ 'poc_dependency_gob.one',
+ 'poc_dependency_gob.three',
+ 'poc_dependency_gob.two']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.4.1,<2.0.0']
+
+extras_require = \
+{'skinny': ['sklearn>=0.0,<0.1'],
+ 'standard': ['torch>=1.11.0,<2.0.0', 'matplotlib>=3.5.1,<4.0.0']}
+
+setup_kwargs = {
+    'name': 'poc-dependency-gob',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@gobsingh.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
