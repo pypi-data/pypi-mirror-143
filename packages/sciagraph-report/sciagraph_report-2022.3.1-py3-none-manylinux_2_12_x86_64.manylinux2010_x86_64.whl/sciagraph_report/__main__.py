dea3a4fef113ba1d91f8305056bc201b2d3bc65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:34 +0000

    Add package sciagraph-report/2022.3.1/sciagraph_report-2022.3.1-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/sciagraph-report/sciagraph_report-2022.3.1-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/sciagraph_report/__main__.py
@@ -0,0 +1,20 @@
+"""Launch the actual Rust binary."""
+
+from importlib.metadata import files
+import sys
+from subprocess import run
+
+
+def main():
+    args = sys.argv[1:]
+    # Find the executable in the installed package:
+    for f in files("sciagraph-report"):
+        if f.name.lower() in ("sciagraph-report", "sciagraph-report.exe"):
+            path = f.locate()
+            break
+    result = run([path] + args)
+    sys.exit(result.returncode)
+
+
+if __name__ == "__main__":
+    main()
