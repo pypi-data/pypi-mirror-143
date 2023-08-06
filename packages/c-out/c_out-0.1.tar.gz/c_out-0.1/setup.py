6c8f2e64bf01ed5b79f1508b8393ade13184184e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:52 +0000

    Add package c-out/0.1/c_out-0.1.tar.gz

---
+++ packages/c-out/c_out-0.1.tar.gz/c_out-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='c_out',
+    description='Colored Output - c_out. Python print with a style!',
+    version='0.1',
+    license='GPL-3.0',
+    author="Gautam Rajeev Singh",
+    author_email='gautamsingh1997@gmail.com',
+    url='https://github.com/singhgautam7/Python-GoldMine/tree/master/color_print',
+    keywords='pyton print with color and styles',
+    install_requires=[],
+)
