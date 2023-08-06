9a4f88e23cfe7dcd5fccf4a9c4ca30c9882235de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:11 +0000

    Add package slck-cli/0.2.0/slck_cli-0.2.0-py3-none-any.whl

---
+++ packages/slck-cli/slck_cli-0.2.0-py3-none-any.whl/slck/utils.py
@@ -0,0 +1,38 @@
+import sys
+from typing import Dict
+
+
+def confirm_user_input(question: str, default: str = "yes") -> bool:
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid: Dict[str, bool] = {
+        "yes": True,
+        "y": True,
+        "ye": True,
+        "no": False,
+        "n": False,
+    }
+    prompt: str = " [y/n] "
+    if default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice: str = input().lower()
+        if choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
