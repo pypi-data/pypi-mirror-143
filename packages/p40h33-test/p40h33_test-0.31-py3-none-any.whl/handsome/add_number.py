ac74a4063323034405b2f666e656f22b21f90a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:59 +0000

    Add package p40h33-test/0.31/p40h33_test-0.31-py3-none-any.whl

---
+++ packages/p40h33-test/p40h33_test-0.31-py3-none-any.whl/handsome/add_number.py
@@ -0,0 +1,9 @@
+def add(*numbers):
+    """
+    description:  this method is add all number get the total
+    return number
+    """
+    count = 0
+    for num in numbers:
+        count += num
+    return count
