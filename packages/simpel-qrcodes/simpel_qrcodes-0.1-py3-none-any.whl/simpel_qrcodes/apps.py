28e1f32531e91920508f572b3afed36b29bfd361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:20 +0000

    Add package simpel-qrcodes/0.1/simpel_qrcodes-0.1-py3-none-any.whl

---
+++ packages/simpel-qrcodes/simpel_qrcodes-0.1-py3-none-any.whl/simpel_qrcodes/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+from django.conf import settings
+from django.db.models.signals import post_migrate
+from django.utils.translation import gettext_lazy as _
+
+
+class SimpelQrcodesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = "simpel_qrcodes"
+    label = "simpel_qrcodes"
+    verbose_name = _("Simpel QRCode")
+
+    def ready(self):
+        post_migrate.connect(init_app, sender=self)
+
+
+def init_app(sender, **kwargs):
+    pass
