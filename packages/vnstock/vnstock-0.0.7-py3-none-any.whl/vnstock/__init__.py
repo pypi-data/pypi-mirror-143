16e6d0bee4e1d6de8303cff92d093220736603f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:27 +0000

    Add package vnstock/0.0.7/vnstock-0.0.7-py3-none-any.whl

---
+++ packages/vnstock/vnstock-0.0.7-py3-none-any.whl/vnstock/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2022 Thinh Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Thinh Vu @thinh-vu in GitHub"
+__version__ = "0.0.7"
+
+from .stock import (
+    listing_companies,
+    last_xd,
+    start_xm,
+    stock_historical_data,
+    stock_intraday_data,
+    financial_report,
+    financial_ratio_compare,
+    financial_ratio,
+    financial_flow,
+    dividend_history,
+    general_rating,
+    biz_model_rating,
+    biz_operation_rating,
+    financial_health_rating,
+    valuation_rating,
+    industry_financial_health,
+    industry_analysis,
+    stock_ls_analysis,
+    market_top_mover
+)
