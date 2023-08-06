58efb4bbb1754d486d7c29614cc041146902652f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:02 +0000

    Add package kerchunk/0.0.6/kerchunk-0.0.6.tar.gz

---
+++ packages/kerchunk/kerchunk-0.0.6.tar.gz/kerchunk-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from os.path import exists
+from setuptools import setup
+import versioneer
+
+setup(
+    name='kerchunk',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['kerchunk', 'fsspec_reference_maker'],
+    url='https://github.com/fsspec/kerchunk',
+    license='MIT',
+    author='Martin Durant',
+    author_email='martin.durant@alumni.utoronto.ca',
+    description='Functions to make reference descriptions for ReferenceFileSystem',
+    python_requires='>=3.7',
+    long_description=(open('README.md').read() if exists('README.md') else ''),
+    long_description_content_type='text/markdown',
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
+    entry_points={
+        'numcodecs.codecs': [
+            'grib = kerchunk.grib2:GRIBCodec',
+            'FITSAscii = kerchunk.fits:AsciiTableCodec'
+        ],
+    },
+    zip_safe=False
+)
