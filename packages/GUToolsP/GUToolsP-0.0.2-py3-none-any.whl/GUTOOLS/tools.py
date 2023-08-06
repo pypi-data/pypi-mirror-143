31dbc0eaa4861ebf0812882f4d74b461370608fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:58 +0000

    Add package GUToolsP/0.0.2/GUToolsP-0.0.2-py3-none-any.whl

---
+++ packages/GUToolsP/GUToolsP-0.0.2-py3-none-any.whl/GUTOOLS/tools.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+
+from util import send_mail_util, json_util
+
+class Tool(object):
+    def __init__(self):
+        self.mail_from_user = ''  # 邮件发送者账号
+        self.mail_from_user_pwd = ''  # 邮件发送者密码
+        self.mail_from_host = ''  # 邮件发送host
+
+    def send_mail_msg(self, to_user, subject, content):
+        send_mail_util(self.mail_from_user, self.mail_from_user_pwd, self.mail_from_host, to_user, subject, content)
+    
+    def json_change(self, data):
+        json_util(data) 
+
+
+
+
+if __name__ == '__main__':
+    a = {'a':1}
+    A = Tool()
+    A.json_change(a)
+    # print(json_util(a))
