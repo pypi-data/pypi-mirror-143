fb0eba4f73164c19c12db6cc6a70e4d96a991b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:46 +0000

    Add package harvestnet/0.0.1/harvestnet-0.0.1.tar.gz

---
+++ packages/harvestnet/harvestnet-0.0.1.tar.gz/harvestnet-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+ name = 'harvestnet',
+ version = '0.0.1',
+ description = 'HarvestNet',
+ long_description = 'HarvestNet',
+ author = 'waterlily',
+ author_email = 'b1929595403@gmail.com',
+ platforms = 'any',
+ python_requires = '>=3.7.*',
+ install_requires = [],
+ package_dir = {'': 'HarvestNet'},
+ packages = find_packages('HarvestNet')
+ )
