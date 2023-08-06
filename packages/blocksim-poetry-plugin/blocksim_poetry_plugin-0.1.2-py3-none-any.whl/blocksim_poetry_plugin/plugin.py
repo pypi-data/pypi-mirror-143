260f2c0c1353e1c01795578c548d78810114f1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:21 +0000

    Add package blocksim-poetry-plugin/0.1.2/blocksim_poetry_plugin-0.1.2-py3-none-any.whl

---
+++ packages/blocksim-poetry-plugin/blocksim_poetry_plugin-0.1.2-py3-none-any.whl/blocksim_poetry_plugin/plugin.py
@@ -0,0 +1,54 @@
+import subprocess
+
+from cleo.commands.command import Command
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from sphinx.cmd.build import main
+
+
+class BuildDocCommand(Command):
+
+    name = "doc-command"
+
+    def handle(self) -> int:
+        self.line("My doc command")
+
+        args = [
+            "-T",
+            "-b",
+            "html",
+            "-D",
+            "language=fr",
+            "docs",
+            "htmldoc",
+        ]
+        return main(args)
+
+
+class BaselineCommand(Command):
+
+    name = "baseline"
+
+    def handle(self) -> int:
+        self.line("My baseline command")
+
+        cmd = ["python", "-m", "pytest", "--mpl-generate-path=tests/baseline", "tests"]
+        subprocess.run(cmd)
+
+        return 0
+
+
+def bld_doc_factory():
+    return BuildDocCommand()
+
+
+def baseline_factory():
+    return BaselineCommand()
+
+
+class BlocksimApplicationPlugin(ApplicationPlugin):
+    def activate(self, application):
+        # application.add(BaselineCommand())
+
+        # application.command_loader.register_factory("doc-command", bld_doc_factory)
+        application.command_loader.register_factory("baseline", baseline_factory)
