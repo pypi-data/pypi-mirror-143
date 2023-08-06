f3824848cba658abb5125f55d89a45e4f03d270d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:29 +0000

    Add package litemark/0.0.4/litemark-0.0.4.tar.gz

---
+++ packages/litemark/litemark-0.0.4.tar.gz/litemark-0.0.4/litemark/__init__.py
@@ -0,0 +1,13 @@
+from litemark.core import scanner
+from litemark.core.scanner import Element
+from litemark.core.viewer import Viewer, get_light_style
+from litemark.core.style import Style
+from litemark.core.util import center_window
+
+
+__all__ = ["scan", "Element", "Viewer", "get_light_style", "Style"]
+
+
+def scan(text):
+    """Returns a generator. If you need a list: list(scan(text))"""
+    return scanner.scan(text)
