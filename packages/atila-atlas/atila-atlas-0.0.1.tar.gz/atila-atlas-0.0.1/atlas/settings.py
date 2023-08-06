be953cf4e001f4038f45cb3f63313a55a5e6bf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:46 +0000

    Add package atila-atlas/0.0.1/atila-atlas-0.0.1.tar.gz

---
+++ packages/atila-atlas/atila-atlas-0.0.1.tar.gz/atila-atlas-0.0.1/atlas/settings.py
@@ -0,0 +1,5 @@
+import os
+
+ALGOLIA_APPLICATION_ID = os.environ.get("ALGOLIA_APPLICATION_ID", "")
+ALGOLIA_API_KEY = os.environ.get("ALGOLIA_API_KEY", "")
+ALGOLIA_INDEX_NAME = os.environ.get("ALGOLIA_INDEX_NAME", "")
