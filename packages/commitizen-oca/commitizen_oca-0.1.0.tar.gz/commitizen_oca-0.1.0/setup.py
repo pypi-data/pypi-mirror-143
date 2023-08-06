74522385707d0504ce353e511d85d5e4548c0728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:26 +0000

    Add package commitizen-oca/0.1.0/commitizen_oca-0.1.0.tar.gz

---
+++ packages/commitizen-oca/commitizen_oca-0.1.0.tar.gz/commitizen_oca-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="commitizen_oca",
+    version="0.1.0",
+    py_modules=["cz_commitizen_oca"],
+    license="MIT",
+    url="https://gitlab.com/coopdevs/commitizen-oca",
+    long_description="Commitizen for Odoo Community",
+    install_requires=["commitizen"],
+)
