6ac278c6fe6ac510a648ddc3da16be54efb37512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:51:09 +0000

    Add package fief-server/0.4.6/fief_server-0.4.6-py3-none-any.whl

---
+++ packages/fief-server/fief_server-0.4.6-py3-none-any.whl/fief/app.py
@@ -0,0 +1,40 @@
+import sentry_sdk
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from sentry_sdk.integrations.redis import RedisIntegration
+
+from fief import __version__
+from fief.apps import admin_app, admin_frontend_app, auth_app
+from fief.db.workspace import workspace_engine_manager
+from fief.settings import settings
+
+sentry_sdk.init(
+    dsn=settings.sentry_dsn,
+    environment=settings.environment.value,
+    release=__version__,
+    integrations=[RedisIntegration()],
+)
+
+app = FastAPI(openapi_url=None)
+
+app.add_middleware(SentryAsgiMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=settings.allow_origin_regex,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/admin", admin_frontend_app)
+app.mount("/api", admin_app)
+app.mount("/", auth_app)
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await workspace_engine_manager.close_all()
+
+
+__all__ = ["app"]
