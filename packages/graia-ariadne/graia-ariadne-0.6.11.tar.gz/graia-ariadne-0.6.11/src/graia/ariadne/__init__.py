99a3665c7cbf61eb741b3e0edb4237eed29f7a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:34 +0000

    Add package graia-ariadne/0.6.11/graia-ariadne-0.6.11.tar.gz

---
+++ packages/graia-ariadne/graia-ariadne-0.6.11.tar.gz/graia-ariadne-0.6.11/src/graia/ariadne/__init__.py
@@ -0,0 +1,38 @@
+"""Ariadne
+
+一个优雅的 QQ Bot 框架.
+"""
+from typing import Type
+
+import graia.ariadne.event.lifecycle  # noqa: F401
+import graia.ariadne.event.message  # noqa: F401
+import graia.ariadne.event.mirai  # noqa: F401
+
+from .app import Ariadne
+from .typing import T
+
+
+def get_running(type: Type[T] = Ariadne) -> T:
+    """获取正在运行的实例
+
+    Args:
+        type (Type[T]): 实例类型
+
+    Returns:
+        T: 对应类型实例
+    """
+    from asyncio import AbstractEventLoop
+
+    from graia.broadcast import Broadcast
+
+    from .adapter import Adapter
+    from .context import context_map
+
+    if type in {Adapter, Ariadne, Broadcast, AbstractEventLoop}:
+        if ctx := context_map.get(type.__name__):
+            if val := ctx.get(None):
+                return val
+    for ariadne_inst in Ariadne.running:
+        if type in ariadne_inst.info:
+            return ariadne_inst.info[type]  # type: ignore
+    raise ValueError(f"{type.__name__} is not running")
