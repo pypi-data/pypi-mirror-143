655042c379016714f5a03f92fba80a0d90c4bffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:43 +0000

    Add package twc-square/0.1.7/twc_square-0.1.7.tar.gz

---
+++ packages/twc-square/twc_square-0.1.7.tar.gz/twc_square-0.1.7/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "twc_square",
+    version = "0.1.7",
+    author = "Sabbir Rashid",
+    author_email = "rashidsabbir@gmail.com",
+    description = ("The twc_square package contains configuration entries for SQuARE, the SPARQL Query Agent-based Reasoning Engine"),
+    license = "Apache License 2.0",
+    keywords = "rdf semantic inference reasoning engine",
+    url = "http://packages.python.org/twc_square",
+    packages=find_packages(),
+    long_description='''The twc_square package contains configuration entries for SQuARE, the SPARQL Query Agent-based Reasoning Engine.''',
+    include_package_data = False,
+    install_requires = [],
+    entry_points = {
+        'console_scripts': ['twc_square=twc_square:get_rules'],
+        'console_scripts': ['twc_square=twc_square:get_owl_el_list'],
+        'console_scripts': ['twc_square=twc_square:get_owl_ql_list'],
+        'console_scripts': ['twc_square=twc_square:get_owl_rl_list'],
+        'console_scripts': ['twc_square=twc_square:get_owl_dl_list'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
