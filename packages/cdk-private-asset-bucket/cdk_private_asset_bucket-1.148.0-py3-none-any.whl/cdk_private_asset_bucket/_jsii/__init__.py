5c31e1e98124086550dce931b68a700f845e7690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:16 +0000

    Add package cdk-private-asset-bucket/1.148.0/cdk_private_asset_bucket-1.148.0-py3-none-any.whl

---
+++ packages/cdk-private-asset-bucket/cdk_private_asset_bucket-1.148.0-py3-none-any.whl/cdk_private_asset_bucket/_jsii/__init__.py
@@ -0,0 +1,35 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_cloudfront._jsii
+import aws_cdk.aws_cloudfront_origins._jsii
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_route53_targets._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-private-asset-bucket",
+    "1.148.0",
+    __name__[0:-6],
+    "cdk-private-asset-bucket@1.148.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
