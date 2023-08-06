fe48f92e2374447e0fc1a968ada2fec6fad9b33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package convbaspy/1.0.2/convbaspy-1.0.2.tar.gz

---
+++ packages/convbaspy/convbaspy-1.0.2.tar.gz/convbaspy-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='convbaspy',
+    version='1.0.2',
+    license='MIT',
+    author="D Penilla",
+    author_email='dpenillac@correo.usbcali.edu.co',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/etensor/baseconvpy',
+    keywords='convertidor de bases',
+    install_requires=[
+          'rich',
+          'textual',
+          'pyfiglet',
+          'pyperclip'
+      ],
+
+)
