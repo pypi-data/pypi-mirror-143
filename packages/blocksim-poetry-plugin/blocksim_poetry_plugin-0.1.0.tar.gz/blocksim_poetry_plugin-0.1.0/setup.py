7f561871a5b2addde78f212d2ea1d679eeb2ad42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:30 +0000

    Add package blocksim-poetry-plugin/0.1.0/blocksim_poetry_plugin-0.1.0.tar.gz

---
+++ packages/blocksim-poetry-plugin/blocksim_poetry_plugin-0.1.0.tar.gz/blocksim_poetry_plugin-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blocksim_poetry_plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sphinx>=3.3.1,<4.0.0']
+
+entry_points = \
+{'poetry.plugin': ['blocksim_poetry_plugin = '
+                   'blocksim_poetry_plugin.plugin:BlocksimApplicationPlugin']}
+
+setup_kwargs = {
+    'name': 'blocksim-poetry-plugin',
+    'version': '0.1.0',
+    'description': 'A plugin for more poetry commands',
+    'long_description': 'None',
+    'author': 'John Gray',
+    'author_email': 'manawenuz@johncloud.fr',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://gitlab.com/manawenuz/blocksim_poetry_plugin',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
