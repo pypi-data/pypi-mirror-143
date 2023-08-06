a8f800330783c03e8069fa0ce9e6784b4c0d833e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:49 +0000

    Add package jetpack-io/0.5.1.dev202203221647994867/jetpack-io-0.5.1.dev202203221647994867.tar.gz

---
+++ packages/jetpack-io/jetpack-io-0.5.1.dev202203221647994867.tar.gz/jetpack-io-0.5.1.dev202203221647994867/src/jetpack/__init__.py
@@ -0,0 +1,13 @@
+# __version__ is placeholder
+# It gets set in the build/publish process (publish_with_credentials.sh)
+__version__ = "0.5.1-dev202203221647994867"
+
+from jetpack._task.interface import function, jet, jetroutine, schedule, workflow
+from jetpack.cmd import root
+from jetpack.redis import redis
+
+
+def run() -> None:
+    # options can be passed in as env variables with JETPACK prefix
+    # e.g. JETPACK_ENTRYPOINT
+    root.cli(auto_envvar_prefix="JETPACK")
