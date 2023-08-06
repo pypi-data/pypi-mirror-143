d8b01e5319bb05267cc94bcfad5e06df9975a14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:59 +0000

    Add package cosipy/0.0.1/cosipy-0.0.1-py2-none-any.whl

---
+++ packages/cosipy/cosipy-0.0.1-py2-none-any.whl/cosipy/response/__init__.py
@@ -0,0 +1,1 @@
+from .spec_response import SpecResponse
---
+++ packages/cosipy/cosipy-0.0.1-py2-none-any.whl/cosipy/response/spec_response.py
@@ -0,0 +1,8 @@
+
+
+class SpecResponse():
+
+    def __init__(self):
+        """
+        Detector response for spectral analysis.
+        """
