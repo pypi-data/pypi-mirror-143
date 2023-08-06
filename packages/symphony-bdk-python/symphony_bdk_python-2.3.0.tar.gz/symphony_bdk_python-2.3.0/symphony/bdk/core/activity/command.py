98cc7ed4945dacfd9602a79f5a90096a97180138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:37 +0000

    Add package symphony-bdk-python/2.3.0/symphony_bdk_python-2.3.0.tar.gz

---
+++ packages/symphony-bdk-python/symphony_bdk_python-2.3.0.tar.gz/symphony_bdk_python-2.3.0/symphony/bdk/core/activity/command.py
@@ -0,0 +1,95 @@
+import logging
+
+from symphony.bdk.core.activity.api import AbstractActivity, ActivityContext
+from symphony.bdk.core.activity.exception import FatalActivityExecutionException
+from symphony.bdk.core.service.exception import MessageParserError
+from symphony.bdk.core.service.message.message_parser import get_text_content_from_message
+from symphony.bdk.gen.agent_model.v4_initiator import V4Initiator
+from symphony.bdk.gen.agent_model.v4_message_sent import V4MessageSent
+
+logger = logging.getLogger(__name__)
+
+
+class CommandContext(ActivityContext[V4MessageSent]):
+    """Default implementation of the :py:class:`ActivityContext` handled by the :py:class:`CommandActivity`.
+    """
+
+    def __init__(self, initiator: V4Initiator, source_event: V4MessageSent, bot_display_name: str):
+        self._message_id = source_event.message.message_id
+        self._stream_id = source_event.message.stream.stream_id
+        self._bot_display_name = bot_display_name
+        try:
+            self._text_content = get_text_content_from_message(source_event.message)
+        except MessageParserError as exc:
+            raise FatalActivityExecutionException("Unable to parse presentationML") from exc
+        super().__init__(initiator, source_event)
+
+    @property
+    def message_id(self):
+        return self._message_id
+
+    @property
+    def stream_id(self):
+        return self._stream_id
+
+    @property
+    def text_content(self) -> str:
+        return self._text_content
+
+    @property
+    def bot_display_name(self) -> str:
+        return self._bot_display_name
+
+
+class CommandActivity(AbstractActivity[CommandContext]):
+    """A command activity corresponds to any message sent in a chat where the bot is part of.
+    """
+
+    def matches(self, context: CommandContext) -> bool:
+        pass
+
+    async def on_activity(self, context: CommandContext):
+        pass
+
+
+class SlashCommandActivity(CommandActivity):
+    """A slash command is a particular CommandActivity with three parameters:
+      - the command name
+      - if we should mention the bot
+      - the callback function
+
+    The Slash command is triggered if we receive a MESSAGESENT event with message text:
+      - "@{bot_display name} {name}" if ``requires_mention_bot`` is True
+      - "{name}" otherwise
+    """
+
+    def __init__(self, name, requires_mention_bot, callback, description=""):
+        """
+        :param name: the command name
+        :param requires_mention_bot: if the command requires the bot mention to trigger the slash command
+        :param callback: the coroutine to be executed if message text matches
+        """
+        self._name = name
+        self._requires_mention_bot = requires_mention_bot
+        self._callback = callback
+        self._description = description
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def build_command_description(self) -> str:
+        return self._description + " (mention required)" if self._requires_mention_bot \
+            else self._description + " (mention not required)"
+
+    def matches(self, context: CommandContext) -> bool:
+        pattern = rf"@{context.bot_display_name} {self._name}" if self._requires_mention_bot else rf"{self._name}"
+        return pattern == context.text_content
+
+    async def on_activity(self, context: CommandContext):
+        await self._callback(context)
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, SlashCommandActivity):
+            return self._name == o._name and self._requires_mention_bot == o._requires_mention_bot
+        return False
---
+++ packages/symphony-bdk-python/symphony_bdk_python-2.3.0.tar.gz/symphony_bdk_python-2.3.0/symphony/bdk/core/service/datafeed/datafeed_loop_v2.py
@@ -0,0 +1,139 @@
+import logging
+import time
+from typing import Optional
+
+from symphony.bdk.core.auth.auth_session import AuthSession
+from symphony.bdk.core.config.model.bdk_config import BdkConfig
+from symphony.bdk.core.retry import retry
+from symphony.bdk.core.retry.strategy import read_datafeed_retry
+from symphony.bdk.core.service.datafeed.abstract_datafeed_loop import AbstractDatafeedLoop
+from symphony.bdk.core.service.datafeed.exception import EventError
+from symphony.bdk.core.service.session.session_service import SessionService
+from symphony.bdk.gen.agent_api.datafeed_api import DatafeedApi
+from symphony.bdk.gen.agent_model.ack_id import AckId
+from symphony.bdk.gen.agent_model.v5_datafeed import V5Datafeed
+from symphony.bdk.gen.agent_model.v5_datafeed_create_body import V5DatafeedCreateBody
+
+# Based on the DFv2 default visibility timeout, after which an event is re-queued
+EVENT_PROCESSING_MAX_DURATION_SECONDS = 30
+
+# DFv2 API authorizes a maximum length for the tag parameter
+DATAFEED_TAG_MAX_LENGTH = 100
+
+logger = logging.getLogger(__name__)
+
+
+class DatafeedLoopV2(AbstractDatafeedLoop):
+    """A class for implementing the datafeed v2 loop service.
+
+        This service will be started by calling :func:`~DatafeedLoopV2.start`.
+
+        On the very first run, the BDK bot will try to retrieve the list of datafeed to which it is listening.
+        Since each bot should only listening to just one datafeed, the first datafeed in the list will be used by
+        the bot to be listened to. If the retrieved list is empty, the BDK bot will create a new datafeed to listen.
+
+        The BDK bot will listen to this datafeed to get all the received real-time events.
+
+        If this datafeed becomes stale or faulty, the BDK bot will create the new one for listening.
+
+        This service will be stopped by calling :func:`~DatafeedLoopV1.stop`
+
+        If the datafeed service is stopped during a read datafeed call, it has to wait until the last read finish to be
+        really stopped
+        """
+
+    def __init__(self, datafeed_api: DatafeedApi, session_service: SessionService, auth_session: AuthSession,
+                 config: BdkConfig):
+        super().__init__(datafeed_api, session_service, auth_session, config)
+        self._ack_id = ""
+        self._datafeed_id = None
+        if config.bot.username is not None:
+            self._tag = config.bot.username[0:DATAFEED_TAG_MAX_LENGTH]
+
+    async def _prepare_datafeed(self):
+        datafeed = await self._retrieve_datafeed()
+        if not datafeed:
+            datafeed = await self._create_datafeed()
+        self._datafeed_id = datafeed.id
+
+    async def _run_loop_iteration(self):
+        events = await self._read_datafeed()
+
+        is_run_successful = await self._run_all_listener_tasks(events.events)
+        if is_run_successful:
+            # updates ack id so that on next call DFv2 knows that events have been processed
+            # if not updated, events will be requeued after some time, typically 30s
+            self._ack_id = events.ack_id
+
+    async def _run_all_listener_tasks(self, events):
+        start = time.time()
+        done_tasks = await self._run_listener_tasks(events)
+        elapsed = time.time() - start
+
+        if elapsed > EVENT_PROCESSING_MAX_DURATION_SECONDS:
+            logging.warning("Events processing took longer than %s seconds, "
+                            "this might lead to events being re-queued in datafeed and re-dispatched. "
+                            "You might want to consider processing the event in a separated asyncio task if needed.",
+                            EVENT_PROCESSING_MAX_DURATION_SECONDS)
+
+        return await self._are_tasks_successful(done_tasks)
+
+    async def _are_tasks_successful(self, tasks):
+        success = True
+        for task in tasks:
+            exception = task.exception()
+            if exception:
+                if isinstance(exception, EventError):
+                    logger.warning("Failed to process events inside %s, "
+                                   "will not update ack id, events will be re-queued",
+                                   task.get_name(),
+                                   exc_info=exception)
+                    success = False
+                else:
+                    logging.debug("Exception occurred inside %s", task.get_name(), exc_info=exception)
+        return success
+
+    @retry(retry=read_datafeed_retry)
+    async def _read_datafeed(self):
+        params = {
+            "session_token": await self._auth_session.session_token,
+            "key_manager_token": await self._auth_session.key_manager_token,
+            "datafeed_id": self._datafeed_id,
+            "ack_id": AckId(ack_id=self._ack_id)
+        }
+        return await self._datafeed_api.read_datafeed(**params)
+
+    async def recreate_datafeed(self):
+        await self._delete_datafeed()
+        datafeed = await self._create_datafeed()
+
+        self._datafeed_id = datafeed.id
+        self._ack_id = ""
+
+    @retry
+    async def _retrieve_datafeed(self) -> Optional[V5Datafeed]:
+        session_token = await self._auth_session.session_token
+        key_manager_token = await self._auth_session.key_manager_token
+        datafeeds = await self._datafeed_api.list_datafeed(session_token=session_token,
+                                                           key_manager_token=key_manager_token,
+                                                           tag=self._tag)
+        if datafeeds:
+            return datafeeds[0]
+        return None
+
+    @retry
+    async def _create_datafeed(self) -> V5Datafeed:
+        session_token = await self._auth_session.session_token
+        key_manager_token = await self._auth_session.key_manager_token
+
+        return await self._datafeed_api.create_datafeed(session_token=session_token,
+                                                        key_manager_token=key_manager_token,
+                                                        body=V5DatafeedCreateBody(tag=self._tag))
+
+    @retry
+    async def _delete_datafeed(self) -> None:
+        session_token = await self._auth_session.session_token
+        key_manager_token = await self._auth_session.key_manager_token
+        await self._datafeed_api.delete_datafeed(datafeed_id=self._datafeed_id,
+                                                 session_token=session_token,
+                                                 key_manager_token=key_manager_token)
